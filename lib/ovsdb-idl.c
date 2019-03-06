43ac75d1efccd944713745a943ca9e20cf31a71b
Han Zhou <hzhou8@ebay.com>
2019-03-05 17:46:36 -0800

    ovsdb-idl: Fix memory leak of idl->remote.
    Reported by Address Sanitizer.
    
    Signed-off-by: Han Zhou <hzhou8@ebay.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/ovsdb-idl.c
+++ lib/ovsdb-idl.c
@@ -571,6 +571,9 @@ ovsdb_idl_destroy(struct ovsdb_idl *idl)
         ovsdb_idl_db_destroy(&idl->server);
         ovsdb_idl_db_destroy(&idl->data);
         json_destroy(idl->request_id);
+        if (idl->remote) {
+            free(idl->remote);
+        }
         free(idl);
     }
 }
