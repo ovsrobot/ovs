ovsrobot/ovs

commit 88dcf2aa82347da7bfcf4f6e1d72c2d3012b4c12

netdev-provider: add class op to get block_id
Add a new class op for netdevs to get the block_id if one exists. The
block_id is used in offload ops to group multiple qdiscs together.

Stub calls are made to the new class op (implementation to follow in
further patches). The default block_id of 0 (no block) will be used in
these cases.

Signed-off-by: John Hurley <john.hurley@netronome.com>
Reviewed-by: Dirk van der Merwe <dirk.vandermerwe@netronome.com>
Reviewed-by: Simon Horman <simon.horman@netronome.com>
Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
Signed-off-by: Simon Horman <simon.horman@netronome.com>

lib/netdev-tc-offloads.c
@@ -304,6 +304,16 @@ get_prio_for_tc_flower(struct tc_flower *flower)
     return new_data->prio;
 }
 
+static uint32_t
+get_block_id_from_netdev(struct netdev *netdev)
+{
+    if (block_support) {
+        return netdev_get_block_id(netdev);
+    }
+
+    return 0;
+}
+
 int
 netdev_tc_flow_flush(struct netdev *netdev)
 {
@@ -315,6 +325,8 @@ netdev_tc_flow_flush(struct netdev *netdev)
                     netdev_get_name(netdev), ovs_strerror(-ifindex));
         return -ifindex;
     }
+
+    block_id = get_block_id_from_netdev(netdev);
 
     return tc_flush(ifindex, block_id);
 }
@@ -334,6 +346,7 @@ netdev_tc_flow_dump_create(struct netdev *netdev,
         return -ifindex;
     }
 
+    block_id = get_block_id_from_netdev(netdev);
     dump = xzalloc(sizeof *dump);
     dump->nl_dump = xzalloc(sizeof *dump->nl_dump);
     dump->netdev = netdev_ref(netdev);
@@ -1098,6 +1111,7 @@ netdev_tc_flow_put(struct netdev *netdev, struct match *match,
         }
     }
 
+    block_id = get_block_id_from_netdev(netdev);
     handle = get_ufid_tc_mapping(ufid, &prio, NULL);
     if (handle && prio) {
         VLOG_DBG_RL(&rl, "updating old handle: %d prio: %d", handle, prio);
@@ -1157,6 +1171,7 @@ netdev_tc_flow_get(struct netdev *netdev OVS_UNUSED,
 
     VLOG_DBG_RL(&rl, "flow get (dev %s prio %d handle %d)",
                 netdev_get_name(dev), prio, handle);
+    block_id = get_block_id_from_netdev(netdev);
     err = tc_get_flower(ifindex, prio, handle, &flower, block_id);
     netdev_close(dev);
     if (err) {
@@ -1199,6 +1214,8 @@ netdev_tc_flow_del(struct netdev *netdev OVS_UNUSED,
         netdev_close(dev);
         return -ifindex;
     }
+
+    block_id = get_block_id_from_netdev(netdev);
 
     if (stats) {
         memset(stats, 0, sizeof *stats);
@@ -1289,6 +1306,7 @@ netdev_tc_init_flow_api(struct netdev *netdev)
         ovsthread_once_done(&block_once);
     }
 
+    block_id = get_block_id_from_netdev(netdev);
     error = tc_add_del_ingress_qdisc(ifindex, true, block_id);
 
     if (error && error != EEXIST) {

lib/netdev-bsd.c
@@ -1553,7 +1553,8 @@ netdev_bsd_update_flags(struct netdev *netdev_, enum netdev_flags off,
     netdev_bsd_rxq_wait,                             \
     netdev_bsd_rxq_drain,                            \
                                                      \
-    NO_OFFLOAD_API                                   \
+    NO_OFFLOAD_API,                                  \
+    NULL /* get_block_id */   \
 }
 
 const struct netdev_class netdev_bsd_class =

lib/netdev-dpdk.c
@@ -3957,7 +3957,8 @@ unlock:
     RXQ_RECV,                                                 \
     NULL,                       /* rx_wait */                 \
     NULL,                       /* rxq_drain */               \
-    NO_OFFLOAD_API                                            \
+    NO_OFFLOAD_API,                                           \
+    NULL                        /* get_block_id */            \
 }
 
 static const struct netdev_class dpdk_class =

lib/netdev-dummy.c
@@ -1464,7 +1464,8 @@ netdev_dummy_update_flags(struct netdev *netdev_,
     netdev_dummy_rxq_wait,                                      \
     netdev_dummy_rxq_drain,                                     \
                                                                 \
-    NO_OFFLOAD_API                                              \
+    NO_OFFLOAD_API,                                             \
+    NULL                        /* get_block_id */              \
 }
 
 static const struct netdev_class dummy_class =

lib/netdev-linux.c
@@ -3115,7 +3115,8 @@ exit:
     netdev_linux_rxq_wait,                                      \
     netdev_linux_rxq_drain,                                     \
                                                                 \
-    FLOW_OFFLOAD_API                                            \
+    FLOW_OFFLOAD_API,                                           \
+    NULL                        /* get_block_id */              \
 }
 
 const struct netdev_class netdev_linux_class =

lib/netdev-provider.h
@@ -870,6 +870,10 @@ struct netdev_class {
     /* Initializies the netdev flow api.
      * Return 0 if successful, otherwise returns a positive errno value. */
     int (*init_flow_api)(struct netdev *);
+
+    /* Get a block_id from the netdev.
+     * Returns the block_id or 0 if none exists for netdev. */
+    uint32_t (*get_block_id)(struct netdev *);
 };
 
 int netdev_register_provider(const struct netdev_class *);

lib/netdev-vport.c
@@ -1050,7 +1050,8 @@ netdev_vport_get_ifindex(const struct netdev *netdev_)
     NULL,                   /* rx_wait */                   \
     NULL,                   /* rx_drain */                  \
                                                             \
-    NETDEV_FLOW_OFFLOAD_API
+    NETDEV_FLOW_OFFLOAD_API,                                 \
+    NULL                    /* get_block_id */
 
 
 #define TUNNEL_CLASS(NAME, DPIF_PORT, BUILD_HEADER, PUSH_HEADER, POP_HEADER,   \

lib/netdev.c
@@ -2225,6 +2225,16 @@ netdev_init_flow_api(struct netdev *netdev)
             : EOPNOTSUPP);
 }
 
+uint32_t
+netdev_get_block_id(struct netdev *netdev)
+{
+    const struct netdev_class *class = netdev->netdev_class;
+
+    return (class->get_block_id
+            ? class->get_block_id(netdev)
+            : 0);
+}
+
 bool
 netdev_is_flow_api_enabled(void)
 {

commit a8165f23597ab6fd9c1666e3981c69d75ed1b31e

netdev-tc-offloads: Properly get the block id on flow del/get
Currnetly, when a tc flow is installed on a bond port using shared blocks,
we get these failures from the validator threads:

2019-03-17T10:02:58.919Z|13369|dpif(revalidator93)|WARN|system@ovs-system: failed to flow_del \
	(No such file or directory) ufid:ebe2888b-9886-4835-a42e-c2911f6af6e8 skb_priority(0),skb_mark(0),in_port(2), \
	packet_type(ns=0,id=0),eth(src=e4:11:22:33:44:71,dst=24:8a:07:88:28:12),eth_type(0x0806), [..]

The block id must be retrieved from the device we got by ufid lookup and
not from the input to the related function, fix that for flow del and get.

While here, add the block id to existing debug print.

Fixes: 88dcf2aa8234 ('netdev-provider: add class op to get block_id')
Signed-off-by: Or Gerlitz <ogerlitz@mellanox.com>
Signed-off-by: Paul Blakey <paulb@mellanox.com>
Reviewed-by: Roi Dayan <roid@mellanox.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

lib/netdev-tc-offloads.c
@@ -1387,9 +1387,9 @@ netdev_tc_flow_get(struct netdev *netdev OVS_UNUSED,
         return -ifindex;
     }
 
-    VLOG_DBG_RL(&rl, "flow get (dev %s prio %d handle %d)",
-                netdev_get_name(dev), prio, handle);
-    block_id = get_block_id_from_netdev(netdev);
+    block_id = get_block_id_from_netdev(dev);
+    VLOG_DBG_RL(&rl, "flow get (dev %s prio %d handle %d block_id %d)",
+                netdev_get_name(dev), prio, handle, block_id);
     err = tc_get_flower(ifindex, prio, handle, &flower, block_id);
     netdev_close(dev);
     if (err) {
@@ -1433,7 +1433,7 @@ netdev_tc_flow_del(struct netdev *netdev OVS_UNUSED,
         return -ifindex;
     }
 
-    block_id = get_block_id_from_netdev(netdev);
+    block_id = get_block_id_from_netdev(dev);
 
     if (stats) {
         memset(stats, 0, sizeof *stats);