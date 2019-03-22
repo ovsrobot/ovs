ovsrobot/ovs

commit 00fcc832d598d1bd34029f50c2dffac25469e412

Update Python package requirements
The Python ovs package relies on pywin32 for Windows support.
For this reason, pywin32 should be included in the requirements
list.

Signed-off-by: Lucian Petrut <lpetrut@cloudbasesolutions.com>

python/setup.py
@@ -82,7 +82,8 @@ setup_args = dict(
     ext_modules=[setuptools.Extension("ovs._json", sources=["ovs/_json.c"],
                                       libraries=['openvswitch'])],
     cmdclass={'build_ext': try_build_ext},
-    install_requires=['sortedcontainers'],
+    install_requires=['sortedcontainers',
+                      'pywin32>=1.0;platform_system=="Windows"'],
 )
 
 try:

commit 9608951b25803bfe2745d585f13f67b413666330

python: Fix package requirements with old setuptools
Commit 00fcc832d598 ("Update Python package requirements") added a
PEP 508 environment marker to install pywin32 on Windows systems.

This requires a new setuptools version (>= 20.5), but (at least)
RHEL/CentOS7 and Debian Jessie are using an older version of
setuptools and so python extension failed to build.

This commit adds "extras_require" instead of the PEP 508 environment
markers in order to have the conditional dependency of pywin32, but by
remaining compatible with the old setuptools versions.

CC: Alin Gabriel Serdean <aserdean@cloudbasesolutions.com>
CC: Lucian Petrut <lpetrut@cloudbasesolutions.com>
Fixes: 00fcc832d598 ("Update Python package requirements")
Signed-off-by: Timothy Redaelli <tredaelli@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

python/setup.py
@@ -82,8 +82,8 @@ setup_args = dict(
     ext_modules=[setuptools.Extension("ovs._json", sources=["ovs/_json.c"],
                                       libraries=['openvswitch'])],
     cmdclass={'build_ext': try_build_ext},
-    install_requires=['sortedcontainers',
-                      'pywin32>=1.0;platform_system=="Windows"'],
+    install_requires=['sortedcontainers'],
+    extras_require={':platform_system == "Windows"': ['pywin32 >= 1.0']},
 )
 
 try: