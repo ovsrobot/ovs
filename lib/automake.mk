1f6c1bf9d8e2a02ecbdb5037af4fe62ed65e8ec5
Ilya Maximets <i.maximets@samsung.com>
2019-04-23 19:19:19 +0300

    netdev: Dynamic per-port Flow API.
    Current issues with Flow API:
    
    * OVS calls offloading functions regardless of successful
      flow API initialization. (ex. on init_flow_api failure)
    * Static initilaization of Flow API for a netdev_class forbids
      having different offloading types for different instances
      of netdev with the same netdev_class. (ex. different vports in
      'system' and 'netdev' datapaths at the same time)
    
    Solution:
    
    * Move Flow API from the netdev_class to netdev instance.
    * Make Flow API dynamic, i.e. probe the APIs and choose the
      suitable one.
    
    Side effects:
    
    * Flow API providers localized as possible in their modules.
    * Now we have an ability to make runtime checks. For example,
      we could check if particular device supports features we
      need, like if dpdk device supports RSS+MARK action.
    
    Signed-off-by: Ilya Maximets <i.maximets@samsung.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/automake.mk
+++ lib/automake.mk
@@ -138,6 +138,7 @@ lib_libopenvswitch_la_SOURCES = \
 	lib/netdev-dpdk.h \
 	lib/netdev-dummy.c \
 	lib/netdev-provider.h \
+	lib/netdev-offload-provider.h \
 	lib/netdev-rte-offloads.h \
 	lib/netdev-vport.c \
 	lib/netdev-vport.h \
@@ -393,7 +394,6 @@ lib_libopenvswitch_la_SOURCES += \
 	lib/netdev-linux.c \
 	lib/netdev-linux.h \
 	lib/netdev-tc-offloads.c \
-	lib/netdev-tc-offloads.h \
 	lib/netlink-conntrack.c \
 	lib/netlink-conntrack.h \
 	lib/netlink-notifier.c \
