ovsrobot/ovs

commit 879e8238dfdf20aa973ab8cca3958f85afff4686

travis: Update sparse git repo
The old git tree git://git.kernel.org/pub/scm/devel/sparse/chrisl/sparse.git
has not been updated since 2016, and that triggers the following build error
on Ubuntu 18.04 host with 2.27-3 libc6-dev.  So update the sparse git repo
to the new one.

$ .travis/linux-prepare.sh
$  export PATH=$PATH:$HOME/bin
$ .travis/linux-build.sh

/usr/include/stdlib.h:140:17: error: Expected ; at end of declaration
/usr/include/stdlib.h:140:17: error: got strtof32
/usr/include/stdlib.h:146:17: error: Expected ; at end of declaration
/usr/include/stdlib.h:146:17: error: got strtof64
/usr/include/stdlib.h:158:18: error: Expected ; at end of declaration
/usr/include/stdlib.h:158:18: error: got strtof32x
/usr/include/stdlib.h:233:33: error: Expected ) in function declarator
/usr/include/stdlib.h:233:33: error: got __f
/usr/include/stdlib.h:239:33: error: Expected ) in function declarator
/usr/include/stdlib.h:239:33: error: got __f
/usr/include/stdlib.h:251:35: error: Expected ) in function declarator
/usr/include/stdlib.h:251:35: error: got __f
/usr/include/stdlib.h:316:17: error: Expected ; at end of declaration
/usr/include/stdlib.h:316:17: error: got strtof32_l
/usr/include/stdlib.h:323:17: error: Expected ; at end of declaration
/usr/include/stdlib.h:323:17: error: got strtof64_l
/usr/include/stdlib.h:337:18: error: Expected ; at end of declaration
/usr/include/stdlib.h:337:18: error: got strtof32x_l
Makefile:5288: recipe for target 'lib/aes128.lo' failed
make[2]: *** [lib/aes128.lo] Error 1
...

Tested on Jarvis: https://travis-ci.org/YiHungWei/ovs/builds/521979625

Signed-off-by: Yi-Hung Wei <yihung.wei@gmail.com>
Signed-off-by: Ben Pfaff <blp@ovn.org>

.travis/linux-prepare.sh
@@ -7,7 +7,7 @@ set -ev
 # Explicitly disable sparse support for llvm because some travis
 # environments claim to have LLVM (llvm-config exists and works) but
 # linking against it fails.
-git clone git://git.kernel.org/pub/scm/devel/sparse/chrisl/sparse.git
+git clone git://git.kernel.org/pub/scm/devel/sparse/sparse.git
 cd sparse && make -j4 HAVE_LLVM= install && cd ..
 
 pip install --disable-pip-version-check --user six flake8 hacking

commit da1a58402dd5e8839ee35b571a2a303e188b071f

travis: Fix checks skipping by sparse.
Recent commit in "sparse" broke checking the OVS sources, because
'make' uses '-MD' flag to generate dependencies as a side effect
within compilation commands, but "sparse" skips all the build commands
that contains '-MD' and friends.
Let's revert the bad commit as a workaround before installing "sparse"
in TravisCI.

Additionally fixed a false-positive:
./lib/bitmap.h:64:29: error: shift too big (64) for type unsigned long

CC: Yi-Hung Wei <yihung.wei@gmail.com>
Fixes: 879e8238dfdf ("travis: Update sparse git repo")
Signed-off-by: Ilya Maximets <i.maximets@samsung.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

.travis/linux-prepare.sh
@@ -8,7 +8,14 @@ set -ev
 # environments claim to have LLVM (llvm-config exists and works) but
 # linking against it fails.
 git clone git://git.kernel.org/pub/scm/devel/sparse/sparse.git
-cd sparse && make -j4 HAVE_LLVM= install && cd ..
+cd sparse
+# Commit bb1bf748580d ("cgcc: gendeps for -MM, -MD & -MMD too") makes
+# sparse ignore almost all source files, because 'make' uses '-MD' to
+# generate dependencies as a side effect within compilation commands.
+git revert bb1bf748580d --no-commit
+git diff HEAD
+make -j4 HAVE_LLVM= install
+cd ..
 
 pip install --disable-pip-version-check --user six flake8 hacking
 pip install --user --upgrade docutils

lib/bitmap.h
@@ -61,7 +61,7 @@ bitmap_init1(unsigned long *bitmap, size_t n_bits)
 
     memset(bitmap, 0xff, n_bytes);
     if (r_bits) {
-        bitmap[n_longs - 1] >>= BITMAP_ULONG_BITS - r_bits;
+        bitmap[n_longs - 1] = (1UL << r_bits) - 1;
     }
     return bitmap;
 }