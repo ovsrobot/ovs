afb021c21654f1a910df02ee69ed93c9c01d782e
Ben Pfaff <blp@ovn.org>
2019-06-07 11:13:00 -0700

    ofproto-dpif-xlate: Do not use zero-weight buckets in select groups.
    The OpenFlow specification says that buckets in select groups with a weight
    of zero should not be selected, but the ofproto-dpif implementation could
    select them in corner cases.  This fixes the problem.
    
    Reported-by: ychen <ychen103103@163.com>
    Reported-at: https://mail.openvswitch.org/pipermail/ovs-dev/2019-May/359349.html
    Signed-off-by: Ben Pfaff <blp@ovn.org>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- ofproto/ofproto-dpif-xlate.c
+++ ofproto/ofproto-dpif-xlate.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017 Nicira, Inc.
+/* Copyright (c) 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2019 Nicira, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -1929,7 +1929,7 @@ group_best_live_bucket(const struct xlate_ctx *ctx,
 
     struct ofputil_bucket *bucket;
     LIST_FOR_EACH (bucket, list_node, &group->up.buckets) {
-        if (bucket_is_alive(ctx, bucket, 0)) {
+        if (bucket_is_alive(ctx, bucket, 0) && bucket->weight > 0) {
             uint32_t score =
                 (hash_int(bucket->bucket_id, basis) & 0xffff) * bucket->weight;
             if (score >= best_score) {
@@ -4535,7 +4535,7 @@ pick_dp_hash_select_group(struct xlate_ctx *ctx, struct group_dpif *group)
         for (int i = 0; i <= hash_mask; i++) {
             struct ofputil_bucket *b =
                     group->hash_map[(dp_hash + i) & hash_mask];
-            if (bucket_is_alive(ctx, b, 0)) {
+            if (bucket_is_alive(ctx, b, 0) && b->weight > 0) {
                 return b;
             }
         }
