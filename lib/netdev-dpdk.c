4f8da93cba40040991d61beead48ee86b1707c07
Kevin Traynor <ktraynor@redhat.com>
2019-06-21 14:41:58 +0100

    netdev-dpdk: Enable vhost-tx-retries config.
    vhost tx retries can provide some mitigation against
    dropped packets due to a temporarily slow guest/limited queue
    size for an interface, but on the other hand when a system
    is fully loaded those extra cycles retrying could mean
    packets are dropped elsewhere.
    
    Up to now max vhost retries have been hardcoded, which meant
    no tuning and no way to disable for debugging to see if extra
    cycles spent retrying resulted in rx drops on some other
    interface.
    
    Add an option to change the max retries, with a value of
    0 effectively disabling vhost tx retries.
    
    Signed-off-by: Kevin Traynor <ktraynor@redhat.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/netdev-dpdk.c
+++ lib/netdev-dpdk.c
@@ -408,7 +408,8 @@ struct netdev_dpdk {
 
         /* True if vHost device is 'up' and has been reconfigured at least once */
         bool vhost_reconfigured;
-        /* 3 pad bytes here. */
+        atomic_uint8_t vhost_tx_retries;
+        /* 2 pad bytes here. */
     );
 
     PADDED_MEMBERS(CACHE_LINE_SIZE,
@@ -1239,6 +1240,8 @@ vhost_common_construct(struct netdev *netdev)
         return ENOMEM;
     }
 
+    atomic_store_relaxed(&dev->vhost_tx_retries, VHOST_ENQ_RETRY_NUM);
+
     return common_construct(netdev, DPDK_ETH_PORT_ID_INVALID,
                             DPDK_DEV_VHOST, socket_id);
 }
@@ -1898,6 +1901,7 @@ netdev_dpdk_vhost_client_set_config(struct netdev *netdev,
 {
     struct netdev_dpdk *dev = netdev_dpdk_cast(netdev);
     const char *path;
+    int max_tx_retries, cur_max_tx_retries;
 
     ovs_mutex_lock(&dev->mutex);
     if (!(dev->vhost_driver_flags & RTE_VHOST_USER_CLIENT)) {
@@ -1913,6 +1917,15 @@ netdev_dpdk_vhost_client_set_config(struct netdev *netdev,
             }
             netdev_request_reconfigure(netdev);
         }
+    }
+
+    max_tx_retries = smap_get_int(args, "vhost-tx-retries",
+                                  VHOST_ENQ_RETRY_NUM);
+    atomic_read_relaxed(&dev->vhost_tx_retries, &cur_max_tx_retries);
+    if (max_tx_retries != cur_max_tx_retries) {
+        atomic_store_relaxed(&dev->vhost_tx_retries, max_tx_retries);
+        VLOG_INFO("Max Tx retries for vhost device '%s' set to %d",
+                  dev->up.name, max_tx_retries);
     }
     ovs_mutex_unlock(&dev->mutex);
 
@@ -2298,6 +2311,7 @@ netdev_dpdk_vhost_update_tx_counters(struct netdev_stats *stats,
                                      struct dp_packet **packets,
                                      int attempted,
                                      int dropped,
+                                     int max_retries,
                                      int retries)
 {
     int i;
@@ -2305,7 +2319,7 @@ netdev_dpdk_vhost_update_tx_counters(struct netdev_stats *stats,
 
     stats->tx_packets += sent;
     stats->tx_dropped += dropped;
-    stats->tx_retries += MIN(retries, VHOST_ENQ_RETRY_NUM);
+    stats->tx_retries += MIN(retries, max_retries);
 
     for (i = 0; i < sent; i++) {
         stats->tx_bytes += dp_packet_size(packets[i]);
@@ -2322,6 +2336,7 @@ __netdev_dpdk_vhost_send(struct netdev *netdev, int qid,
     unsigned int dropped = 0;
     int i, retries = 0;
     int vid = netdev_dpdk_get_vid(dev);
+    int max_retries = 0;
 
     qid = dev->tx_q[qid % netdev->n_txq].map;
 
@@ -2350,17 +2365,20 @@ __netdev_dpdk_vhost_send(struct netdev *netdev, int qid,
             cnt -= tx_pkts;
             /* Prepare for possible retry.*/
             cur_pkts = &cur_pkts[tx_pkts];
+            if (OVS_UNLIKELY(cnt && !retries)) {
+                atomic_read_relaxed(&dev->vhost_tx_retries, &max_retries);
+            }
         } else {
             /* No packets sent - do not retry.*/
             break;
         }
-    } while (cnt && (retries++ < VHOST_ENQ_RETRY_NUM));
+    } while (cnt && (retries++ < max_retries));
 
     rte_spinlock_unlock(&dev->tx_q[qid].tx_lock);
 
     rte_spinlock_lock(&dev->stats_lock);
     netdev_dpdk_vhost_update_tx_counters(&dev->stats, pkts, total_pkts,
-                                         cnt + dropped, retries);
+                                         cnt + dropped, max_retries, retries);
     rte_spinlock_unlock(&dev->stats_lock);
 
 out:
