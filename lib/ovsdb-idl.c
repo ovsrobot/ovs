ovsrobot/ovs

commit ca545a787ac03fdfd36fd5bef4e6986624250cf3

ovsdb-idl.c: Increase seqno for change-tracking of table references.
This fixes the change-tracking feature.  The seqno change is needed so that
the change-tracking helper function ..._is_new() can work properly.

Fixes: 102781cc02c6 ("ovsdb-idl: Track changes for table references.")
Signed-off-by: Han Zhou <hzhou8@ebay.com>
Signed-off-by: Ben Pfaff <blp@ovn.org>

lib/ovsdb-idl.c
@@ -2359,6 +2359,9 @@ add_tracked_change_for_references(struct ovsdb_idl_row *row)
             ovsdb_idl_track_is_set(row->table)) {
         ovs_list_push_back(&row->table->track_list,
                            &row->track_node);
+        row->change_seqno[OVSDB_IDL_CHANGE_MODIFY]
+            = row->table->change_seqno[OVSDB_IDL_CHANGE_MODIFY]
+            = row->table->db->change_seqno + 1;
 
         const struct ovsdb_idl_arc *arc;
         LIST_FOR_EACH (arc, dst_node, &row->dst_arcs) {

commit 72ab7cb0dd2b4eea40223013fa7a6cf0f2ceb388

ovsdb-idl.c: Fix IDL change tracking xxx_is_new().
The commit ca545a78 made sure that xxx_is_new() for the rows
referencing other rows returned false instead of true. However,
it caused xxx_is_new() for the row being referenced returned false,
even if the row was newly added. This patch fixes it.

Fixes: ca545a787ac0 (ovsdb-idl.c: Increase seqno for change-tracking of table references.)
Signed-off-by: Han Zhou <hzhou8@ebay.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

lib/ovsdb-idl.c
@@ -2509,10 +2509,8 @@ ovsdb_idl_process_update2(struct ovsdb_idl_table *table,
     return true;
 }
 
-/* Recursively add rows to tracked change lists for current row
- * and the rows that reference this row. */
 static void
-add_tracked_change_for_references(struct ovsdb_idl_row *row)
+add_tracked_change_for_references__(struct ovsdb_idl_row *row)
 {
     if (ovs_list_is_empty(&row->track_node) &&
             ovsdb_idl_track_is_set(row->table)) {
@@ -2524,8 +2522,19 @@ add_tracked_change_for_references(struct ovsdb_idl_row *row)
 
         const struct ovsdb_idl_arc *arc;
         LIST_FOR_EACH (arc, dst_node, &row->dst_arcs) {
-            add_tracked_change_for_references(arc->src);
+            add_tracked_change_for_references__(arc->src);
         }
+    }
+}
+
+/* Recursively add rows to tracked change lists for current row
+ * and the rows that reference this row. */
+static void
+add_tracked_change_for_references(struct ovsdb_idl_row *row)
+{
+    const struct ovsdb_idl_arc *arc;
+    LIST_FOR_EACH (arc, dst_node, &row->dst_arcs) {
+        add_tracked_change_for_references__(arc->src);
     }
 }
 
@@ -2593,6 +2602,10 @@ ovsdb_idl_row_change__(struct ovsdb_idl_row *row, const struct json *row_json,
                         = row->table->change_seqno[change]
                         = row->table->db->change_seqno + 1;
                     if (table->modes[column_idx] & OVSDB_IDL_TRACK) {
+                        if (ovs_list_is_empty(&row->track_node)) {
+                            ovs_list_push_back(&row->table->track_list,
+                                               &row->track_node);
+                        }
                         add_tracked_change_for_references(row);
                         if (!row->updated) {
                             row->updated = bitmap_allocate(class->n_columns);