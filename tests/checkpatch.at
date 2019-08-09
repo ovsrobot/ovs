ovsrobot/ovs

commit 16770c6d91793c4f4eca8a739d4cf541fc705cc3

checkpatch: support macro continuation
Added a test to identify a macro and skip
printing errors if the condition or loop
is part of a macro.

Additional tests are added to checkpatch
testsuite that cover conditionals and
loop constructs.

Signed-off-by: Bala Sankaran <bsankara@redhat.com>
Signed-off-by: Ben Pfaff <blp@ovn.org>

utilities/checkpatch.py
@@ -163,6 +163,8 @@ __regex_has_xxx_mark = re.compile(r'.*xxx.*', re.IGNORECASE)
 __regex_added_doc_rst = re.compile(
                     r'\ndiff .*Documentation/.*rst\nnew file mode')
 __regex_empty_return = re.compile(r'\s*return;')
+__regex_if_macros = re.compile(r'^ +(%s) \([\S][\s\S]+[\S]\) { \\' %
+                               __parenthesized_constructs)
 
 skip_leading_whitespace_check = False
 skip_trailing_whitespace_check = False
@@ -252,7 +254,9 @@ def if_and_for_end_with_bracket_check(line):
     if __regex_is_for_if_single_line_bracket.search(line) is not None:
         if not balanced_parens(line):
             return True
-        if __regex_ends_with_bracket.search(line) is None:
+
+        if __regex_ends_with_bracket.search(line) is None and \
+           __regex_if_macros.match(line) is None:
             return False
     if __regex_conditional_else_bracing.match(line) is not None:
         return False

tests/checkpatch.at
@@ -156,3 +156,226 @@ try_checkpatch \
    "ERROR: Patch lacks author."
 
 AT_CLEANUP
+
+AT_SETUP([checkpatch - parenthesized constructs])
+for ctr in 'if' 'while' 'switch'; do
+try_checkpatch \
+   "Author: A
+    
+    Signed-off-by: A
+    ---
+    diff --git a/A.c b/A.c
+    index 0000000..1111111 100644
+    --- a/A.c
+    +++ b/A.c
+    @@ -1,1 +1,1 @@
+    +     $ctr (first_run) {
+    "
+
+try_checkpatch \
+   "Author: A
+
+    Signed-off-by: A
+    ---
+    diff --git a/A.c b/A.c
+    index 0000000..1111111 100644
+    --- a/A.c
+    +++ b/A.c
+    @@ -1,1 +1,1 @@
+    +     $ctr ( first_run) {
+    " \
+    "ERROR: Improper whitespace around control block
+    #8 FILE: A.c:1:
+         $ctr ( first_run) {
+"
+
+try_checkpatch \
+   "Author: A
+
+    Signed-off-by: A
+    ---
+    diff --git a/A.c b/A.c
+    index 0000000..1111111 100644
+    --- a/A.c
+    +++ b/A.c
+    @@ -1,1 +1,1 @@
+    +     $ctr (first_run ) {
+    " \
+    "ERROR: Inappropriate bracing around statement
+    #8 FILE: A.c:1:
+         $ctr (first_run ) {
+"
+
+try_checkpatch \
+   "Author: A
+
+    Signed-off-by: A
+    ---
+    diff --git a/A.c b/A.c
+    index 0000000..1111111 100644
+    --- a/A.c
+    +++ b/A.c
+    @@ -1,1 +1,1 @@
+    +     $ctr (first_run)
+    " \
+    "ERROR: Inappropriate bracing around statement
+    #8 FILE: A.c:1:
+         $ctr (first_run)
+"
+
+try_checkpatch \
+   "Author: A
+
+    Signed-off-by: A
+    ---
+    diff --git a/A.c b/A.c
+    index 0000000..1111111 100644
+    --- a/A.c
+    +++ b/A.c
+    @@ -1,1 +1,1 @@
+    +     $ctr(first_run)
+    " \
+    "ERROR: Improper whitespace around control block
+    #8 FILE: A.c:1:
+         $ctr(first_run)
+"
+
+try_checkpatch \
+   "Author: A
+
+    Signed-off-by: A
+    ---
+    diff --git a/A.c b/A.c
+    index 0000000..1111111 100644
+    --- a/A.c
+    +++ b/A.c
+    @@ -1,1 +1,1 @@
+    +     $ctr (first_run) { /* foo */
+    "
+
+try_checkpatch \
+   "Author: A
+
+    Signed-off-by: A
+    ---
+    diff --git a/A.c b/A.c
+    index 0000000..1111111 100644
+    --- a/A.c
+    +++ b/A.c
+    @@ -1,1 +1,1 @@
+    +     $ctr (first_run) { \\
+    "
+done
+AT_CLEANUP
+
+
+
+AT_SETUP([checkpatch - parenthesized constructs - for])
+try_checkpatch \
+   "Author: A
+
+    Signed-off-by: A
+    ---
+    diff --git a/A.c b/A.c
+    index 0000000..1111111 100644
+    --- a/A.c
+    +++ b/A.c
+    @@ -1,1 +1,1 @@
+    +     for (init; condition; increment) {
+    "
+
+try_checkpatch \
+   "Author: A
+
+    Signed-off-by: A
+    ---
+    diff --git a/A.c b/A.c
+    index 0000000..1111111 100644
+    --- a/A.c
+    +++ b/A.c
+    @@ -1,1 +1,1 @@
+    +     for ( init; condition; increment) {
+    " \
+    "ERROR: Improper whitespace around control block
+    #8 FILE: A.c:1:
+         for ( init; condition; increment) {
+"
+
+try_checkpatch \
+   "Author: A
+
+    Signed-off-by: A
+    ---
+    diff --git a/A.c b/A.c
+    index 0000000..1111111 100644
+    --- a/A.c
+    +++ b/A.c
+    @@ -1,1 +1,1 @@
+    +     for (init; condition; increment ) {
+    " \
+    "ERROR: Inappropriate bracing around statement
+    #8 FILE: A.c:1:
+         for (init; condition; increment ) {
+"
+
+try_checkpatch \
+   "Author: A
+
+    Signed-off-by: A
+    ---
+    diff --git a/A.c b/A.c
+    index 0000000..1111111 100644
+    --- a/A.c
+    +++ b/A.c
+    @@ -1,1 +1,1 @@
+    +     for (init; condition; increment)
+    " \
+    "ERROR: Inappropriate bracing around statement
+    #8 FILE: A.c:1:
+         for (init; condition; increment)
+"
+
+try_checkpatch \
+   "Author: A
+
+    Signed-off-by: A
+    ---
+    diff --git a/A.c b/A.c
+    index 0000000..1111111 100644
+    --- a/A.c
+    +++ b/A.c
+    @@ -1,1 +1,1 @@
+    +     for(init; condition; increment)
+    " \
+    "ERROR: Improper whitespace around control block
+    #8 FILE: A.c:1:
+         for(init; condition; increment)
+"
+
+try_checkpatch \
+   "Author: A
+
+    Signed-off-by: A
+    ---
+    diff --git a/A.c b/A.c
+    index 0000000..1111111 100644
+    --- a/A.c
+    +++ b/A.c
+    @@ -1,1 +1,1 @@
+    +     for (init; condition; increment) { /* foo */
+    "
+
+try_checkpatch \
+   "Author: A
+
+    Signed-off-by: A
+    ---
+    diff --git a/A.c b/A.c
+    index 0000000..1111111 100644
+    --- a/A.c
+    +++ b/A.c
+    @@ -1,1 +1,1 @@
+    +     for (init; condition; increment) { \\
+    "
+
+AT_CLEANUP

commit 81e63c4026dc0eaeec3e280d11194c93c7f5af38

checkpatch: Fix regexp for if, while, etc inside macros.
This allows to use a one-character expression inside the 'if'
statement and multiple spaces before the line continuation character.

Fixes false positive in case like this:

  #define MACRO(ARG)     \
      if (a) {           \
          do_work(ARG);  \
      }

Fixes: 16770c6d9179 ("checkpatch: support macro continuation")
Signed-off-by: Ilya Maximets <i.maximets@samsung.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

utilities/checkpatch.py
@@ -172,7 +172,7 @@ __regex_has_xxx_mark = re.compile(r'.*xxx.*', re.IGNORECASE)
 __regex_added_doc_rst = re.compile(
                     r'\ndiff .*Documentation/.*rst\nnew file mode')
 __regex_empty_return = re.compile(r'\s*return;')
-__regex_if_macros = re.compile(r'^ +(%s) \([\S][\s\S]+[\S]\) { \\' %
+__regex_if_macros = re.compile(r'^ +(%s) \([\S]([\s\S]+[\S])*\) { +\\' %
                                __parenthesized_constructs)
 
 skip_leading_whitespace_check = False

tests/checkpatch.at
@@ -230,6 +230,11 @@ try_checkpatch \
    "COMMON_PATCH_HEADER
     +     $ctr (first_run) { \\
     "
+
+try_checkpatch \
+   "COMMON_PATCH_HEADER
+    +     $ctr (a) {    \\
+    "
 done
 AT_CLEANUP
 