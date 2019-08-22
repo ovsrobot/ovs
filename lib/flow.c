f5d584e9d9e6b3e7716daf354ec9818edc84959e
Yanqin Wei <Yanqin.Wei@arm.com>
2019-08-22 17:30:14 +0800

    flow: fix incorrect padding length checking and combine branch in ipv6_sanity_check
    The padding length is (packet size - ipv6 header length - ipv6 plen).  This
    patch fixes incorrect ipv6 size checking and improves it by combining branch.
    
    Change-Id: I7a6212872f89ef9c11e3fbcec4dbecbcc6e89c47
    Reviewed-by: Gavin Hu <Gavin.Hu@arm.com>
    Signed-off-by: Yanqin Wei <Yanqin.Wei@arm.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/flow.c
+++ lib/flow.c
@@ -688,18 +688,16 @@ ipv4_get_nw_frag(const struct ip_header *nh)
 static inline bool
 ipv6_sanity_check(const struct ovs_16aligned_ip6_hdr *nh, size_t size)
 {
-    uint16_t plen;
+    int pad_len;
 
     if (OVS_UNLIKELY(size < sizeof *nh)) {
         return false;
     }
 
-    plen = ntohs(nh->ip6_plen);
-    if (OVS_UNLIKELY(plen + IPV6_HEADER_LEN > size)) {
-        return false;
-    }
+    pad_len = size - IPV6_HEADER_LEN - ntohs(nh->ip6_plen);
+
     /* Jumbo Payload option not supported yet. */
-    if (OVS_UNLIKELY(size - plen > UINT8_MAX)) {
+    if (OVS_UNLIKELY(pad_len < 0 || pad_len > UINT8_MAX)) {
         return false;
     }
 
