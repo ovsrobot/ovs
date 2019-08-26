e2f949b93be351868844255e24bb1f5b9a3b033b
Aliasgar Ginwala <aginwala@ebay.com>
2019-08-26 14:01:52 -0700

    ovsdb-tool: Convert clustered db to standalone db.
    Add support in ovsdb-tool for migrating clustered dbs to standalone dbs.
    E.g. usage to migrate nb/sb db to standalone db from raft:
    ovsdb-tool cluster-to-standalone ovnnb_db.db ovnnb_db_cluster.db
    
    Signed-off-by: Aliasgar Ginwala <aginwala@ebay.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- ovsdb/ovsdb-tool.c
+++ ovsdb/ovsdb-tool.c
@@ -173,6 +173,9 @@ usage(void)
            "  compare-versions A OP B  compare OVSDB schema version numbers\n"
            "  query [DB] TRNS         execute read-only transaction on DB\n"
            "  transact [DB] TRNS      execute read/write transaction on DB\n"
+           "  cluster-to-standalone [DB [DB]]    Convert clustered DB to\n"
+                   "standalone DB when cluster is down and cannot be\n"
+                    "revived\n"
            "  [-m]... show-log [DB]   print DB's log entries\n"
            "The default DB is %s.\n"
            "The default SCHEMA is %s.\n",
@@ -943,6 +946,63 @@ print_raft_record(const struct raft_record *r,
 }
 
 static void
+write_raft_header_to_file(const struct json *data,
+                          struct ovsdb_log *db_log_data)
+{
+    if (!data || json_array(data)->n != 2) {
+       ovs_fatal(0, "***Invalid data***\n");
+    }
+
+    struct json *schema_json = json_array(data)->elems[0];
+    if (schema_json->type != JSON_NULL) {
+        struct ovsdb_schema *schema;
+        check_ovsdb_error(ovsdb_schema_from_json(schema_json, &schema));
+        check_ovsdb_error(ovsdb_log_write_and_free(db_log_data, schema_json));
+    }
+
+    struct json *data_json = json_array(data)->elems[1];
+    if (!data_json || data_json->type != JSON_OBJECT) {
+        ovs_fatal(0, "***invalid data***\n");
+    }
+    if (data_json->type != JSON_NULL) {
+        check_ovsdb_error(ovsdb_log_write_and_free(db_log_data, data_json));
+    }
+}
+
+static void
+write_raft_header(const struct raft_header *h, struct ovsdb_log *db_log_data)
+{
+    if (h->snap_index) {
+        write_raft_header_to_file(h->snap.data, db_log_data);
+    }
+}
+
+static void
+write_raft_record_to_file(const struct json *data,
+                          struct ovsdb_log *db_log_data)
+{
+    if (json_array(data)->n != 2) {
+        ovs_fatal(0, "***invalid data***\n");
+    }
+
+    struct json *data_json = json_array(data)->elems[1];
+    if (data_json->type != JSON_NULL) {
+        check_ovsdb_error(ovsdb_log_write_and_free(db_log_data, data_json));
+    }
+}
+
+static void
+write_raft_record(const struct raft_record *r, struct ovsdb_log *db_log_data)
+{
+    if (r->type == RAFT_REC_ENTRY) {
+        if (!r->entry.data) {
+            return;
+        }
+        write_raft_record_to_file(r->entry.data, db_log_data);
+    }
+}
+
+static void
 do_show_log_cluster(struct ovsdb_log *log)
 {
     struct smap names = SMAP_INITIALIZER(&names);
@@ -1511,6 +1571,54 @@ do_compare_versions(struct ovs_cmdl_context *ctx)
     exit(result ? 0 : 2);
 }
 
+static void
+do_convert_to_standalone(struct ovsdb_log *log, struct ovsdb_log *db_log_data)
+{
+    for (unsigned int i = 0; ; i++) {
+        struct json *json;
+        check_ovsdb_error(ovsdb_log_read(log, &json));
+        if (!json) {
+            break;
+        }
+
+        struct ovsdb_error *error;
+        if (i == 0) {
+            struct raft_header h;
+            check_ovsdb_error(raft_header_from_json(&h, json));
+            write_raft_header(&h, db_log_data);
+            check_ovsdb_error(ovsdb_log_commit_block(db_log_data));
+            raft_header_uninit(&h);
+
+        } else {
+            struct raft_record r;
+            check_ovsdb_error(raft_record_from_json(&r, json));
+            write_raft_record(&r, db_log_data);
+            check_ovsdb_error(ovsdb_log_commit_block(db_log_data));
+            raft_record_uninit(&r);
+        }
+    }
+}
+
+static void
+do_cluster_standalone(struct ovs_cmdl_context *ctx)
+{
+    const char *db_file_name = ctx->argv[1];
+    const char *cluster_db_file_name = ctx->argv[2];
+    struct ovsdb_log *log;
+    struct ovsdb_log *db_log_data;
+
+    check_ovsdb_error(ovsdb_log_open(cluster_db_file_name,
+                                     OVSDB_MAGIC"|"RAFT_MAGIC,
+                                     OVSDB_LOG_READ_ONLY, -1, &log));
+    check_ovsdb_error(ovsdb_log_open(db_file_name, OVSDB_MAGIC,
+                                     OVSDB_LOG_CREATE_EXCL, -1, &db_log_data));
+    if (strcmp(ovsdb_log_get_magic(log), RAFT_MAGIC) != 0) {
+        ovs_fatal(0, "Database is not clustered db.\n");
+    }
+    do_convert_to_standalone(log, db_log_data);
+    ovsdb_log_close(db_log_data);
+    ovsdb_log_close(log);
+}
 
 static void
 do_help(struct ovs_cmdl_context *ctx OVS_UNUSED)
@@ -1550,7 +1658,9 @@ static const struct ovs_cmdl_command all_commands[] = {
     { "compare-versions", "a op b", 3, 3, do_compare_versions, OVS_RO },
     { "help", NULL, 0, INT_MAX, do_help, OVS_RO },
     { "list-commands", NULL, 0, INT_MAX, do_list_commands, OVS_RO },
-    { NULL, NULL, 0, 0, NULL, OVS_RO },
+    { "cluster-to-standalone", "[db [clusterdb]]", 2, 2,
+    do_cluster_standalone, OVS_RW },
+    { NULL, NULL, 2, 2, NULL, OVS_RO },
 };
 
 static const struct ovs_cmdl_command *get_all_commands(void)
