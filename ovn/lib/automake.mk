ovsrobot/ovs

commit f3e24610ea18eb873dc860f1710432e9aacd27fd

Remove OVN.
OVN is separated into its own repo. This commit removes the OVN source,
OVN tests, and OVN documentation. It also removes mentions of OVN from
most documentation. The only place where OVN has been left is in
changelogs/NEWS, since we shouldn't mess with the history of the
project.

There is an exception here. The ovsdb-cluster tests rely on ovn-nbctl
and ovn-sbctl to run. Therefore those ovn utilities, as well as their
dependencies remain in the repo with this commit.

Acked-by: Numan Siddique <nusiddiq@redhat.com>
Signed-off-by: Mark Michelson <mmichels@redhat.com>
Signed-off-by: Ben Pfaff <blp@ovn.org>

ovn/lib/automake.mk
@@ -6,23 +6,8 @@ ovn_lib_libovn_la_LDFLAGS = \
 ovn_lib_libovn_la_SOURCES = \
 	ovn/lib/acl-log.c \
 	ovn/lib/acl-log.h \
-	ovn/lib/actions.c \
-	ovn/lib/chassis-index.c \
-	ovn/lib/chassis-index.h \
-	ovn/lib/expr.c \
-	ovn/lib/extend-table.h \
-	ovn/lib/extend-table.c \
-	ovn/lib/ip-mcast-index.c \
-	ovn/lib/ip-mcast-index.h \
-	ovn/lib/mcast-group-index.c \
-	ovn/lib/mcast-group-index.h \
-	ovn/lib/lex.c \
-	ovn/lib/ovn-l7.h \
 	ovn/lib/ovn-util.c \
 	ovn/lib/ovn-util.h \
-	ovn/lib/logical-fields.c \
-	ovn/lib/inc-proc-eng.c \
-	ovn/lib/inc-proc-eng.h
 nodist_ovn_lib_libovn_la_SOURCES = \
 	ovn/lib/ovn-nb-idl.c \
 	ovn/lib/ovn-nb-idl.h \

Documentation/automake.mk
@@ -18,7 +18,6 @@ DOC_SOURCE = \
 	Documentation/intro/install/fedora.rst \
 	Documentation/intro/install/general.rst \
 	Documentation/intro/install/netbsd.rst \
-	Documentation/intro/install/ovn-upgrades.rst \
 	Documentation/intro/install/rhel.rst \
 	Documentation/intro/install/userspace.rst \
 	Documentation/intro/install/windows.rst \
@@ -26,12 +25,8 @@ DOC_SOURCE = \
 	Documentation/tutorials/index.rst \
 	Documentation/tutorials/faucet.rst \
 	Documentation/tutorials/ovs-advanced.rst \
-	Documentation/tutorials/ovn-openstack.rst \
-	Documentation/tutorials/ovn-sandbox.rst \
 	Documentation/tutorials/ovs-conntrack.rst \
 	Documentation/tutorials/ipsec.rst \
-	Documentation/tutorials/ovn-ipsec.rst \
-	Documentation/tutorials/ovn-rbac.rst \
 	Documentation/topics/index.rst \
 	Documentation/topics/bonding.rst \
 	Documentation/topics/idl-compound-indexes.rst \
@@ -54,28 +49,22 @@ DOC_SOURCE = \
 	Documentation/topics/fuzzing/ovs-fuzzers.rst \
 	Documentation/topics/fuzzing/security-analysis-of-ovs-fuzzers.rst \
 	Documentation/topics/testing.rst \
-	Documentation/topics/high-availability.rst \
 	Documentation/topics/integration.rst \
 	Documentation/topics/language-bindings.rst \
 	Documentation/topics/networking-namespaces.rst \
 	Documentation/topics/openflow.rst \
-	Documentation/topics/ovn-news-2.8.rst \
 	Documentation/topics/ovsdb-replication.rst \
 	Documentation/topics/porting.rst \
-	Documentation/topics/role-based-access-control.rst \
 	Documentation/topics/tracing.rst \
 	Documentation/topics/windows.rst \
 	Documentation/howto/index.rst \
-	Documentation/howto/docker.rst \
 	Documentation/howto/dpdk.rst \
-	Documentation/howto/firewalld.rst \
 	Documentation/howto/ipsec.rst \
 	Documentation/howto/kvm.rst \
 	Documentation/howto/libvirt.rst \
 	Documentation/howto/selinux.rst \
 	Documentation/howto/ssl.rst \
 	Documentation/howto/lisp.rst \
-	Documentation/howto/openstack-containers.rst \
 	Documentation/howto/qos.png \
 	Documentation/howto/qos.rst \
 	Documentation/howto/sflow.png \
@@ -94,7 +83,6 @@ DOC_SOURCE = \
 	Documentation/faq/general.rst \
 	Documentation/faq/issues.rst \
 	Documentation/faq/openflow.rst \
-	Documentation/faq/ovn.rst \
 	Documentation/faq/qos.rst \
 	Documentation/faq/releases.rst \
 	Documentation/faq/terminology.rst \

debian/automake.mk
@@ -52,28 +52,6 @@ EXTRA_DIST += \
 	debian/openvswitch-vtep.init \
 	debian/openvswitch-vtep.install \
 	debian/openvswitch-vtep.manpages \
-	debian/ovn-central.dirs \
-	debian/ovn-central.init \
-	debian/ovn-central.install \
-	debian/ovn-central.manpages \
-	debian/ovn-central.postinst \
-	debian/ovn-central.postrm \
-	debian/ovn-central.template \
-	debian/ovn-controller-vtep.init \
-	debian/ovn-controller-vtep.install \
-	debian/ovn-controller-vtep.manpages \
-	debian/ovn-common.install \
-	debian/ovn-common.manpages \
-	debian/ovn-common.postinst \
-	debian/ovn-common.postrm \
-	debian/ovn-docker.install \
-	debian/ovn-host.dirs \
-	debian/ovn-host.init \
-	debian/ovn-host.install \
-	debian/ovn-host.manpages \
-	debian/ovn-host.postinst \
-	debian/ovn-host.postrm \
-	debian/ovn-host.template \
 	debian/python-openvswitch.dirs \
 	debian/python-openvswitch.install \
 	debian/rules \

include/automake.mk
@@ -11,7 +11,6 @@ include/odp-netlink-macros.h: include/odp-netlink.h \
 EXTRA_DIST += build-aux/extract-odp-netlink-h build-aux/extract-odp-netlink-macros-h
 CLEANFILES += include/odp-netlink.h include/odp-netlink-macros.h
 
-include include/ovn/automake.mk
 include include/openflow/automake.mk
 include include/openvswitch/automake.mk
 include include/sparse/automake.mk

include/ovn/automake.mk
@@ -1,6 +1,0 @@
-ovnincludedir = $(includedir)/ovn
-ovninclude_HEADERS = \
-	include/ovn/actions.h \
-	include/ovn/expr.h \
-	include/ovn/lex.h  \
-	include/ovn/logical-fields.h

ovn/automake.mk
@@ -66,13 +66,6 @@ ovn/ovn-nb.5: \
 		$(srcdir)/ovn/ovn-nb.xml > $@.tmp && \
 	mv $@.tmp $@
 
-man_MANS += ovn/ovn-architecture.7
-EXTRA_DIST += ovn/ovn-architecture.7.xml
-CLEANFILES += ovn/ovn-architecture.7
-
-EXTRA_DIST += \
-	ovn/TODO.rst
-
 # Version checking for ovn-nb.ovsschema.
 ALL_LOCAL += ovn/ovn-nb.ovsschema.stamp
 ovn/ovn-nb.ovsschema.stamp: ovn/ovn-nb.ovsschema
@@ -85,8 +78,5 @@ ovn/ovn-sb.ovsschema.stamp: ovn/ovn-sb.ovsschema
 	$(srcdir)/build-aux/cksum-schema-check $? $@
 CLEANFILES += ovn/ovn-sb.ovsschema.stamp
 
-include ovn/controller/automake.mk
-include ovn/controller-vtep/automake.mk
 include ovn/lib/automake.mk
-include ovn/northd/automake.mk
 include ovn/utilities/automake.mk

ovn/controller-vtep/automake.mk
@@ -1,14 +1,0 @@
-bin_PROGRAMS += ovn/controller-vtep/ovn-controller-vtep
-ovn_controller_vtep_ovn_controller_vtep_SOURCES = \
-	ovn/controller-vtep/binding.c \
-	ovn/controller-vtep/binding.h \
-	ovn/controller-vtep/gateway.c \
-	ovn/controller-vtep/gateway.h \
-	ovn/controller-vtep/ovn-controller-vtep.c \
-	ovn/controller-vtep/ovn-controller-vtep.h \
-	ovn/controller-vtep/vtep.c \
-	ovn/controller-vtep/vtep.h
-ovn_controller_vtep_ovn_controller_vtep_LDADD = ovn/lib/libovn.la lib/libopenvswitch.la vtep/libvtep.la
-man_MANS += ovn/controller-vtep/ovn-controller-vtep.8
-EXTRA_DIST += ovn/controller-vtep/ovn-controller-vtep.8.xml
-CLEANFILES += ovn/controller-vtep/ovn-controller-vtep.8

ovn/controller/automake.mk
@@ -1,32 +1,0 @@
-bin_PROGRAMS += ovn/controller/ovn-controller
-ovn_controller_ovn_controller_SOURCES = \
-	ovn/controller/bfd.c \
-	ovn/controller/bfd.h \
-	ovn/controller/binding.c \
-	ovn/controller/binding.h \
-	ovn/controller/chassis.c \
-	ovn/controller/chassis.h \
-	ovn/controller/encaps.c \
-	ovn/controller/encaps.h \
-	ovn/controller/ha-chassis.c \
-	ovn/controller/ha-chassis.h \
-	ovn/controller/ip-mcast.c \
-	ovn/controller/ip-mcast.h \
-	ovn/controller/lflow.c \
-	ovn/controller/lflow.h \
-	ovn/controller/lport.c \
-	ovn/controller/lport.h \
-	ovn/controller/ofctrl.c \
-	ovn/controller/ofctrl.h \
-	ovn/controller/pinctrl.c \
-	ovn/controller/pinctrl.h \
-	ovn/controller/patch.c \
-	ovn/controller/patch.h \
-	ovn/controller/ovn-controller.c \
-	ovn/controller/ovn-controller.h \
-	ovn/controller/physical.c \
-	ovn/controller/physical.h
-ovn_controller_ovn_controller_LDADD = ovn/lib/libovn.la lib/libopenvswitch.la
-man_MANS += ovn/controller/ovn-controller.8
-EXTRA_DIST += ovn/controller/ovn-controller.8.xml
-CLEANFILES += ovn/controller/ovn-controller.8

commit bc5e6953819b0e6ff0161041b0ebd5f2468d833e

ovn: Fix broken dist* targets.
Redundant line continuation was missed while removing OVN
that broke distdir target and subsequently all the dependent
targets, e.g. distcheck:

  make[1]:
  *** No rule to make target 'nodist_ovn_lib_libovn_la_SOURCES',
      needed by 'distdir'.  Stop.

CC: Mark Michelson <mmichels@redhat.com>
Fixes: f3e24610ea18 ("Remove OVN.")
Signed-off-by: Ilya Maximets <i.maximets@samsung.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

ovn/lib/automake.mk
@@ -7,7 +7,7 @@ ovn_lib_libovn_la_SOURCES = \
 	ovn/lib/acl-log.c \
 	ovn/lib/acl-log.h \
 	ovn/lib/ovn-util.c \
-	ovn/lib/ovn-util.h \
+	ovn/lib/ovn-util.h
 nodist_ovn_lib_libovn_la_SOURCES = \
 	ovn/lib/ovn-nb-idl.c \
 	ovn/lib/ovn-nb-idl.h \