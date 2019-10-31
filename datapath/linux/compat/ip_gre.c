ovsrobot/ovs

commit d5822f428814d1224b8d278ebe3ee5e1de16625c

gre: Remove dead ipgre code
Open vSwitch uses gretap, not ipgre for gre tunneling.  Remove dead
code for ipgre type tunnels.

Acked-by: William Tu <u9012063@gmail.com>
Signed-off-by: Greg Rose <gvrose8192@gmail.com>
Signed-off-by: Ben Pfaff <blp@ovn.org>

datapath/linux/compat/ip_gre.c
@@ -64,13 +64,11 @@
 #include "vport-netdev.h"
 
 static int gre_tap_net_id __read_mostly;
-static int ipgre_net_id __read_mostly;
 static unsigned int erspan_net_id __read_mostly;
 static void erspan_build_header(struct sk_buff *skb,
 				__be32 id, u32 index,
 				bool truncate, bool is_ipv4);
 
-static struct rtnl_link_ops ipgre_link_ops __read_mostly;
 static bool ip_gre_loaded = false;
 
 #define ip_gre_calc_hlen rpl_ip_gre_calc_hlen
@@ -316,16 +314,10 @@ static int ipgre_rcv(struct sk_buff *skb, const struct tnl_ptk_info *tpi,
 		 tpi->proto == htons(ETH_P_ERSPAN2))
 		itn = net_generic(net, erspan_net_id);
 	else
-		itn = net_generic(net, ipgre_net_id);
+		return PACKET_RCVD;
 
 	res = __ipgre_rcv(skb, tpi, itn, hdr_len, false);
-	if (res == PACKET_NEXT && tpi->proto == htons(ETH_P_TEB)) {
-		/* ipgre tunnels in collect metadata mode should receive
-		 * also ETH_P_TEB traffic.
-		 */
-		itn = net_generic(net, ipgre_net_id);
-		res = __ipgre_rcv(skb, tpi, itn, hdr_len, true);
-	}
+
 	return res;
 }
 
@@ -725,25 +717,6 @@ void __gre_err(struct sk_buff *skb, u32 info)
 static const struct gre_protocol ipgre_protocol = {
 	.handler     = __gre_rcv,
 	.err_handler = __gre_err,
-};
-
-static int __net_init ipgre_init_net(struct net *net)
-{
-	return ip_tunnel_init_net(net, ipgre_net_id, &ipgre_link_ops, NULL);
-}
-
-static void __net_exit ipgre_exit_net(struct net *net)
-{
-	struct ip_tunnel_net *itn = net_generic(net, ipgre_net_id);
-
-	ip_tunnel_delete_net(itn, &ipgre_link_ops);
-}
-
-static struct pernet_operations ipgre_net_ops = {
-	.init = ipgre_init_net,
-	.exit = ipgre_exit_net,
-	.id   = &ipgre_net_id,
-	.size = sizeof(struct ip_tunnel_net),
 };
 
 #ifdef HAVE_EXT_ACK_IN_RTNL_LINKOPS
@@ -1205,13 +1178,6 @@ static const struct net_device_ops erspan_netdev_ops = {
 #endif
 };
 
-static void ipgre_tunnel_setup(struct net_device *dev)
-{
-	dev->netdev_ops		= &ipgre_netdev_ops;
-	dev->type		= ARPHRD_IPGRE;
-	ip_tunnel_setup(dev, ipgre_net_id);
-}
-
 static void ipgre_tap_setup(struct net_device *dev)
 {
 	ether_setup(dev);
@@ -1350,22 +1316,6 @@ static const struct nla_policy ipgre_policy[RPL_IFLA_GRE_MAX + 1] = {
 	[IFLA_GRE_ERSPAN_VER]	= { .type = NLA_U8 },
 	[IFLA_GRE_ERSPAN_DIR]	= { .type = NLA_U8 },
 	[IFLA_GRE_ERSPAN_HWID]	= { .type = NLA_U16 },
-};
-
-static struct rtnl_link_ops ipgre_link_ops __read_mostly = {
-	.kind		= "gre",
-	.maxtype	= RPL_IFLA_GRE_MAX,
-	.policy		= ipgre_policy,
-	.priv_size	= sizeof(struct ip_tunnel),
-	.setup		= ipgre_tunnel_setup,
-	.validate	= ipgre_tunnel_validate,
-	.newlink	= ipgre_newlink,
-	.dellink	= ip_tunnel_dellink,
-	.get_size	= ipgre_get_size,
-	.fill_info	= ipgre_fill_info,
-#ifdef HAVE_GET_LINK_NET
-	.get_link_net	= ip_tunnel_get_link_net,
-#endif
 };
 
 static struct rtnl_link_ops ipgre_tap_ops __read_mostly = {
@@ -1655,14 +1605,6 @@ int rpl_ipgre_init(void)
 			goto pnet_erspan_failed;
 	}
 
-	err = register_pernet_device(&ipgre_net_ops);
-	if (err < 0) {
-		if (err == -EEXIST)
-			goto ip_gre_loaded;
-		else
-			goto pnet_ipgre_failed;
-	}
-
 	err = gre_add_protocol(&ipgre_protocol, GREPROTO_CISCO);
 	if (err < 0) {
 		pr_info("%s: can't add protocol\n", __func__);
@@ -1696,8 +1638,6 @@ ip_gre_loaded:
 	return 0;
 
 add_proto_failed:
-	unregister_pernet_device(&ipgre_net_ops);
-pnet_ipgre_failed:
 	unregister_pernet_device(&erspan_net_ops);
 pnet_erspan_failed:
 	unregister_pernet_device(&ipgre_tap_net_ops);
@@ -1713,7 +1653,6 @@ void rpl_ipgre_fini(void)
 
 	if (!ip_gre_loaded) {
 		gre_del_protocol(&ipgre_protocol, GREPROTO_CISCO);
-		unregister_pernet_device(&ipgre_net_ops);
 		unregister_pernet_device(&erspan_net_ops);
 		unregister_pernet_device(&ipgre_tap_net_ops);
 	}

commit 1951e073baa837d4cd6423e95e6f865c56707755

ip_gre: Remove even more unused code
There is a confusing mix of ipgre and gretap functions with some
needed for gretap still having ipgre_ prefixes.  This time though
I think I got the rest of the unused ipgre code.

Fixes: d5822f428814 ("gre: Remove dead ipgre code")
Signed-off-by: Greg Rose <gvrose8192@gmail.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

datapath/linux/compat/ip_gre.c
@@ -922,44 +922,6 @@ static int erspan_tunnel_init(struct net_device *dev)
 	return ip_tunnel_init(dev);
 }
 
-static int ipgre_header(struct sk_buff *skb, struct net_device *dev,
-			unsigned short type,
-			const void *daddr, const void *saddr, unsigned int len)
-{
-	struct ip_tunnel *t = netdev_priv(dev);
-	struct iphdr *iph;
-	struct gre_base_hdr *greh;
-
-	iph = (struct iphdr *)__skb_push(skb, t->hlen + sizeof(*iph));
-	greh = (struct gre_base_hdr *)(iph+1);
-	greh->flags = gre_tnl_flags_to_gre_flags(t->parms.o_flags);
-	greh->protocol = htons(type);
-
-	memcpy(iph, &t->parms.iph, sizeof(struct iphdr));
-
-	/* Set the source hardware address. */
-	if (saddr)
-		memcpy(&iph->saddr, saddr, 4);
-	if (daddr)
-		memcpy(&iph->daddr, daddr, 4);
-	if (iph->daddr)
-		return t->hlen + sizeof(*iph);
-
-	return -(t->hlen + sizeof(*iph));
-}
-
-static int ipgre_header_parse(const struct sk_buff *skb, unsigned char *haddr)
-{
-	const struct iphdr *iph = (const struct iphdr *) skb_mac_header(skb);
-	memcpy(haddr, &iph->saddr, 4);
-	return 4;
-}
-
-static const struct header_ops ipgre_header_ops = {
-	.create	= ipgre_header,
-	.parse	= ipgre_header_parse,
-};
-
 static int ipgre_tunnel_init(struct net_device *dev)
 {
 	struct ip_tunnel *tunnel = netdev_priv(dev);