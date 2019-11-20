a29f823eb43b6ba98acbc524605288d3841778c6
Lance Yang <Lance.Yang@arm.com>
2019-11-20 16:14:45 +0800

    travis: Move x86-only addon packages
    To enable multiple CPU architectures support, it is necessary to move the
    x86-only addon packages from .travis.yml file. Otherwise, the x86-only
    addon packages will break the builds on some other CPU architectures.
    
    Reviewed-by: Yangqin Wei <Yanqin.Wei@arm.com>
    Reviewed-by: Malvika Gupta <Malvika.Gupta@arm.com>
    Reviewed-by: Gavin Hu <Galvin.Hu@arm.com>
    Reviewed-by: Ruifeng Wang <Ruifeng.Wang@arm.com>
    Signed-off-by: Lance Yang <Lance.Yang@arm.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- .travis/linux-prepare.sh
+++ .travis/linux-prepare.sh
@@ -15,10 +15,14 @@ cd ..
 pip install --disable-pip-version-check --user six flake8 hacking
 pip install --user --upgrade docutils
 
-if [ "$M32" ]; then
-    # 32-bit and 64-bit libunwind can not be installed at the same time.
-    # This will remove the 64-bit libunwind and install 32-bit version.
-    sudo apt-get install -y libunwind-dev:i386
+if [[ "$TRAVIS_ARCH" == "amd64" ]] || [[ -z "$TRAVIS_ARCH" ]]; then
+    if [ "$M32" ]; then
+        # 32-bit and 64-bit libunwind can not be installed at the same time.
+        # This will remove the 64-bit libunwind and install 32-bit version.
+        sudo apt-get install \
+        -y libunwind-dev:i386 libunbound-dev:i386 gcc-multilib
+    fi
+
 fi
 
 # IPv6 is supported by kernel but disabled in TravisCI images:
