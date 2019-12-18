044d5d97884b1614a6fbefc2de760b835ddad88d
Yi-Hung Wei <yihung.wei@gmail.com>
2019-12-18 12:31:06 -0800

    netdev-afxdp: NUMA-aware memory allocation for XSK related memory
    Currently, the AF_XDP socket (XSK) related memory are allocated by main
    thread in the main thread's NUMA domain.  With the patch that detects
    netdev-linux's NUMA node id, the PMD thread of AF_XDP port will be run on
    the AF_XDP netdev's NUMA domain.  If the net device's NUMA domain
    is different from the main thread's NUMA domain, we will have two
    cross-NUMA memory accesses (netdev <-> memory, memory <-> CPU).
    
    This patch addresses the aforementioned issue by allocating
    the memory in the net device's NUMA domain.
    
    Signed-off-by: Yi-Hung Wei <yihung.wei@gmail.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/netdev-afxdp.c
+++ lib/netdev-afxdp.c
@@ -26,6 +26,7 @@
 #include <linux/rtnetlink.h>
 #include <linux/if_xdp.h>
 #include <net/if.h>
+#include <numa.h>
 #include <poll.h>
 #include <stdlib.h>
 #include <sys/resource.h>
@@ -661,6 +662,14 @@ netdev_afxdp_reconfigure(struct netdev *netdev)
     struct rlimit r = {RLIM_INFINITY, RLIM_INFINITY};
     int err = 0;
 
+    /* Allocate all the xsk related memory in the netdev's NUMA domain. */
+    struct bitmask *old_bm = NULL;
+    int numa_id = netdev_get_numa_id(netdev);
+    if (numa_id != NETDEV_NUMA_UNSPEC) {
+        old_bm = numa_get_membind();
+        numa_set_preferred(numa_id);
+    }
+
     ovs_mutex_lock(&dev->mutex);
 
     if (netdev->n_rxq == dev->requested_n_rxq
@@ -692,6 +701,10 @@ netdev_afxdp_reconfigure(struct netdev *netdev)
     netdev_change_seq_changed(netdev);
 out:
     ovs_mutex_unlock(&dev->mutex);
+    if (old_bm) {
+        numa_set_membind(old_bm);
+        numa_bitmask_free(old_bm);
+    }
     return err;
 }
 
