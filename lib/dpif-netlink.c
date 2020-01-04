ovsrobot/ovs

commit 3cd99886191e1d35c29a110ba67b437481d5fe83

dpif-netlink: Use dpif logging functions
Remove redundant logging functions and reuse
the exposed dpif logging functions.

Signed-off-by: Roi Dayan <roid@mellanox.com>
Reviewed-by: Paul Blakey <paulb@mellanox.com>
Acked-by: Flavio Leitner <fbl@sysclose.org>
Signed-off-by: Simon Horman <simon.horman@netronome.com>

lib/dpif-netlink.c
@@ -2186,32 +2186,6 @@ out:
     return err;
 }
 
-static void
-dbg_print_flow(const struct nlattr *key, size_t key_len,
-               const struct nlattr *mask, size_t mask_len,
-               const struct nlattr *actions, size_t actions_len,
-               const ovs_u128 *ufid,
-               const char *op)
-{
-        struct ds s;
-
-        ds_init(&s);
-        ds_put_cstr(&s, op);
-        ds_put_cstr(&s, " (");
-        odp_format_ufid(ufid, &s);
-        ds_put_cstr(&s, ")");
-        if (key_len) {
-            ds_put_cstr(&s, "\nflow (verbose): ");
-            odp_flow_format(key, key_len, mask, mask_len, NULL, &s, true);
-            ds_put_cstr(&s, "\nflow: ");
-            odp_flow_format(key, key_len, mask, mask_len, NULL, &s, false);
-        }
-        ds_put_cstr(&s, "\nactions: ");
-        format_odp_actions(&s, actions, actions_len);
-        VLOG_DBG("\n%s", ds_cstr(&s));
-        ds_destroy(&s);
-}
-
 static int
 try_send_to_netdev(struct dpif_netlink *dpif, struct dpif_op *op)
 {
@@ -2224,9 +2198,8 @@ try_send_to_netdev(struct dpif_netlink *dpif, struct dpif_op *op)
         if (!put->ufid) {
             break;
         }
-        dbg_print_flow(put->key, put->key_len, put->mask, put->mask_len,
-                       put->actions, put->actions_len, put->ufid,
-                       (put->flags & DPIF_FP_MODIFY ? "PUT(MODIFY)" : "PUT"));
+
+        log_flow_put_message(&dpif->dpif, &this_module, put, 0);
         err = parse_flow_put(dpif, put);
         break;
     }
@@ -2236,8 +2209,8 @@ try_send_to_netdev(struct dpif_netlink *dpif, struct dpif_op *op)
         if (!del->ufid) {
             break;
         }
-        dbg_print_flow(del->key, del->key_len, NULL, 0, NULL, 0,
-                       del->ufid, "DEL");
+
+        log_flow_del_message(&dpif->dpif, &this_module, del, 0);
         err = netdev_ports_flow_del(DPIF_HMAP_KEY(&dpif->dpif), del->ufid,
                                     del->stats);
         break;
@@ -2248,8 +2221,8 @@ try_send_to_netdev(struct dpif_netlink *dpif, struct dpif_op *op)
         if (!op->u.flow_get.ufid) {
             break;
         }
-        dbg_print_flow(get->key, get->key_len, NULL, 0, NULL, 0,
-                       get->ufid, "GET");
+
+        log_flow_get_message(&dpif->dpif, &this_module, get, 0);
         err = parse_flow_get(dpif, get);
         break;
     }

commit b9c155935e3856983247cc6b90341e11ff1f7caa

dpif-netlink: Fix dumping uninitialized netdev flow stats.
dpif logging functions expects to be called after the operation.
log_flow_del_message() dumps flow stats on success which are not
initialized before the actual call to netdev_flow_del():

 Conditional jump or move depends on uninitialised value(s)
    at 0x6090875: _itoa_word (_itoa.c:179)
    by 0x6093F0D: vfprintf (vfprintf.c:1642)
    by 0x60C090F: vsnprintf (vsnprintf.c:114)
    by 0xE5E7EC: ds_put_format_valist (dynamic-string.c:155)
    by 0xE5E755: ds_put_format (dynamic-string.c:142)
    by 0xE5A5E6: dpif_flow_stats_format (dpif.c:903)
    by 0xE5B708: log_flow_message (dpif.c:1763)
    by 0xE5BCA4: log_flow_del_message (dpif.c:1809)
    by 0xFA6076: try_send_to_netdev (dpif-netlink.c:2190)
    by 0xFA0D3C: dpif_netlink_operate (dpif-netlink.c:2248)
    by 0xE5AFAC: dpif_operate (dpif.c:1376)
    by 0xDF176E: push_dp_ops (ofproto-dpif-upcall.c:2367)
    by 0xDF04C8: push_ukey_ops (ofproto-dpif-upcall.c:2447)
    by 0xDF008F: revalidator_sweep__ (ofproto-dpif-upcall.c:2805)
    by 0xDF5DC6: revalidator_sweep (ofproto-dpif-upcall.c:2816)
    by 0xDF1E83: udpif_revalidator (ofproto-dpif-upcall.c:949)
    by 0xF3A3FE: ovsthread_wrapper (ovs-thread.c:383)
    by 0x565F6DA: start_thread (pthread_create.c:463)
    by 0x615988E: clone (clone.S:95)
  Uninitialised value was created by a stack allocation
    at 0xDEFC24: revalidator_sweep__ (ofproto-dpif-upcall.c:2733)

Also, by calling the log functions after the actual operation we could
pass the real error code instead of reporting success unconditionally.

CC: Roi Dayan <roid@mellanox.com>
Fixes: 3cd99886191e ("dpif-netlink: Use dpif logging functions")
Signed-off-by: Ilya Maximets <i.maximets@ovn.org>
Signed-off-by: 0-day Robot <robot@bytheb.org>

lib/dpif-netlink.c
@@ -2176,8 +2176,8 @@ try_send_to_netdev(struct dpif_netlink *dpif, struct dpif_op *op)
             break;
         }
 
-        log_flow_put_message(&dpif->dpif, &this_module, put, 0);
         err = parse_flow_put(dpif, put);
+        log_flow_put_message(&dpif->dpif, &this_module, put, err);
         break;
     }
     case DPIF_OP_FLOW_DEL: {
@@ -2187,9 +2187,9 @@ try_send_to_netdev(struct dpif_netlink *dpif, struct dpif_op *op)
             break;
         }
 
-        log_flow_del_message(&dpif->dpif, &this_module, del, 0);
         err = netdev_ports_flow_del(dpif->dpif.dpif_class, del->ufid,
                                     del->stats);
+        log_flow_del_message(&dpif->dpif, &this_module, del, err);
         break;
     }
     case DPIF_OP_FLOW_GET: {
@@ -2199,8 +2199,8 @@ try_send_to_netdev(struct dpif_netlink *dpif, struct dpif_op *op)
             break;
         }
 
-        log_flow_get_message(&dpif->dpif, &this_module, get, 0);
         err = parse_flow_get(dpif, get);
+        log_flow_get_message(&dpif->dpif, &this_module, get, err);
         break;
     }
     case DPIF_OP_EXECUTE: