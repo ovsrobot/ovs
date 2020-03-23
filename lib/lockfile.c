ovsrobot/ovs

commit f21fa45f30856d07b712661566ae8d1bcb62a31a

lockfile: Minor code cleanup.
There were more superficial differences between Windows and non-Windows
versions of the code due to naming.  This commit reduces those differences.

CC: Gurucharan Shetty <shettyg@nicira.com>
Signed-off-by: Ben Pfaff <blp@nicira.com>

lib/lockfile.c
@@ -1,4 +1,4 @@
- /* Copyright (c) 2008, 2009, 2010, 2011, 2012, 2013 Nicira, Inc.
+ /* Copyright (c) 2008, 2009, 2010, 2011, 2012, 2013, 2014 Nicira, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -59,14 +59,11 @@ static struct hmap *const lock_table OVS_GUARDED_BY(lock_table_mutex)
     = &lock_table__;
 
 static void lockfile_unhash(struct lockfile *);
-#ifdef _WIN32
-static int lockfile_try_lock_windows(const char *name, pid_t *pidp,
-                                     struct lockfile **lockfilep);
-static void lockfile_unlock_windows(struct lockfile * lockfile);
-#else
-static int lockfile_try_lock_posix(const char *name, pid_t *pidp,
-                                   struct lockfile **lockfilep);
-#endif
+static int lockfile_try_lock(const char *name, pid_t *pidp,
+                             struct lockfile **lockfilep)
+    OVS_REQUIRES(&lock_table_mutex);
+static void lockfile_do_unlock(struct lockfile * lockfile)
+    OVS_REQUIRES(&lock_table_mutex);
 
 /* Returns the name of the lockfile that would be created for locking a file
  * named 'filename_'.  The caller is responsible for freeing the returned name,
@@ -117,11 +114,7 @@ lockfile_lock(const char *file, struct lockfile **lockfilep)
     lock_name = lockfile_name(file);
 
     ovs_mutex_lock(&lock_table_mutex);
-#ifdef _WIN32
-    error = lockfile_try_lock_windows(lock_name, &pid, lockfilep);
-#else
-    error = lockfile_try_lock_posix(lock_name, &pid, lockfilep);
-#endif
+    error = lockfile_try_lock(lock_name, &pid, lockfilep);
     ovs_mutex_unlock(&lock_table_mutex);
 
     if (error) {
@@ -149,11 +142,7 @@ lockfile_unlock(struct lockfile *lockfile)
 {
     if (lockfile) {
         ovs_mutex_lock(&lock_table_mutex);
-#ifdef _WIN32
-        lockfile_unlock_windows(lockfile);
-#else
-        lockfile_unhash(lockfile);
-#endif
+        lockfile_do_unlock(lockfile);
         ovs_mutex_unlock(&lock_table_mutex);
 
         COVERAGE_INC(lockfile_unlock);
@@ -235,7 +224,7 @@ lockfile_register(const char *name, dev_t device, ino_t inode, int fd)
 
 #ifdef _WIN32
 static void
-lockfile_unlock_windows(struct lockfile *lockfile)
+lockfile_do_unlock(struct lockfile *lockfile)
     OVS_REQUIRES(&lock_table_mutex)
 {
     if (lockfile->fd >= 0) {
@@ -251,8 +240,7 @@ lockfile_unlock_windows(struct lockfile *lockfile)
 }
 
 static int
-lockfile_try_lock_windows(const char *name, pid_t *pidp,
-                          struct lockfile **lockfilep)
+lockfile_try_lock(const char *name, pid_t *pidp, struct lockfile **lockfilep)
     OVS_REQUIRES(&lock_table_mutex)
 {
     HANDLE lock_handle;
@@ -297,12 +285,15 @@ lockfile_try_lock_windows(const char *name, pid_t *pidp,
     *lockfilep = lockfile;
     return 0;
 }
-#endif
+#else /* !_WIN32 */
+static void
+lockfile_do_unlock(struct lockfile *lockfile)
+{
+    lockfile_unhash(lockfile);
+}
 
-#ifndef _WIN32
 static int
-lockfile_try_lock_posix(const char *name, pid_t *pidp,
-                        struct lockfile **lockfilep)
+lockfile_try_lock(const char *name, pid_t *pidp, struct lockfile **lockfilep)
     OVS_REQUIRES(&lock_table_mutex)
 {
     struct flock l;

commit bfea8a3cbbce7773009d38a31382d6c8d2c8350a

lockfile: Fix OVS_REQUIRES macro.
Pass lock objects, not their addresses, to the annotation macros.

Fixes: f21fa45f3085 ("lockfile: Minor code cleanup.")
Tested-at: https://travis-ci.org/github/williamtu/ovs-travis/builds/666098338
Signed-off-by: William Tu <u9012063@gmail.com>
Acked-by: Ben Pfaff <blp@ovn.org>
Signed-off-by: 0-day Robot <robot@bytheb.org>

lib/lockfile.c
@@ -61,9 +61,9 @@ static struct hmap *const lock_table OVS_GUARDED_BY(lock_table_mutex)
 static void lockfile_unhash(struct lockfile *);
 static int lockfile_try_lock(const char *name, pid_t *pidp,
                              struct lockfile **lockfilep)
-    OVS_REQUIRES(&lock_table_mutex);
+    OVS_REQUIRES(lock_table_mutex);
 static void lockfile_do_unlock(struct lockfile * lockfile)
-    OVS_REQUIRES(&lock_table_mutex);
+    OVS_REQUIRES(lock_table_mutex);
 
 /* Returns the name of the lockfile that would be created for locking a file
  * named 'filename_'.  The caller is responsible for freeing the returned name,
@@ -188,7 +188,7 @@ lockfile_hash(dev_t device, ino_t inode)
 }
 
 static struct lockfile *
-lockfile_find(dev_t device, ino_t inode) OVS_REQUIRES(&lock_table_mutex)
+lockfile_find(dev_t device, ino_t inode) OVS_REQUIRES(lock_table_mutex)
 {
     struct lockfile *lockfile;
 
@@ -202,7 +202,7 @@ lockfile_find(dev_t device, ino_t inode) OVS_REQUIRES(&lock_table_mutex)
 }
 
 static void
-lockfile_unhash(struct lockfile *lockfile) OVS_REQUIRES(&lock_table_mutex)
+lockfile_unhash(struct lockfile *lockfile) OVS_REQUIRES(lock_table_mutex)
 {
     if (lockfile->fd >= 0) {
         close(lockfile->fd);
@@ -213,7 +213,7 @@ lockfile_unhash(struct lockfile *lockfile) OVS_REQUIRES(&lock_table_mutex)
 
 static struct lockfile *
 lockfile_register(const char *name, dev_t device, ino_t inode, int fd)
-    OVS_REQUIRES(&lock_table_mutex)
+    OVS_REQUIRES(lock_table_mutex)
 {
     struct lockfile *lockfile;
 
@@ -236,7 +236,7 @@ lockfile_register(const char *name, dev_t device, ino_t inode, int fd)
 #ifdef _WIN32
 static void
 lockfile_do_unlock(struct lockfile *lockfile)
-    OVS_REQUIRES(&lock_table_mutex)
+    OVS_REQUIRES(lock_table_mutex)
 {
     if (lockfile->fd >= 0) {
         OVERLAPPED overl;
@@ -252,7 +252,7 @@ lockfile_do_unlock(struct lockfile *lockfile)
 
 static int
 lockfile_try_lock(const char *name, pid_t *pidp, struct lockfile **lockfilep)
-    OVS_REQUIRES(&lock_table_mutex)
+    OVS_REQUIRES(lock_table_mutex)
 {
     HANDLE lock_handle;
     BOOL retval;
@@ -306,7 +306,7 @@ lockfile_do_unlock(struct lockfile *lockfile)
 
 static int
 lockfile_try_lock(const char *name, pid_t *pidp, struct lockfile **lockfilep)
-    OVS_REQUIRES(&lock_table_mutex)
+    OVS_REQUIRES(lock_table_mutex)
 {
     struct flock l;
     struct stat s;