8f12f5cf4c140a5ab52b3460a314a033a6f2ea85
Weili Zhang <305753229@qq.com>
2020-09-07 18:58:12 +0800

    We need strip the inner L2 padding bytes, before enapcasulate a packet.
    Signed-off-by: Weili Zhang <305753229@qq.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/netdev-native-tnl.c
+++ lib/netdev-native-tnl.c
@@ -149,11 +149,15 @@ void *
 netdev_tnl_push_ip_header(struct dp_packet *packet,
                const void *header, int size, int *ip_tot_size)
 {
+    int padding = dp_packet_l2_pad_size(packet);
     struct eth_header *eth;
     struct ip_header *ip;
     struct ovs_16aligned_ip6_hdr *ip6;
 
     eth = dp_packet_push_uninit(packet, size);
+    if (padding) {
+        dp_packet_set_size(packet, dp_packet_size(packet) - padding);
+    }
     *ip_tot_size = dp_packet_size(packet) - sizeof (struct eth_header);
 
     memcpy(eth, header, size);
