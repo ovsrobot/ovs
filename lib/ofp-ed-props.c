ovsrobot/ovs

commit 1fc11c5948cf83b47588ec1eaa4eef437c2df373

Generic encap and decap support for NSH
This commit adds translation and netdev datapath support for generic
encap and decap actions for the NSH MD1 header. The generic encap and
decap actions are mapped to specific encap_nsh and decap_nsh actions
in the datapath.

The translation follows that general scheme that decap() of an NSH
packet triggers recirculation after decapsulation, while encap(nsh)
just modifies struct flow and sets the ctx->pending_encap flag to
generate the encap_nsh action at the next commit to be able to include
subsequent set_field actions for NSH headers.

Support for the flexible MD2 format using TLV properties is foreseen
in encap(nsh), but not yet fully implemented.

The CLI syntax for encap of NSH is
encap(nsh(md_type=1))
encap(nsh(md_type=2[,tlv(<tlv_class>,<tlv_type>,<hex_string>),...]))

Signed-off-by: Jan Scheurich <jan.scheurich@ericsson.com>
Signed-off-by: Yi Yang <yi.y.yang@intel.com>
Signed-off-by: Ben Pfaff <blp@ovn.org>

lib/ofp-ed-props.c
@@ -30,6 +30,7 @@ decode_ed_prop(const struct ofp_ed_prop_header **ofp_prop,
                size_t *remaining)
 {
     uint16_t prop_class = ntohs((*ofp_prop)->prop_class);
+    uint8_t prop_type = (*ofp_prop)->type;
     size_t len = (*ofp_prop)->len;
     size_t pad_len = ROUND_UP(len, 8);
 
@@ -38,6 +39,45 @@ decode_ed_prop(const struct ofp_ed_prop_header **ofp_prop,
     }
 
     switch (prop_class) {
+    case OFPPPC_NSH: {
+        switch (prop_type) {
+        case OFPPPT_PROP_NSH_MDTYPE: {
+            struct ofp_ed_prop_nsh_md_type *opnmt =
+                ALIGNED_CAST(struct ofp_ed_prop_nsh_md_type *, *ofp_prop);
+            if (len > sizeof(*opnmt) || len > *remaining) {
+                return OFPERR_NXBAC_BAD_ED_PROP;
+            }
+            struct ofpact_ed_prop_nsh_md_type *pnmt =
+                    ofpbuf_put_uninit(out, sizeof(*pnmt));
+            pnmt->header.prop_class = prop_class;
+            pnmt->header.type = prop_type;
+            pnmt->header.len = len;
+            pnmt->md_type = opnmt->md_type;
+            break;
+        }
+        case OFPPPT_PROP_NSH_TLV: {
+            struct ofp_ed_prop_nsh_tlv *opnt =
+                ALIGNED_CAST(struct ofp_ed_prop_nsh_tlv *, *ofp_prop);
+            size_t tlv_pad_len = ROUND_UP(opnt->tlv_len, 8);
+            if (len != sizeof(*opnt) + tlv_pad_len || len > *remaining) {
+                return OFPERR_NXBAC_BAD_ED_PROP;
+            }
+            struct ofpact_ed_prop_nsh_tlv *pnt =
+                    ofpbuf_put_uninit(out, sizeof(*pnt));
+            pnt->header.prop_class = prop_class;
+            pnt->header.type = prop_type;
+            pnt->header.len = len;
+            pnt->tlv_class = opnt->tlv_class;
+            pnt->tlv_type = opnt->tlv_type;
+            pnt->tlv_len = opnt->tlv_len;
+            ofpbuf_put(out, opnt->data, tlv_pad_len);
+            break;
+        }
+        default:
+            return OFPERR_NXBAC_UNKNOWN_ED_PROP;
+        }
+        break;
+    }
     default:
         return OFPERR_NXBAC_UNKNOWN_ED_PROP;
     }
@@ -55,6 +95,43 @@ encode_ed_prop(const struct ofpact_ed_prop **prop,
     size_t prop_len;
 
     switch ((*prop)->prop_class) {
+    case OFPPPC_NSH: {
+        switch ((*prop)->type) {
+        case OFPPPT_PROP_NSH_MDTYPE: {
+            struct ofpact_ed_prop_nsh_md_type *pnmt =
+                ALIGNED_CAST(struct ofpact_ed_prop_nsh_md_type *, *prop);
+            struct ofp_ed_prop_nsh_md_type *opnmt =
+                    ofpbuf_put_uninit(out, sizeof(*opnmt));
+            opnmt->header.prop_class = htons((*prop)->prop_class);
+            opnmt->header.type = (*prop)->type;
+            opnmt->header.len =
+                    offsetof(struct ofp_ed_prop_nsh_md_type, pad);
+            opnmt->md_type = pnmt->md_type;
+            prop_len = sizeof(*pnmt);
+            break;
+        }
+        case OFPPPT_PROP_NSH_TLV: {
+            struct ofpact_ed_prop_nsh_tlv *pnt =
+                ALIGNED_CAST(struct ofpact_ed_prop_nsh_tlv *, *prop);
+            struct ofp_ed_prop_nsh_tlv *opnt;
+            size_t tlv_pad_len = ROUND_UP(pnt->tlv_len, 8);
+            size_t len = sizeof(*opnt) + tlv_pad_len;
+            opnt = ofpbuf_put_uninit(out, len);
+            opnt->header.prop_class = htons((*prop)->prop_class);
+            opnt->header.type = (*prop)->type;
+            opnt->header.len = len;
+            opnt->tlv_class = pnt->tlv_class;
+            opnt->tlv_type = pnt->tlv_type;
+            opnt->tlv_len = pnt->tlv_len;
+            memcpy(opnt->data, pnt->data, tlv_pad_len);
+            prop_len = sizeof(*pnt) + tlv_pad_len;
+            break;
+        }
+        default:
+            return OFPERR_OFPBAC_BAD_ARGUMENT;
+        }
+        break;
+    }
     default:
         return OFPERR_OFPBAC_BAD_ARGUMENT;
     }
@@ -78,6 +155,8 @@ parse_ed_prop_class(const char *str OVS_UNUSED,
         *prop_class = OFPPPC_GRE;
     } else if (!strcmp(str,"gtp")) {
         *prop_class = OFPPPC_GTP;
+    } else if (!strcmp(str,"nsh")) {
+        *prop_class = OFPPPC_NSH;
     } else {
         return false;
     }
@@ -90,6 +169,16 @@ parse_ed_prop_type(uint16_t prop_class,
                    uint8_t *type OVS_UNUSED)
 {
     switch (prop_class) {
+    case OFPPPC_NSH:
+        if (!strcmp(str, "md_type")) {
+            *type = OFPPPT_PROP_NSH_MDTYPE;
+            return true;
+        } else if (!strcmp(str, "tlv")) {
+            *type = OFPPPT_PROP_NSH_TLV;
+            return true;
+        } else {
+            return false;
+        }
     default:
         return false;
     }
@@ -106,12 +195,68 @@ char *
 parse_ed_prop_value(uint16_t prop_class, uint8_t prop_type OVS_UNUSED,
                     const char *value, struct ofpbuf *out OVS_UNUSED)
 {
+    char *error = NULL;
 
     if (value == NULL || *value == '\0') {
         return xstrdup("Value missing for encap property");
     }
 
     switch (prop_class) {
+    case OFPPPC_NSH:
+        switch (prop_type) {
+        case OFPPPT_PROP_NSH_MDTYPE: {
+            /* Format: "<md_type>:uint8_t". */
+            uint8_t md_type;
+            error = str_to_u8(value, "md_type", &md_type);
+            if (error != NULL) {
+                return error;
+            }
+            if (md_type < 1 || md_type > 2) {
+                return xstrdup("invalid md_type");
+            }
+            struct ofpact_ed_prop_nsh_md_type *pnmt =
+                    ofpbuf_put_uninit(out, sizeof(*pnmt));
+            pnmt->header.prop_class = prop_class;
+            pnmt->header.type = prop_type;
+            pnmt->header.len =
+                    offsetof(struct ofp_ed_prop_nsh_md_type, pad);
+            pnmt->md_type = md_type;
+            break;
+        }
+        case OFPPPT_PROP_NSH_TLV: {
+            /* Format: "<class>:ovs_be16,<type>:uint8_t,<val>:hex_string" */
+            struct ofpact_ed_prop_nsh_tlv *pnt;
+            uint16_t tlv_class;
+            uint8_t tlv_type;
+            char buf[256];
+            size_t tlv_value_len, padding;
+            size_t start_ofs = out->size;
+
+            if (!ovs_scan(value, "0x%"SCNx16",%"SCNu8",0x%251[0-9a-fA-F]",
+                          &tlv_class, &tlv_type, buf)) {
+                return xasprintf("Invalid NSH TLV header: %s", value);
+            }
+            ofpbuf_put_uninit(out, sizeof(*pnt));
+            ofpbuf_put_hex(out, buf, &tlv_value_len);
+            pnt = ALIGNED_CAST(struct ofpact_ed_prop_nsh_tlv *,
+                               ((char *)out->data + start_ofs));
+            padding = ROUND_UP(tlv_value_len, 8) - tlv_value_len;
+            pnt->header.prop_class = prop_class;
+            pnt->header.type = prop_type;
+            pnt->header.len = sizeof(*pnt) + tlv_value_len + padding;
+            pnt->tlv_class = htons(tlv_class);
+            pnt->tlv_type = tlv_type;
+            pnt->tlv_len = tlv_value_len;
+            if (padding > 0) {
+                ofpbuf_put_zeros(out, padding);
+            }
+            break;
+        }
+        default:
+            /* Unsupported property types rejected before. */
+            OVS_NOT_REACHED();
+        }
+        break;
     default:
         /* Unsupported property classes rejected before. */
         OVS_NOT_REACHED();
@@ -132,6 +277,8 @@ format_ed_prop_class(const struct ofpact_ed_prop *prop)
         return "gre";
     case OFPPPC_GTP:
         return "gtp";
+    case OFPPPC_NSH:
+        return "nsh";
     default:
         OVS_NOT_REACHED();
     }
@@ -141,6 +288,16 @@ char *
 format_ed_prop_type(const struct ofpact_ed_prop *prop)
 {
     switch (prop->prop_class) {
+    case OFPPPC_NSH:
+        switch (prop->type) {
+        case OFPPPT_PROP_NSH_MDTYPE:
+            return "md_type";
+        case OFPPPT_PROP_NSH_TLV:
+            return "tlv";
+        default:
+            OVS_NOT_REACHED();
+        }
+        break;
     default:
         OVS_NOT_REACHED();
     }
@@ -151,6 +308,28 @@ format_ed_prop(struct ds *s OVS_UNUSED,
                      const struct ofpact_ed_prop *prop)
 {
     switch (prop->prop_class) {
+    case OFPPPC_NSH:
+        switch (prop->type) {
+        case OFPPPT_PROP_NSH_MDTYPE: {
+            struct ofpact_ed_prop_nsh_md_type *pnmt =
+                ALIGNED_CAST(struct ofpact_ed_prop_nsh_md_type *, prop);
+            ds_put_format(s, "%s=%d", format_ed_prop_type(prop),
+                          pnmt->md_type);
+            return;
+        }
+        case OFPPPT_PROP_NSH_TLV: {
+            struct ofpact_ed_prop_nsh_tlv *pnt =
+                ALIGNED_CAST(struct ofpact_ed_prop_nsh_tlv *, prop);
+            ds_put_format(s, "%s(0x%04x,%d,",
+                          format_ed_prop_type(prop),
+                          ntohs(pnt->tlv_class), pnt->tlv_type);
+            ds_put_hex(s, pnt->data, pnt->tlv_len);
+            ds_put_cstr(s,")");
+            return;
+        }
+        default:
+            OVS_NOT_REACHED();
+        }
     default:
         OVS_NOT_REACHED();
     }

datapath/linux/compat/include/linux/openvswitch.h
@@ -793,6 +793,25 @@ struct ovs_action_push_eth {
 	struct ovs_key_ethernet addresses;
 };
 
+#define OVS_ENCAP_NSH_MAX_MD_LEN 16
+/*
+ * struct ovs_action_encap_nsh - %OVS_ACTION_ATTR_ENCAP_NSH
+ * @flags: NSH header flags.
+ * @mdtype: NSH metadata type.
+ * @mdlen: Length of NSH metadata in bytes.
+ * @np: NSH next_protocol: Inner packet type.
+ * @path_hdr: NSH service path id and service index.
+ * @metadata: NSH metadata for MD type 1 or 2
+ */
+struct ovs_action_encap_nsh {
+    uint8_t flags;
+    uint8_t mdtype;
+    uint8_t mdlen;
+    uint8_t np;
+    __be32 path_hdr;
+    uint8_t metadata[OVS_ENCAP_NSH_MAX_MD_LEN];
+};
+
 /**
  * enum ovs_nat_attr - Attributes for %OVS_CT_ATTR_NAT.
  *
@@ -868,6 +887,8 @@ enum ovs_nat_attr {
  * @OVS_ACTION_ATTR_PUSH_ETH: Push a new outermost Ethernet header onto the
  * packet.
  * @OVS_ACTION_ATTR_POP_ETH: Pop the outermost Ethernet header off the packet.
+ * @OVS_ACTION_ATTR_ENCAP_NSH: encap NSH action to push NSH header.
+ * @OVS_ACTION_ATTR_DECAP_NSH: decap NSH action to remove NSH header.
  *
  * Only a single header can be set with a single %OVS_ACTION_ATTR_SET.  Not all
  * fields within a header are modifiable, e.g. the IPv4 protocol and fragment
@@ -909,6 +930,8 @@ enum ovs_action_attr {
 	OVS_ACTION_ATTR_TUNNEL_POP,    /* u32 port number. */
 	OVS_ACTION_ATTR_CLONE,         /* Nested OVS_CLONE_ATTR_*.  */
 	OVS_ACTION_ATTR_METER,         /* u32 meter number. */
+	OVS_ACTION_ATTR_ENCAP_NSH,    /* struct ovs_action_encap_nsh. */
+	OVS_ACTION_ATTR_DECAP_NSH,    /* No argument. */
 #endif
 	__OVS_ACTION_ATTR_MAX,	      /* Nothing past this will be accepted
 				       * from userspace. */

include/openvswitch/ofp-ed-props.h
@@ -27,9 +27,10 @@ extern "C" {
 
 enum ofp_ed_prop_class {
     OFPPPC_BASIC = 0,            /* ONF Basic class. */
-    OFPPPC_MPLS  = 1,            /* MPLS property  class. */
-    OFPPPC_GRE   = 2,            /* GRE property  class. */
-    OFPPPC_GTP   = 3,            /* GTP property  class. */
+    OFPPPC_MPLS  = 1,            /* MPLS property class. */
+    OFPPPC_GRE   = 2,            /* GRE property class. */
+    OFPPPC_GTP   = 3,            /* GTP property class. */
+    OFPPPC_NSH   = 4,            /* NSH property class */
 
     /* Experimenter property class.
      *
@@ -37,6 +38,12 @@ enum ofp_ed_prop_class {
      * is exp id after that is the experimenter property data.
      */
     OFPPPC_EXPERIMENTER=0xffff
+};
+
+enum ofp_ed_nsh_prop_type {
+    OFPPPT_PROP_NSH_NONE = 0,    /* unused */
+    OFPPPT_PROP_NSH_MDTYPE = 1,  /* property MDTYPE in NSH */
+    OFPPPT_PROP_NSH_TLV = 2,     /* property TLV in NSH */
 };
 
 /*
@@ -49,6 +56,22 @@ struct ofp_ed_prop_header {
     uint8_t len;
 };
 
+struct ofp_ed_prop_nsh_md_type {
+    struct ofp_ed_prop_header header;
+    uint8_t md_type;         /* NSH MD type .*/
+    uint8_t pad[3];          /* Padding to 8 bytes. */
+};
+
+struct ofp_ed_prop_nsh_tlv {
+    struct ofp_ed_prop_header header;
+    ovs_be16 tlv_class;      /* Metadata class. */
+    uint8_t tlv_type;        /* Metadata type including C bit. */
+    uint8_t tlv_len;         /* Metadata value length (0-127). */
+
+    /* tlv_len octets of metadata value, padded to a multiple of 8 bytes. */
+    uint8_t data[0];
+};
+
 /*
  * Internal representation of encap/decap properties
  */
@@ -58,6 +81,21 @@ struct ofpact_ed_prop {
     uint8_t len;
 };
 
+struct ofpact_ed_prop_nsh_md_type {
+    struct ofpact_ed_prop header;
+    uint8_t md_type;         /* NSH MD type .*/
+    uint8_t pad[3];          /* Padding to 8 bytes. */
+};
+
+struct ofpact_ed_prop_nsh_tlv {
+    struct ofpact_ed_prop header;
+    ovs_be16 tlv_class;      /* Metadata class. */
+    uint8_t tlv_type;        /* Metadata type including C bit. */
+    uint8_t tlv_len;         /* Metadata value length (0-127). */
+
+    /* tlv_len octets of metadata value, padded to a multiple of 8 bytes. */
+    uint8_t data[0];
+};
 enum ofperr decode_ed_prop(const struct ofp_ed_prop_header **ofp_prop,
                            struct ofpbuf *out, size_t *remaining);
 enum ofperr encode_ed_prop(const struct ofpact_ed_prop **prop,

lib/dpif-netdev.c
@@ -5407,6 +5407,8 @@ dp_execute_cb(void *aux_, struct dp_packet_batch *packets_,
     case OVS_ACTION_ATTR_PUSH_ETH:
     case OVS_ACTION_ATTR_POP_ETH:
     case OVS_ACTION_ATTR_CLONE:
+    case OVS_ACTION_ATTR_ENCAP_NSH:
+    case OVS_ACTION_ATTR_DECAP_NSH:
     case __OVS_ACTION_ATTR_MAX:
         OVS_NOT_REACHED();
     }

lib/dpif.c
@@ -1255,6 +1255,8 @@ dpif_execute_helper_cb(void *aux_, struct dp_packet_batch *packets_,
     case OVS_ACTION_ATTR_PUSH_ETH:
     case OVS_ACTION_ATTR_POP_ETH:
     case OVS_ACTION_ATTR_CLONE:
+    case OVS_ACTION_ATTR_ENCAP_NSH:
+    case OVS_ACTION_ATTR_DECAP_NSH:
     case OVS_ACTION_ATTR_UNSPEC:
     case __OVS_ACTION_ATTR_MAX:
         OVS_NOT_REACHED();

lib/odp-execute.c
@@ -289,10 +289,9 @@ odp_set_nsh(struct dp_packet *packet, const struct ovs_key_nsh *key,
                 }
                 break;
             case NSH_M_TYPE2:
-                /* TODO */
-                break;
             default:
-                OVS_NOT_REACHED();
+                /* No support for setting any other metadata format yet. */
+                break;
         }
     } else {
         uint8_t flags = (ntohs(nsh->ver_flags_len) & NSH_FLAGS_MASK) >>
@@ -314,10 +313,9 @@ odp_set_nsh(struct dp_packet *packet, const struct ovs_key_nsh *key,
                 }
                 break;
             case NSH_M_TYPE2:
-                /* TODO */
-                break;
             default:
-                OVS_NOT_REACHED();
+                /* No support for setting any other metadata format yet. */
+                break;
         }
     }
 }
@@ -654,6 +652,8 @@ requires_datapath_assistance(const struct nlattr *a)
     case OVS_ACTION_ATTR_PUSH_ETH:
     case OVS_ACTION_ATTR_POP_ETH:
     case OVS_ACTION_ATTR_CLONE:
+    case OVS_ACTION_ATTR_ENCAP_NSH:
+    case OVS_ACTION_ATTR_DECAP_NSH:
         return false;
 
     case OVS_ACTION_ATTR_UNSPEC:
@@ -817,6 +817,26 @@ odp_execute_actions(void *dp, struct dp_packet_batch *batch, bool steal,
                 pop_eth(packet);
             }
             break;
+
+        case OVS_ACTION_ATTR_ENCAP_NSH: {
+            const struct ovs_action_encap_nsh *enc_nsh = nl_attr_get(a);
+            DP_PACKET_BATCH_FOR_EACH (packet, batch) {
+                encap_nsh(packet, enc_nsh);
+            }
+            break;
+        }
+        case OVS_ACTION_ATTR_DECAP_NSH: {
+            size_t i, num = batch->count;
+
+            DP_PACKET_BATCH_REFILL_FOR_EACH (i, num, packet, batch) {
+                if (decap_nsh(packet)) {
+                    dp_packet_batch_refill(batch, packet, i);
+                } else {
+                    dp_packet_delete(packet);
+                }
+            }
+            break;
+        }
 
         case OVS_ACTION_ATTR_OUTPUT:
         case OVS_ACTION_ATTR_TUNNEL_PUSH:

commit 94a303df8f801a21ab6ed4841f4d95d451425fdb

ofp-ed-props: Fix using uninitialized padding for NSH encap actions.
OVS uses memcmp to compare actions of existing and new flows, but
'struct ofp_ed_prop_nsh_md_type' has 3 bytes of padding that never
initialized and passed around within OF data structures and messages.

  Uninitialized bytes in MemcmpInterceptorCommon
    at offset 21 inside [0x7090000003f8, 136)
  WARNING: MemorySanitizer: use-of-uninitialized-value
    #0 0x4a184e in bcmp (vswitchd/ovs-vswitchd+0x4a184e)
    #1 0x896c8a in ofpacts_equal lib/ofp-actions.c:9121:31
    #2 0x564403 in replace_rule_finish ofproto/ofproto.c:5650:37
    #3 0x563462 in add_flow_finish ofproto/ofproto.c:5218:13
    #4 0x54a1ff in ofproto_flow_mod_finish ofproto/ofproto.c:8091:17
    #5 0x5433b2 in handle_flow_mod__ ofproto/ofproto.c:6216:17
    #6 0x56a2fc in handle_flow_mod ofproto/ofproto.c:6190:17
    #7 0x565bda in handle_single_part_openflow ofproto/ofproto.c:8504:16
    #8 0x540b25 in handle_openflow ofproto/ofproto.c:8685:21
    #9 0x6697fd in ofconn_run ofproto/connmgr.c:1329:13
    #10 0x668e6e in connmgr_run ofproto/connmgr.c:356:9
    #11 0x53f1bc in ofproto_run ofproto/ofproto.c:1890:5
    #12 0x4ead0c in bridge_run__ vswitchd/bridge.c:3250:9
    #13 0x4e9bc8 in bridge_run vswitchd/bridge.c:3309:5
    #14 0x51c072 in main vswitchd/ovs-vswitchd.c:127:9
    #15 0x7f23a99011a2 in __libc_start_main (/lib64/libc.so.6)
    #16 0x46b92d in _start (vswitchd/ovs-vswitchd+0x46b92d)

  Uninitialized value was stored to memory at
    #0 0x4745aa in __msan_memcpy.part.0 (vswitchd/ovs-vswitchd)
    #1 0x54529f in rule_actions_create ofproto/ofproto.c:3134:5
    #2 0x54915e in ofproto_rule_create ofproto/ofproto.c:5284:11
    #3 0x55d419 in add_flow_init ofproto/ofproto.c:5123:17
    #4 0x54841f in ofproto_flow_mod_init ofproto/ofproto.c:7987:17
    #5 0x543250 in handle_flow_mod__ ofproto/ofproto.c:6206:13
    #6 0x56a2fc in handle_flow_mod ofproto/ofproto.c:6190:17
    #7 0x565bda in handle_single_part_openflow ofproto/ofproto.c:8504:16
    #8 0x540b25 in handle_openflow ofproto/ofproto.c:8685:21
    #9 0x6697fd in ofconn_run ofproto/connmgr.c:1329:13
    #10 0x668e6e in connmgr_run ofproto/connmgr.c:356:9
    #11 0x53f1bc in ofproto_run ofproto/ofproto.c:1890:5
    #12 0x4ead0c in bridge_run__ vswitchd/bridge.c:3250:9
    #13 0x4e9bc8 in bridge_run vswitchd/bridge.c:3309:5
    #14 0x51c072 in main vswitchd/ovs-vswitchd.c:127:9
    #15 0x7f23a99011a2 in __libc_start_main (/lib64/libc.so.6)

  Uninitialized value was created by an allocation of 'ofpacts_stub'
  in the stack frame of function 'handle_flow_mod'
    #0 0x569e80 in handle_flow_mod ofproto/ofproto.c:6170

This could cause issues with flow modifications or other operations.

To reproduce, some NSH tests could be run under valgrind or clang
MemorySantizer. Ex. "nsh - md1 encap over a veth link" test.

Fix that by clearing padding bytes while encoding, and checking that
these bytes are all zeros on decoding.

New tests added to tests/ofp-actions.at.

Fixes: 1fc11c5948cf ("Generic encap and decap support for NSH")

lib/ofp-ed-props.c
@@ -48,6 +48,9 @@ decode_ed_prop(const struct ofp_ed_prop_header **ofp_prop,
             if (len > sizeof(*opnmt) || len > *remaining) {
                 return OFPERR_NXBAC_BAD_ED_PROP;
             }
+            if (!is_all_zeros(opnmt->pad, sizeof opnmt->pad)) {
+                return OFPERR_NXBRC_MUST_BE_ZERO;
+            }
             struct ofpact_ed_prop_nsh_md_type *pnmt =
                     ofpbuf_put_uninit(out, sizeof(*pnmt));
             pnmt->header.prop_class = prop_class;
@@ -108,6 +111,7 @@ encode_ed_prop(const struct ofpact_ed_prop **prop,
             opnmt->header.len =
                     offsetof(struct ofp_ed_prop_nsh_md_type, pad);
             opnmt->md_type = pnmt->md_type;
+            memset(opnmt->pad, 0, sizeof opnmt->pad);
             prop_len = sizeof(*pnmt);
             break;
         }

tests/ofp-actions.at
@@ -769,6 +769,17 @@ dnl Check OpenFlow v1.3.4 Conformance Test: 430.510.
 & 00000010  00 00 00 10 00 00 00 01-
 0019 0010 80000807 000102030405 000000000010 00000001
 
+dnl Check NSH encap (experimenter extension).
+# actions=encap(nsh(md_type=1))
+ffff 0018 00002320 002e 0000 0001894f 0004 01 05 01 000000
+
+dnl NSH encap with non-zero padding.
+# bad OpenFlow13 actions: NXBRC_MUST_BE_ZERO
+& ofp_actions|WARN|bad action at offset 0 (NXBRC_MUST_BE_ZERO):
+& 00000000  ff ff 00 18 00 00 23 20-00 2e 00 00 00 01 89 4f
+& 00000010  00 04 01 05 01 00 00 01-
+ffff 0018 00002320 002e 0000 0001894f 0004 01 05 01 000001
+
 ])
 sed '/^[[#&]]/d' < test-data > input.txt
 sed -n 's/^# //p; /^$/p' < test-data > expout