ovsrobot/ovs

commit 540e70fba6d514040d4418aa3992c9754b985a92

build: Add support for DPDK meson build.
Make based build is deprecated in DPDK. Meson based
build to be used for future DPDK releases.

This updates travis, configure script and documentation
for using DPDK Meson with OVS.

Tested-at: https://travis-ci.org/github/Sunil-Pai-G/ovs-copy/builds/723510063
Signed-off-by: Sunil Pai G <sunil.pai.g@intel.com>
Signed-off-by: Ian Stokes <ian.stokes@intel.com>

.travis/linux-build.sh
@@ -87,16 +87,28 @@ function install_dpdk()
 {
     local DPDK_VER=$1
     local VERSION_FILE="dpdk-dir/travis-dpdk-cache-version"
+    local DPDK_OPTS=""
+    local DPDK_LIB=""
 
     if [ -z "$TRAVIS_ARCH" ] ||
        [ "$TRAVIS_ARCH" == "amd64" ]; then
-        TARGET="x86_64-native-linuxapp-gcc"
+        DPDK_LIB=$(pwd)/dpdk-dir/build/lib/x86_64-linux-gnu
     elif [ "$TRAVIS_ARCH" == "aarch64" ]; then
-        TARGET="arm64-armv8a-linuxapp-gcc"
+        DPDK_LIB=$(pwd)/dpdk-dir/build/lib/aarch64-linux-gnu
     else
         echo "Target is unknown"
         exit 1
     fi
+
+    if [ "$DPDK_SHARED" ]; then
+        EXTRA_OPTS="$EXTRA_OPTS --with-dpdk=shared"
+        export LD_LIBRARY_PATH=$DPDK_LIB/:$LD_LIBRARY_PATH
+    else
+        EXTRA_OPTS="$EXTRA_OPTS --with-dpdk=static"
+    fi
+
+    # Export the following path for pkg-config to find the .pc file.
+    export PKG_CONFIG_PATH=$DPDK_LIB/pkgconfig/:$PKG_CONFIG_PATH
 
     if [ "${DPDK_VER##refs/*/}" != "${DPDK_VER}" ]; then
         # Avoid using cache for git tree build.
@@ -110,7 +122,8 @@ function install_dpdk()
         if [ -f "${VERSION_FILE}" ]; then
             VER=$(cat ${VERSION_FILE})
             if [ "${VER}" = "${DPDK_VER}" ]; then
-                EXTRA_OPTS="${EXTRA_OPTS} --with-dpdk=$(pwd)/dpdk-dir/build"
+                # Update the library paths.
+                sudo ldconfig
                 echo "Found cached DPDK ${VER} build in $(pwd)/dpdk-dir"
                 return
             fi
@@ -124,16 +137,20 @@ function install_dpdk()
         pushd dpdk-dir
     fi
 
-    make config CC=gcc T=$TARGET
+    # Disable building DPDK unit tests. Not needed for OVS build or tests.
+    DPDK_OPTS="$DPDK_OPTS -Dtests=false"
 
-    if [ "$DPDK_SHARED" ]; then
-        sed -i '/CONFIG_RTE_BUILD_SHARED_LIB=n/s/=n/=y/' build/.config
-        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/$TARGET/lib
-    fi
+    # Install DPDK using prefix.
+    DPDK_OPTS="$DPDK_OPTS --prefix=$(pwd)/build"
 
-    make -j4 CC=gcc EXTRA_CFLAGS='-fPIC'
-    EXTRA_OPTS="$EXTRA_OPTS --with-dpdk=$(pwd)/build"
-    echo "Installed DPDK source in $(pwd)"
+    CC=gcc meson $DPDK_OPTS build
+    ninja -C build
+    sudo ninja -C build install
+
+    # Update the library paths.
+    sudo ldconfig
+
+    echo "Installed DPDK source"
     popd
     echo "${DPDK_VER}" > ${VERSION_FILE}
 }

acinclude.m4
@@ -334,8 +334,10 @@ dnl
 dnl Configure DPDK source tree
 AC_DEFUN([OVS_CHECK_DPDK], [
   AC_ARG_WITH([dpdk],
-              [AC_HELP_STRING([--with-dpdk=/path/to/dpdk],
-                              [Specify the DPDK build directory])],
+              [AC_HELP_STRING([--with-dpdk=static|shared|/path/to/dpdk],
+                              [Specify "static" or "shared" depending on the
+                              DPDK libraries to use only if built using Meson
+                              OR the DPDK build directory in case of Make])],
               [have_dpdk=true])
 
   AC_MSG_CHECKING([whether dpdk is enabled])
@@ -345,13 +347,24 @@ AC_DEFUN([OVS_CHECK_DPDK], [
   else
     AC_MSG_RESULT([yes])
     case "$with_dpdk" in
-      yes)
+      "shared" | "static" | "yes")
         DPDK_AUTO_DISCOVER="true"
-        PKG_CHECK_MODULES_STATIC([DPDK], [libdpdk], [
-            DPDK_INCLUDE="$DPDK_CFLAGS"
-            DPDK_LIB="$DPDK_LIBS"], [
-            DPDK_INCLUDE="-I/usr/local/include/dpdk -I/usr/include/dpdk"
-            DPDK_LIB="-ldpdk"])
+        case "$with_dpdk" in
+          "shared" | "yes")
+             PKG_CHECK_MODULES([DPDK], [libdpdk], [
+                 DPDK_INCLUDE="$DPDK_CFLAGS"
+                 DPDK_LIB="$DPDK_LIBS"], [
+                 DPDK_INCLUDE="-I/usr/local/include/dpdk -I/usr/include/dpdk"
+                 DPDK_LIB="-ldpdk"])
+                 ;;
+           "static")
+             PKG_CHECK_MODULES_STATIC([DPDK], [libdpdk], [
+                 DPDK_INCLUDE="$DPDK_CFLAGS"
+                 DPDK_LIB="$DPDK_LIBS"], [
+                 DPDK_INCLUDE="-I/usr/local/include/dpdk -I/usr/include/dpdk"
+                 DPDK_LIB="-ldpdk"])
+                ;;
+        esac
         ;;
       *)
         DPDK_AUTO_DISCOVER="false"
@@ -424,8 +437,9 @@ AC_DEFUN([OVS_CHECK_DPDK], [
       [AC_MSG_RESULT([no])
        if test "$DPDK_AUTO_DISCOVER" = "true"; then
          AC_MSG_ERROR(m4_normalize([
-            Could not find DPDK library in default search path, Use --with-dpdk
-            to specify the DPDK library installed in non-standard location]))
+            Could not find DPDK library in default search path, update
+            PKG_CONFIG_PATH for pkg-config to find the .pc file in
+            non-standard location]))
        else
          AC_MSG_ERROR([Could not find DPDK libraries in $DPDK_LIB_DIR])
        fi
@@ -451,10 +465,12 @@ AC_DEFUN([OVS_CHECK_DPDK], [
     # OTOH newer versions of dpdk pkg-config (generated with Meson)
     # will already have flagged just the right set of libs with
     # --whole-archive - in those cases do not wrap it once more.
-    case "$DPDK_LIB" in
-      *whole-archive*) DPDK_vswitchd_LDFLAGS=$DPDK_LIB;;
-      *) DPDK_vswitchd_LDFLAGS=-Wl,--whole-archive,$DPDK_LIB,--no-whole-archive
-    esac
+    if [[ "$pkg_failed" != "no" ]];then
+      DPDK_vswitchd_LDFLAGS=-Wl,--whole-archive,$DPDK_LIB,--no-whole-archive
+    else
+      DPDK_vswitchd_LDFLAGS=`python3 ${srcdir}/python/build/pkgcfg.py $DPDK_LIB`
+    fi
+
     AC_SUBST([DPDK_vswitchd_LDFLAGS])
     AC_DEFINE([DPDK_NETDEV], [1], [System uses the DPDK module.])
   fi

python/automake.mk
@@ -47,7 +47,8 @@ ovs_pyfiles = \
 EXTRA_DIST += \
 	python/build/__init__.py \
 	python/build/nroff.py \
-	python/build/soutil.py
+	python/build/soutil.py \
+	python/build/pkgcfg.py
 
 # PyPI support.
 EXTRA_DIST += \

python/build/pkgcfg.py
@@ -1,0 +1,30 @@
+# Copyright (c) 2020 Intel, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License")
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# Distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# Limitations under the License.
+
+# The purpose of this script is to parse the libraries
+# From pkg-config in case of DPDK Meson builds.
+
+import sys
+def parse_pkg_cfg_libs(arg):
+    linker_prefix = "-Wl,"
+    # Libtool expects libraries to be comma separated
+    # And -Wl must appear only once.
+    final_string = ','.join(map(str.strip,arg[1:])).replace('-Wl,','')
+    final_string = arg[0]+" "+linker_prefix+final_string
+    # Ld only understands -lpthread.
+    final_string = final_string.replace('-pthread','-lpthread')
+    return final_string
+
+if __name__ == "__main__":
+    print(parse_pkg_cfg_libs(sys.argv[1:]))

.travis.yml
@@ -27,6 +27,9 @@ addons:
       - selinux-policy-dev
       - libunbound-dev
       - libunwind-dev
+      - python3-setuptools
+      - python3-wheel
+      - ninja-build
 
 before_install: ./.travis/${TRAVIS_OS_NAME}-prepare.sh
 

.travis/linux-prepare.sh
@@ -22,6 +22,7 @@ cd ..
 
 pip3 install --disable-pip-version-check --user flake8 hacking
 pip3 install --user --upgrade docutils
+pip3 install --user  'meson==0.47.1'
 
 if [ "$M32" ]; then
     # Installing 32-bit libraries.

commit 9557a98c48a6b1a3a493ffde61d31a1e5a488b2e

build: Remove DPDK make build references.
Building DPDK using Make is removed since DPDK 20.08.
Hence, remove its references in OVS as well.

Fixes: 540e70fba6d5 ("build: Add support for DPDK meson build.")
Tested-at: https://travis-ci.org/github/Sunil-Pai-G/ovs/builds/742699277
Signed-off-by: Sunil Pai G <sunil.pai.g@intel.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

.travis/linux-build.sh
@@ -145,12 +145,12 @@ function install_dpdk()
 
     CC=gcc meson $DPDK_OPTS build
     ninja -C build
-    sudo ninja -C build install
+    ninja -C build install
 
     # Update the library paths.
     sudo ldconfig
 
-    echo "Installed DPDK source"
+    echo "Installed DPDK source in $(pwd)"
     popd
     echo "${DPDK_VER}" > ${VERSION_FILE}
 }

acinclude.m4
@@ -334,10 +334,9 @@ dnl
 dnl Configure DPDK source tree
 AC_DEFUN([OVS_CHECK_DPDK], [
   AC_ARG_WITH([dpdk],
-              [AC_HELP_STRING([--with-dpdk=static|shared|/path/to/dpdk],
+              [AC_HELP_STRING([--with-dpdk=static|shared|yes],
                               [Specify "static" or "shared" depending on the
-                              DPDK libraries to use only if built using Meson
-                              OR the DPDK build directory in case of Make])],
+                              DPDK libraries to use])],
               [have_dpdk=true])
 
   AC_MSG_CHECKING([whether dpdk is enabled])
@@ -347,46 +346,25 @@ AC_DEFUN([OVS_CHECK_DPDK], [
   else
     AC_MSG_RESULT([yes])
     case "$with_dpdk" in
-      "shared" | "static" | "yes")
-        DPDK_AUTO_DISCOVER="true"
-        case "$with_dpdk" in
-          "shared" | "yes")
-             PKG_CHECK_MODULES([DPDK], [libdpdk], [
-                 DPDK_INCLUDE="$DPDK_CFLAGS"
-                 DPDK_LIB="$DPDK_LIBS"], [
-                 DPDK_INCLUDE="-I/usr/local/include/dpdk -I/usr/include/dpdk"
-                 DPDK_LIB="-ldpdk"])
-                 ;;
-           "static")
-             PKG_CHECK_MODULES_STATIC([DPDK], [libdpdk], [
-                 DPDK_INCLUDE="$DPDK_CFLAGS"
-                 DPDK_LIB="$DPDK_LIBS"], [
-                 DPDK_INCLUDE="-I/usr/local/include/dpdk -I/usr/include/dpdk"
-                 DPDK_LIB="-ldpdk"])
-                ;;
-        esac
-        ;;
-      *)
-        DPDK_AUTO_DISCOVER="false"
-        DPDK_INCLUDE_PATH="$with_dpdk/include"
-        # If 'with_dpdk' is passed install directory, point to headers
-        # installed in $DESTDIR/$prefix/include/dpdk
-        if test -e "$DPDK_INCLUDE_PATH/rte_config.h"; then
-           DPDK_INCLUDE="-I$DPDK_INCLUDE_PATH"
-        elif test -e "$DPDK_INCLUDE_PATH/dpdk/rte_config.h"; then
-           DPDK_INCLUDE="-I$DPDK_INCLUDE_PATH/dpdk"
-        fi
-        DPDK_LIB_DIR="$with_dpdk/lib"
-        DPDK_LIB="-ldpdk"
-        ;;
+      "shared")
+          PKG_CHECK_MODULES([DPDK], [libdpdk], [
+              DPDK_INCLUDE="$DPDK_CFLAGS"
+              DPDK_LIB="$DPDK_LIBS"], [
+              DPDK_INCLUDE="-I/usr/local/include/dpdk -I/usr/include/dpdk"
+              DPDK_LIB="-ldpdk"])
+              ;;
+      "static" | "yes")
+          PKG_CHECK_MODULES_STATIC([DPDK], [libdpdk], [
+              DPDK_INCLUDE="$DPDK_CFLAGS"
+              DPDK_LIB="$DPDK_LIBS"], [
+              DPDK_INCLUDE="-I/usr/local/include/dpdk -I/usr/include/dpdk"
+              DPDK_LIB="-ldpdk"])
+              ;;
     esac
 
     ovs_save_CFLAGS="$CFLAGS"
     ovs_save_LDFLAGS="$LDFLAGS"
     CFLAGS="$CFLAGS $DPDK_INCLUDE"
-    if test "$DPDK_AUTO_DISCOVER" = "false"; then
-      LDFLAGS="$LDFLAGS -L${DPDK_LIB_DIR}"
-    fi
 
     AC_CHECK_HEADERS([rte_config.h], [], [
       AC_MSG_ERROR([unable to find rte_config.h in $with_dpdk])
@@ -435,21 +413,14 @@ AC_DEFUN([OVS_CHECK_DPDK], [
       [AC_MSG_RESULT([yes])
        DPDKLIB_FOUND=true],
       [AC_MSG_RESULT([no])
-       if test "$DPDK_AUTO_DISCOVER" = "true"; then
-         AC_MSG_ERROR(m4_normalize([
-            Could not find DPDK library in default search path, update
-            PKG_CONFIG_PATH for pkg-config to find the .pc file in
-            non-standard location]))
-       else
-         AC_MSG_ERROR([Could not find DPDK libraries in $DPDK_LIB_DIR])
-       fi
+       AC_MSG_ERROR(m4_normalize([
+          Could not find DPDK library in default search path, update
+          PKG_CONFIG_PATH for pkg-config to find the .pc file in
+          non-standard location]))
       ])
 
     CFLAGS="$ovs_save_CFLAGS"
     LDFLAGS="$ovs_save_LDFLAGS"
-    if test "$DPDK_AUTO_DISCOVER" = "false"; then
-      OVS_LDFLAGS="$OVS_LDFLAGS -L$DPDK_LIB_DIR"
-    fi
     OVS_CFLAGS="$OVS_CFLAGS $DPDK_INCLUDE"
     OVS_ENABLE_OPTION([-mssse3])
 
@@ -458,18 +429,14 @@ AC_DEFUN([OVS_CHECK_DPDK], [
     # This happens because the rest of the DPDK code doesn't use any symbol in
     # the pmd driver objects, and the drivers register themselves using an
     # __attribute__((constructor)) function.
-    #
-    # These options are specified inside a single -Wl directive to prevent
-    # autotools from reordering them.
-    #
-    # OTOH newer versions of dpdk pkg-config (generated with Meson)
-    # will already have flagged just the right set of libs with
-    # --whole-archive - in those cases do not wrap it once more.
-    if [[ "$pkg_failed" != "no" ]];then
-      DPDK_vswitchd_LDFLAGS=-Wl,--whole-archive,$DPDK_LIB,--no-whole-archive
-    else
-      DPDK_vswitchd_LDFLAGS=`python3 ${srcdir}/python/build/pkgcfg.py $DPDK_LIB`
-    fi
+
+    # Wrap the DPDK libraries inside a single -Wl directive
+    # after comma separation to prevent autotools from reordering them.
+    DPDK_vswitchd_LDFLAGS=$(echo "$DPDK_LIB"| tr -s ' ' ',' | \
+                            sed 's/-Wl,//g' | sed 's/,/ -Wl,/')
+    # Replace -pthread with -lpthread for LD and remove the last extra comma.
+    DPDK_vswitchd_LDFLAGS=$(echo "$DPDK_vswitchd_LDFLAGS"| sed 's/,$//' | \
+                            sed 's/-pthread/-lpthread/g')
 
     AC_SUBST([DPDK_vswitchd_LDFLAGS])
     AC_DEFINE([DPDK_NETDEV], [1], [System uses the DPDK module.])

python/automake.mk
@@ -47,8 +47,7 @@ ovs_pyfiles = \
 EXTRA_DIST += \
 	python/build/__init__.py \
 	python/build/nroff.py \
-	python/build/soutil.py \
-	python/build/pkgcfg.py
+	python/build/soutil.py
 
 # PyPI support.
 EXTRA_DIST += \

python/build/pkgcfg.py
@@ -1,30 +1,0 @@
-# Copyright (c) 2020 Intel, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License")
-# You may not use this file except in compliance with the License.
-# You may obtain a copy of the License at:
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# Distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# Limitations under the License.
-
-# The purpose of this script is to parse the libraries
-# From pkg-config in case of DPDK Meson builds.
-
-import sys
-def parse_pkg_cfg_libs(arg):
-    linker_prefix = "-Wl,"
-    # Libtool expects libraries to be comma separated
-    # And -Wl must appear only once.
-    final_string = ','.join(map(str.strip,arg[1:])).replace('-Wl,','')
-    final_string = arg[0]+" "+linker_prefix+final_string
-    # Ld only understands -lpthread.
-    final_string = final_string.replace('-pthread','-lpthread')
-    return final_string
-
-if __name__ == "__main__":
-    print(parse_pkg_cfg_libs(sys.argv[1:]))