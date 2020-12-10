ovsrobot/ovs

commit af06184705072804a4c1374f9c824c9e4c241c26

dpif-netlink: Count the number of offloaded rules
Add a counter for the offloaded rules, and display it in the command
of "ovs-appctl upcall/show".

Signed-off-by: Jianbo Liu <jianbol@nvidia.com>
Reviewed-by: Roi Dayan <roid@nvidia.com>
Signed-off-by: Simon Horman <simon.horman@netronome.com>

lib/dpif-netlink.c
@@ -208,6 +208,7 @@ struct dpif_netlink {
     /* Change notification. */
     struct nl_sock *port_notifier; /* vport multicast group subscriber. */
     bool refresh_channels;
+    struct atomic_count n_offloaded_flows;
 };
 
 static void report_loss(struct dpif_netlink *, struct dpif_channel *,
@@ -653,6 +654,7 @@ dpif_netlink_run(struct dpif *dpif_)
 static int
 dpif_netlink_get_stats(const struct dpif *dpif_, struct dpif_dp_stats *stats)
 {
+    struct dpif_netlink *dpif = dpif_netlink_cast(dpif_);
     struct dpif_netlink_dp dp;
     struct ofpbuf *buf;
     int error;
@@ -678,6 +680,7 @@ dpif_netlink_get_stats(const struct dpif *dpif_, struct dpif_dp_stats *stats)
         }
         ofpbuf_delete(buf);
     }
+    stats->n_offloaded_flows = atomic_count_get(&dpif->n_offloaded_flows);
     return error;
 }
 
@@ -2189,6 +2192,9 @@ try_send_to_netdev(struct dpif_netlink *dpif, struct dpif_op *op)
         }
 
         err = parse_flow_put(dpif, put);
+        if (!err && (put->flags & DPIF_FP_CREATE)) {
+            atomic_count_inc(&dpif->n_offloaded_flows);
+        }
         log_flow_put_message(&dpif->dpif, &this_module, put, 0);
         break;
     }
@@ -2203,6 +2209,9 @@ try_send_to_netdev(struct dpif_netlink *dpif, struct dpif_op *op)
                                 dpif_normalize_type(dpif_type(&dpif->dpif)),
                                 del->ufid,
                                 del->stats);
+        if (!err) {
+            atomic_count_dec(&dpif->n_offloaded_flows);
+        }
         log_flow_del_message(&dpif->dpif, &this_module, del, 0);
         break;
     }

lib/dpif.h
@@ -429,6 +429,7 @@ struct dpif_dp_stats {
     uint64_t n_missed;          /* Number of flow table misses. */
     uint64_t n_lost;            /* Number of misses not sent to userspace. */
     uint64_t n_flows;           /* Number of flows present. */
+    uint64_t n_offloaded_flows; /* Number of offloaded flows present. */
     uint64_t n_mask_hit;        /* Number of mega flow masks visited for
                                    flow table matches. */
     uint32_t n_masks;           /* Number of mega flow masks. */

ofproto/ofproto-dpif-upcall.c
@@ -175,6 +175,7 @@ struct udpif {
 
     /* n_flows_mutex prevents multiple threads updating these concurrently. */
     atomic_uint n_flows;               /* Number of flows in the datapath. */
+    atomic_uint n_offloaded_flows;     /* Number of the offloaded flows. */
     atomic_llong n_flows_timestamp;    /* Last time n_flows was updated. */
     struct ovs_mutex n_flows_mutex;
 
@@ -730,6 +731,8 @@ udpif_get_n_flows(struct udpif *udpif)
         dpif_get_dp_stats(udpif->dpif, &stats);
         flow_count = stats.n_flows;
         atomic_store_relaxed(&udpif->n_flows, flow_count);
+        atomic_store_relaxed(&udpif->n_offloaded_flows,
+                             stats.n_offloaded_flows);
         ovs_mutex_unlock(&udpif->n_flows_mutex);
     } else {
         atomic_read_relaxed(&udpif->n_flows, &flow_count);
@@ -2904,6 +2907,7 @@ upcall_unixctl_show(struct unixctl_conn *conn, int argc OVS_UNUSED,
     struct udpif *udpif;
 
     LIST_FOR_EACH (udpif, list_node, &all_udpifs) {
+        unsigned int n_offloaded_flows;
         unsigned int flow_limit;
         bool ufid_enabled;
         size_t i;
@@ -2915,6 +2919,8 @@ upcall_unixctl_show(struct unixctl_conn *conn, int argc OVS_UNUSED,
         ds_put_format(&ds, "  flows         : (current %lu)"
             " (avg %u) (max %u) (limit %u)\n", udpif_get_n_flows(udpif),
             udpif->avg_n_flows, udpif->max_n_flows, flow_limit);
+        atomic_read_relaxed(&udpif->n_offloaded_flows, &n_offloaded_flows);
+        ds_put_format(&ds, "  offloaded flows : %u\n", n_offloaded_flows);
         ds_put_format(&ds, "  dump duration : %lldms\n", udpif->dump_duration);
         ds_put_format(&ds, "  ufid enabled : ");
         if (ufid_enabled) {

commit 25c355a2bd5b998e46df2dd533e057d60c87380d

dpif-netlink: Fix issues of the offloaded flows counter
The n_offloaded_flows counter is saved in dpif, and this is the
first one when ofproto is created. When flow operation is done by
ovs-appctl commands, such as, dpctl/add-flows, dpctl/del-flow and
dpctl/del-flows, a new dpif is opened, so n_offloaded_flows in it
can't be used. To fix this, move n_offloaded_flows to dp_netlink.

Besides, this stats is set to zero for userspace datapath as it is
intented for counting the rules offloaded by tc flower

Fixes: af0618470507 ("dpif-netlink: Count the number of offloaded rules")
Signed-off-by: Jianbo Liu <jianbol@nvidia.com>
Reviewed-by: Roi Dayan <roid@nvidia.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

lib/dpif-netdev.c
@@ -2023,6 +2023,7 @@ dpif_netdev_get_stats(const struct dpif *dpif, struct dpif_dp_stats *stats)
     }
     stats->n_masks = UINT32_MAX;
     stats->n_mask_hit = UINT64_MAX;
+    stats->n_offloaded_flows = 0;
 
     return 0;
 }

lib/dpif-netlink.c
@@ -202,6 +202,8 @@ struct dp_netlink {
     const struct dpif_class *const class;
     const char *const name;
     struct ovs_refcount ref_cnt;
+
+    struct atomic_count n_offloaded_flows;
 };
 
 /* Datapath interface for the openvswitch Linux kernel module. */
@@ -221,7 +223,6 @@ struct dpif_netlink {
     /* Change notification. */
     struct nl_sock *port_notifier; /* vport multicast group subscriber. */
     bool refresh_channels;
-    struct atomic_count n_offloaded_flows;
     struct dp_netlink *dp;
 };
 
@@ -752,7 +753,7 @@ dpif_netlink_get_stats(const struct dpif *dpif_, struct dpif_dp_stats *stats)
         }
         ofpbuf_delete(buf);
     }
-    stats->n_offloaded_flows = atomic_count_get(&dpif->n_offloaded_flows);
+    stats->n_offloaded_flows = atomic_count_get(&dpif->dp->n_offloaded_flows);
     return error;
 }
 
@@ -1205,6 +1206,7 @@ dpif_netlink_flow_flush(struct dpif *dpif_)
 
     if (netdev_is_flow_api_enabled()) {
         netdev_ports_flow_flush(dpif_type_str);
+        atomic_count_set(&dpif->dp->n_offloaded_flows, 0);
     }
 
     return dpif_netlink_flow_transact(&flow, NULL, NULL);
@@ -2253,6 +2255,7 @@ out:
 static int
 try_send_to_netdev(struct dpif_netlink *dpif, struct dpif_op *op)
 {
+    struct dp_netlink *dp = dpif->dp;
     int err = EOPNOTSUPP;
 
     switch (op->type) {
@@ -2265,7 +2268,7 @@ try_send_to_netdev(struct dpif_netlink *dpif, struct dpif_op *op)
 
         err = parse_flow_put(dpif, put);
         if (!err && (put->flags & DPIF_FP_CREATE)) {
-            atomic_count_inc(&dpif->n_offloaded_flows);
+            atomic_count_inc(&dp->n_offloaded_flows);
         }
         log_flow_put_message(&dpif->dpif, &this_module, put, 0);
         break;
@@ -2282,7 +2285,7 @@ try_send_to_netdev(struct dpif_netlink *dpif, struct dpif_op *op)
                                 del->ufid,
                                 del->stats);
         if (!err) {
-            atomic_count_dec(&dpif->n_offloaded_flows);
+            atomic_count_dec(&dp->n_offloaded_flows);
         }
         log_flow_del_message(&dpif->dpif, &this_module, del, 0);
         break;

tests/system-offloads-traffic.at
@@ -32,6 +32,8 @@ in_port(3),eth(macs),eth_type(0x0800),ipv4(frag=no), packets:9, bytes:882, used:
 
 AT_CHECK([ovs-appctl dpctl/dump-flows type=offloaded], [0], [])
 
+AT_CHECK([ovs-appctl upcall/show | grep "offloaded flows : 0"], [0], [ignore])
+
 OVS_TRAFFIC_VSWITCHD_STOP
 AT_CLEANUP
 
@@ -63,6 +65,8 @@ AT_CHECK([ovs-appctl dpctl/dump-flows type=tc,offloaded | grep "eth_type(0x0800)
 in_port(2),eth(macs),eth_type(0x0800),ipv4(frag=no), packets:9, bytes:756, used:0.001s, actions:output
 in_port(3),eth(macs),eth_type(0x0800),ipv4(frag=no), packets:9, bytes:756, used:0.001s, actions:output
 ])
+
+AT_CHECK([ovs-appctl upcall/show | grep -E "offloaded flows : [[1-9]]"], [0], [ignore])
 
 OVS_TRAFFIC_VSWITCHD_STOP
 AT_CLEANUP