9b8fbc093792d1b7ee426ae6a5a830cf4196794d
Adrian Moreno <amorenoz@redhat.com>
2021-04-27 13:00:22 +0200

    ofp_actions: fix typo in set_mpls_tc formatting
    I guess this was a cut-and-paste error from set_mpls_ttl
    
    Signed-off-by: Adrian Moreno <amorenoz@redhat.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/ofp-actions.c
+++ lib/ofp-actions.c
@@ -3850,7 +3850,7 @@ static void
 format_SET_MPLS_TC(const struct ofpact_mpls_tc *a,
                    const struct ofpact_format_params *fp)
 {
-    ds_put_format(fp->s, "%sset_mpls_ttl(%s%"PRIu8"%s)%s",
+    ds_put_format(fp->s, "%sset_mpls_tc(%s%"PRIu8"%s)%s",
                   colors.paren, colors.end, a->tc,
                   colors.paren, colors.end);
 }
