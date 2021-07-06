ovsrobot/ovs

commit 807152a4ddfb89b65ef75c6b12937ecd68ea8cb3

Use primary/secondary, not master/slave, as names for OpenFlow roles.
Signed-off-by: Ben Pfaff <blp@ovn.org>
Acked-by: Alin Gabriel Serdean <aserdean@ovn.org>

vswitchd/bridge.c
@@ -3017,10 +3017,10 @@ ofp12_controller_role_to_str(enum ofp12_controller_role role)
     switch (role) {
     case OFPCR12_ROLE_EQUAL:
         return "other";
-    case OFPCR12_ROLE_MASTER:
-        return "master";
-    case OFPCR12_ROLE_SLAVE:
-        return "slave";
+    case OFPCR12_ROLE_PRIMARY:
+        return "primary";
+    case OFPCR12_ROLE_SECONDARY:
+        return "secondary";
     case OFPCR12_ROLE_NOCHANGE:
     default:
         return NULL;

include/openflow/nicira-ext.h
@@ -296,16 +296,16 @@ enum nx_packet_in2_prop_type {
  *
  * The other possible roles are a related pair:
  *
- *    - Master (NX_ROLE_MASTER) is equivalent to Other, except that there may
- *      be at most one Master controller at a time: when a controller
- *      configures itself as Master, any existing Master is demoted to the
- *      Slave role.
+ *    - Primary (NX_ROLE_PRIMARY) is equivalent to Other, except that there may
+ *      be at most one Primary controller at a time: when a controller
+ *      configures itself as Primary, any existing Primary is demoted to the
+ *      Secondary role.
  *
- *    - Slave (NX_ROLE_SLAVE) allows the controller read-only access to
+ *    - Secondary (NX_ROLE_SECONDARY) allows the controller read-only access to
  *      OpenFlow features.  In particular attempts to modify the flow table
  *      will be rejected with an OFPBRC_EPERM error.
  *
- *      Slave controllers do not receive OFPT_PACKET_IN or OFPT_FLOW_REMOVED
+ *      Secondary controllers do not receive OFPT_PACKET_IN or OFPT_FLOW_REMOVED
  *      messages, but they do receive OFPT_PORT_STATUS messages.
  */
 struct nx_role_request {
@@ -315,23 +315,23 @@ OFP_ASSERT(sizeof(struct nx_role_request) == 4);
 
 enum nx_role {
     NX_ROLE_OTHER,              /* Default role, full access. */
-    NX_ROLE_MASTER,             /* Full access, at most one. */
-    NX_ROLE_SLAVE               /* Read-only access. */
+    NX_ROLE_PRIMARY,            /* Full access, at most one. */
+    NX_ROLE_SECONDARY           /* Read-only access. */
 };
 
 /* NXT_SET_ASYNC_CONFIG.
  *
  * Sent by a controller, this message configures the asynchronous messages that
  * the controller wants to receive.  Element 0 in each array specifies messages
- * of interest when the controller has an "other" or "master" role; element 1,
- * when the controller has a "slave" role.
+ * of interest when the controller has an "other" or "primary" role; element 1,
+ * when the controller has a "secondary" role.
  *
  * Each array element is a bitmask in which a 0-bit disables receiving a
  * particular message and a 1-bit enables receiving it.  Each bit controls the
  * message whose 'reason' corresponds to the bit index.  For example, the bit
  * with value 1<<2 == 4 in port_status_mask[1] determines whether the
  * controller will receive OFPT_PORT_STATUS messages with reason OFPPR_MODIFY
- * (value 2) when the controller has a "slave" role.
+ * (value 2) when the controller has a "secondary" role.
  *
  * As a side effect, for service controllers, this message changes the
  * miss_send_len from default of zero to OFP_DEFAULT_MISS_SEND_LEN (128).

include/openflow/openflow-1.2.h
@@ -176,7 +176,7 @@ enum ofp12_group_capabilities {
 struct ofp12_role_request {
     ovs_be32 role;            /* One of OFPCR12_ROLE_*. */
     uint8_t pad[4];           /* Align to 64 bits. */
-    ovs_be64 generation_id;   /* Master Election Generation Id */
+    ovs_be64 generation_id;   /* Primary Election Generation Id */
 };
 OFP_ASSERT(sizeof(struct ofp12_role_request) == 16);
 
@@ -184,8 +184,8 @@ OFP_ASSERT(sizeof(struct ofp12_role_request) == 16);
 enum ofp12_controller_role {
     OFPCR12_ROLE_NOCHANGE,    /* Don't change current role. */
     OFPCR12_ROLE_EQUAL,       /* Default role, full access. */
-    OFPCR12_ROLE_MASTER,      /* Full access, at most one master. */
-    OFPCR12_ROLE_SLAVE,       /* Read-only access. */
+    OFPCR12_ROLE_PRIMARY,     /* Full access, at most one primary. */
+    OFPCR12_ROLE_SECONDARY,   /* Read-only access. */
 };
 
 /* Packet received on port (datapath -> controller). */

include/openflow/openflow-1.4.h
@@ -274,7 +274,7 @@ struct ofp14_role_status {
     ovs_be32 role;              /* One of OFPCR_ROLE_*. */
     uint8_t  reason;            /* One of OFPCRR_*. */
     uint8_t  pad[3];            /* Align to 64 bits. */
-    ovs_be64 generation_id;     /* Master Election Generation Id */
+    ovs_be64 generation_id;     /* Primary Election Generation Id */
 
     /* Followed by a list of struct ofp14_role_prop_header */
 };
@@ -282,9 +282,9 @@ OFP_ASSERT(sizeof(struct ofp14_role_status) == 16);
 
 /* What changed about the controller role */
 enum ofp14_controller_role_reason {
-    OFPCRR_MASTER_REQUEST = 0,  /* Another controller asked to be master. */
-    OFPCRR_CONFIG         = 1,  /* Configuration changed on the switch. */
-    OFPCRR_EXPERIMENTER   = 2,  /* Experimenter data changed. */
+    OFPCRR_PRIMARY_REQUEST = 0, /* Another controller asked to be primary. */
+    OFPCRR_CONFIG          = 1, /* Configuration changed on the switch. */
+    OFPCRR_EXPERIMENTER    = 2, /* Experimenter data changed. */
     OFPCRR_N_REASONS            /* Denotes number of reasons. */
 };
 

include/openvswitch/ofp-connection.h
@@ -69,10 +69,10 @@ enum ofputil_async_msg_type {
 const char *ofputil_async_msg_type_to_string(enum ofputil_async_msg_type);
 
 struct ofputil_async_cfg {
-    uint32_t master[OAM_N_TYPES];
-    uint32_t slave[OAM_N_TYPES];
+    uint32_t primary[OAM_N_TYPES];
+    uint32_t secondary[OAM_N_TYPES];
 };
-#define OFPUTIL_ASYNC_CFG_INIT (struct ofputil_async_cfg) { .master[0] = 0 }
+#define OFPUTIL_ASYNC_CFG_INIT (struct ofputil_async_cfg) { .primary[0] = 0 }
 
 enum ofperr ofputil_decode_set_async_config(const struct ofp_header *,
                                             bool loose,

include/openvswitch/ofp-errors.h
@@ -115,10 +115,10 @@ enum ofperr {
      * OFPBIC_BAD_EXP_TYPE. */
 
     /* Expected: 0x0,1,5 in OF1.0 means both OFPBRC_EPERM and
-     * OFPBRC_IS_SLAVE. */
+     * OFPBRC_IS_SECONDARY. */
 
     /* Expected: 0x0,1,5 in OF1.1 means both OFPBRC_EPERM and
-     * OFPBRC_IS_SLAVE. */
+     * OFPBRC_IS_SECONDARY. */
 
 /* ## ------------------ ## */
 /* ## OFPET_HELLO_FAILED ## */
@@ -168,8 +168,9 @@ enum ofperr {
      *   code defined the specification. ] */
     OFPERR_OFPBRC_BAD_TABLE_ID,
 
-    /* OF1.0-1.1(1,5), OF1.2+(1,10).  Denied because controller is slave. */
-    OFPERR_OFPBRC_IS_SLAVE,
+    /* OF1.0-1.1(1,5), OF1.2+(1,10).  Denied because controller has secondary
+     * role.  (Secondary controllers have only read-only access.) */
+    OFPERR_OFPBRC_IS_SECONDARY,
 
     /* NX1.0-1.1(1,514), OF1.2+(1,11).  Invalid or missing port.  [ A
      * non-standard error (1,514), formerly OFPERR_NXBRC_BAD_IN_PORT is used

lib/ofp-connection.c
@@ -48,8 +48,8 @@ ofputil_decode_role_message(const struct ofp_header *oh,
 
         if (orr->role != htonl(OFPCR12_ROLE_NOCHANGE) &&
             orr->role != htonl(OFPCR12_ROLE_EQUAL) &&
-            orr->role != htonl(OFPCR12_ROLE_MASTER) &&
-            orr->role != htonl(OFPCR12_ROLE_SLAVE)) {
+            orr->role != htonl(OFPCR12_ROLE_PRIMARY) &&
+            orr->role != htonl(OFPCR12_ROLE_SECONDARY)) {
             return OFPERR_OFPRRFC_BAD_ROLE;
         }
 
@@ -68,12 +68,12 @@ ofputil_decode_role_message(const struct ofp_header *oh,
         const struct nx_role_request *nrr = b.msg;
 
         BUILD_ASSERT(NX_ROLE_OTHER + 1 == OFPCR12_ROLE_EQUAL);
-        BUILD_ASSERT(NX_ROLE_MASTER + 1 == OFPCR12_ROLE_MASTER);
-        BUILD_ASSERT(NX_ROLE_SLAVE + 1 == OFPCR12_ROLE_SLAVE);
+        BUILD_ASSERT(NX_ROLE_PRIMARY + 1 == OFPCR12_ROLE_PRIMARY);
+        BUILD_ASSERT(NX_ROLE_SECONDARY + 1 == OFPCR12_ROLE_SECONDARY);
 
         if (nrr->role != htonl(NX_ROLE_OTHER) &&
-            nrr->role != htonl(NX_ROLE_MASTER) &&
-            nrr->role != htonl(NX_ROLE_SLAVE)) {
+            nrr->role != htonl(NX_ROLE_PRIMARY) &&
+            nrr->role != htonl(NX_ROLE_SECONDARY)) {
             return OFPERR_OFPRRFC_BAD_ROLE;
         }
 
@@ -100,11 +100,11 @@ format_role_generic(struct ds *string, enum ofp12_controller_role role,
     case OFPCR12_ROLE_EQUAL:
         ds_put_cstr(string, "equal"); /* OF 1.2 wording */
         break;
-    case OFPCR12_ROLE_MASTER:
-        ds_put_cstr(string, "master");
+    case OFPCR12_ROLE_PRIMARY:
+        ds_put_cstr(string, "primary");
         break;
-    case OFPCR12_ROLE_SLAVE:
-        ds_put_cstr(string, "slave");
+    case OFPCR12_ROLE_SECONDARY:
+        ds_put_cstr(string, "secondary");
         break;
     default:
         OVS_NOT_REACHED();
@@ -148,8 +148,8 @@ ofputil_encode_role_reply(const struct ofp_header *request,
         struct nx_role_request *nrr;
 
         BUILD_ASSERT(NX_ROLE_OTHER == OFPCR12_ROLE_EQUAL - 1);
-        BUILD_ASSERT(NX_ROLE_MASTER == OFPCR12_ROLE_MASTER - 1);
-        BUILD_ASSERT(NX_ROLE_SLAVE == OFPCR12_ROLE_SLAVE - 1);
+        BUILD_ASSERT(NX_ROLE_PRIMARY == OFPCR12_ROLE_PRIMARY - 1);
+        BUILD_ASSERT(NX_ROLE_SECONDARY == OFPCR12_ROLE_SECONDARY - 1);
 
         buf = ofpraw_alloc_reply(OFPRAW_NXT_ROLE_REPLY, request, 0);
         nrr = ofpbuf_put_zeros(buf, sizeof *nrr);
@@ -197,8 +197,8 @@ ofputil_decode_role_status(const struct ofp_header *oh,
     const struct ofp14_role_status *r = b.msg;
     if (r->role != htonl(OFPCR12_ROLE_NOCHANGE) &&
         r->role != htonl(OFPCR12_ROLE_EQUAL) &&
-        r->role != htonl(OFPCR12_ROLE_MASTER) &&
-        r->role != htonl(OFPCR12_ROLE_SLAVE)) {
+        r->role != htonl(OFPCR12_ROLE_PRIMARY) &&
+        r->role != htonl(OFPCR12_ROLE_SECONDARY)) {
         return OFPERR_OFPRRFC_BAD_ROLE;
     }
 
@@ -218,8 +218,8 @@ ofputil_format_role_status(struct ds *string,
     ds_put_cstr(string, " reason=");
 
     switch (rs->reason) {
-    case OFPCRR_MASTER_REQUEST:
-        ds_put_cstr(string, "master_request");
+    case OFPCRR_PRIMARY_REQUEST:
+        ds_put_cstr(string, "primary_request");
         break;
     case OFPCRR_CONFIG:
         ds_put_cstr(string, "configuration_changed");
@@ -254,13 +254,13 @@ ofputil_async_msg_type_to_string(enum ofputil_async_msg_type type)
 struct ofp14_async_prop {
     uint64_t prop_type;
     enum ofputil_async_msg_type oam;
-    bool master;
+    bool primary;
     uint32_t allowed10, allowed14;
 };
 
-#define AP_PAIR(SLAVE_PROP_TYPE, OAM, A10, A14) \
-    { SLAVE_PROP_TYPE,       OAM, false, A10, (A14) ? (A14) : (A10) },  \
-    { (SLAVE_PROP_TYPE + 1), OAM, true,  A10, (A14) ? (A14) : (A10) }
+#define AP_PAIR(SECONDARY_PROP_TYPE, OAM, A10, A14) \
+    { SECONDARY_PROP_TYPE,       OAM, false, A10, (A14) ? (A14) : (A10) },  \
+    { (SECONDARY_PROP_TYPE + 1), OAM, true,  A10, (A14) ? (A14) : (A10) }
 
 static const struct ofp14_async_prop async_props[] = {
     AP_PAIR( 0, OAM_PACKET_IN,      OFPR10_BITS, OFPR14_BITS),
@@ -288,10 +288,10 @@ get_ofp14_async_config_prop_by_prop_type(uint64_t prop_type)
 
 static const struct ofp14_async_prop *
 get_ofp14_async_config_prop_by_oam(enum ofputil_async_msg_type oam,
-                                   bool master)
+                                   bool primary)
 {
     FOR_EACH_ASYNC_PROP (ap) {
-        if (ap->oam == oam && ap->master == master) {
+        if (ap->oam == oam && ap->primary == primary) {
             return ap;
         }
     }
@@ -310,7 +310,9 @@ encode_async_mask(const struct ofputil_async_cfg *src,
                   const struct ofp14_async_prop *ap,
                   enum ofp_version version)
 {
-    uint32_t mask = ap->master ? src->master[ap->oam] : src->slave[ap->oam];
+    uint32_t mask = (ap->primary
+                     ? src->primary[ap->oam]
+                     : src->secondary[ap->oam]);
     return htonl(mask & ofp14_async_prop_allowed(ap, version));
 }
 
@@ -342,7 +344,7 @@ decode_async_mask(ovs_be32 src,
         }
     }
 
-    uint32_t *array = ap->master ? dst->master : dst->slave;
+    uint32_t *array = ap->primary ? dst->primary : dst->secondary;
     array[ap->oam] = mask;
     return 0;
 }
@@ -362,20 +364,20 @@ parse_async_tlv(const struct ofpbuf *property,
     }
 
     if (ofpprop_is_experimenter(ap->prop_type)) {
-        /* For experimenter properties, whether a property is for the master or
-         * slave role is indicated by both 'type' and 'exp_type' in struct
+        /* For experimenter properties, whether a property is for the primary or
+         * secondary role is indicated by both 'type' and 'exp_type' in struct
          * ofp_prop_experimenter.  Check that these are consistent. */
         const struct ofp_prop_experimenter *ope = property->data;
-        bool should_be_master = ope->type == htons(0xffff);
-        if (should_be_master != ap->master) {
+        bool should_be_primary = ope->type == htons(0xffff);
+        if (should_be_primary != ap->primary) {
             static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(1, 5);
             VLOG_WARN_RL(&rl, "async property type %#"PRIx16" "
                          "indicates %s role but exp_type %"PRIu32" indicates "
                          "%s role",
                          ntohs(ope->type),
-                         should_be_master ? "master" : "slave",
+                         should_be_primary ? "primary" : "secondary",
                          ntohl(ope->exp_type),
-                         ap->master ? "master" : "slave");
+                         ap->primary ? "primary" : "secondary");
             return OFPERR_OFPBPC_BAD_EXP_TYPE;
         }
     }
@@ -390,9 +392,9 @@ decode_legacy_async_masks(const ovs_be32 masks[2],
                           struct ofputil_async_cfg *dst)
 {
     for (int i = 0; i < 2; i++) {
-        bool master = i == 0;
+        bool primary = i == 0;
         const struct ofp14_async_prop *ap
-            = get_ofp14_async_config_prop_by_oam(oam, master);
+            = get_ofp14_async_config_prop_by_oam(oam, primary);
         decode_async_mask(masks[i], ap, version, true, dst);
     }
 }
@@ -479,9 +481,9 @@ encode_legacy_async_masks(const struct ofputil_async_cfg *ac,
                           ovs_be32 masks[2])
 {
     for (int i = 0; i < 2; i++) {
-        bool master = i == 0;
+        bool primary = i == 0;
         const struct ofp14_async_prop *ap
-            = get_ofp14_async_config_prop_by_oam(oam, master);
+            = get_ofp14_async_config_prop_by_oam(oam, primary);
         masks[i] = encode_async_mask(ac, ap, version);
     }
 }
@@ -507,11 +509,11 @@ ofputil_put_async_config__(const struct ofputil_async_cfg *ac,
                                  encode_async_mask(ac, ap, version));
 
                 /* For experimenter properties, we need to use type 0xfffe for
-                 * master and 0xffff for slaves. */
+                 * primary and 0xffff for secondaries. */
                 if (ofpprop_is_experimenter(ap->prop_type)) {
                     struct ofp_prop_experimenter *ope
                         = ofpbuf_at_assert(buf, ofs, sizeof *ope);
-                    ope->type = ap->master ? htons(0xffff) : htons(0xfffe);
+                    ope->type = ap->primary ? htons(0xffff) : htons(0xfffe);
                 }
             }
         }
@@ -592,8 +594,8 @@ ofp_role_reason_to_string(enum ofp14_controller_role_reason reason,
                           char *reasonbuf, size_t bufsize)
 {
     switch (reason) {
-    case OFPCRR_MASTER_REQUEST:
-        return "master_request";
+    case OFPCRR_PRIMARY_REQUEST:
+        return "primary_request";
 
     case OFPCRR_CONFIG:
         return "configuration_changed";
@@ -664,12 +666,12 @@ ofputil_format_set_async_config(struct ds *string,
                                 const struct ofputil_async_cfg *ac)
 {
     for (int i = 0; i < 2; i++) {
-        ds_put_format(string, "\n %s:\n", i == 0 ? "master" : "slave");
+        ds_put_format(string, "\n %s:\n", i == 0 ? "primary" : "secondary");
         for (uint32_t type = 0; type < OAM_N_TYPES; type++) {
             ds_put_format(string, "%16s:",
                           ofputil_async_msg_type_to_string(type));
 
-            uint32_t role = i == 0 ? ac->master[type] : ac->slave[type];
+            uint32_t role = i == 0 ? ac->primary[type] : ac->secondary[type];
             for (int j = 0; j < 32; j++) {
                 if (role & (1u << j)) {
                     char reasonbuf[INT_STRLEN(int) + 1];
@@ -705,17 +707,17 @@ ofputil_async_cfg_default(enum ofp_version version)
     }
 
     struct ofputil_async_cfg oac = {
-        .master[OAM_PACKET_IN] = pin,
-        .master[OAM_PORT_STATUS] = OFPPR_BITS,
-        .slave[OAM_PORT_STATUS] = OFPPR_BITS
+        .primary[OAM_PACKET_IN] = pin,
+        .primary[OAM_PORT_STATUS] = OFPPR_BITS,
+        .secondary[OAM_PORT_STATUS] = OFPPR_BITS
     };
 
     if (version >= OFP14_VERSION) {
-        oac.master[OAM_FLOW_REMOVED] = OFPRR14_BITS;
+        oac.primary[OAM_FLOW_REMOVED] = OFPRR14_BITS;
     } else if (version == OFP13_VERSION) {
-        oac.master[OAM_FLOW_REMOVED] = OFPRR13_BITS;
+        oac.primary[OAM_FLOW_REMOVED] = OFPRR13_BITS;
     } else {
-        oac.master[OAM_FLOW_REMOVED] = OFPRR10_BITS;
+        oac.primary[OAM_FLOW_REMOVED] = OFPRR10_BITS;
     }
 
     return oac;

ofproto/connmgr.c
@@ -212,9 +212,9 @@ struct connmgr {
      * traversals from other threads can be made safe by holding the
      * ofproto_mutex.*/
     struct ovs_list conns;       /* All ofconns. */
-    uint64_t master_election_id; /* monotonically increasing sequence number
-                                  * for master election */
-    bool master_election_id_defined;
+    uint64_t primary_election_id; /* monotonically increasing sequence number
+                                   * for primary election */
+    bool primary_election_id_defined;
 
     /* OpenFlow connection establishment. */
     struct hmap services;       /* Contains "struct ofservice"s. */
@@ -253,8 +253,8 @@ connmgr_create(struct ofproto *ofproto,
     mgr->local_port_name = xstrdup(local_port_name);
 
     ovs_list_init(&mgr->conns);
-    mgr->master_election_id = 0;
-    mgr->master_election_id_defined = false;
+    mgr->primary_election_id = 0;
+    mgr->primary_election_id_defined = false;
 
     hmap_init(&mgr->services);
     mgr->snoops = NULL;
@@ -773,11 +773,11 @@ snoop_preference(const struct ofservice *ofservice)
     }
 
     switch (ofconn->role) {
-    case OFPCR12_ROLE_MASTER:
+    case OFPCR12_ROLE_PRIMARY:
         return 3;
     case OFPCR12_ROLE_EQUAL:
         return 2;
-    case OFPCR12_ROLE_SLAVE:
+    case OFPCR12_ROLE_SECONDARY:
         return 1;
     case OFPCR12_ROLE_NOCHANGE:
     default:
@@ -818,33 +818,33 @@ ofconn_get_type(const struct ofconn *ofconn)
     return ofconn->type;
 }
 
-/* If a master election id is defined, stores it into '*idp' and returns
+/* If a primary election id is defined, stores it into '*idp' and returns
  * true.  Otherwise, stores UINT64_MAX into '*idp' and returns false. */
 bool
-ofconn_get_master_election_id(const struct ofconn *ofconn, uint64_t *idp)
+ofconn_get_primary_election_id(const struct ofconn *ofconn, uint64_t *idp)
 {
-    *idp = (ofconn->connmgr->master_election_id_defined
-            ? ofconn->connmgr->master_election_id
+    *idp = (ofconn->connmgr->primary_election_id_defined
+            ? ofconn->connmgr->primary_election_id
             : UINT64_MAX);
-    return ofconn->connmgr->master_election_id_defined;
+    return ofconn->connmgr->primary_election_id_defined;
 }
 
-/* Sets the master election id.
+/* Sets the primary election id.
  *
  * Returns true if successful, false if the id is stale
  */
 bool
-ofconn_set_master_election_id(struct ofconn *ofconn, uint64_t id)
+ofconn_set_primary_election_id(struct ofconn *ofconn, uint64_t id)
 {
-    if (ofconn->connmgr->master_election_id_defined
+    if (ofconn->connmgr->primary_election_id_defined
         &&
         /* Unsigned difference interpreted as a two's complement signed
          * value */
-        (int64_t)(id - ofconn->connmgr->master_election_id) < 0) {
+        (int64_t)(id - ofconn->connmgr->primary_election_id) < 0) {
         return false;
     }
-    ofconn->connmgr->master_election_id = id;
-    ofconn->connmgr->master_election_id_defined = true;
+    ofconn->connmgr->primary_election_id = id;
+    ofconn->connmgr->primary_election_id_defined = true;
 
     return true;
 }
@@ -864,7 +864,7 @@ ofconn_send_role_status(struct ofconn *ofconn, uint32_t role, uint8_t reason)
     struct ofputil_role_status status;
     status.reason = reason;
     status.role = role;
-    ofconn_get_master_election_id(ofconn, &status.generation_id);
+    ofconn_get_primary_election_id(ofconn, &status.generation_id);
 
     struct ofpbuf *buf
         = ofputil_encode_role_status(&status, ofconn_get_protocol(ofconn));
@@ -873,19 +873,19 @@ ofconn_send_role_status(struct ofconn *ofconn, uint32_t role, uint8_t reason)
     }
 }
 
-/* Changes 'ofconn''s role to 'role'.  If 'role' is OFPCR12_ROLE_MASTER then
- * any existing master is demoted to a slave. */
+/* Changes 'ofconn''s role to 'role'.  If 'role' is OFPCR12_ROLE_PRIMARY then
+ * any existing primary is demoted to a secondary. */
 void
 ofconn_set_role(struct ofconn *ofconn, enum ofp12_controller_role role)
 {
-    if (role != ofconn->role && role == OFPCR12_ROLE_MASTER) {
+    if (role != ofconn->role && role == OFPCR12_ROLE_PRIMARY) {
         struct ofconn *other;
 
         LIST_FOR_EACH (other, connmgr_node, &ofconn->connmgr->conns) {
-            if (other->role == OFPCR12_ROLE_MASTER) {
-                other->role = OFPCR12_ROLE_SLAVE;
-                ofconn_send_role_status(other, OFPCR12_ROLE_SLAVE,
-                                        OFPCRR_MASTER_REQUEST);
+            if (other->role == OFPCR12_ROLE_PRIMARY) {
+                other->role = OFPCR12_ROLE_SECONDARY;
+                ofconn_send_role_status(other, OFPCR12_ROLE_SECONDARY,
+                                        OFPCRR_PRIMARY_REQUEST);
             }
         }
     }
@@ -898,9 +898,9 @@ ofconn_set_invalid_ttl_to_controller(struct ofconn *ofconn, bool enable)
     struct ofputil_async_cfg ac = ofconn_get_async_config(ofconn);
     uint32_t bit = 1u << OFPR_INVALID_TTL;
     if (enable) {
-        ac.master[OAM_PACKET_IN] |= bit;
+        ac.primary[OAM_PACKET_IN] |= bit;
     } else {
-        ac.master[OAM_PACKET_IN] &= ~bit;
+        ac.primary[OAM_PACKET_IN] &= ~bit;
     }
     ofconn_set_async_config(ofconn, &ac);
 }
@@ -910,7 +910,7 @@ ofconn_get_invalid_ttl_to_controller(struct ofconn *ofconn)
 {
     struct ofputil_async_cfg ac = ofconn_get_async_config(ofconn);
     uint32_t bit = 1u << OFPR_INVALID_TTL;
-    return (ac.master[OAM_PACKET_IN] & bit) != 0;
+    return (ac.primary[OAM_PACKET_IN] & bit) != 0;
 }
 
 /* Returns the currently configured protocol for 'ofconn', one of OFPUTIL_P_*.
@@ -1002,11 +1002,11 @@ ofconn_set_async_config(struct ofconn *ofconn,
 
     if (ofputil_protocol_to_ofp_version(ofconn_get_protocol(ofconn))
         < OFP14_VERSION) {
-        if (ofconn->async_cfg->master[OAM_PACKET_IN] & (1u << OFPR_ACTION)) {
-            ofconn->async_cfg->master[OAM_PACKET_IN] |= OFPR14_ACTION_BITS;
+        if (ofconn->async_cfg->primary[OAM_PACKET_IN] & (1u << OFPR_ACTION)) {
+            ofconn->async_cfg->primary[OAM_PACKET_IN] |= OFPR14_ACTION_BITS;
         }
-        if (ofconn->async_cfg->slave[OAM_PACKET_IN] & (1u << OFPR_ACTION)) {
-            ofconn->async_cfg->slave[OAM_PACKET_IN] |= OFPR14_ACTION_BITS;
+        if (ofconn->async_cfg->secondary[OAM_PACKET_IN] & (1u << OFPR_ACTION)) {
+            ofconn->async_cfg->secondary[OAM_PACKET_IN] |= OFPR14_ACTION_BITS;
         }
     }
 }
@@ -1441,9 +1441,9 @@ ofconn_receives_async_msg(const struct ofconn *ofconn,
     }
 
     struct ofputil_async_cfg ac = ofconn_get_async_config(ofconn);
-    uint32_t *masks = (ofconn->role == OFPCR12_ROLE_SLAVE
-                       ? ac.slave
-                       : ac.master);
+    uint32_t *masks = (ofconn->role == OFPCR12_ROLE_SECONDARY
+                       ? ac.secondary
+                       : ac.primary);
     return (masks[type] & (1u << reason)) != 0;
 }
 

commit 8cfb5a5e8c49c388ca44dc4d7debea2454d80c54

bridge: Use correct (legacy) role names in database.
The vswitchd database schema requires role names to be "master" or
"slave", but this code tried to use "primary" and "secondary".

Signed-off-by: Ben Pfaff <blp@ovn.org>
Reported-at: https://github.com/openvswitch/ovs-issues/issues/218
Fixes: 807152a4ddfb ("Use primary/secondary, not master/slave, as names for OpenFlow roles.")
Signed-off-by: 0-day Robot <robot@bytheb.org>

vswitchd/bridge.c
@@ -3019,9 +3019,9 @@ ofp12_controller_role_to_str(enum ofp12_controller_role role)
     case OFPCR12_ROLE_EQUAL:
         return "other";
     case OFPCR12_ROLE_PRIMARY:
-        return "primary";
+        return "master";
     case OFPCR12_ROLE_SECONDARY:
-        return "secondary";
+        return "slave";
     case OFPCR12_ROLE_NOCHANGE:
     default:
         return NULL;