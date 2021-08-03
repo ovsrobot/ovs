ovsrobot/ovs

commit abb807e27dd4b1449429eb813858ef237665fbfd

dpif-netdev: Add command to switch dpif implementation.
This commit adds a new command to allow the user to switch
the active DPIF implementation at runtime. A probe function
is executed before switching the DPIF implementation, to ensure
the CPU is capable of running the ISA required. For example, the
below code will switch to the AVX512 enabled DPIF assuming
that the runtime CPU is capable of running AVX512 instructions:

 $ ovs-appctl dpif-netdev/dpif-impl-set dpif_avx512

A new configuration flag is added to allow selection of the
default DPIF. This is useful for running the unit-tests against
the available DPIF implementations, without modifying each unit test.

The design of the testing & validation for ISA optimized DPIF
implementations is based around the work already upstream for DPCLS.
Note however that a DPCLS lookup has no state or side-effects, allowing
the auto-validator implementation to perform multiple lookups and
provide consistent statistic counters.

The DPIF component does have state, so running two implementations in
parallel and comparing output is not a valid testing method, as there
are changes in DPIF statistic counters (side effects). As a result, the
DPIF is tested directly against the unit-tests.

Signed-off-by: Harry van Haaren <harry.van.haaren@intel.com>
Co-authored-by: Cian Ferriter <cian.ferriter@intel.com>
Signed-off-by: Cian Ferriter <cian.ferriter@intel.com>
Acked-by: Flavio Leitner <fbl@sysclose.org>
Signed-off-by: Ian Stokes <ian.stokes@intel.com>

acinclude.m4
@@ -30,6 +30,21 @@ AC_DEFUN([OVS_CHECK_DPCLS_AUTOVALIDATOR], [
   fi
 ])
 
+dnl Set OVS DPIF default implementation at configure time for running the unit
+dnl tests on the whole codebase without modifying tests per DPIF impl
+AC_DEFUN([OVS_CHECK_DPIF_AVX512_DEFAULT], [
+  AC_ARG_ENABLE([dpif-default-avx512],
+                [AC_HELP_STRING([--enable-dpif-default-avx512], [Enable DPIF AVX512 implementation as default.])],
+                [dpifavx512=yes],[dpifavx512=no])
+  AC_MSG_CHECKING([whether DPIF AVX512 is default implementation])
+  if test "$dpifavx512" != yes; then
+    AC_MSG_RESULT([no])
+  else
+    OVS_CFLAGS="$OVS_CFLAGS -DDPIF_AVX512_DEFAULT"
+    AC_MSG_RESULT([yes])
+  fi
+])
+
 dnl OVS_ENABLE_WERROR
 AC_DEFUN([OVS_ENABLE_WERROR],
   [AC_ARG_ENABLE(

configure.ac
@@ -185,6 +185,7 @@ OVS_ENABLE_WERROR
 OVS_ENABLE_SPARSE
 OVS_CTAGS_IDENTIFIERS
 OVS_CHECK_DPCLS_AUTOVALIDATOR
+OVS_CHECK_DPIF_AVX512_DEFAULT
 OVS_CHECK_BINUTILS_AVX512
 
 AC_ARG_VAR(KARCH, [Kernel Architecture String])

lib/automake.mk
@@ -116,6 +116,7 @@ lib_libopenvswitch_la_SOURCES = \
 	lib/dpif-netdev-private-dfc.c \
 	lib/dpif-netdev-private-dfc.h \
 	lib/dpif-netdev-private-dpcls.h \
+	lib/dpif-netdev-private-dpif.c \
 	lib/dpif-netdev-private-dpif.h \
 	lib/dpif-netdev-private-flow.h \
 	lib/dpif-netdev-private-thread.h \

lib/dpif-netdev-avx512.c
@@ -24,6 +24,7 @@
 #include "dpif-netdev-perf.h"
 #include "dpif-netdev-private.h"
 
+#include <errno.h>
 #include <immintrin.h>
 
 #include "dp-packet.h"
@@ -56,6 +57,19 @@ struct dpif_userdata {
     OVS_ALIGNED_VAR(CACHE_LINE_SIZE)
         struct pkt_flow_meta pkt_meta[NETDEV_MAX_BURST];
 };
+
+int32_t
+dp_netdev_input_outer_avx512_probe(void)
+{
+    bool avx512f_available = dpdk_get_cpu_has_isa("x86_64", "avx512f");
+    bool bmi2_available = dpdk_get_cpu_has_isa("x86_64", "bmi2");
+
+    if (!avx512f_available || !bmi2_available) {
+        return -ENOTSUP;
+    }
+
+    return 0;
+}
 
 int32_t
 dp_netdev_input_outer_avx512(struct dp_netdev_pmd_thread *pmd,

lib/dpif-netdev-private-dpif.c
@@ -1,0 +1,124 @@
+/*
+ * Copyright (c) 2021 Intel Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <config.h>
+
+#include "dpif-netdev-private-dpif.h"
+#include "dpif-netdev-private-thread.h"
+
+#include <errno.h>
+#include <string.h>
+
+#include "openvswitch/dynamic-string.h"
+#include "openvswitch/vlog.h"
+#include "util.h"
+
+VLOG_DEFINE_THIS_MODULE(dpif_netdev_impl);
+
+enum dpif_netdev_impl_info_idx {
+    DPIF_NETDEV_IMPL_SCALAR,
+    DPIF_NETDEV_IMPL_AVX512
+};
+
+/* Actual list of implementations goes here. */
+static struct dpif_netdev_impl_info_t dpif_impls[] = {
+    /* The default scalar C code implementation. */
+    [DPIF_NETDEV_IMPL_SCALAR] = { .input_func = dp_netdev_input,
+      .probe = NULL,
+      .name = "dpif_scalar", },
+
+#if (__x86_64__ && HAVE_AVX512F && HAVE_LD_AVX512_GOOD && __SSE4_2__)
+    /* Only available on x86_64 bit builds with SSE 4.2 used for OVS core. */
+    [DPIF_NETDEV_IMPL_AVX512] = { .input_func = dp_netdev_input_outer_avx512,
+      .probe = dp_netdev_input_outer_avx512_probe,
+      .name = "dpif_avx512", },
+#endif
+};
+
+static dp_netdev_input_func default_dpif_func;
+
+dp_netdev_input_func
+dp_netdev_impl_get_default(void)
+{
+    /* For the first call, this will be NULL. Compute the compile time default.
+     */
+    if (!default_dpif_func) {
+        int dpif_idx = DPIF_NETDEV_IMPL_SCALAR;
+
+/* Configure-time overriding to run test suite on all implementations. */
+#if (__x86_64__ && HAVE_AVX512F && HAVE_LD_AVX512_GOOD && __SSE4_2__)
+#ifdef DPIF_AVX512_DEFAULT
+        dp_netdev_input_func_probe probe;
+
+        /* Check if the compiled default is compatible. */
+        probe = dpif_impls[DPIF_NETDEV_IMPL_AVX512].probe;
+        if (!probe || !probe()) {
+            dpif_idx = DPIF_NETDEV_IMPL_AVX512;
+        }
+#endif
+#endif
+
+        VLOG_INFO("Default DPIF implementation is %s.\n",
+                  dpif_impls[dpif_idx].name);
+        default_dpif_func = dpif_impls[dpif_idx].input_func;
+    }
+
+    return default_dpif_func;
+}
+
+/* This function checks all available DPIF implementations, and selects the
+ * returns the function pointer to the one requested by "name".
+ */
+static int32_t
+dp_netdev_impl_get_by_name(const char *name, dp_netdev_input_func *out_func)
+{
+    ovs_assert(name);
+    ovs_assert(out_func);
+
+    uint32_t i;
+
+    for (i = 0; i < ARRAY_SIZE(dpif_impls); i++) {
+        if (strcmp(dpif_impls[i].name, name) == 0) {
+            /* Probe function is optional - so check it is set before exec. */
+            if (dpif_impls[i].probe) {
+                int probe_err = dpif_impls[i].probe();
+                if (probe_err) {
+                    *out_func = NULL;
+                    return probe_err;
+                }
+            }
+            *out_func = dpif_impls[i].input_func;
+            return 0;
+        }
+    }
+
+    return -EINVAL;
+}
+
+int32_t
+dp_netdev_impl_set_default_by_name(const char *name)
+{
+    dp_netdev_input_func new_default;
+
+    int32_t err = dp_netdev_impl_get_by_name(name, &new_default);
+
+    if (!err) {
+        default_dpif_func = new_default;
+    }
+
+    return err;
+
+}

lib/dpif-netdev-private-dpif.h
@@ -23,7 +23,48 @@
 struct dp_netdev_pmd_thread;
 struct dp_packet_batch;
 
+/* Typedef for DPIF functions.
+ * Returns whether all packets were processed successfully.
+ */
+typedef int32_t (*dp_netdev_input_func)(struct dp_netdev_pmd_thread *pmd,
+                                        struct dp_packet_batch *packets,
+                                        odp_port_t port_no);
+
+/* Probe a DPIF implementation. This allows the implementation to validate CPU
+ * ISA availability. Returns -ENOTSUP if not available, returns 0 if valid to
+ * use.
+ */
+typedef int32_t (*dp_netdev_input_func_probe)(void);
+
+/* Structure describing each available DPIF implementation. */
+struct dpif_netdev_impl_info_t {
+    /* Function pointer to execute to have this DPIF implementation run. */
+    dp_netdev_input_func input_func;
+    /* Function pointer to execute to check the CPU ISA is available to run. If
+     * not necessary, it must be set to NULL which implies that it is always
+     * valid to use. */
+    dp_netdev_input_func_probe probe;
+    /* Name used to select this DPIF implementation. */
+    const char *name;
+};
+
+/* Returns the default DPIF which is first ./configure selected, but can be
+ * overridden at runtime. */
+dp_netdev_input_func dp_netdev_impl_get_default(void);
+
+/* Overrides the default DPIF with the user set DPIF. */
+int32_t dp_netdev_impl_set_default_by_name(const char *name);
+
 /* Available DPIF implementations below. */
+int32_t
+dp_netdev_input(struct dp_netdev_pmd_thread *pmd,
+                struct dp_packet_batch *packets,
+                odp_port_t in_port);
+
+/* AVX512 enabled DPIF implementation and probe functions. */
+int32_t
+dp_netdev_input_outer_avx512_probe(void);
+
 int32_t
 dp_netdev_input_outer_avx512(struct dp_netdev_pmd_thread *pmd,
                              struct dp_packet_batch *packets,

lib/dpif-netdev-private-thread.h
@@ -50,16 +50,6 @@ struct dp_netdev_pmd_thread_ctx {
     bool smc_enable_db;
 };
 
-/* Forward declaration for typedef. */
-struct dp_netdev_pmd_thread;
-
-/* Typedef for DPIF functions.
- * Returns a bitmask of packets to handle, possibly including upcall/misses.
- */
-typedef int32_t (*dp_netdev_input_func)(struct dp_netdev_pmd_thread *pmd,
-                                        struct dp_packet_batch *packets,
-                                        odp_port_t port_no);
-
 /* PMD: Poll modes drivers.  PMD accesses devices via polling to eliminate
  * the performance overhead of interrupt processing.  Therefore netdev can
  * not implement rx-wait for these devices.  dpif-netdev needs to poll

lib/dpif-netdev-unixctl.man
@@ -226,3 +226,6 @@ recirculation (only in balance-tcp mode).
 When this is the case, the above command prints the load-balancing information
 of the bonds configured in datapath \fIdp\fR showing the interface associated
 with each bucket (hash).
+.
+.IP "\fBdpif-netdev/dpif-impl-set\fR \fIdpif_impl\fR"
+Sets the DPIF to be used to \fIdpif_impl\fR. By default "dpif_scalar" is used.

commit a436d2afbe199b9197e76111fa971a87fc3eaf0e

acinclude: Don't set AVX512-related configuration via CFLAGS.
The correct way to pass configuration options is to define them
inside the config.h.  Additionally, few long lines wrapped and
fixed the unnecessary double check for -mavx512f.

Fixes: abb807e27dd4 ("dpif-netdev: Add command to switch dpif implementation.")
Fixes: 5324b54e606a ("dpif-netdev: Add configure to enable autovalidator at build time.")
Fixes: e90e115a01af ("dpif-netdev: implement subtable lookup validation.")
Fixes: 352b6c7116cd ("dpif-lookup: add avx512 gather implementation.")
Signed-off-by: Ilya Maximets <i.maximets@ovn.org>
Signed-off-by: 0-day Robot <robot@bytheb.org>

acinclude.m4
@@ -19,13 +19,16 @@ dnl This enables automatically running all unit tests with all MFEX
 dnl implementations.
 AC_DEFUN([OVS_CHECK_MFEX_AUTOVALIDATOR], [
   AC_ARG_ENABLE([mfex-default-autovalidator],
-                [AC_HELP_STRING([--enable-mfex-default-autovalidator], [Enable MFEX autovalidator as default miniflow_extract implementation.])],
+                [AC_HELP_STRING([--enable-mfex-default-autovalidator],
+                                [Enable MFEX autovalidator as default
+                                 miniflow_extract implementation.])],
                 [autovalidator=yes],[autovalidator=no])
   AC_MSG_CHECKING([whether MFEX Autovalidator is default implementation])
   if test "$autovalidator" != yes; then
     AC_MSG_RESULT([no])
   else
-    OVS_CFLAGS="$OVS_CFLAGS -DMFEX_AUTOVALIDATOR_DEFAULT"
+    AC_DEFINE([MFEX_AUTOVALIDATOR_DEFAULT], [1],
+              [Autovalidator for miniflow_extract is a default implementation.])
     AC_MSG_RESULT([yes])
   fi
 ])
@@ -35,13 +38,17 @@ dnl This enables automatically running all unit tests with all DPCLS
 dnl implementations.
 AC_DEFUN([OVS_CHECK_DPCLS_AUTOVALIDATOR], [
   AC_ARG_ENABLE([autovalidator],
-                [AC_HELP_STRING([--enable-autovalidator], [Enable DPCLS autovalidator as default subtable search implementation.])],
+                [AC_HELP_STRING([--enable-autovalidator],
+                                [Enable DPCLS autovalidator as default subtable
+                                 search implementation.])],
                 [autovalidator=yes],[autovalidator=no])
   AC_MSG_CHECKING([whether DPCLS Autovalidator is default implementation])
   if test "$autovalidator" != yes; then
     AC_MSG_RESULT([no])
   else
-    OVS_CFLAGS="$OVS_CFLAGS -DDPCLS_AUTOVALIDATOR_DEFAULT"
+    AC_DEFINE([DPCLS_AUTOVALIDATOR_DEFAULT], [1],
+              [Autovalidator for the userspace datapath classifier is a
+               default implementation.])
     AC_MSG_RESULT([yes])
   fi
 ])
@@ -50,14 +57,31 @@ dnl Set OVS DPIF default implementation at configure time for running the unit
 dnl tests on the whole codebase without modifying tests per DPIF impl
 AC_DEFUN([OVS_CHECK_DPIF_AVX512_DEFAULT], [
   AC_ARG_ENABLE([dpif-default-avx512],
-                [AC_HELP_STRING([--enable-dpif-default-avx512], [Enable DPIF AVX512 implementation as default.])],
+                [AC_HELP_STRING([--enable-dpif-default-avx512],
+                                [Enable DPIF AVX512 implementation as default.])],
                 [dpifavx512=yes],[dpifavx512=no])
   AC_MSG_CHECKING([whether DPIF AVX512 is default implementation])
   if test "$dpifavx512" != yes; then
     AC_MSG_RESULT([no])
   else
-    OVS_CFLAGS="$OVS_CFLAGS -DDPIF_AVX512_DEFAULT"
+    AC_DEFINE([DPIF_AVX512_DEFAULT], [1],
+              [DPIF AVX512 is a default implementation of the userspace
+               datapath interface.])
     AC_MSG_RESULT([yes])
+  fi
+])
+
+dnl OVS_CHECK_AVX512
+dnl
+dnl Checks if compiler and binutils supports AVX512.
+AC_DEFUN([OVS_CHECK_AVX512], [
+  OVS_CHECK_BINUTILS_AVX512
+  OVS_CHECK_CC_OPTION(
+    [-mavx512f], [ovs_have_cc_mavx512f=yes], [ovs_have_cc_mavx512f=no])
+  AM_CONDITIONAL([HAVE_AVX512F], [test $ovs_have_cc_mavx512f = yes])
+  if test "$ovs_have_cc_mavx512f" = yes; then
+    AC_DEFINE([HAVE_AVX512F], [1],
+              [Define to 1 if compiler supports AVX512.])
   fi
 ])
 

configure.ac
@@ -179,15 +179,13 @@ OVS_ENABLE_OPTION([-Wno-null-pointer-arithmetic])
 OVS_ENABLE_OPTION([-Warray-bounds-pointer-arithmetic])
 OVS_CONDITIONAL_CC_OPTION([-Wno-unused], [HAVE_WNO_UNUSED])
 OVS_CONDITIONAL_CC_OPTION([-Wno-unused-parameter], [HAVE_WNO_UNUSED_PARAMETER])
-OVS_CONDITIONAL_CC_OPTION([-mavx512f], [HAVE_AVX512F])
-OVS_CHECK_CC_OPTION([-mavx512f], [CFLAGS="$CFLAGS -DHAVE_AVX512F"])
 OVS_ENABLE_WERROR
 OVS_ENABLE_SPARSE
 OVS_CTAGS_IDENTIFIERS
 OVS_CHECK_DPCLS_AUTOVALIDATOR
 OVS_CHECK_DPIF_AVX512_DEFAULT
 OVS_CHECK_MFEX_AUTOVALIDATOR
-OVS_CHECK_BINUTILS_AVX512
+OVS_CHECK_AVX512
 
 AC_ARG_VAR(KARCH, [Kernel Architecture String])
 AC_SUBST(KARCH)

m4/openvswitch.m4
@@ -412,7 +412,6 @@ AC_DEFUN([OVS_CHECK_BINUTILS_AVX512],
      if ($CC -dumpmachine | grep x86_64) >/dev/null 2>&1; then
        if (objdump -d  --no-show-raw-insn $OBJFILE | grep -q $GATHER_PARAMS) >/dev/null 2>&1; then
          ovs_cv_binutils_avx512_good=yes
-         CFLAGS="$CFLAGS -DHAVE_LD_AVX512_GOOD"
        else
          ovs_cv_binutils_avx512_good=no
          dnl Explicitly disallow avx512f to stop compiler auto-vectorizing
@@ -424,6 +423,10 @@ AC_DEFUN([OVS_CHECK_BINUTILS_AVX512],
        ovs_cv_binutils_avx512_good=no
      fi])
      rm $OBJFILE
+   if test "$ovs_cv_binutils_avx512_good" = yes; then
+     AC_DEFINE([HAVE_LD_AVX512_GOOD], [1],
+               [Define to 1 if binutils correctly supports AVX512.])
+   fi
    AM_CONDITIONAL([HAVE_LD_AVX512_GOOD],
                   [test "$ovs_cv_binutils_avx512_good" = yes])])
 