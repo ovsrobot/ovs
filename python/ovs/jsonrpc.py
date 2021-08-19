a6d098d06d9dc0b4c3da188dd3db878bbdb78638
Renat Nurgaliyev <impleman@gmail.com>
2021-08-19 14:17:58 +0200

    python: Set probe interval before the connection is open
    Setting probe interval before calling Reconnect.enable() ensures
    that initial OVSDB connection will not use the default probe interval
    of 5000 ms, as it may cause connection problems to large databases.
    
    Signed-off-by: Renat Nurgaliyev <impleman@gmail.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- python/ovs/jsonrpc.py
+++ python/ovs/jsonrpc.py
@@ -408,15 +408,16 @@ class Session(object):
         reconnect = ovs.reconnect.Reconnect(ovs.timeval.msec())
         session = Session(reconnect, None, remotes)
         session.pick_remote()
-        reconnect.enable(ovs.timeval.msec())
-        reconnect.set_backoff_free_tries(len(remotes))
-        if ovs.stream.PassiveStream.is_valid_name(reconnect.get_name()):
-            reconnect.set_passive(True, ovs.timeval.msec())
 
         if not ovs.stream.stream_or_pstream_needs_probes(reconnect.get_name()):
             reconnect.set_probe_interval(0)
         elif probe_interval is not None:
             reconnect.set_probe_interval(probe_interval)
+
+        reconnect.enable(ovs.timeval.msec())
+        reconnect.set_backoff_free_tries(len(remotes))
+        if ovs.stream.PassiveStream.is_valid_name(reconnect.get_name()):
+            reconnect.set_passive(True, ovs.timeval.msec())
 
         return session
 
