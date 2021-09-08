318cb4bac61708db1e480014656120e15e0bf7e5
Gaetan Rivet <grive@u256.net>
2021-09-08 11:47:26 +0200

    dpif-netdev: Rename flow offload thread
    ovs_strlcpy silently fails to copy the thread name if it is too long.
    Rename the flow offload thread to differentiate it from the main thread.
    
    Fixes: 02bb2824e51d ("dpif-netdev: do hw flow offload in a thread")
    Signed-off-by: Gaetan Rivet <grive@u256.net>
    Reviewed-by: Maxime Coquelin <maxime.coquelin@redhat.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/dpif-netdev.c
+++ lib/dpif-netdev.c
@@ -2706,8 +2706,7 @@ queue_netdev_flow_del(struct dp_netdev_pmd_thread *pmd,
 
     if (ovsthread_once_start(&offload_thread_once)) {
         xpthread_cond_init(&dp_flow_offload.cond, NULL);
-        ovs_thread_create("dp_netdev_flow_offload",
-                          dp_netdev_flow_offload_main, NULL);
+        ovs_thread_create("hw_offload", dp_netdev_flow_offload_main, NULL);
         ovsthread_once_done(&offload_thread_once);
     }
 
@@ -2795,8 +2794,7 @@ queue_netdev_flow_put(struct dp_netdev_pmd_thread *pmd,
 
     if (ovsthread_once_start(&offload_thread_once)) {
         xpthread_cond_init(&dp_flow_offload.cond, NULL);
-        ovs_thread_create("dp_netdev_flow_offload",
-                          dp_netdev_flow_offload_main, NULL);
+        ovs_thread_create("hw_offload", dp_netdev_flow_offload_main, NULL);
         ovsthread_once_done(&offload_thread_once);
     }
 
