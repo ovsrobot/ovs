0454cc6af0bb6bf201e414b86e426435f0b0fa61
Adrian Moreno <amorenoz@redhat.com>
2021-11-22 12:22:46 +0100

    python: add ovs datapath flow parsing
    A ODPFlow is a Flow with the following sections:
    ufid
    info (e.g: bytes, packets, dp, etc)
    match
    actions
    
    Use a factory class ODPFlowFactory to cache the decoder objects.
    
    Only three datapath actions require special handling:
    gre: because it has double parenthesys
    geneve: because it supports many concatenated lists of options
    nat: we reuse the decoder used for openflow actions
    
    Signed-off-by: Adrian Moreno <amorenoz@redhat.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- python/automake.mk
+++ python/automake.mk
@@ -48,7 +48,8 @@ ovs_pyfiles = \
 	python/ovs/flows/list.py \
 	python/ovs/flows/flow.py \
 	python/ovs/flows/ofp.py \
-	python/ovs/flows/ofp_act.py
+	python/ovs/flows/ofp_act.py \
+	python/ovs/flows/odp.py
 
 # These python files are used at build time but not runtime,
 # so they are not installed.
