96979964960c415cba922811700ca2f57af5f15b
Paolo Valerio <pvalerio@redhat.com>
2021-11-27 00:12:58 +0100

    Native tunnel: Do not refresh the entry while revalidating.
    This is a minor issue but visible e.g. when you try to flush the neigh
    cache while the ARP flow is still present in the datapath, triggering
    the revalidation of the datapath flows which subsequently
    refreshes/adds the entry in the cache.
    
    Signed-off-by: Paolo Valerio <pvalerio@redhat.com>
    Acked-by: Flavio Leitner <fbl@sysclose.org>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- ofproto/ofproto-dpif-xlate.c
+++ ofproto/ofproto-dpif-xlate.c
@@ -4118,7 +4118,8 @@ terminate_native_tunnel(struct xlate_ctx *ctx, const struct xport *xport,
             (flow->dl_type == htons(ETH_TYPE_ARP) ||
              flow->nw_proto == IPPROTO_ICMPV6) &&
              is_neighbor_reply_correct(ctx, flow)) {
-            tnl_neigh_snoop(flow, wc, ctx->xbridge->name);
+            tnl_neigh_snoop(flow, wc, ctx->xbridge->name,
+                            ctx->xin->allow_side_effects);
         }
     }
 
