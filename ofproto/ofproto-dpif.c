ovsrobot/ovs

commit 1df7f7aac8c976690167261fec9a50d832ef9e7e

ofproto-dpif: Restore packet metadata when a continuation is resumed.
Recirculations due to NXT_RESUME are failing if the packet metadata is not
restored prior to the packet execution.

Reported-at: http://openvswitch.org/pipermail/dev/2016-May/070723.html
Signed-off-by: Numan Siddique <nusiddiq@redhat.com>
Signed-off-by: Jarno Rajahalme <jarno@ovn.org>

ofproto/ofproto-dpif.c
@@ -3656,6 +3656,16 @@ rule_expire(struct rule_dpif *rule)
     }
 }
 
+static void
+ofproto_dpif_set_packet_odp_port(const struct ofproto_dpif *ofproto,
+                                 ofp_port_t in_port, struct dp_packet *packet)
+{
+    if (in_port == OFPP_NONE) {
+        in_port = OFPP_LOCAL;
+    }
+    packet->md.in_port.odp_port = ofp_port_to_odp_port(ofproto, in_port);
+}
+
 int
 ofproto_dpif_execute_actions__(struct ofproto_dpif *ofproto,
                                const struct flow *flow,
@@ -3667,7 +3677,6 @@ ofproto_dpif_execute_actions__(struct ofproto_dpif *ofproto,
     struct dpif_flow_stats stats;
     struct xlate_out xout;
     struct xlate_in xin;
-    ofp_port_t in_port;
     struct dpif_execute execute;
     int error;
 
@@ -3704,11 +3713,7 @@ ofproto_dpif_execute_actions__(struct ofproto_dpif *ofproto,
     execute.mtu = 0;
 
     /* Fix up in_port. */
-    in_port = flow->in_port.ofp_port;
-    if (in_port == OFPP_NONE) {
-        in_port = OFPP_LOCAL;
-    }
-    execute.packet->md.in_port.odp_port = ofp_port_to_odp_port(ofproto, in_port);
+    ofproto_dpif_set_packet_odp_port(ofproto, flow->in_port.ofp_port, packet);
 
     error = dpif_execute(ofproto->backer->dpif, &execute);
 out:
@@ -4398,6 +4403,13 @@ nxt_resume(struct ofproto *ofproto_,
     struct dp_packet packet;
     dp_packet_init(&packet, pin->public.packet_len);
     dp_packet_put(&packet, pin->public.packet, pin->public.packet_len);
+
+    pkt_metadata_from_flow(&packet.md, &pin->public.flow_metadata.flow);
+
+    /* Fix up in_port. */
+    ofproto_dpif_set_packet_odp_port(ofproto,
+                                     pin->public.flow_metadata.flow.in_port.ofp_port,
+                                     &packet);
 
     struct flow headers;
     flow_extract(&packet, &headers);

commit 521157997635a177cc02da3123accc43315b8008

ofproto-dpif: fix packet_execute_prepare
In the commit 1df7f7aac8c976690167261fec9a50d832ef9e7e, the packet
metadata's in_port has been changed from ofp port into odp port,
however, the odp->flow->in_port is still ofp_port. This patch changes
the odp->flow->in_port into odp_port.

Fixes: 1df7f7aac8 ("ofproto-dpif: Restore packet metadata when a
continuation is resumed.")
Signed-off-by: Peng He <hepeng.0320@bytedance.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

ofproto/ofproto-dpif.c
@@ -5011,6 +5011,7 @@ packet_execute_prepare(struct ofproto *ofproto_,
     ofproto_dpif_set_packet_odp_port(ofproto, opo->flow->in_port.ofp_port,
                                      opo->packet);
 
+    opo->flow->in_port = opo->packet->md.in_port;
     ofproto_dpif_packet_out_delete(aux);
     opo->aux = execute;
 }