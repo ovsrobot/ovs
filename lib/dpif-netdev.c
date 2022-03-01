ovsrobot/ovs

commit 6193e03267c163096189ca935c6fd29a4ced7f11

dpif-netdev: Allow pin rxq and non-isolate PMD.
Pinning an rxq to a PMD with pmd-rxq-affinity may be done for
various reasons such as reserving a full PMD for an rxq, or to
ensure that multiple rxqs from a port are handled on different PMDs.

Previously pmd-rxq-affinity always isolated the PMD so no other rxqs
could be assigned to it by OVS. There may be cases where there is
unused cycles on those pmds and the user would like other rxqs to
also be able to be assigned to it by OVS.

Add an option to pin the rxq and non-isolate the PMD. The default
behaviour is unchanged, which is pin and isolate the PMD.

In order to pin and non-isolate:
ovs-vsctl set Open_vSwitch . other_config:pmd-rxq-isolate=false

Note this is available only with group assignment type, as pinning
conflicts with the operation of the other rxq assignment algorithms.

Signed-off-by: Kevin Traynor <ktraynor@redhat.com>
Acked-by: Sunil Pai G <sunil.pai.g@intel.com>
Acked-by: David Marchand <david.marchand@redhat.com>
Signed-off-by: Ian Stokes <ian.stokes@intel.com>

lib/dpif-netdev.c
@@ -292,6 +292,7 @@ struct dp_netdev {
     struct ovs_mutex tx_qid_pool_mutex;
     /* Rxq to pmd assignment type. */
     enum sched_assignment_type pmd_rxq_assign_type;
+    bool pmd_iso;
 
     /* Protects the access of the 'struct dp_netdev_pmd_thread'
      * instance for non-pmd thread. */
@@ -4236,6 +4237,24 @@ dpif_netdev_set_config(struct dpif *dpif, const struct smap *other_config)
         dp_netdev_request_reconfigure(dp);
     }
 
+    bool pmd_iso = smap_get_bool(other_config, "pmd-rxq-isolate", true);
+
+    if (pmd_rxq_assign_type != SCHED_GROUP && pmd_iso == false) {
+        /* Invalid combination. */
+        VLOG_WARN("pmd-rxq-isolate can only be set false "
+                  "when using pmd-rxq-assign=group");
+        pmd_iso = true;
+    }
+    if (dp->pmd_iso != pmd_iso) {
+        dp->pmd_iso = pmd_iso;
+        if (pmd_iso) {
+            VLOG_INFO("pmd-rxq-affinity isolates PMD core");
+        } else {
+            VLOG_INFO("pmd-rxq-affinity does not isolate PMD core");
+        }
+        dp_netdev_request_reconfigure(dp);
+    }
+
     struct pmd_auto_lb *pmd_alb = &dp->pmd_alb;
     bool cur_rebalance_requested = pmd_alb->auto_lb_requested;
     pmd_alb->auto_lb_requested = smap_get_bool(other_config, "pmd-auto-lb",
@@ -4965,7 +4984,7 @@ sched_numa_list_assignments(struct sched_numa_list *numa_list,
 
             sched_pmd = sched_pmd_find_by_pmd(numa_list, rxq->pmd);
             if (sched_pmd) {
-                if (rxq->core_id != OVS_CORE_UNSPEC) {
+                if (rxq->core_id != OVS_CORE_UNSPEC && dp->pmd_iso) {
                     sched_pmd->isolated = true;
                 }
                 sched_pmd_add_rxq(sched_pmd, rxq, proc_cycles);
@@ -5231,6 +5250,7 @@ sched_numa_list_schedule(struct sched_numa_list *numa_list,
                 struct sched_pmd *sched_pmd;
                 struct dp_netdev_pmd_thread *pmd;
                 struct sched_numa *numa;
+                bool iso = dp->pmd_iso;
                 uint64_t proc_cycles;
                 char rxq_cyc_log[MAX_RXQ_CYC_STRLEN];
 
@@ -5253,11 +5273,13 @@ sched_numa_list_schedule(struct sched_numa_list *numa_list,
                     rxqs[n_rxqs++] = rxq;
                     continue;
                 }
-                /* Mark PMD as isolated if not done already. */
-                if (sched_pmd->isolated == false) {
-                    sched_pmd->isolated = true;
-                    numa = sched_pmd->numa;
-                    numa->n_isolated++;
+                if (iso) {
+                    /* Mark PMD as isolated if not done already. */
+                    if (sched_pmd->isolated == false) {
+                        sched_pmd->isolated = true;
+                        numa = sched_pmd->numa;
+                        numa->n_isolated++;
+                    }
                 }
                 proc_cycles = dp_netdev_rxq_get_cycles(rxq,
                                                        RXQ_CYCLES_PROC_HIST);

tests/pmd.at
@@ -612,6 +612,16 @@ p1 2 0 2
 p1 3 0 1
 ])
 
+# Check they are pinned when those pmds are available again
+AT_CHECK([ovs-vsctl set Open_vSwitch . other_config:pmd-cpu-mask=0x1fe])
+
+AT_CHECK([ovs-appctl dpif-netdev/pmd-rxq-show | parse_pmd_rxq_show], [0], [dnl
+p1 0 0 3
+p1 1 0 7
+p1 2 0 2
+p1 3 0 8
+])
+
 AT_CHECK([ovs-vsctl remove Interface p1 other_config pmd-rxq-affinity])
 
 dnl We removed the rxq-affinity request.  dpif-netdev should assign queues
@@ -624,6 +634,7 @@ p1 2
 p1 3
 ])
 
+AT_CHECK([ovs-vsctl set Open_vSwitch . other_config:pmd-cpu-mask=6])
 AT_CHECK([ovs-vsctl set Interface p1 other_config:pmd-rxq-affinity='0:1'])
 
 dnl We explicitly requested core 1 for queue 0.  Core 1 becomes isolated and
@@ -637,6 +648,100 @@ p1 3 0 2
 
 OVS_VSWITCHD_STOP(["/cannot be pinned with port/d"])
 AT_CLEANUP
+
+AT_SETUP([PMD - rxq affinity - non-isolate])
+OVS_VSWITCHD_START(
+  [], [], [], [--dummy-numa 0,0,0,0,0,0,0,0,0])
+AT_CHECK([ovs-appctl vlog/set dpif:dbg dpif_netdev:dbg])
+
+AT_CHECK([ovs-ofctl add-flow br0 actions=controller])
+
+AT_CHECK([ovs-vsctl set Open_vSwitch . other_config:pmd-cpu-mask=0x1fe])
+
+AT_CHECK([ovs-vsctl add-port br0 p1 -- set Interface p1 type=dummy-pmd ofport_request=1 options:n_rxq=4 other_config:pmd-rxq-affinity="0:3,1:7,2:2,3:8"])
+
+dnl The rxqs should be on the requested cores.
+AT_CHECK([ovs-appctl dpif-netdev/pmd-rxq-show | parse_pmd_rxq_show], [0], [dnl
+p1 0 0 3
+p1 1 0 7
+p1 2 0 2
+p1 3 0 8
+])
+
+# change rxq assignment algorithm
+AT_CHECK([ovs-vsctl set Open_vSwitch . other_config:pmd-rxq-assign=group])
+
+dnl The rxqs should be on the requested cores.
+AT_CHECK([ovs-appctl dpif-netdev/pmd-rxq-show | parse_pmd_rxq_show], [0], [dnl
+p1 0 0 3
+p1 1 0 7
+p1 2 0 2
+p1 3 0 8
+])
+
+# try to pin & non-isolate
+TMP=$(($(cat ovs-vswitchd.log | wc -l | tr -d [[:blank:]])+1))
+AT_CHECK([ovs-vsctl set Open_vSwitch . other_config:pmd-rxq-isolate=false])
+OVS_WAIT_UNTIL([tail -n +$TMP ovs-vswitchd.log | grep "pmd-rxq-affinity does not isolate PMD core"])
+
+# should not impact - all rxqs are still pinned
+dnl The rxqs should be on the requested cores.
+AT_CHECK([ovs-appctl dpif-netdev/pmd-rxq-show | parse_pmd_rxq_show], [0], [dnl
+p1 0 0 3
+p1 1 0 7
+p1 2 0 2
+p1 3 0 8
+])
+
+# remove some pinning - see if non-isolate pmd are used for ovs rxq assignment of other rxqs
+AT_CHECK([ovs-vsctl remove Interface p1 other_config pmd-rxq-affinity])
+AT_CHECK([ovs-vsctl set Interface p1 other_config:pmd-rxq-affinity='0:1'])
+AT_CHECK([ovs-vsctl set Open_vSwitch . other_config:pmd-cpu-mask=6])
+
+dnl We explicitly requested core 1 for queue 0.  Core 1 is not isolated so it is
+dnl use for other rxqs.
+AT_CHECK([ovs-appctl dpif-netdev/pmd-rxq-show | parse_pmd_rxq_show], [0], [dnl
+p1 0 0 1
+p1 1 0 2
+p1 2 0 1
+p1 3 0 2
+])
+
+# change to algorithm that does not support pin & non-isolate
+TMP=$(($(cat ovs-vswitchd.log | wc -l | tr -d [[:blank:]])+1))
+AT_CHECK([ovs-vsctl set Open_vSwitch . other_config:pmd-rxq-assign=cycles])
+OVS_WAIT_UNTIL([tail -n +$TMP ovs-vswitchd.log | grep "pmd-rxq-isolate can only be set false when using pmd-rxq-assign=group"])
+OVS_WAIT_UNTIL([tail -n +$TMP ovs-vswitchd.log | grep "pmd-rxq-affinity isolates PMD core"])
+OVS_WAIT_UNTIL([tail -n +$TMP ovs-vswitchd.log | grep "Performing pmd to rx queue assignment using cycles algorithm"])
+
+dnl We explicitly requested core 1 for queue 0.  Core 1 becomes isolated and
+dnl every other queue goes to core 2.
+AT_CHECK([ovs-appctl dpif-netdev/pmd-rxq-show | parse_pmd_rxq_show], [0], [dnl
+p1 0 0 1
+p1 1 0 2
+p1 2 0 2
+p1 3 0 2
+])
+
+# change rxq assignment algorithm to one that support pin & non-isolate
+TMP=$(($(cat ovs-vswitchd.log | wc -l | tr -d [[:blank:]])+1))
+AT_CHECK([ovs-vsctl set Open_vSwitch . other_config:pmd-rxq-assign=group])
+OVS_WAIT_UNTIL([tail -n +$TMP ovs-vswitchd.log | grep "pmd-rxq-affinity does not isolate PMD core"])
+OVS_WAIT_UNTIL([tail -n +$TMP ovs-vswitchd.log | grep "Performing pmd to rx queue assignment using group algorithm"])
+
+#dnl We explicitly requested core 1 for queue 0.  Core 1 becomes isolated and
+#dnl every other queue goes to core 2.
+AT_CHECK([ovs-appctl dpif-netdev/pmd-rxq-show | parse_pmd_rxq_show], [0], [dnl
+p1 0 0 1
+p1 1 0 2
+p1 2 0 1
+p1 3 0 2
+])
+
+OVS_VSWITCHD_STOP(["/cannot be pinned with port/d
+/pmd-rxq-isolate can only be set false when using pmd-rxq-assign=group/d"])
+AT_CLEANUP
+
 
 AT_SETUP([PMD - rxq affinity - NUMA])
 OVS_VSWITCHD_START(

vswitchd/vswitch.xml
@@ -548,6 +548,25 @@
         </p>
       </column>
 
+      <column name="other_config" key="pmd-rxq-isolate"
+              type='{"type": "boolean"}'>
+        <p>
+          Specifies if a CPU core will be isolated after being pinned with
+          an Rx queue.
+        <p/>
+          Set this value to <code>false</code> to non-isolate a CPU core after
+          it is pinned with an Rxq using <code>pmd-rxq-affinity</code>. This
+          will allow OVS to assign other Rxqs to that CPU core.
+        </p>
+        <p>
+          The default value is <code>true</code>.
+        </p>
+        <p>
+          This can only be <code>false</code> when <code>pmd-rxq-assign</code>
+          is set to <code>group</code>.
+        </p>
+      </column>
+
       <column name="other_config" key="n-handler-threads"
               type='{"type": "integer", "minInteger": 1}'>
         <p>

commit c9aae2f00c3bbf26dc04c85af9ccb06886b3d292

dpif-netdev: Fix PMD auto load balance with pmd-rxq-isolate.
There are currently some checks for cross-numa polling cases to
ensure that they won't effect the accuracy of the PMD ALB.

If an rxq is pinned to a pmd core by the user it will not be
reassigned by OVS, so even if it is non-local numa polled it
will not impact PMD ALB accuracy.

To establish this, a check was made on whether the pmd core was
isolated or not. However, since other_config:pmd-rxq-isolate was
introduced, rxqs may be pinned but the pmd core not isolated.

It means that by setting pmd-rxq-isolate=false and doing non-local
numa pinning, PMD ALB may not run where it should.

If the core is isolated we can skip individual rxq checks but if not,
we should check the individual rxqs for pinning before we disallow
PMD ALB.

Fixes: 6193e03267c1 ("dpif-netdev: Allow pin rxq and non-isolate PMD.")

Signed-off-by: Kevin Traynor <ktraynor@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

lib/dpif-netdev.c
@@ -5696,11 +5696,14 @@ sched_numa_list_cross_numa_polling(struct sched_numa_list *numa_list)
             struct sched_pmd *sched_pmd;
 
             sched_pmd = &numa->pmds[i];
+            if (sched_pmd->isolated) {
+                continue;
+            }
             /* For each rxq. */
             for (unsigned k = 0; k < sched_pmd->n_rxq; k++) {
                 struct dp_netdev_rxq *rxq = sched_pmd->rxqs[k];
 
-                if (!sched_pmd->isolated &&
+                if (rxq->core_id == OVS_CORE_UNSPEC &&
                     rxq->pmd->numa_id !=
                         netdev_get_numa_id(rxq->port->netdev)) {
                     return true;