ead57c3794cfebc1abc8bf290bf313f50dc0232b
Adrian Moreno <amorenoz@redhat.com>
2022-03-11 16:21:17 +0100

    python: introduce OpenFlow Flow parsing
    Introduce OFPFlow class and all its decoders.
    
    Most of the decoders are generic (from decoders.py). Some have special
    syntax and need a specific implementation.
    
    Decoders for nat are moved to the common decoders.py because it's syntax
    is shared with other types of flows (e.g: dpif flows).
    
    Signed-off-by: Adrian Moreno <amorenoz@redhat.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- python/automake.mk
+++ python/automake.mk
@@ -31,6 +31,8 @@ ovs_pyfiles = \
 	python/ovs/flows/flow.py \
 	python/ovs/flows/kv.py \
 	python/ovs/flows/list.py \
+	python/ovs/flows/ofp.py \
+	python/ovs/flows/ofp_act.py \
 	python/ovs/json.py \
 	python/ovs/jsonrpc.py \
 	python/ovs/ovsuuid.py \
