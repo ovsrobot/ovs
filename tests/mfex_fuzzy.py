b5a1eb0ab1135d4ff533072e4393f6822d797127
Kumar Amber <kumar.amber@intel.com>
2022-04-12 18:58:27 +0530

    tests/mfex: Improve pcap script for mfex tests.
    The mfex pcap generation script is improved for varied length
    traffic and also removes the hard coded mfex_pcap and instead uses
    the script itself to generate complex traffic patterns for testing.
    
    Signed-off-by: Kumar Amber <kumar.amber@intel.com>
    Acked-by: Cian Ferriter <cian.ferriter@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- tests/mfex_fuzzy.py
+++ tests/mfex_fuzzy.py
@@ -3,30 +3,58 @@
 import sys
 
 from scapy.all import RandMAC, RandIP, PcapWriter, RandIP6, RandShort, fuzz
-from scapy.all import IPv6, Dot1Q, IP, Ether, UDP, TCP
+from scapy.all import IPv6, Dot1Q, IP, Ether, UDP, TCP, random
 
+# Relative path for the pcap file location.
 path = str(sys.argv[1]) + "/pcap/fuzzy.pcap"
+# The number of packets generated will be size * 8.
+size = int(sys.argv[2])
+# Traffic option is used to choose between fuzzy or simple packet type.
+traffic_opt = str(sys.argv[3])
+
 pktdump = PcapWriter(path, append=False, sync=True)
 
-for i in range(0, 2000):
+pkt = []
 
-    # Generate random protocol bases, use a fuzz() over the combined packet
-    # for full fuzzing.
+for i in range(0, size):
+
     eth = Ether(src=RandMAC(), dst=RandMAC())
     vlan = Dot1Q()
-    ipv4 = IP(src=RandIP(), dst=RandIP())
-    ipv6 = IPv6(src=RandIP6(), dst=RandIP6())
     udp = UDP(dport=RandShort(), sport=RandShort())
-    tcp = TCP(dport=RandShort(), sport=RandShort())
 
-    # IPv4 packets with fuzzing
-    pktdump.write(fuzz(eth / ipv4 / udp))
-    pktdump.write(fuzz(eth / ipv4 / tcp))
-    pktdump.write(fuzz(eth / vlan / ipv4 / udp))
-    pktdump.write(fuzz(eth / vlan / ipv4 / tcp))
+    if traffic_opt == "fuzzy":
 
-    # IPv6 packets with fuzzing
-    pktdump.write(fuzz(eth / ipv6 / udp))
-    pktdump.write(fuzz(eth / ipv6 / tcp))
-    pktdump.write(fuzz(eth / vlan / ipv6 / udp))
-    pktdump.write(fuzz(eth / vlan / ipv6 / tcp))
+        ipv4 = IP(src=RandIP(), dst=RandIP(), len=random.randint(0, 100))
+        ipv6 = IPv6(src=RandIP6(), dst=RandIP6(), plen=random.randint(0, 100))
+        tcp = TCP(dport=RandShort(), sport=RandShort(), flags='S',
+                  dataofs=random.randint(0, 20))
+        # IPv4 packets with fuzzing
+        pkt.append(fuzz(eth / ipv4 / udp))
+        pkt.append(fuzz(eth / ipv4 / tcp))
+        pkt.append(fuzz(eth / vlan / ipv4 / udp))
+        pkt.append(fuzz(eth / vlan / ipv4 / tcp))
+
+        # IPv6 packets with fuzzing
+        pkt.append(fuzz(eth / ipv6 / udp))
+        pkt.append(fuzz(eth / ipv6 / tcp))
+        pkt.append(fuzz(eth / vlan / ipv6 / udp))
+        pkt.append(fuzz(eth / vlan / ipv6 / tcp))
+
+    else:
+
+        ipv4 = IP(src=RandIP(), dst=RandIP())
+        ipv6 = IPv6(src=RandIP6(), dst=RandIP6())
+        tcp = TCP(dport=RandShort(), sport=RandShort(), flags='S')
+        # IPv4 packets
+        pkt.append(eth / ipv4 / udp)
+        pkt.append(eth / ipv4 / tcp)
+        pkt.append(eth / vlan / ipv4 / udp)
+        pkt.append(eth / vlan / ipv4 / tcp)
+
+        # IPv6 packets
+        pkt.append(eth / ipv6 / udp)
+        pkt.append(eth / ipv6 / tcp)
+        pkt.append(eth / vlan / ipv6 / udp)
+        pkt.append(eth / vlan / ipv6 / tcp)
+
+pktdump.write(pkt)
