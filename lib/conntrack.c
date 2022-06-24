ovsrobot/ovs

commit 92edd073ce6ca72636600a64e0645bf23027660a

conntrack: Hash entire NAT data structure in nat_range_hash().
Part of the hash input for nat_range_hash() was accidentally
omitted, so this fixes the problem.  Also, add a missing call to
hash_finish().

Fixes: 286de2729955 ("dpdk: Userspace Datapath: Introduce NAT Support.")
Co-authored-by: Ben Pfaff <blp@ovn.org>
Signed-off-by: Ben Pfaff <blp@ovn.org>
Signed-off-by: Darrell Ball <dlu998@gmail.com>
Signed-off-by: Ben Pfaff <blp@ovn.org>

lib/conntrack.c
@@ -1509,6 +1509,20 @@ conn_key_extract(struct conntrack *ct, struct dp_packet *pkt, ovs_be16 dl_type,
 
     return false;
 }
+
+static uint32_t
+ct_addr_hash_add(uint32_t hash, const struct ct_addr *addr)
+{
+    BUILD_ASSERT_DECL(sizeof *addr % 4 == 0);
+    return hash_add_bytes32(hash, (const uint32_t *) addr, sizeof *addr);
+}
+
+static uint32_t
+ct_endpoint_hash_add(uint32_t hash, const struct ct_endpoint *ep)
+{
+    BUILD_ASSERT_DECL(sizeof *ep % 4 == 0);
+    return hash_add_bytes32(hash, (const uint32_t *) ep, sizeof *ep);
+}
 
 /* Symmetric */
 static uint32_t
@@ -1616,33 +1630,24 @@ static uint32_t
 nat_range_hash(const struct conn *conn, uint32_t basis)
 {
     uint32_t hash = basis;
-    int i;
-    uint16_t port;
 
-    for (i = 0;
-         i < sizeof(conn->nat_info->min_addr) / sizeof(uint32_t);
-         i++) {
-        hash = hash_add(hash, ((uint32_t *) &conn->nat_info->min_addr)[i]);
-        hash = hash_add(hash, ((uint32_t *) &conn->nat_info->max_addr)[i]);
-    }
+    hash = ct_addr_hash_add(hash, &conn->nat_info->min_addr);
+    hash = ct_addr_hash_add(hash, &conn->nat_info->max_addr);
+    hash = hash_add(hash,
+                    (conn->nat_info->max_port << 16)
+                    | conn->nat_info->min_port);
 
-    memcpy(&port, &conn->nat_info->min_port, sizeof port);
-    hash = hash_add(hash, port);
-
-    for (i = 0; i < sizeof(conn->key.src.addr) / sizeof(uint32_t); i++) {
-        hash = hash_add(hash, ((uint32_t *) &conn->key.src)[i]);
-        hash = hash_add(hash, ((uint32_t *) &conn->key.dst)[i]);
-    }
-
-    memcpy(&port, &conn->key.src.port, sizeof port);
-    hash = hash_add(hash, port);
-    memcpy(&port, &conn->key.dst.port, sizeof port);
-    hash = hash_add(hash, port);
+    hash = ct_endpoint_hash_add(hash, &conn->key.src);
+    hash = ct_endpoint_hash_add(hash, &conn->key.dst);
 
     hash = hash_add(hash, (OVS_FORCE uint32_t) conn->key.dl_type);
     hash = hash_add(hash, conn->key.nw_proto);
     hash = hash_add(hash, conn->key.zone);
-    return hash;
+
+    /* The purpose of the second parameter is to distinguish hashes of data of
+     * different length; our data always has the same length so there is no
+     * value in counting. */
+    return hash_finish(hash, 0);
 }
 
 static bool

lib/conntrack-private.h
@@ -42,6 +42,10 @@ struct ct_endpoint {
     };
 };
 
+/* Verify that there is no padding in struct ct_endpoint, to facilitate
+ * hashing in ct_endpoint_hash_add(). */
+BUILD_ASSERT_DECL(sizeof(struct ct_endpoint) == sizeof(struct ct_addr) + 4);
+
 /* Changes to this structure need to be reflected in conn_key_hash() */
 struct conn_key {
     struct ct_endpoint src;

commit 8b1db203a3a71152b6e27933e1edfe7a68141b8a

conntrack: Fix incorrect bit shift while hashing nat range.
'max_port' is 16bit field, shift expands it to 'int', not unsigned int.

 lib/conntrack.c:2245:41: runtime error:
   left shift of 34568 by 16 places cannot be represented in type 'int'.

     0 0xec45f4 in nat_range_hash lib/conntrack.c:2245:41
     1 0xec45f4 in nat_get_unique_tuple lib/conntrack.c:2422:21
     2 0xec45f4 in conn_not_found lib/conntrack.c:1035:32
     3 0xeaf0a5 in process_one lib/conntrack.c:1407:20
     4 0xea9390 in conntrack_execute lib/conntrack.c:1465:13
     5 0x839530 in dp_execute_cb lib/dpif-netdev.c:9060:9
     6 0x9909cc in odp_execute_actions lib/odp-execute.c:868:17
     7 0x830946 in dp_netdev_execute_actions lib/dpif-netdev.c:9106:5
     8 0x830946 in handle_packet_upcall lib/dpif-netdev.c:8294:5
     9 0x82ea5e in fast_path_processing lib/dpif-netdev.c:8390:25
     10 0x7ed87f in dp_netdev_input__ lib/dpif-netdev.c:8479:9
     11 0x7eb5fc in dp_netdev_input lib/dpif-netdev.c:8517:5
     12 0x81dada in dp_netdev_process_rxq_port lib/dpif-netdev.c:5329:19
     13 0x7f0063 in dpif_netdev_run lib/dpif-netdev.c:6664:25
     14 0x85f036 in dpif_run lib/dpif.c:467:16
     15 0x61833a in type_run ofproto/ofproto-dpif.c:366:9
     16 0x5c210e in ofproto_type_run ofproto/ofproto.c:1822:31
     17 0x565db2 in bridge_run__ vswitchd/bridge.c:3245:9
     18 0x562f82 in bridge_run vswitchd/bridge.c:3310:5
     19 0x59a98c in main vswitchd/ovs-vswitchd.c:129:9
     20 0x7f8864c3acf2 in __libc_start_main (/lib64/libc.so.6+0x3acf2)
     21 0x47e60d in _start (vswitchd/ovs-vswitchd+0x47e60d)

Fixes: 92edd073ce6c ("conntrack: Hash entire NAT data structure in nat_range_hash().")
Signed-off-by: Ilya Maximets <i.maximets@ovn.org>
Signed-off-by: 0-day Robot <robot@bytheb.org>

lib/conntrack.c
@@ -2242,7 +2242,7 @@ nat_range_hash(const struct conn *conn, uint32_t basis,
     hash = ct_addr_hash_add(hash, &nat_info->min_addr);
     hash = ct_addr_hash_add(hash, &nat_info->max_addr);
     hash = hash_add(hash,
-                    (nat_info->max_port << 16)
+                    ((uint32_t) nat_info->max_port << 16)
                     | nat_info->min_port);
     hash = ct_endpoint_hash_add(hash, &conn->key.src);
     hash = ct_endpoint_hash_add(hash, &conn->key.dst);