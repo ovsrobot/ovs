ovsrobot/ovs

commit 3deca69b08f2c6dcbf17dd6e42615c7fd8eb9aeb

doc: Convert AUTHORS to rST
Signed-off-by: Stephen Finucane <stephen@that.guru>
Signed-off-by: Russell Bryant <russell@ovn.org>

debian/automake.mk
@@ -89,12 +89,12 @@ check-debian-changelog-version:
 ALL_LOCAL += check-debian-changelog-version
 DIST_HOOKS += check-debian-changelog-version
 
-$(srcdir)/debian/copyright: AUTHORS debian/copyright.in
+$(srcdir)/debian/copyright: AUTHORS.rst debian/copyright.in
 	$(AM_V_GEN) \
 	{ sed -n -e '/%AUTHORS%/q' -e p < $(srcdir)/debian/copyright.in;   \
-	  sed '1,/^$$/d' $(srcdir)/AUTHORS |				   \
+	  sed '34,/^$$/d' $(srcdir)/AUTHORS.rst |			   \
 		sed -n -e '/^$$/q' -e 's/^/  /p';			   \
-	  sed -e '1,/%AUTHORS%/d' $(srcdir)/debian/copyright.in;	   \
+	  sed -e '34,/%AUTHORS%/d' $(srcdir)/debian/copyright.in;	   \
 	} > $@
 
 DISTCLEANFILES += debian/copyright

commit 5f41797bebd312aa69d34d3f8aabeee1a6b8803c

debian: Fix generation of copyright file.
The recipe for inserting authors from AUTHORS.rst is wrong.

Fixes: 3deca69b08f2 ("doc: Convert AUTHORS to rST")
Signed-off-by: Frode Nordahl <frode.nordahl@canonical.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

debian/automake.mk
@@ -83,9 +83,9 @@ DIST_HOOKS += check-debian-changelog-version
 $(srcdir)/debian/copyright: AUTHORS.rst debian/copyright.in
 	$(AM_V_GEN) \
 	{ sed -n -e '/%AUTHORS%/q' -e p < $(srcdir)/debian/copyright.in;   \
-	  sed '34,/^$$/d' $(srcdir)/AUTHORS.rst  |				   \
+	  tail +28 $(srcdir)/AUTHORS.rst | sed '1,/^$$/d' |		   \
 		sed -n -e '/^$$/q' -e 's/^/  /p';			   \
-	  sed -e '34,/%AUTHORS%/d' $(srcdir)/debian/copyright.in;	   \
+	  sed -e '1,/%AUTHORS%/d' $(srcdir)/debian/copyright.in;	   \
 	} > $@
 
 DISTCLEANFILES += debian/copyright