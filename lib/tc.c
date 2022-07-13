ovsrobot/ovs

commit a9b8cdde69de73eaecd0cd6ae07077e6aa7eaa8e

tc: Add support parsing tc police action
Add function to parse police action from netlink message.

Signed-off-by: Jianbo Liu <jianbol@nvidia.com>
Acked-by: Eelco Chaudron <echaudro@redhat.com>
Signed-off-by: Simon Horman <simon.horman@corigine.com>

lib/tc.c
@@ -1340,6 +1340,54 @@ nl_parse_act_gact(struct nlattr *options, struct tc_flower *flower)
     return 0;
 }
 
+static const struct nl_policy police_policy[] = {
+    [TCA_POLICE_TBF] = { .type = NL_A_UNSPEC,
+                         .min_len = sizeof(struct tc_police),
+                         .optional = false, },
+    [TCA_POLICE_RATE] = { .type = NL_A_UNSPEC,
+                          .min_len = 1024,
+                          .optional = true, },
+    [TCA_POLICE_PEAKRATE] = { .type = NL_A_UNSPEC,
+                              .min_len = 1024,
+                              .optional = true, },
+    [TCA_POLICE_AVRATE] = { .type = NL_A_U32,
+                            .optional = true, },
+    [TCA_POLICE_RESULT] = { .type = NL_A_U32,
+                            .optional = true, },
+    [TCA_POLICE_TM] = { .type = NL_A_UNSPEC,
+                        .min_len = sizeof(struct tcf_t),
+                        .optional = true, },
+};
+
+static int
+nl_parse_act_police(const struct nlattr *options, struct tc_flower *flower)
+{
+    struct nlattr *police_attrs[ARRAY_SIZE(police_policy)] = {};
+    const struct tc_police *police;
+    struct tc_action *action;
+    struct nlattr *police_tm;
+    const struct tcf_t *tm;
+
+    if (!nl_parse_nested(options, police_policy, police_attrs,
+                         ARRAY_SIZE(police_policy))) {
+        VLOG_ERR_RL(&error_rl, "Failed to parse police action options");
+        return EPROTO;
+    }
+
+    police = nl_attr_get_unspec(police_attrs[TCA_POLICE_TBF], sizeof *police);
+    action = &flower->actions[flower->action_count++];
+    action->type = TC_ACT_POLICE;
+    action->police.index = police->index;
+
+    police_tm = police_attrs[TCA_POLICE_TM];
+    if (police_tm) {
+        tm = nl_attr_get_unspec(police_tm, sizeof *tm);
+        nl_parse_tcf(tm, flower);
+    }
+
+    return 0;
+}
+
 static const struct nl_policy mirred_policy[] = {
     [TCA_MIRRED_PARMS] = { .type = NL_A_UNSPEC,
                            .min_len = sizeof(struct tc_mirred),
@@ -1760,6 +1808,8 @@ nl_parse_single_action(struct nlattr *action, struct tc_flower *flower,
         /* Added for TC rule only (not in OvS rule) so ignore. */
     } else if (!strcmp(act_kind, "ct")) {
         nl_parse_act_ct(act_options, flower);
+    } else if (!strcmp(act_kind, "police")) {
+        nl_parse_act_police(act_options, flower);
     } else {
         VLOG_ERR_RL(&error_rl, "unknown tc action kind: %s", act_kind);
         err = EINVAL;
@@ -2774,6 +2824,10 @@ nl_msg_put_flower_acts(struct ofpbuf *request, struct tc_flower *flower)
                 nl_msg_put_act_ct(request, action);
                 nl_msg_put_act_cookie(request, &flower->act_cookie);
                 nl_msg_end_nested(request, act_offset);
+            }
+            break;
+            case TC_ACT_POLICE: {
+                /* Not supported yet */
             }
             break;
             }

lib/netdev-offload-tc.c
@@ -1017,6 +1017,10 @@ parse_tc_flower_to_match(struct tc_flower *flower,
                 nl_msg_put_u32(buf, OVS_ACTION_ATTR_RECIRC, action->chain);
             }
             break;
+            case TC_ACT_POLICE: {
+                /* Not supported yet */
+            }
+            break;
             }
         }
     }

lib/tc.h
@@ -174,6 +174,7 @@ enum tc_action_type {
     TC_ACT_MPLS_SET,
     TC_ACT_GOTO,
     TC_ACT_CT,
+    TC_ACT_POLICE,
 };
 
 enum nat_type {
@@ -261,6 +262,10 @@ struct tc_action {
             struct tc_flower_key key;
             struct tc_flower_key mask;
         } rewrite;
+
+        struct {
+            uint32_t index;
+        } police;
      };
 
      enum tc_action_type type;

commit e2949bdb32764415b25e4a70c153a6660c49cf29

tc: Fix misaligned access to struct tcf_t for police action.
lib/tc.c:1334:44: runtime error: member access within misaligned address
  0x6210001f5a2c for type 'const struct tcf_t', which requires 8 byte alignment
  0x6210001f5a2c: note: pointer points here
      24 00 06 00 c3 00 00 00  00 00 00 00 c3 00 00 00 ...
                  ^
     0 0xd7c7ea in nl_parse_tcf lib/tc.c:1334:44
     1 0xd7bd3a in nl_parse_act_police lib/tc.c:1433:9
     2 0xd68b1a in nl_parse_single_action lib/tc.c:1922:9
     3 0xd62c7e in nl_parse_flower_actions lib/tc.c:1992:19
     4 0xd62c7e in nl_parse_flower_options lib/tc.c:2107:12
     5 0xd5fa32 in parse_netlink_to_tc_flower lib/tc.c:2155:12
     6 0xd21760 in netdev_tc_flow_dump_next lib/netdev-offload-tc.c:1158:13
     7 0x94f442 in netdev_flow_dump_next lib/netdev-offload.c:301:14
     8 0xcba2f6 in dpif_netlink_flow_dump_next lib/dpif-netlink.c:1901:20
     9 0x8665b6 in dpif_flow_dump_next lib/dpif.c:1135:9
    10 0xee5f0f in dpctl_dump_flows lib/dpctl.c:1106:12
    11 0xee27a3 in dpctl_unixctl_handler lib/dpctl.c:3035:17
    12 0xc7f78b in process_command lib/unixctl.c:310:13
    13 0xc7f78b in run_connection lib/unixctl.c:344:17
    14 0xc7f78b in unixctl_server_run lib/unixctl.c:395:21
    15 0x59acb4 in main vswitchd/ovs-vswitchd.c:130:9
    16 0x7f1be043acf2 in __libc_start_main (/lib64/libc.so.6+0x3acf2)
    17 0x47e91d in _start (vswitchd/ovs-vswitchd+0x47e91d)

Fixes: a9b8cdde69de ("tc: Add support parsing tc police action")
Signed-off-by: Ilya Maximets <i.maximets@ovn.org>
Signed-off-by: 0-day Robot <robot@bytheb.org>

lib/tc.c
@@ -1380,7 +1380,7 @@ nl_parse_act_police(const struct nlattr *options, struct tc_flower *flower)
     const struct tc_police *police;
     struct tc_action *action;
     struct nlattr *police_tm;
-    const struct tcf_t *tm;
+    struct tcf_t tm;
 
     if (!nl_parse_nested(options, police_policy, police_attrs,
                          ARRAY_SIZE(police_policy))) {
@@ -1395,8 +1395,8 @@ nl_parse_act_police(const struct nlattr *options, struct tc_flower *flower)
 
     police_tm = police_attrs[TCA_POLICE_TM];
     if (police_tm) {
-        tm = nl_attr_get_unspec(police_tm, sizeof *tm);
-        nl_parse_tcf(tm, flower);
+        memcpy(&tm, nl_attr_get_unspec(police_tm, sizeof tm), sizeof tm);
+        nl_parse_tcf(&tm, flower);
     }
 
     return 0;