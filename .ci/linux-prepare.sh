ovsrobot/ovs

commit 445dceb88461161bc230417fdf78c71e4d6683c1

python: Introduce unit tests.
Use pytest to run unit tests as part of the standard testsuite.

Acked-by: Eelco Chaudron <echaudro@redhat.com>
Signed-off-by: Adrian Moreno <amorenoz@redhat.com>
Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

.ci/linux-prepare.sh
@@ -42,6 +42,9 @@ if [ "$M32" ]; then
     sudo apt-get install -y $pkgs
 fi
 
+# Install python test dependencies
+pip install -r python/test_requirements.txt
+
 # IPv6 is supported by kernel but disabled in TravisCI images:
 #   https://github.com/travis-ci/travis-ci/issues/8891
 # Enable it to avoid skipping of IPv6 related tests.

python/automake.mk
@@ -53,6 +53,9 @@ ovs_pyfiles = \
 	python/ovs/vlog.py \
 	python/ovs/winutils.py
 
+ovs_pytests = \
+	python/ovs/tests/test_kv.py
+
 # These python files are used at build time but not runtime,
 # so they are not installed.
 EXTRA_DIST += \
@@ -65,12 +68,14 @@ EXTRA_DIST += \
 EXTRA_DIST += \
 	python/ovs/compat/sortedcontainers/LICENSE \
 	python/README.rst \
-	python/setup.py
+	python/setup.py \
+	python/test_requirements.txt
 
 # C extension support.
 EXTRA_DIST += python/ovs/_json.c
 
-PYFILES = $(ovs_pyfiles) python/ovs/dirs.py $(ovstest_pyfiles)
+PYFILES = $(ovs_pyfiles) python/ovs/dirs.py $(ovstest_pyfiles) $(ovs_pytests)
+
 EXTRA_DIST += $(PYFILES)
 PYCOV_CLEAN_FILES += $(PYFILES:.py=.py,cover)
 

python/ovs/tests/test_kv.py
@@ -1,0 +1,76 @@
+import pytest
+
+from ovs.flow.kv import KVParser, KeyValue
+
+
+@pytest.mark.parametrize(
+    "input_data,expected",
+    [
+        (
+            (
+                "cookie=0x0, duration=147566.365s, table=0, n_packets=39, n_bytes=2574, idle_age=65534, hard_age=65534",  # noqa: E501
+                None,
+            ),
+            [
+                KeyValue("cookie", 0),
+                KeyValue("duration", "147566.365s"),
+                KeyValue("table", 0),
+                KeyValue("n_packets", 39),
+                KeyValue("n_bytes", 2574),
+                KeyValue("idle_age", 65534),
+                KeyValue("hard_age", 65534),
+            ],
+        ),
+        (
+            (
+                "load:0x4->NXM_NX_REG13[],load:0x9->NXM_NX_REG11[],load:0x8->NXM_NX_REG12[],load:0x1->OXM_OF_METADATA[],load:0x1->NXM_NX_REG14[],mod_dl_src:0a:58:a9:fe:00:02,resubmit(,8)",  # noqa: E501
+                None,
+            ),
+            [
+                KeyValue("load", "0x4->NXM_NX_REG13[]"),
+                KeyValue("load", "0x9->NXM_NX_REG11[]"),
+                KeyValue("load", "0x8->NXM_NX_REG12[]"),
+                KeyValue("load", "0x1->OXM_OF_METADATA[]"),
+                KeyValue("load", "0x1->NXM_NX_REG14[]"),
+                KeyValue("mod_dl_src", "0a:58:a9:fe:00:02"),
+                KeyValue("resubmit", ",8"),
+            ],
+        ),
+        (
+            ("l1(l2(l3(l4())))", None),
+            [KeyValue("l1", "l2(l3(l4()))")]
+        ),
+        (
+            ("l1(l2(l3(l4()))),foo:bar", None),
+            [KeyValue("l1", "l2(l3(l4()))"), KeyValue("foo", "bar")],
+        ),
+        (
+            ("enqueue:1:2,output=2", None),
+            [KeyValue("enqueue", "1:2"), KeyValue("output", 2)],
+        ),
+        (
+            ("value_to_reg(100)->someReg[10],foo:bar", None),
+            [
+                KeyValue("value_to_reg", "(100)->someReg[10]"),
+                KeyValue("foo", "bar"),
+            ],
+        ),
+    ],
+)
+def test_kv_parser(input_data, expected):
+    input_string = input_data[0]
+    decoders = input_data[1]
+    tparser = KVParser(input_string, decoders)
+    tparser.parse()
+    result = tparser.kv()
+    assert len(expected) == len(result)
+    for i in range(0, len(result)):
+        assert result[i].key == expected[i].key
+        assert result[i].value == expected[i].value
+        kpos = result[i].meta.kpos
+        kstr = result[i].meta.kstring
+        vpos = result[i].meta.vpos
+        vstr = result[i].meta.vstring
+        assert input_string[kpos : kpos + len(kstr)] == kstr
+        if vpos != -1:
+            assert input_string[vpos : vpos + len(vstr)] == vstr

tests/atlocal.in
@@ -238,3 +238,23 @@ export ASAN_OPTIONS
 # for the build.
 UBSAN_OPTIONS=print_stacktrace=1:halt_on_error=true:log_path=ubsan:$UBSAN_OPTIONS
 export UBSAN_OPTIONS
+
+# Check whether Python test requirements are available.
+REQUIREMENT_PATH=$abs_top_srcdir/python/test_requirements.txt $PYTHON3 -c '
+import os
+import pathlib
+import pkg_resources
+import sys
+
+with pathlib.Path(os.path.join(os.getenv("REQUIREMENT_PATH"))).open() as reqs:
+    for req in pkg_resources.parse_requirements(reqs):
+        try:
+            pkg_resources.require(str(req))
+        except pkg_resources.DistributionNotFound:
+            sys.exit(2)
+'
+case $? in
+    0) HAVE_PYTEST=yes ;;
+    2) HAVE_PYTEST=no ;;
+    *) echo "$0: unexpected error probing Python unit test requirements" >&2 ;;
+esac

tests/automake.mk
@@ -100,6 +100,7 @@ TESTSUITE_AT = \
 	tests/ovsdb-rbac.at \
 	tests/ovs-vsctl.at \
 	tests/ovs-xapi-sync.at \
+	tests/pytest.at \
 	tests/stp.at \
 	tests/rstp.at \
 	tests/interface-reconfigure.at \

tests/pytest.at
@@ -1,0 +1,8 @@
+AT_BANNER([Python unit tests])
+
+# Run pytest unit tests.
+AT_SETUP([Pytest unit tests - Python3])
+AT_KEYWORDS([python])
+AT_SKIP_IF([test "$HAVE_PYTEST" = "no"])
+AT_CHECK([$PYTHON3 -m pytest $top_srcdir/python/ovs],[0], [ignore], [ignore])
+AT_CLEANUP()

tests/testsuite.at
@@ -78,3 +78,4 @@ m4_include([tests/mcast-snooping.at])
 m4_include([tests/packet-type-aware.at])
 m4_include([tests/nsh.at])
 m4_include([tests/drop-stats.at])
+m4_include([tests/pytest.at])

commit 4d3cd2bc834838801f91d9a9f2354b1afd59d1fc

ci: Prefer pip3 to install unit test dependencies
While it looks like the right python3 versions of those dependencies
seems to be installed in the CI, prefer calling this via pip3 like the
rest of the script.

Fixes: 445dceb88461 ("python: Introduce unit tests.")
Signed-off-by: David Marchand <david.marchand@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

.ci/linux-prepare.sh
@@ -43,7 +43,7 @@ if [ "$M32" ]; then
 fi
 
 # Install python test dependencies
-pip install -r python/test_requirements.txt
+pip3 install -r python/test_requirements.txt
 
 # IPv6 is supported by kernel but disabled in TravisCI images:
 #   https://github.com/travis-ci/travis-ci/issues/8891