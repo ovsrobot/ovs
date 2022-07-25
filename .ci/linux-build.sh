ovsrobot/ovs

commit 6ad3be9749abe6d840a45e5be7ed28abb4c45262

debian: Fix build of python json C extension.
Signed-off-by: Frode Nordahl <frode.nordahl@canonical.com>
Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

debian/rules
@@ -85,10 +85,15 @@ override_dh_auto_install:
 
 execute_after_dh_install:
 	set -e && for pyvers in $(PYTHON3S); do \
-		cd python && python$$pyvers setup.py install --install-layout=deb \
-			--root $(CURDIR)/debian/python3-openvswitch; cd ..; \
+		cd python && \
+		enable_shared=no \
+		CFLAGS=-I$(CURDIR)/debian/tmp/usr/include \
+		LDFLAGS=$(CURDIR)/debian/tmp/usr/lib/libopenvswitch.a \
+		python$$pyvers setup.py install --install-layout=deb \
+			--root $(CURDIR)/debian/python3-openvswitch; \
+		cd ..; \
 		mkdir -p $(CURDIR)/debian/openvswitch-test/usr/lib/python$$pyvers/dist-packages/ovstest; \
-		install -v -D -m 644 python/ovstest/*.py \
+		install -v -D python/ovstest/*.py \
 			$(CURDIR)/debian/openvswitch-test/usr/lib/python$$pyvers/dist-packages/ovstest; \
 	done
 
@@ -108,7 +113,7 @@ override_dh_strip:
 	dh_strip --dbgsym-migration='openvswitch-dbg (<< 2.17~)'
 
 override_dh_python3:
-	dh_python3 --shebang=/usr/bin/python3
+	DEB_HOST_ARCH=$(DEB_HOST_ARCH) dh_python3 --shebang=/usr/bin/python3
 
 # Helper target for creating snapshots from upstream git
 DATE=$(shell date +%Y%m%d)

debian/control
@@ -278,11 +278,12 @@ Description: Open vSwitch VTEP utilities
  VTEP-configured database and a VTEP emulator.
 
 Package: python3-openvswitch
-Architecture: all
+Architecture: linux-any
 Section: python
 Depends:
  ${misc:Depends},
  ${python3:Depends},
+ ${shlibs:Depends},
 Description: Python 3 bindings for Open vSwitch
  Open vSwitch is a production quality, multilayer, software-based,
  Ethernet virtual switch. It is designed to enable massive network

commit 6bb2341ff6e27de7a75a1063b692e89f7f905563

debian: Fix incorrect linkage of the python C extension.
Current version of debian/rules simply passes the libopenvswitch.a
as a command line argument via LDFLAGS, but that doesn't actually
lead to this library being statically linked into python extension,
which is a shared library.  Instead, the build "succeeds", but the
resulted extension is not usable, because most of the symbols are
missing:

  from ovs import _json

  ImportError:
    /usr/lib/python3/dist-packages/ovs/_json.cpython-310-x86_64-linux-gnu.so:
      undefined symbol: json_parser_finish

'-lopenvswitch' with a path to a static library should be passed
instead to make it actually statically linked.  But even that is not
enough as all the libraries that libopenvswitch.a was built with also
has to be passed.  Otherwise, we'll have unresolved symbols like ssl,
cap-ng, etc.

The most convenient way to get all the required libraries and cflags
seems to be by using pkg-config.

Setting several environment variables for pkg-config, so it can find
the libopenvswitch.pc in non-standard directory, not skip default
locations and also report them with the right base directory.

Extra '-Wl,-Bstatic -lopenvswitch -Wl,-Bdynamic' is added before all
the libs to ensure static linking of libopenvswitch even if the
dynamic library is available in a system.

One more problem here is that it is not possible to link static
library into dynamic library if the static one is not position
independent.  So, we have to build everything with -fPIC, otherwise
it's not possible to build C extensions.

Also added a simple CI script to check that we're able to use python
C extension after installing a package.

Fixes: 6ad3be9749ab ("debian: Fix build of python json C extension.")
Signed-off-by: Ilya Maximets <i.maximets@ovn.org>
Signed-off-by: 0-day Robot <robot@bytheb.org>

.ci/linux-build.sh
@@ -212,6 +212,13 @@ if [ "$DEB_PACKAGE" ]; then
     echo $deps | xargs sudo apt -y install
     # install the locally built openvswitch packages
     sudo dpkg -i $packages
+
+    # Check that python C extension is built correctly.
+    python3 -c "
+from ovs import _json
+import ovs.json
+assert ovs.json.from_string('{\"a\": 42}') == {'a': 42}"
+
     exit 0
 fi
 

debian/rules
@@ -2,6 +2,7 @@
 # -*- makefile -*-
 #export DH_VERBOSE=1
 export DEB_BUILD_MAINT_OPTIONS = hardening=+all
+export DEB_CFLAGS_MAINT_APPEND = -fPIC
 
 %:
 	dh $@
@@ -91,10 +92,14 @@ override_dh_auto_install:
 
 execute_after_dh_install:
 	set -e && for pyvers in $(PYTHON3S); do \
-		cd python && \
+		cd python; \
+		export PKG_CONFIG_PATH=$(CURDIR)/debian/tmp/usr/lib/pkgconfig; \
+		export PKG_CONFIG_SYSROOT_DIR=$(CURDIR)/debian/tmp; \
+		export PKG_CONFIG_SYSTEM_INCLUDE_PATH=/; \
+		export PKG_CONFIG_SYSTEM_LIBRARY_PATH=/; \
 		enable_shared=no \
-		CFLAGS=-I$(CURDIR)/debian/tmp/usr/include \
-		LDFLAGS=$(CURDIR)/debian/tmp/usr/lib/libopenvswitch.a \
+		extra_cflags="`pkg-config --cflags libopenvswitch`" \
+		extra_libs="-Wl,-Bstatic -lopenvswitch -Wl,-Bdynamic `pkg-config --libs --static libopenvswitch`" \
 		python$$pyvers setup.py install --install-layout=deb \
 			--root $(CURDIR)/debian/python3-openvswitch; \
 		cd ..; \