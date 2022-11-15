633fdfcf4ca2df7bbca5aa78887ff3780b3deaf7
Eelco Chaudron <echaudro@redhat.com>
2022-11-15 13:29:32 +0100

    tests: Include working system-traffic tests into the system-offloads-testsuite.
    Include and run the system-traffic.at tests as part of the system offload
    testsuite. Exclude all the tests that will not run without any special
    modifications.
    
    Signed-off-by: Eelco Chaudron <echaudro@redhat.com>
    Acked-by: Roi Dayan <roid@nvidia.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- tests/automake.mk
+++ tests/automake.mk
@@ -183,6 +183,7 @@ SYSTEM_TESTSUITE_AT = \
 
 SYSTEM_OFFLOADS_TESTSUITE_AT = \
 	tests/system-common-macros.at \
+	tests/system-offloads.at \
 	tests/system-offloads-traffic.at \
 	tests/system-offloads-testsuite.at
 
