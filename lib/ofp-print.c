38e20f436472ef88080f0f109232b68200fd5055
Wan Junjie <wanjunjie@bytedance.com>
2022-11-15 12:16:17 +0800

    utilities/ofctl: add-meters for save and restore
    put dump-meters' result in one line so add-meters can handle.
    save and restore meters when restart ovs.
    bundle functions are not implemented in this patch.
    
    Signed-off-by: Wan Junjie <wanjunjie@bytedance.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/ofp-print.c
+++ lib/ofp-print.c
@@ -378,8 +378,9 @@ ofp_print_meter_config_reply(struct ds *s, const struct ofp_header *oh)
         if (retval) {
             break;
         }
+
         ds_put_char(s, '\n');
-        ofputil_format_meter_config(s, &mc);
+        ofputil_format_meter_config(s, &mc, 0);
     }
     ofpbuf_uninit(&bands);
 
@@ -1269,7 +1270,7 @@ ofp_to_string(const void *oh_, size_t len,
     ds_put_hex_dump(&string, oh, len, 0, true);
     return ds_steal_cstr(&string);
 }
-
+
 static void
 print_and_free(FILE *stream, char *string)
 {
