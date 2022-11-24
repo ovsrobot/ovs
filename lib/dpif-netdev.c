9f6480b62e52920b63dbd92caa10d726ce0b9c85
Flavio Leitner <fbl@sysclose.org>
2022-11-24 00:30:48 -0500

    userspace: Enable IP checksum offloading by default.
    The netdev receiving packets is supposed to provide the flags
    indicating if the IP checksum was verified and it is GOOD or BAD,
    otherwise the stack will check when appropriate by software.
    
    If the packet comes with good checksum, then postpone the
    checksum calculation to the egress device if needed.
    
    When encapsulate a packet with that flag, set the checksum
    of the inner IP header since that is not yet supported.
    
    Calculate the IP checksum when the packet is going to be sent over
    a device that doesn't support the feature.
    
    Linux devices don't support IP checksum offload alone, so the
    support is not enabled.
    
    Signed-off-by: Flavio Leitner <fbl@sysclose.org>
    Co-authored-by: Mike Pattrick <mkp@redhat.com>
    Signed-off-by: Mike Pattrick <mkp@redhat.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/dpif-netdev.c
+++ lib/dpif-netdev.c
@@ -7861,6 +7861,10 @@ dp_netdev_upcall(struct dp_netdev_pmd_thread *pmd, struct dp_packet *packet_,
         ds_destroy(&ds);
     }
 
+    /* The packet is going to be encapsulated and sent to
+     * the controller. */
+    dp_packet_ol_send_prepare(packet_, 0);
+
     return dp->upcall_cb(packet_, flow, ufid, pmd->core_id, type, userdata,
                          actions, wc, put_actions, dp->upcall_aux);
 }
