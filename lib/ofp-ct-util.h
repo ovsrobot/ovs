7f7d6677addcf6167a1166d516a28d5cd846d92f
Ales Musil <amusil@redhat.com>
2022-12-08 15:26:33 +0100

    ofp, dpif: Allow CT flush based on partial match
    Currently, the CT can be flushed by dpctl only be specifying
    the whole 5-tuple. This is not very convenient when there are
    only some fields known to the user of CT flush. Add new struct
    ofputil_ct_match which represents the generic filtering that can
    be done for CT flush. The match is done only on fields that are
    non-zero with exception to the icmp fields.
    
    This allows the filtering just within dpctl, however
    it is a preparation for OpenFlow extension.
    
    Reported-at: https://bugzilla.redhat.com/2120546
    Signed-off-by: Ales Musil <amusil@redhat.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ lib/ofp-ct-util.h
@@ -0,0 +1,34 @@
+/* Copyright (c) 2022, Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OVS_OFP_CT_UTIL_H
+#define OVS_OFP_CT_UTIL_H
+
+#include "ct-dpif.h"
+#include "openvswitch/ofp-util.h"
+
+bool ofputil_ct_match_cmp(const struct ofputil_ct_match *match,
+                                 const struct ct_dpif_entry *entry);
+
+bool ofputil_ct_tuple_is_five_tuple(const struct ofputil_ct_tuple *tuple,
+                                    uint8_t ip_proto);
+
+void ofputil_ct_match_format(struct ds *ds,
+                             const struct ofputil_ct_match *match);
+
+bool ofputil_ct_match_parse(struct ofputil_ct_match *match, const char *s,
+                            struct ds *ds);
+
+#endif // lib/ofp-ct-util.h
