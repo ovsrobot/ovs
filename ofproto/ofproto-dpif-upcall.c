e9a676abc6a31228be725444bdd4f8fa6f5ba5eb
Balazs Nemeth <bnemeth@redhat.com>
2022-12-22 10:26:24 +0100

    ofproto-dpif-upcall: Don't set statistics to 0 when they jump back
    The only way that stats->{n_packets,n_bytes} would decrease is due to an
    overflow, or if there are bugs in how statistics are handled. In the
    past, there were multiple bugs that caused a jump backward. A
    workaround was in place to set the statistics to 0 in that case. When
    this happened while the revalidator was under heavy load, the workaround
    had an unintended side effect where should_revalidate returned false
    causing the flow to be removed because the metric it calculated was
    based on a bogus value. Since many of those bugs have now been
    identified and resolved, there is no need to set the statistics to 0. In
    addition, the (unlikely) overflow still needs to be handled
    appropriately.
    
    Signed-off-by: Balazs Nemeth <bnemeth@redhat.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- ofproto/ofproto-dpif-upcall.c
+++ ofproto/ofproto-dpif-upcall.c
@@ -2334,12 +2334,8 @@ revalidate_ukey(struct udpif *udpif, struct udpif_key *ukey,
 
     push.used = stats->used;
     push.tcp_flags = stats->tcp_flags;
-    push.n_packets = (stats->n_packets > ukey->stats.n_packets
-                      ? stats->n_packets - ukey->stats.n_packets
-                      : 0);
-    push.n_bytes = (stats->n_bytes > ukey->stats.n_bytes
-                    ? stats->n_bytes - ukey->stats.n_bytes
-                    : 0);
+    push.n_packets = stats->n_packets - ukey->stats.n_packets;
+    push.n_bytes = stats->n_bytes - ukey->stats.n_bytes;
 
     if (need_revalidate) {
         if (should_revalidate(udpif, push.n_packets, ukey->stats.used)) {
