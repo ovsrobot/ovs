15b4bc8f71a0f45a9a8346f890acfb6b9b31ccc2
Peng Zhang <peng.zhang@corigine.com>
2023-03-09 14:02:49 +0100

    dpif-netdev: Offloading meter with DPDK
    OVS-DPDK meters are created in advance and OpenFlow rules refer to them by
    their unique ID. A new API is used to offload them. By calling the API,
    meters are created and try to be offload by port in the bridge with the
    proxy port id.
    
    Signed-off-by: Peng Zhang <peng.zhang@corigine.com>
    Signed-off-by: Jin Liu <jin.liu@corigine.com>
    Signed-off-by: Simon Horman <simon.horman@corigine.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/dpif-netdev.c
+++ lib/dpif-netdev.c
@@ -7387,6 +7387,11 @@ dpif_netdev_meter_set(struct dpif *dpif, ofproto_meter_id meter_id,
 
     ovs_mutex_unlock(&dp->meters_lock);
 
+    if (netdev_is_flow_api_enabled()) {
+        dpdk_meter_offload_set(dpif_normalize_type(dpif_type(dpif)),
+                               meter_id, config);
+    }
+
     return 0;
 }
 
@@ -7423,8 +7428,18 @@ dpif_netdev_meter_get(const struct dpif *dpif,
 
         ovs_mutex_unlock(&meter->lock);
         stats->n_bands = i;
-    }
 
+        if (netdev_is_flow_api_enabled()) {
+            dpdk_meter_offload_get(dpif_normalize_type(dpif_type(dpif)),
+                                   meter_id_, stats);
+
+            /* nit: Meter offload currently only supports one band */
+            if (meter->n_bands) {
+                stats->bands[0].packet_count = stats->packet_in_count;
+                stats->bands[0].byte_count = stats->byte_in_count;
+            }
+        }
+    }
     return 0;
 }
 
@@ -7439,6 +7454,11 @@ dpif_netdev_meter_del(struct dpif *dpif,
     error = dpif_netdev_meter_get(dpif, meter_id_, stats, n_bands);
     if (!error) {
         uint32_t meter_id = meter_id_.uint32;
+
+        if (netdev_is_flow_api_enabled()) {
+            dpdk_meter_offload_del(dpif_normalize_type(dpif_type(dpif)),
+                                   meter_id_, stats);
+        }
 
         ovs_mutex_lock(&dp->meters_lock);
         dp_meter_detach_free(&dp->meters, meter_id);
