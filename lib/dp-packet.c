79d8b5f0095a9368307824aa50b697745b281ee0
Flavio Leitner <fbl@sysclose.org>
2023-06-21 13:16:57 -0400

    userspace: Respect tso/gso segment size.
    Currently OVS will calculate the segment size based on the
    MTU of the egress port. That usually happens to be correct
    when the ports share the same MTU, but that is not always true.
    
    Therefore, if the segment size is provided, then use that and
    make sure the over sized packets are dropped.
    
    Signed-off-by: Flavio Leitner <fbl@sysclose.org>
    Co-authored-by: Mike Pattrick <mkp@redhat.com>
    Signed-off-by: Mike Pattrick <mkp@redhat.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/dp-packet.c
+++ lib/dp-packet.c
@@ -34,6 +34,7 @@ dp_packet_init__(struct dp_packet *b, size_t allocated, enum dp_packet_source so
     pkt_metadata_init(&b->md, 0);
     dp_packet_reset_cutlen(b);
     dp_packet_reset_offload(b);
+    dp_packet_set_tso_segsz(p, 0);
     /* Initialize implementation-specific fields of dp_packet. */
     dp_packet_init_specific(b);
     /* By default assume the packet type to be Ethernet. */
