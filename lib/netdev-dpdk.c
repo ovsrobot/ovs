c709062c561c5bed9ff611d6b885758cf6f53590
David Marchand <david.marchand@redhat.com>
2023-08-24 17:19:22 +0200

    netdev-dpdk: Disable net/tap Tx L4 checksum offloads.
    As reported by Ales when doing some OVN integration tests with OVS 3.2,
    net/tap has broken L4 checksum offloads.
    
    Fixes are pending on DPDK side.
    Until they get in a LTS release used by OVS, disable those Tx offloads.
    
    Signed-off-by: David Marchand <david.marchand@redhat.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/netdev-dpdk.c
+++ lib/netdev-dpdk.c
@@ -1312,6 +1312,14 @@ dpdk_eth_dev_init(struct netdev_dpdk *dev)
         dev->hw_ol_features &= ~NETDEV_RX_HW_SCATTER;
     }
 
+    if (!strcmp(info.driver_name, "net_tap")) {
+        VLOG_WARN("%s: disabled Tx L4 checksum offloads for a net/tap port.",
+                  netdev_get_name(&dev->up));
+        info.tx_offload_capa &= ~RTE_ETH_TX_OFFLOAD_UDP_CKSUM;
+        info.tx_offload_capa &= ~RTE_ETH_TX_OFFLOAD_TCP_CKSUM;
+        info.tx_offload_capa &= ~RTE_ETH_TX_OFFLOAD_TCP_TSO;
+    }
+
     if (info.tx_offload_capa & RTE_ETH_TX_OFFLOAD_IPV4_CKSUM) {
         dev->hw_ol_features |= NETDEV_TX_IPV4_CKSUM_OFFLOAD;
     } else {
