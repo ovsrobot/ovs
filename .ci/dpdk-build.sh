a4e06080343f3e91aeb522274e1762b04b9afbc0
David Marchand <david.marchand@redhat.com>
2023-09-04 16:05:02 +0200

    system-dpdk: Run traffic tests.
    Integrate system-traffic.at tests as part of check-dpdk.
    
    Some tests that can't work with the userspace datapath are skipped by
    overriding some OVS_CHECK_* macros.
    
    ADD_VETH is implemented using the net/af_xdp DPDK driver.
    
    Signed-off-by: David Marchand <david.marchand@redhat.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- .ci/dpdk-build.sh
+++ .ci/dpdk-build.sh
@@ -39,7 +39,7 @@ function build_dpdk()
     # check-dpdk unit tests requires testpmd and some net/ driver.
     # We can disable all drivers but them, in order to save compilation time.
     DPDK_OPTS="$DPDK_OPTS -Denable_apps=test-pmd"
-    DPDK_OPTS="$DPDK_OPTS -Denable_drivers=net/null,net/tap,net/virtio"
+    DPDK_OPTS="$DPDK_OPTS -Denable_drivers=net/null,net/af_xdp,net/tap,net/virtio"
 
     # Install DPDK using prefix.
     DPDK_OPTS="$DPDK_OPTS --prefix=$(pwd)/build"
