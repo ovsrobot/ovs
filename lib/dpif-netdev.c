ac42574f3ac7f0be9e39cc118fda55a37bb1bf81
Dexia Li <dexia.li@jaguarmicro.com>
2023-10-18 17:46:03 +0800

    userspace: Support vxlan and geneve tso.
    For userspace datapath, this patch provides vxlan and geneve tunnel tso.
     Only support userspace vxlan or geneve tunnel, meanwhile support
     tunnel outter and inner csum offload. If netdev do not support offload
     features, there is a software fallback.If netdev do not support vxlan
     and geneve tso,packets will drop. Front-end devices can close offload
     features by ethtool also.
    
    Signed-off-by: Dexia Li <dexia.li@jaguarmicro.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/dpif-netdev.c
+++ lib/dpif-netdev.c
@@ -7976,8 +7976,6 @@ dp_netdev_upcall(struct dp_netdev_pmd_thread *pmd, struct dp_packet *packet_,
         ds_destroy(&ds);
     }
 
-    dp_packet_ol_send_prepare(packet_, 0);
-
     return dp->upcall_cb(packet_, flow, ufid, pmd->core_id, type, userdata,
                          actions, wc, put_actions, dp->upcall_aux);
 }
