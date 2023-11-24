453252bffb94e34110fad0665dab62f8d5d63ad3
Eelco Chaudron <echaudro@redhat.com>
2023-11-24 13:16:32 +0100

    ci: Add clang-analyze to GitHub actions.
    This patch detects new static analyze issues, and report them.
    It does this by reporting on the delta for this branch, compared
    to the previous branch.
    
    For example the error might look like this:
    
      error level: +0 -0 no changes
      warning level: +2 +0
        New issue "deadcode.DeadStores Value stored to 'remote' is never read" (1 occurrence)
          file:///home/runner/work/ovs/ovs/vswitchd/ovs-vswitchd.c:86
        New issue "unix.Malloc Potential leak of memory pointed to by 'remote'" (1 occurrence)
          file:///home/runner/work/ovs/ovs/vswitchd/ovs-vswitchd.c:95
      note level: +0 -0 no changes
      all levels: +2 +0
    
    Signed-off-by: Eelco Chaudron <echaudro@redhat.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- .ci/linux-build.sh
+++ .ci/linux-build.sh
@@ -49,6 +49,30 @@ function build_ovs()
     make -j4
 }
 
+function clang_analyze()
+{
+    [ -d "./upstream-clang-analyzer-results" ] && cache_build=false \
+                                               || cache_build=true
+    if [ "$cache_build" = true ]; then
+        # If this is a cache build, proceed to the upstream branch's directory.
+        cd upstream_ovs_main
+    fi;
+
+    configure_ovs $OPTS
+    make clean
+    scan-build -o ./clang-analyzer-results -sarif --use-cc=clang make -j4
+
+    if [ "$cache_build" = true ]; then
+        # Move results, so it will be picked up by the cache.
+        mv ./clang-analyzer-results ../upstream-clang-analyzer-results
+        cd ..
+    else
+        # Only do the compare on the none cache builds.
+        sarif --check note diff ./upstream-clang-analyzer-results \
+                                ./clang-analyzer-results/
+    fi;
+}
+
 if [ "$DEB_PACKAGE" ]; then
     ./boot.sh && ./configure --with-dpdk=$DPDK && make debian
     mk-build-deps --install --root-cmd sudo --remove debian/control
@@ -115,6 +139,11 @@ if [ "$UBSAN" ]; then
 fi
 
 OPTS="${EXTRA_OPTS} ${OPTS} $*"
+
+if [ "$CLANG_ANALYZE" ]; then
+    clang_analyze
+    exit 0
+fi
 
 if [ "$TESTSUITE" = 'test' ]; then
     # 'distcheck' will reconfigure with required options.
