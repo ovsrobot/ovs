df2e254d54b6afc5f11ad17e44999cd263cfaed2
Adrian Moreno <amorenoz@redhat.com>
2023-12-01 20:14:37 +0100

    python: ovs: Add flowviz scheleton.
    Add a new python package (just the scheleton for now) to hold a flow
    visualization tool based on the flow parsing library.
    
    Signed-off-by: Adrian Moreno <amorenoz@redhat.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- python/automake.mk
+++ python/automake.mk
@@ -63,6 +63,14 @@ ovs_pytests = \
 	python/ovs/tests/test_odp.py \
 	python/ovs/tests/test_ofp.py
 
+ovs_flowviz = \
+	python/ovs/flowviz/__init__.py \
+	python/ovs/flowviz/main.py \
+	python/ovs/flowviz/odp/__init__.py \
+	python/ovs/flowviz/ofp/__init__.py \
+	python/ovs/flowviz/ovs-flowviz
+
+
 # These python files are used at build time but not runtime,
 # so they are not installed.
 EXTRA_DIST += \
@@ -81,7 +89,7 @@ EXTRA_DIST += \
 # C extension support.
 EXTRA_DIST += python/ovs/_json.c
 
-PYFILES = $(ovs_pyfiles) python/ovs/dirs.py $(ovstest_pyfiles) $(ovs_pytests)
+PYFILES = $(ovs_pyfiles) python/ovs/dirs.py $(ovstest_pyfiles) $(ovs_pytests) $(ovs_flowviz)
 
 EXTRA_DIST += $(PYFILES)
 PYCOV_CLEAN_FILES += $(PYFILES:.py=.py,cover)
@@ -95,7 +103,7 @@ FLAKE8_PYFILES += \
 	python/ovs/dirs.py.template \
 	python/setup.py
 
-nobase_pkgdata_DATA = $(ovs_pyfiles) $(ovstest_pyfiles)
+nobase_pkgdata_DATA = $(ovs_pyfiles) $(ovstest_pyfiles) $(ovs_flowviz)
 ovs-install-data-local:
 	$(MKDIR_P) python/ovs
 	sed \
