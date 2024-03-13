358b9cd725293ddaac8876d9b1734e4ab534a3db
Adrian Moreno <amorenoz@redhat.com>
2024-03-13 10:03:22 +0100

    python: ovs: flowviz: Add file processing infra.
    process.py contains a useful base class that processes files
    odp.py and ofp.py: contain datapath and openflow subcommand definitions
    as well as the first formatting option: json.
    
    Also, this patch adds basic filtering support.
    
    Examples:
    $ ovs-ofctl dump-flows br-int | ovs-flowviz openflow json
    $ ovs-ofctl dump-flows br-int > flows.txt && ovs-flowviz -i flows.txt openflow json
    $ ovs-ofctl appctl dpctl/dump-flows | ovs-flowviz -f 'ct' datapath json
    $ ovs-ofctl appctl dpctl/dump-flows > flows.txt && ovs-flowviz -i flows.txt -f 'drop' datapath json
    
    Signed-off-by: Adrian Moreno <amorenoz@redhat.com>
    Acked-by: Eelco Chaudron <echaudro@redhat.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ python/ovs/flowviz/ofp/cli.py
@@ -0,0 +1,42 @@
+# Copyright (c) 2023 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import click
+
+from ovs.flowviz.main import maincli
+from ovs.flowviz.process import (
+    OpenFlowFactory,
+    JSONProcessor,
+)
+
+
+@maincli.group(subcommand_metavar="FORMAT")
+@click.pass_obj
+def openflow(opts):
+    """Process OpenFlow Flows."""
+    pass
+
+
+class JSONPrint(OpenFlowFactory, JSONProcessor):
+    def __init__(self, opts):
+        super().__init__(opts)
+
+
+@openflow.command()
+@click.pass_obj
+def json(opts):
+    """Print the flows in JSON format."""
+    proc = JSONPrint(opts)
+    proc.process()
+    print(proc.json_string())
