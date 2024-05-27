5e98290ebca7a834cca576bb951c3f8dd211b50e
Eelco Chaudron <echaudro@redhat.com>
2024-05-27 13:01:51 +0200

    db-ctl-base: Initialize the output variable in the ctx structure.
    Coverity was flagged that the uninitialized output variable was used
    in the ctl_context_init_command() function. This patch initializes
    the variable.
    
    Fixes: 07ff77ccb82a ("db-ctl-base: Make common database command code into library.")
    Signed-off-by: Eelco Chaudron <echaudro@redhat.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/db-ctl-base.c
+++ lib/db-ctl-base.c
@@ -2656,6 +2656,7 @@ ctl_context_init(struct ctl_context *ctx, struct ctl_command *command,
                  struct ovsdb_symbol_table *symtab,
                  void (*invalidate_cache_cb)(struct ctl_context *))
 {
+    ds_init(&ctx->output);
     if (command) {
         ctl_context_init_command(ctx, command, false);
     }
