ovsrobot/ovs

commit 53178986d7fc86bcfc2f297b547a97ee71a21bb7

ovsdb: Add support for online schema conversion.
With this change, "ovsdb-client convert" can be used to convert a database
from one schema to another without taking the database offline.

This can be useful to minimize downtime for a database during a software
upgrade.

Signed-off-by: Ben Pfaff <blp@ovn.org>
Acked-by: Justin Pettit <jpettit@ovn.org>

ovsdb/transaction.c
@@ -806,8 +806,14 @@ update_version(struct ovsdb_txn *txn OVS_UNUSED, struct ovsdb_txn_row *txn_row)
     return NULL;
 }
 
-static struct ovsdb_error *
-ovsdb_txn_commit_(struct ovsdb_txn *txn, bool durable)
+static bool
+ovsdb_txn_is_empty(const struct ovsdb_txn *txn)
+{
+    return ovs_list_is_empty(&txn->txn_tables);
+}
+
+struct ovsdb_error * OVS_WARN_UNUSED_RESULT
+ovsdb_txn_start_commit(struct ovsdb_txn *txn)
 {
     struct ovsdb_error *error;
 
@@ -818,29 +824,25 @@ ovsdb_txn_commit_(struct ovsdb_txn *txn, bool durable)
         ovsdb_txn_abort(txn);
         return OVSDB_WRAP_BUG("can't happen", error);
     }
-    if (ovs_list_is_empty(&txn->txn_tables)) {
-        ovsdb_txn_abort(txn);
+    if (ovsdb_txn_is_empty(txn)) {
         return NULL;
     }
 
     /* Update reference counts and check referential integrity. */
     error = update_ref_counts(txn);
     if (error) {
-        ovsdb_txn_abort(txn);
         return error;
     }
 
     /* Delete unreferenced, non-root rows. */
     error = for_each_txn_row(txn, collect_garbage);
     if (error) {
-        ovsdb_txn_abort(txn);
         return OVSDB_WRAP_BUG("can't happen", error);
     }
 
     /* Check maximum rows table constraints. */
     error = check_max_rows(txn);
     if (error) {
-        ovsdb_txn_abort(txn);
         return error;
     }
 
@@ -848,14 +850,12 @@ ovsdb_txn_commit_(struct ovsdb_txn *txn, bool durable)
      * integrity. */
     error = for_each_txn_row(txn, assess_weak_refs);
     if (error) {
-        ovsdb_txn_abort(txn);
         return error;
     }
 
     /* Verify that the indexes will still be unique post-transaction. */
     error = for_each_txn_row(txn, check_index_uniqueness);
     if (error) {
-        ovsdb_txn_abort(txn);
         return error;
     }
 
@@ -865,14 +865,23 @@ ovsdb_txn_commit_(struct ovsdb_txn *txn, bool durable)
         return OVSDB_WRAP_BUG("can't happen", error);
     }
 
-    /* Commit to disk and send the commit to each replica. */
+    return NULL;
+}
+
+struct ovsdb_error *
+ovsdb_txn_finish_commit(struct ovsdb_txn *txn, bool durable)
+{
+    /* Commit to disk. */
     if (txn->db->file) {
-        error = ovsdb_file_commit(txn->db->file, txn, durable);
+        struct ovsdb_error *error = ovsdb_file_commit(txn->db->file, txn,
+                                                      durable);
         if (error) {
             ovsdb_txn_abort(txn);
             return error;
         }
     }
+
+    /* Send the transaction to each monitor. */
     ovsdb_monitors_commit(txn->db, txn);
 
     /* Finalize commit. */
@@ -887,10 +896,12 @@ ovsdb_txn_commit_(struct ovsdb_txn *txn, bool durable)
 struct ovsdb_error *
 ovsdb_txn_commit(struct ovsdb_txn *txn, bool durable)
 {
-   struct ovsdb_error *err;
-
-   PERF(__func__, err = ovsdb_txn_commit_(txn, durable));
-   return err;
+    struct ovsdb_error *error = ovsdb_txn_start_commit(txn);
+    if (error || ovsdb_txn_is_empty(txn)) {
+        ovsdb_txn_abort(txn);
+        return error;
+    }
+    return ovsdb_txn_finish_commit(txn, durable);
 }
 
 void

lib/ovsdb-data.c
@@ -1684,6 +1684,19 @@ ovsdb_datum_from_smap(struct ovsdb_datum *datum, const struct smap *smap)
     ovsdb_datum_sort_unique(datum, OVSDB_TYPE_STRING, OVSDB_TYPE_STRING);
 }
 
+struct ovsdb_error * OVS_WARN_UNUSED_RESULT
+ovsdb_datum_convert(struct ovsdb_datum *dst,
+                    const struct ovsdb_type *dst_type,
+                    const struct ovsdb_datum *src,
+                    const struct ovsdb_type *src_type)
+{
+    struct json *json = ovsdb_datum_to_json(src, src_type);
+    struct ovsdb_error *error = ovsdb_datum_from_json(dst, dst_type, json,
+                                                      NULL);
+    json_destroy(json);
+    return error;
+}
+
 static uint32_t
 hash_atoms(enum ovsdb_atomic_type type, const union ovsdb_atom *atoms,
            unsigned int n, uint32_t basis)

lib/ovsdb-data.h
@@ -192,6 +192,12 @@ void ovsdb_datum_to_bare(const struct ovsdb_datum *,
 
 void ovsdb_datum_from_smap(struct ovsdb_datum *, const struct smap *);
 
+struct ovsdb_error *ovsdb_datum_convert(struct ovsdb_datum *dst,
+                                        const struct ovsdb_type *dst_type,
+                                        const struct ovsdb_datum *src,
+                                        const struct ovsdb_type *src_type)
+    OVS_WARN_UNUSED_RESULT;
+
 /* Comparison. */
 uint32_t ovsdb_datum_hash(const struct ovsdb_datum *,
                           const struct ovsdb_type *, uint32_t basis);

ovsdb/file.c
@@ -570,23 +570,31 @@ ovsdb_file_txn_annotate(struct json *json, const char *comment)
     return json;
 }
 
+/* Returns 'txn' transformed into the JSON format that is used in OVSDB files.
+ * (But the caller must use ovsdb_file_txn_annotate() to add the _comment and
+ * _date members.)  If 'txn' doesn't actually change anything, returns NULL */
+static struct json *
+ovsdb_file_txn_to_json(const struct ovsdb_txn *txn)
+{
+    struct ovsdb_file_txn ftxn;
+
+    ovsdb_file_txn_init(&ftxn);
+    ovsdb_txn_for_each_change(txn, ovsdb_file_change_cb, &ftxn);
+    return ftxn.json;
+}
+
 struct ovsdb_error *
 ovsdb_file_commit(struct ovsdb_file *file,
                   const struct ovsdb_txn *txn, bool durable)
 {
-    struct ovsdb_file_txn ftxn;
-    struct ovsdb_error *error;
-    long long int current_time;
-
-    ovsdb_file_txn_init(&ftxn);
-    ovsdb_txn_for_each_change(txn, ovsdb_file_change_cb, &ftxn);
-    if (!ftxn.json) {
+    struct json *txn_json = ovsdb_file_txn_to_json(txn);
+    if (!txn_json) {
         /* Nothing to commit. */
         return NULL;
     }
 
-    error = ovsdb_file_txn_commit(ftxn.json, ovsdb_txn_get_comment(txn),
-                                  durable, file->log);
+    struct ovsdb_error *error = ovsdb_file_txn_commit(
+        txn_json, ovsdb_txn_get_comment(txn), durable, file->log);
     if (error) {
         return error;
     }
@@ -599,7 +607,7 @@ ovsdb_file_commit(struct ovsdb_file *file,
      * size of the previous snapshot, then compact the database. However, if
      * it has been over COMPACT_MAX_MSEC ms since the last compaction, the
      * database size is not taken into account. */
-    current_time = time_msec();
+    long long int current_time = time_msec();
     if (current_time >= file->next_compact
         && file->n_transactions >= 100
         && (current_time - file->last_compact >= COMPACT_MAX_MSEC
@@ -856,4 +864,116 @@ ovsdb_file_txn_commit(struct json *json, const char *comment,
     }
 
     return NULL;
+}
+
+static struct ovsdb_error * OVS_WARN_UNUSED_RESULT
+ovsdb_convert_table(struct ovsdb_txn *txn,
+                    const struct ovsdb_table *src_table,
+                    struct ovsdb_table *dst_table)
+{
+    const struct ovsdb_row *src_row;
+    HMAP_FOR_EACH (src_row, hmap_node, &src_table->rows) {
+        struct ovsdb_row *dst_row = ovsdb_row_create(dst_table);
+        *ovsdb_row_get_uuid_rw(dst_row) = *ovsdb_row_get_uuid(src_row);
+
+        struct shash_node *node;
+        SHASH_FOR_EACH (node, &src_table->schema->columns) {
+            const struct ovsdb_column *src_column = node->data;
+            if (src_column->index == OVSDB_COL_UUID ||
+                src_column->index == OVSDB_COL_VERSION) {
+                continue;
+            }
+
+            const struct ovsdb_column *dst_column
+                = shash_find_data(&dst_table->schema->columns,
+                                  src_column->name);
+            if (!dst_column) {
+                continue;
+            }
+
+            struct ovsdb_error *error = ovsdb_datum_convert(
+                &dst_row->fields[dst_column->index], &dst_column->type,
+                &src_row->fields[src_column->index], &src_column->type);
+            if (error) {
+                ovsdb_row_destroy(dst_row);
+                return error;
+            }
+        }
+
+        ovsdb_txn_row_insert(txn, dst_row);
+    }
+    return NULL;
+}
+
+struct ovsdb_error * OVS_WARN_UNUSED_RESULT
+ovsdb_file_convert(const struct ovsdb_file *file,
+                   const struct ovsdb_schema *new_schema)
+{
+    struct ovsdb *new_db = ovsdb_create(ovsdb_schema_clone(new_schema));
+    struct ovsdb_txn *txn = ovsdb_txn_create(new_db);
+    struct ovsdb_error *error = NULL;
+
+    struct shash_node *node;
+    SHASH_FOR_EACH (node, &file->db->tables) {
+        const char *table_name = node->name;
+        const struct ovsdb_table *src_table = node->data;
+        struct ovsdb_table *dst_table = shash_find_data(&new_db->tables,
+                                                        table_name);
+        if (!dst_table) {
+            continue;
+        }
+
+        error = ovsdb_convert_table(txn, src_table, dst_table);
+        if (error) {
+            goto error;
+        }
+    }
+
+    error = ovsdb_txn_start_commit(txn);
+    if (error) {
+        goto error;
+    }
+
+    struct ovsdb_log *new;
+    error = ovsdb_log_replace_start(file->log, &new);
+    if (error) {
+        goto error;
+    }
+
+    /* Write schema. */
+    struct json *schema_json = ovsdb_schema_to_json(new_schema);
+    error = ovsdb_log_write(new, schema_json);
+    json_destroy(schema_json);
+    if (error) {
+        goto error;
+    }
+
+    /* Write data. */
+    struct json *txn_json = ovsdb_file_txn_to_json(txn);
+    if (txn_json) {
+        error = ovsdb_log_write(new, txn_json);
+        json_destroy(txn_json);
+        if (error) {
+            goto error;
+        }
+    }
+
+    error = ovsdb_log_replace_commit(file->log, new);
+    if (error) {
+        goto error;
+    }
+
+    error = ovsdb_txn_finish_commit(txn, true);
+    ovs_assert(!error);         /* Can't happen. */
+
+    ovsdb_replace(file->db, new_db);
+
+    return NULL;
+
+error:
+    ovsdb_destroy(new_db);
+    if (txn) {
+        ovsdb_txn_abort(txn);
+    }
+    return error;
 }

ovsdb/file.h
@@ -51,4 +51,8 @@ void ovsdb_file_destroy(struct ovsdb_file *);
 
 struct json *ovsdb_file_txn_annotate(struct json *, const char *comment);
 
+struct ovsdb_error *ovsdb_file_convert(const struct ovsdb_file *,
+                                       const struct ovsdb_schema *)
+    OVS_WARN_UNUSED_RESULT;
+
 #endif /* ovsdb/file.h */

ovsdb/jsonrpc-server.c
@@ -82,7 +82,7 @@ static void ovsdb_jsonrpc_session_send(struct ovsdb_jsonrpc_session *,
 /* Triggers. */
 static void ovsdb_jsonrpc_trigger_create(struct ovsdb_jsonrpc_session *,
                                          struct ovsdb *,
-                                         struct json *id, struct json *params);
+                                         struct jsonrpc_msg *request);
 static struct ovsdb_jsonrpc_trigger *ovsdb_jsonrpc_trigger_find(
     struct ovsdb_jsonrpc_session *, const struct json *id, size_t hash);
 static void ovsdb_jsonrpc_trigger_complete(struct ovsdb_jsonrpc_trigger *);
@@ -930,17 +930,6 @@ ovsdb_jsonrpc_session_unlock(struct ovsdb_jsonrpc_session *s,
 }
 
 static struct jsonrpc_msg *
-execute_transaction(struct ovsdb_jsonrpc_session *s, struct ovsdb *db,
-                    struct jsonrpc_msg *request)
-{
-    ovsdb_jsonrpc_trigger_create(s, db, request->id, request->params);
-    request->id = NULL;
-    request->params = NULL;
-    jsonrpc_msg_destroy(request);
-    return NULL;
-}
-
-static struct jsonrpc_msg *
 ovsdb_jsonrpc_session_set_db_change_aware(struct ovsdb_jsonrpc_session *s,
                                           const struct jsonrpc_msg *request)
 {
@@ -961,10 +950,11 @@ ovsdb_jsonrpc_session_got_request(struct ovsdb_jsonrpc_session *s,
 {
     struct jsonrpc_msg *reply;
 
-    if (!strcmp(request->method, "transact")) {
+    if (!strcmp(request->method, "transact") ||
+        !strcmp(request->method, "convert")) {
         struct ovsdb *db = ovsdb_jsonrpc_lookup_db(s, request, &reply);
         if (!reply) {
-            reply = execute_transaction(s, db, request);
+            ovsdb_jsonrpc_trigger_create(s, db, request);
         }
     } else if (!strcmp(request->method, "monitor") ||
                (monitor_cond_enable__ && !strcmp(request->method,
@@ -1076,36 +1066,34 @@ struct ovsdb_jsonrpc_trigger {
 
 static void
 ovsdb_jsonrpc_trigger_create(struct ovsdb_jsonrpc_session *s, struct ovsdb *db,
-                             struct json *id, struct json *params)
+                             struct jsonrpc_msg *request)
 {
-    struct ovsdb_jsonrpc_trigger *t;
-    size_t hash;
-
     /* Check for duplicate ID. */
-    hash = json_hash(id, 0);
-    t = ovsdb_jsonrpc_trigger_find(s, id, hash);
+    size_t hash = json_hash(request->id, 0);
+    struct ovsdb_jsonrpc_trigger *t
+        = ovsdb_jsonrpc_trigger_find(s, request->id, hash);
     if (t) {
-        struct jsonrpc_msg *msg;
-
-        msg = jsonrpc_create_error(json_string_create("duplicate request ID"),
-                                   id);
-        ovsdb_jsonrpc_session_send(s, msg);
-        json_destroy(id);
-        json_destroy(params);
+        ovsdb_jsonrpc_session_send(
+            s, syntax_error_reply(request, "duplicate request ID"));
+        jsonrpc_msg_destroy(request);
         return;
     }
 
     /* Insert into trigger table. */
     t = xmalloc(sizeof *t);
-    ovsdb_trigger_init(&s->up, db, &t->trigger, params, time_msec(),
-                       s->read_only, s->remote->role,
-                       jsonrpc_session_get_id(s->js));
-    t->id = id;
+    bool disconnect_all = ovsdb_trigger_init(
+        &s->up, db, &t->trigger, request, time_msec(), s->read_only,
+        s->remote->role, jsonrpc_session_get_id(s->js));
+    t->id = json_clone(request->id);
     hmap_insert(&s->triggers, &t->hmap_node, hash);
 
     /* Complete early if possible. */
     if (ovsdb_trigger_is_complete(&t->trigger)) {
         ovsdb_jsonrpc_trigger_complete(t);
+    }
+
+    if (disconnect_all) {
+        ovsdb_jsonrpc_server_reconnect(s->remote->server, false);
     }
 }
 
@@ -1133,12 +1121,9 @@ ovsdb_jsonrpc_trigger_complete(struct ovsdb_jsonrpc_trigger *t)
 
     if (jsonrpc_session_is_connected(s->js)) {
         struct jsonrpc_msg *reply;
-        struct json *result;
 
-        result = ovsdb_trigger_steal_result(&t->trigger);
-        if (result) {
-            reply = jsonrpc_create_reply(result, t->id);
-        } else {
+        reply = ovsdb_trigger_steal_reply(&t->trigger);
+        if (!reply) {
             reply = jsonrpc_create_error(json_string_create("canceled"),
                                          t->id);
         }
@@ -1153,7 +1138,7 @@ ovsdb_jsonrpc_trigger_complete(struct ovsdb_jsonrpc_trigger *t)
 
 static void
 ovsdb_jsonrpc_trigger_remove__(struct ovsdb_jsonrpc_session *s,
-                                   struct ovsdb *db)
+                               struct ovsdb *db)
 {
     struct ovsdb_jsonrpc_trigger *t, *next;
     HMAP_FOR_EACH_SAFE (t, next, hmap_node, &s->triggers) {
@@ -1183,11 +1168,9 @@ ovsdb_jsonrpc_trigger_complete_all(struct ovsdb_jsonrpc_session *s)
 static void
 ovsdb_jsonrpc_trigger_complete_done(struct ovsdb_jsonrpc_session *s)
 {
-    while (!ovs_list_is_empty(&s->up.completions)) {
-        struct ovsdb_jsonrpc_trigger *t
-            = CONTAINER_OF(s->up.completions.next,
-                           struct ovsdb_jsonrpc_trigger, trigger.node);
-        ovsdb_jsonrpc_trigger_complete(t);
+    struct ovsdb_jsonrpc_trigger *trigger, *next;
+    LIST_FOR_EACH_SAFE (trigger, next, trigger.node, &s->up.completions) {
+        ovsdb_jsonrpc_trigger_complete(trigger);
     }
 }
 
@@ -1435,7 +1418,7 @@ ovsdb_jsonrpc_monitor_create(struct ovsdb_jsonrpc_session *s, struct ovsdb *db,
 
 error:
     if (m) {
-        ovsdb_jsonrpc_monitor_destroy(m);
+        ovsdb_jsonrpc_monitor_destroy(m, false);
     }
 
     return jsonrpc_create_error(ovsdb_error_to_json_free(error), request_id);
@@ -1592,28 +1575,8 @@ ovsdb_jsonrpc_monitor_cancel(struct ovsdb_jsonrpc_session *s,
             return jsonrpc_create_error(json_string_create("unknown monitor"),
                                         request_id);
         } else {
-            ovsdb_jsonrpc_monitor_destroy(m);
+            ovsdb_jsonrpc_monitor_destroy(m, false);
             return jsonrpc_create_reply(json_object_create(), request_id);
-        }
-    }
-}
-
-static void
-ovsdb_jsonrpc_monitor_remove__(struct ovsdb_jsonrpc_session *s,
-                               struct ovsdb *db)
-{
-    struct ovsdb_jsonrpc_monitor *m, *next;
-
-    HMAP_FOR_EACH_SAFE (m, next, node, &s->monitors) {
-        if (!db || m->db == db) {
-            if (db && jsonrpc_session_is_connected(s->js)
-                && s->db_change_aware) {
-                struct jsonrpc_msg *notify = jsonrpc_create_notify(
-                    "monitor_canceled",
-                    json_array_create_1(json_clone(m->monitor_id)));
-                ovsdb_jsonrpc_session_send(s, notify);
-            }
-            ovsdb_jsonrpc_monitor_destroy(m);
         }
     }
 }
@@ -1625,14 +1588,24 @@ ovsdb_jsonrpc_monitor_preremove_db(struct ovsdb_jsonrpc_session *s,
                                    struct ovsdb *db)
 {
     ovs_assert(db);
-    ovsdb_jsonrpc_monitor_remove__(s, db);
+
+    struct ovsdb_jsonrpc_monitor *m, *next;
+    HMAP_FOR_EACH_SAFE (m, next, node, &s->monitors) {
+        if (m->db == db) {
+            ovsdb_jsonrpc_monitor_destroy(m, true);
+        }
+    }
 }
 
 /* Cancels all monitors in 's'. */
 static void
 ovsdb_jsonrpc_monitor_remove_all(struct ovsdb_jsonrpc_session *s)
 {
-    ovsdb_jsonrpc_monitor_remove__(s, NULL);
+    struct ovsdb_jsonrpc_monitor *m, *next;
+
+    HMAP_FOR_EACH_SAFE (m, next, node, &s->monitors) {
+        ovsdb_jsonrpc_monitor_destroy(m, false);
+    }
 }
 
 static struct json *
@@ -1663,8 +1636,19 @@ ovsdb_jsonrpc_monitor_needs_flush(struct ovsdb_jsonrpc_session *s)
 }
 
 void
-ovsdb_jsonrpc_monitor_destroy(struct ovsdb_jsonrpc_monitor *m)
+ovsdb_jsonrpc_monitor_destroy(struct ovsdb_jsonrpc_monitor *m,
+                              bool notify_cancellation)
 {
+    if (notify_cancellation) {
+        struct ovsdb_jsonrpc_session *s = m->session;
+        if (jsonrpc_session_is_connected(s->js) && s->db_change_aware) {
+            struct jsonrpc_msg *notify = jsonrpc_create_notify(
+                "monitor_canceled",
+                json_array_create_1(json_clone(m->monitor_id)));
+            ovsdb_jsonrpc_session_send(s, notify);
+        }
+    }
+
     json_destroy(m->monitor_id);
     hmap_remove(&m->session->monitors, &m->node);
     ovsdb_monitor_remove_jsonrpc_monitor(m->dbmon, m, m->unflushed);

ovsdb/jsonrpc-server.h
@@ -79,7 +79,8 @@ const struct uuid *ovsdb_jsonrpc_server_get_uuid(
     const struct ovsdb_jsonrpc_server *);
 
 struct ovsdb_jsonrpc_monitor;
-void ovsdb_jsonrpc_monitor_destroy(struct ovsdb_jsonrpc_monitor *);
+void ovsdb_jsonrpc_monitor_destroy(struct ovsdb_jsonrpc_monitor *,
+                                   bool notify_cancellation);
 void ovsdb_jsonrpc_disable_monitor_cond(void);
 
 #endif /* ovsdb/jsonrpc-server.h */

ovsdb/monitor.c
@@ -1612,7 +1612,7 @@ ovsdb_monitors_remove(struct ovsdb *db)
         /* Delete all front-end monitors.  Removing the last front-end monitor
          * will also destroy the corresponding ovsdb_monitor. */
         LIST_FOR_EACH_SAFE (jm, next_jm, node, &m->jsonrpc_monitors) {
-            ovsdb_jsonrpc_monitor_destroy(jm->jsonrpc_monitor);
+            ovsdb_jsonrpc_monitor_destroy(jm->jsonrpc_monitor, false);
         }
     }
 }
@@ -1627,5 +1627,21 @@ ovsdb_monitor_get_memory_usage(struct simap *usage)
 
     HMAP_FOR_EACH(dbmon, hmap_node,  &ovsdb_monitors) {
         simap_increase(usage, "json-caches", hmap_count(&dbmon->json_cache));
+    }
+}
+
+void
+ovsdb_monitor_prereplace_db(struct ovsdb *db)
+{
+    struct ovsdb_monitor *m, *next_m;
+
+    LIST_FOR_EACH_SAFE (m, next_m, list_node, &db->monitors) {
+        struct jsonrpc_monitor_node *jm, *next_jm;
+
+        /* Delete all front-end monitors.  Removing the last front-end monitor
+         * will also destroy the corresponding ovsdb_monitor. */
+        LIST_FOR_EACH_SAFE (jm, next_jm, node, &m->jsonrpc_monitors) {
+            ovsdb_jsonrpc_monitor_destroy(jm->jsonrpc_monitor, true);
+        }
     }
 }

commit 60e64f0b247f565a647f37a6c0be9c9682a41347

ovsdb: transaction: Remove incorrect transaction abort in pre-commit.
Pre-commit must not abort the transaction, otherwise the upper layers
may crash accessing it.  E.g. ovsdb_trigger_try() checks the state of
the transaction after trying to commit it.

This particular failure can't actually happen, because the function
determine_changes() can't fail.  However, the code is still wrong and
a bit misleading, so should be fixed.

Fixes: 53178986d7fc ("ovsdb: Add support for online schema conversion.")
Signed-off-by: Ilya Maximets <i.maximets@ovn.org>
Signed-off-by: 0-day Robot <robot@bytheb.org>

ovsdb/transaction.c
@@ -1090,7 +1090,6 @@ ovsdb_txn_precommit(struct ovsdb_txn *txn)
      * was really a no-op. */
     error = for_each_txn_row(txn, determine_changes);
     if (error) {
-        ovsdb_txn_abort(txn);
         return OVSDB_WRAP_BUG("can't happen", error);
     }
     if (ovs_list_is_empty(&txn->txn_tables)) {