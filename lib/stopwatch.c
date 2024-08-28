ovsrobot/ovs

commit 0e124db81f6374ce37b3e73b066d33f534c96327

stopwatch: Add API for waiting until samples have been processed
Will be used for testing the module.

Signed-off-by: Jakub Sitnicki <jkbs@redhat.com>
Signed-off-by: Mark Michelson <mmichels@redhat.com>
Signed-off-by: Ben Pfaff <blp@ovn.org>

lib/stopwatch.c
@@ -66,6 +66,7 @@ struct stopwatch {
 enum stopwatch_op {
     OP_START_SAMPLE,
     OP_END_SAMPLE,
+    OP_SYNC,
     OP_RESET,
     OP_SHUTDOWN,
 };
@@ -78,6 +79,7 @@ struct stopwatch_packet {
 
 static struct shash stopwatches = SHASH_INITIALIZER(&stopwatches);
 static struct ovs_mutex stopwatches_lock = OVS_MUTEX_INITIALIZER;
+static pthread_cond_t stopwatches_sync = PTHREAD_COND_INITIALIZER;
 
 static int stopwatch_pipe[2];
 static pthread_t stopwatch_thread_id;
@@ -233,12 +235,12 @@ add_sample(struct stopwatch *sw, unsigned long long new_sample)
 }
 
 static bool
-performance_get_stats_protected(const char *name,
-                                struct performance_stats *stats)
+stopwatch_get_stats_protected(const char *name,
+                                struct stopwatch_stats *stats)
 {
-    struct performance *perf;
+    struct stopwatch *perf;
 
-    perf = shash_find_data(&performances, name);
+    perf = shash_find_data(&stopwatches, name);
     if (!perf) {
         return false;
     }
@@ -255,13 +257,13 @@ performance_get_stats_protected(const char *name,
 }
 
 bool
-performance_get_stats(const char *name, struct performance_stats *stats)
+stopwatch_get_stats(const char *name, struct stopwatch_stats *stats)
 {
     bool found = false;
 
-    ovs_mutex_lock(&performances_lock);
-    found = performance_get_stats_protected(name, stats);
-    ovs_mutex_unlock(&performances_lock);
+    ovs_mutex_lock(&stopwatches_lock);
+    found = stopwatch_get_stats_protected(name, stats);
+    ovs_mutex_unlock(&stopwatches_lock);
 
     return found;
 }
@@ -413,6 +415,9 @@ stopwatch_thread(void *ign OVS_UNUSED)
             case OP_END_SAMPLE:
                 stopwatch_end_sample_protected(&pkt);
                 break;
+            case OP_SYNC:
+                xpthread_cond_signal(&stopwatches_sync);
+                break;
             case OP_RESET:
                 stopwatch_reset_protected(&pkt);
                 break;
@@ -514,4 +519,17 @@ stopwatch_stop(const char *name, unsigned long long ts)
     };
     ovs_strlcpy(pkt.name, name, sizeof(pkt.name));
     write(stopwatch_pipe[1], &pkt, sizeof(pkt));
+}
+
+void
+stopwatch_sync(void)
+{
+    struct stopwatch_packet pkt = {
+        .op = OP_SYNC,
+    };
+
+    ovs_mutex_lock(&stopwatches_lock);
+    write(stopwatch_pipe[1], &pkt, sizeof(pkt));
+    ovs_mutex_cond_wait(&stopwatches_sync, &stopwatches_lock);
+    ovs_mutex_unlock(&stopwatches_lock);
 }

lib/stopwatch.h
@@ -51,4 +51,7 @@ void stopwatch_stop(const char *name, unsigned long long ts);
 /* Retrieve statistics calculated from collected samples */
 bool stopwatch_get_stats(const char *name, struct stopwatch_stats *stats);
 
+/* Block until all enqueued samples have been processed. */
+void stopwatch_sync(void);
+
 #endif /* stopwatch.h */

commit 07a19fa860e2abc6aad529d3a2949698d082ea26

stopwatch: Handle spurious wakeups of ovs_mutex_cond_wait().
This fix makes sure spurious wakeups are detected using a
conditional variable in a loop.

Fixes: 0e124db81f63 ("stopwatch: Add API for waiting until samples have been processed")
Signed-off-by: Eelco Chaudron <echaudro@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

lib/stopwatch.c
@@ -84,6 +84,7 @@ struct stopwatch_packet {
 static struct shash stopwatches = SHASH_INITIALIZER(&stopwatches);
 static struct ovs_mutex stopwatches_lock = OVS_MUTEX_INITIALIZER;
 static pthread_cond_t stopwatches_sync = PTHREAD_COND_INITIALIZER;
+static bool stopwatches_sync_requested = false;
 
 static struct latch stopwatch_latch;
 static struct guarded_list stopwatch_commands;
@@ -439,6 +440,7 @@ stopwatch_thread(void *ign OVS_UNUSED)
                 stopwatch_end_sample_protected(pkt);
                 break;
             case OP_SYNC:
+                stopwatches_sync_requested = true;
                 xpthread_cond_signal(&stopwatches_sync);
                 break;
             case OP_RESET:
@@ -547,6 +549,9 @@ stopwatch_sync(void)
     struct stopwatch_packet *pkt = stopwatch_packet_create(OP_SYNC);
     ovs_mutex_lock(&stopwatches_lock);
     stopwatch_packet_write(pkt);
-    ovs_mutex_cond_wait(&stopwatches_sync, &stopwatches_lock);
+    while (!stopwatches_sync_requested) {
+        ovs_mutex_cond_wait(&stopwatches_sync, &stopwatches_lock);
+    }
+    stopwatches_sync_requested = false;
     ovs_mutex_unlock(&stopwatches_lock);
 }