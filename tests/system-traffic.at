ovsrobot/ovs

commit 600125b2c380c02869351e96f0c62ac1aac06547

ofproto: Add ofproto/detrace command to map UFIDs to OpenFlow.
It improves the debugging experience if we can easily get a list of
OpenFlow rules and groups that contribute to the creation of a datapath
flow.

The suggested workflow is:
a. dump datapath flows (along with UUIDs), this also prints the core IDs
(PMD IDs) when applicable.

  $ ovs-appctl dpctl/dump-flows -m
  flow-dump from pmd on cpu core: 7
  ufid:7460db8f..., recirc_id(0), ....

b. dump related OpenFlow rules and groups:
  $ ovs-appctl ofproto/detrace ufid:7460db8f... pmd=7
  cookie=0x12345678, table=0 priority=100,ip,in_port=2,nw_dst=10.0.0.2,actions=resubmit(,1)
  cookie=0x0, table=1 priority=200,actions=group:1
  group_id=1,bucket=bucket_id:0,actions=ct(commit,table=2,nat(dst=20.0.0.2))
  cookie=0x0, table=2 actions=output:1

The new command only shows rules and groups attached to ukeys that are
in states UKEY_VISIBLE or UKEY_OPERATIONAL.  That should be fine as all
other ukeys should not be relevant for the use case presented above.

This commit tries to mimic the output format of the ovs-ofctl
dump-flows/dump-groups commands.

Signed-off-by: Dumitru Ceara <dceara@redhat.com>
Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

ofproto/ofproto-dpif-upcall.c
@@ -383,6 +383,7 @@ static void upcall_unixctl_disable_ufid(struct unixctl_conn *, int argc,
                                               const char *argv[], void *aux);
 static void upcall_unixctl_enable_ufid(struct unixctl_conn *, int argc,
                                              const char *argv[], void *aux);
+
 static void upcall_unixctl_set_flow_limit(struct unixctl_conn *conn, int argc,
                                             const char *argv[], void *aux);
 static void upcall_unixctl_dump_wait(struct unixctl_conn *conn, int argc,
@@ -393,6 +394,9 @@ static void upcall_unixctl_pause(struct unixctl_conn *conn, int argc,
                                  const char *argv[], void *aux);
 static void upcall_unixctl_resume(struct unixctl_conn *conn, int argc,
                                   const char *argv[], void *aux);
+
+static void upcall_unixctl_ofproto_detrace(struct unixctl_conn *, int argc,
+                                           const char *argv[], void *aux);
 
 static struct udpif_key *ukey_create_from_upcall(struct upcall *,
                                                  struct flow_wildcards *);
@@ -470,6 +474,8 @@ udpif_init(void)
                                  upcall_unixctl_pause, NULL);
         unixctl_command_register("revalidator/resume", NULL, 0, 0,
                                  upcall_unixctl_resume, NULL);
+        unixctl_command_register("ofproto/detrace", "UFID [pmd=PMD-ID]", 1, 2,
+                                 upcall_unixctl_ofproto_detrace, NULL);
         ovsthread_once_done(&once);
     }
 }
@@ -3308,6 +3314,51 @@ upcall_unixctl_resume(struct unixctl_conn *conn, int argc OVS_UNUSED,
         udpif_resume_revalidators(udpif);
     }
     unixctl_command_reply(conn, "");
+}
+
+static void
+upcall_unixctl_ofproto_detrace(struct unixctl_conn *conn, int argc,
+                               const char *argv[], void *aux OVS_UNUSED)
+{
+    unsigned int pmd_id = NON_PMD_CORE_ID;
+    const char *key_s = argv[1];
+    ovs_u128 ufid;
+
+    if (odp_ufid_from_string(key_s, &ufid) <= 0) {
+        unixctl_command_reply_error(conn, "failed to parse ufid");
+        return;
+    }
+
+    if (argc == 3) {
+        const char *pmd_str = argv[2];
+        if (!ovs_scan(pmd_str, "pmd=%d", &pmd_id)) {
+            unixctl_command_reply_error(conn,
+                                        "Invalid pmd argument format. "
+                                        "Expecting 'pmd=PMD-ID'");
+            return;
+        }
+    }
+
+    struct ds ds = DS_EMPTY_INITIALIZER;
+    struct udpif *udpif;
+
+    LIST_FOR_EACH (udpif, list_node, &all_udpifs) {
+        struct udpif_key *ukey = ukey_lookup(udpif, &ufid, pmd_id);
+        if (!ukey) {
+            continue;
+        }
+
+        ovs_mutex_lock(&ukey->mutex);
+        /* It only makes sense to format rules for ukeys that are (still)
+         * in use. */
+        if ((ukey->state == UKEY_VISIBLE || ukey->state == UKEY_OPERATIONAL)
+            && ukey->xcache) {
+            xlate_xcache_format(&ds, ukey->xcache);
+        }
+        ovs_mutex_unlock(&ukey->mutex);
+    }
+    unixctl_command_reply(conn, ds_cstr(&ds));
+    ds_destroy(&ds);
 }
 
 

tests/ofproto-dpif.at
@@ -12439,3 +12439,59 @@ Datapath actions: psample(group=42,cookie=0x64000000c8),drop
 
 OVS_VSWITCHD_STOP("/Enabling an unsupported feature is very dangerous/d")
 AT_CLEANUP
+
+AT_SETUP([ofproto-dpif - Dump OF rules corresponding to UFID])
+OVS_VSWITCHD_START
+
+add_of_ports br0 1 2 3
+
+dnl Add some OpenFlow rules and groups.
+AT_DATA([groups.txt], [dnl
+group_id=1,type=select,selection_method=dp_hash,bucket=bucket_id:0,weight:100,actions=ct(commit,table=2,nat(dst=20.0.0.2))
+group_id=2,type=all,bucket=resubmit(,3),bucket=resubmit(,4)
+])
+AT_DATA([flows.txt], [dnl
+table=0,priority=100,cookie=0x12345678,in_port=p1,ip,nw_dst=10.0.0.2,actions=resubmit(,1)
+table=1,priority=200,ip,actions=group:1
+table=2,ip,actions=group:2
+table=3,ip,actions=p2
+table=4,ip,actions=p3
+])
+AT_CHECK([ovs-ofctl add-groups br0 groups.txt])
+AT_CHECK([ovs-ofctl add-flows br0 flows.txt])
+
+AT_CHECK([ovs-appctl netdev-dummy/receive p1 'ipv4(src=10.0.0.1,dst=10.0.0.2,proto=6),tcp(src=1,dst=2)'])
+AT_CHECK([ovs-appctl netdev-dummy/receive p1 'ipv4(src=10.0.0.1,dst=10.0.0.2,proto=6),tcp(src=1,dst=2)'])
+AT_CHECK([ovs-appctl revalidator/wait])
+AT_CHECK([ovs-appctl revalidator/pause])
+
+AT_CHECK([ovs-appctl dpctl/dump-flows | strip_used | strip_stats | strip_duration | strip_dp_hash | sort], [0], [dnl
+flow-dump from the main thread:
+recirc_id(0),in_port(1),packet_type(ns=0,id=0),eth_type(0x0800),ipv4(dst=10.0.0.2,frag=no), packets:0, bytes:0, used:0.0s, actions:hash(l4(0)),recirc(0x1)
+recirc_id(0x1),in_port(1),packet_type(ns=0,id=0),eth_type(0x0800),ipv4(frag=no), packets:0, bytes:0, used:0.0s, actions:ct(commit,nat(dst=20.0.0.2)),recirc(0x2)
+recirc_id(0x2),in_port(1),packet_type(ns=0,id=0),eth_type(0x0800),ipv4(frag=no), packets:0, bytes:0, used:0.0s, actions:2,3
+])
+
+ufid=$(ovs-appctl dpctl/dump-flows -m filter='recirc_id(0)' | parse_ufid)
+AT_CHECK([ovs-appctl ofproto/detrace $ufid | ofctl_strip], [0], [dnl
+cookie=0x12345678, n_packets=2, n_bytes=236, priority=100,ip,in_port=1,nw_dst=10.0.0.2,actions=resubmit(,1)
+table_id=1, n_packets=2, n_bytes=236, priority=200,ip,actions=group:1
+])
+
+ufid=$(ovs-appctl dpctl/dump-flows -m filter='recirc_id(0x1)' | parse_ufid)
+AT_CHECK([ovs-appctl ofproto/detrace $ufid | ofctl_strip], [0], [dnl
+group_id=1,type=select,selection_method=dp_hash,bucket=bucket_id:0,weight:100,actions=ct(commit,table=2,nat(dst=20.0.0.2))
+])
+
+ufid=$(ovs-appctl dpctl/dump-flows -m filter='recirc_id(0x2)' | parse_ufid)
+AT_CHECK([ovs-appctl ofproto/detrace $ufid | ofctl_strip], [0], [dnl
+table_id=2, n_packets=2, n_bytes=236, ip,actions=group:2
+table_id=3, n_packets=2, n_bytes=236, ip,actions=output:2
+table_id=4, n_packets=2, n_bytes=236, ip,actions=output:3
+group_id=2,type=all,bucket=bucket_id:0,actions=resubmit(,3),bucket=bucket_id:1,actions=resubmit(,4)
+])
+
+AT_CHECK([ovs-appctl revalidator/resume])
+
+OVS_VSWITCHD_STOP
+AT_CLEANUP

include/openvswitch/ofp-group.h
@@ -70,6 +70,10 @@ struct ofputil_bucket *ofputil_bucket_find(const struct ovs_list *,
 bool ofputil_bucket_check_duplicate_id(const struct ovs_list *);
 struct ofputil_bucket *ofputil_bucket_list_front(const struct ovs_list *);
 struct ofputil_bucket *ofputil_bucket_list_back(const struct ovs_list *);
+void ofputil_bucket_format(struct ds *, const struct ofputil_bucket *,
+                           enum ofp11_group_type, enum ofp_version,
+                           const struct ofputil_port_map *,
+                           const struct ofputil_table_map *);
 
 static inline bool
 ofputil_bucket_has_liveness(const struct ofputil_bucket *bucket)
@@ -88,6 +92,8 @@ struct ofputil_group_props {
 void ofputil_group_properties_destroy(struct ofputil_group_props *);
 void ofputil_group_properties_copy(struct ofputil_group_props *to,
                                    const struct ofputil_group_props *from);
+void ofputil_group_properties_format(const struct ofputil_group_props *,
+                                     struct ds *);
 /* Protocol-independent group_mod. */
 struct ofputil_group_mod {
     uint16_t command;             /* One of OFPGC15_*. */
@@ -199,6 +205,14 @@ enum ofperr ofputil_group_desc_format(struct ds *, const struct ofp_header *,
 enum ofperr ofputil_group_features_format(struct ds *,
                                           const struct ofp_header *);
 
+/* Group formatting. */
+void ofputil_group_format(struct ds *s, uint32_t group_id, uint8_t type,
+                          const struct ofputil_bucket *,
+                          const struct ovs_list *p_buckets,
+                          const struct ofputil_group_props *,
+                          enum ofp_version, bool suppress_type,
+                          const struct ofputil_port_map *,
+                          const struct ofputil_table_map *);
 #ifdef __cplusplus
 }
 #endif

lib/ofp-group.c
@@ -1526,6 +1526,31 @@ ofputil_group_properties_destroy(struct ofputil_group_props *gp)
     free(gp->fields.values);
 }
 
+void
+ofputil_group_properties_format(const struct ofputil_group_props *gp,
+                                struct ds *ds)
+{
+    if (!gp->selection_method[0]) {
+        return;
+    }
+
+    ds_put_format(ds, ",selection_method=%s", gp->selection_method);
+    if (gp->selection_method_param) {
+        ds_put_format(ds, ",selection_method_param=%"PRIu64,
+                      gp->selection_method_param);
+    }
+
+    size_t n = bitmap_count1(gp->fields.used.bm, MFF_N_IDS);
+    if (n == 1) {
+        ds_put_cstr(ds, ",fields=");
+        oxm_format_field_array(ds, &gp->fields);
+    } else if (n > 1) {
+        ds_put_cstr(ds, ",fields(");
+        oxm_format_field_array(ds, &gp->fields);
+        ds_put_char(ds, ')');
+    }
+}
+
 static enum ofperr
 parse_group_prop_ntr_selection_method(struct ofpbuf *payload,
                                       enum ofp11_group_type group_type,
@@ -1813,16 +1838,45 @@ ofp_print_bucket_id(struct ds *s, const char *label, uint32_t bucket_id,
     ds_put_char(s, ',');
 }
 
-static void
-ofp_print_group(struct ds *s, uint32_t group_id, uint8_t type,
-                const struct ovs_list *p_buckets,
-                const struct ofputil_group_props *props,
-                enum ofp_version ofp_version, bool suppress_type,
-                const struct ofputil_port_map *port_map,
-                const struct ofputil_table_map *table_map)
+void
+ofputil_bucket_format(struct ds * s, const struct ofputil_bucket *bucket,
+                      enum ofp11_group_type type, enum ofp_version ofp_version,
+                      const struct ofputil_port_map *port_map,
+                      const struct ofputil_table_map *table_map)
 {
-    struct ofputil_bucket *bucket;
+    ds_put_cstr(s, "bucket=");
 
+    ofp_print_bucket_id(s, "bucket_id:", bucket->bucket_id, ofp_version);
+    if (bucket->weight != (type == OFPGT11_SELECT ? 1 : 0)) {
+        ds_put_format(s, "weight:%"PRIu16",", bucket->weight);
+    }
+    if (bucket->watch_port != OFPP_NONE) {
+        ds_put_cstr(s, "watch_port:");
+        ofputil_format_port(bucket->watch_port, port_map, s);
+        ds_put_char(s, ',');
+    }
+    if (bucket->watch_group != OFPG_ANY) {
+        ds_put_format(s, "watch_group:%"PRIu32",", bucket->watch_group);
+    }
+
+    ds_put_cstr(s, "actions=");
+    struct ofpact_format_params fp = {
+        .port_map = port_map,
+        .table_map = table_map,
+        .s = s,
+    };
+    ofpacts_format(bucket->ofpacts, bucket->ofpacts_len, &fp);
+}
+
+void
+ofputil_group_format(struct ds *s, uint32_t group_id, uint8_t type,
+                     const struct ofputil_bucket *bucket,
+                     const struct ovs_list *p_buckets,
+                     const struct ofputil_group_props *props,
+                     enum ofp_version ofp_version, bool suppress_type,
+                     const struct ofputil_port_map *port_map,
+                     const struct ofputil_table_map *table_map)
+{
     ds_put_format(s, "group_id=%"PRIu32, group_id);
 
     if (!suppress_type) {
@@ -1831,57 +1885,24 @@ ofp_print_group(struct ds *s, uint32_t group_id, uint8_t type,
         ds_put_format(s, ",type=%s", type_str[type > 4 ? 4 : type]);
     }
 
-    if (props->selection_method[0]) {
-        ds_put_format(s, ",selection_method=%s", props->selection_method);
-        if (props->selection_method_param) {
-            ds_put_format(s, ",selection_method_param=%"PRIu64,
-                          props->selection_method_param);
-        }
+    ofputil_group_properties_format(props, s);
 
-        size_t n = bitmap_count1(props->fields.used.bm, MFF_N_IDS);
-        if (n == 1) {
-            ds_put_cstr(s, ",fields=");
-            oxm_format_field_array(s, &props->fields);
-        } else if (n > 1) {
-            ds_put_cstr(s, ",fields(");
-            oxm_format_field_array(s, &props->fields);
-            ds_put_char(s, ')');
-        }
-    }
-
-    if (!p_buckets) {
+    if (!bucket && !p_buckets) {
         return;
     }
 
     ds_put_char(s, ',');
 
-    LIST_FOR_EACH (bucket, list_node, p_buckets) {
-        ds_put_cstr(s, "bucket=");
-
-        ofp_print_bucket_id(s, "bucket_id:", bucket->bucket_id, ofp_version);
-        if (bucket->weight != (type == OFPGT11_SELECT ? 1 : 0)) {
-            ds_put_format(s, "weight:%"PRIu16",", bucket->weight);
-        }
-        if (bucket->watch_port != OFPP_NONE) {
-            ds_put_cstr(s, "watch_port:");
-            ofputil_format_port(bucket->watch_port, port_map, s);
+    if (bucket) {
+        ofputil_bucket_format(s, bucket, type, ofp_version, NULL, NULL);
+    } else {
+        LIST_FOR_EACH (bucket, list_node, p_buckets) {
+            ofputil_bucket_format(s, bucket, type, ofp_version,
+                                port_map, table_map);
             ds_put_char(s, ',');
         }
-        if (bucket->watch_group != OFPG_ANY) {
-            ds_put_format(s, "watch_group:%"PRIu32",", bucket->watch_group);
-        }
-
-        ds_put_cstr(s, "actions=");
-        struct ofpact_format_params fp = {
-            .port_map = port_map,
-            .table_map = table_map,
-            .s = s,
-        };
-        ofpacts_format(bucket->ofpacts, bucket->ofpacts_len, &fp);
-        ds_put_char(s, ',');
+        ds_chomp(s, ',');
     }
-
-    ds_chomp(s, ',');
 }
 
 enum ofperr
@@ -1901,8 +1922,9 @@ ofputil_group_desc_format(struct ds *s, const struct ofp_header *oh,
 
         ds_put_char(s, '\n');
         ds_put_char(s, ' ');
-        ofp_print_group(s, gd.group_id, gd.type, &gd.buckets, &gd.props,
-                        oh->version, false, port_map, table_map);
+        ofputil_group_format(s, gd.group_id, gd.type, NULL, &gd.buckets,
+                             &gd.props, oh->version, false,
+                             port_map, table_map);
         ofputil_uninit_group_desc(&gd);
      }
 }
@@ -2368,8 +2390,9 @@ ofputil_group_mod_format__(struct ds *s, enum ofp_version ofp_version,
                             gm->command_bucket_id, ofp_version);
     }
 
-    ofp_print_group(s, gm->group_id, gm->type, &gm->buckets, &gm->props,
-                    ofp_version, bucket_command, port_map, table_map);
+    ofputil_group_format(s, gm->group_id, gm->type, NULL, &gm->buckets,
+                         &gm->props, ofp_version, bucket_command,
+                         port_map, table_map);
 }
 
 enum ofperr

ofproto/ofproto-dpif-xlate-cache.c
@@ -301,3 +301,37 @@ xlate_cache_steal_entries(struct xlate_cache *dst, struct xlate_cache *src)
     memcpy(p, src_entries->data, src_entries->size);
     ofpbuf_clear(src_entries);
 }
+
+void
+xlate_xcache_format(struct ds *s, const struct xlate_cache *xcache)
+{
+    struct ofpbuf entries = xcache->entries;
+    struct xc_entry *entry;
+    struct ofgroup *ofg;
+
+    XC_ENTRY_FOR_EACH (entry, &entries) {
+        switch (entry->type) {
+        case XC_RULE:
+            ofproto_rule_stats_ds(s, &entry->rule->up, true);
+            break;
+        case XC_GROUP:
+            ofg = &entry->group.group->up;
+            ofputil_group_format(s, ofg->group_id, ofg->type,
+                                 entry->group.bucket, &ofg->buckets,
+                                 &ofg->props, OFP15_VERSION,
+                                 false, NULL, NULL);
+            break;
+        case XC_TABLE:
+        case XC_BOND:
+        case XC_NETDEV:
+        case XC_NETFLOW:
+        case XC_MIRROR:
+        case XC_LEARN:
+        case XC_NORMAL:
+        case XC_FIN_TIMEOUT:
+        case XC_TNL_NEIGH:
+        case XC_TUNNEL_HEADER:
+            break;
+        }
+    }
+}

ofproto/ofproto-dpif-xlate-cache.h
@@ -151,4 +151,6 @@ void xlate_cache_uninit(struct xlate_cache *);
 void xlate_cache_delete(struct xlate_cache *);
 void xlate_cache_steal_entries(struct xlate_cache *, struct xlate_cache *);
 
+void xlate_xcache_format(struct ds *, const struct xlate_cache *);
+
 #endif /* ofproto-dpif-xlate-cache.h */

ofproto/ofproto-provider.h
@@ -450,6 +450,8 @@ void ofproto_rule_ref(struct rule *);
 bool ofproto_rule_try_ref(struct rule *);
 void ofproto_rule_unref(struct rule *);
 
+void ofproto_rule_stats_ds(struct ds *, struct rule *, bool offload_stats);
+
 static inline const struct rule_actions * rule_get_actions(const struct rule *);
 static inline bool rule_is_table_miss(const struct rule *);
 static inline bool rule_is_hidden(const struct rule *);

ofproto/ofproto.c
@@ -4865,9 +4865,9 @@ handle_flow_stats_request(struct ofconn *ofconn,
     return 0;
 }
 
-static void
-flow_stats_ds(struct ofproto *ofproto, struct rule *rule, struct ds *results,
-              bool offload_stats)
+void
+ofproto_rule_stats_ds(struct ds *results, struct rule *rule,
+                      bool offload_stats)
 {
     struct pkt_stats stats;
     const struct rule_actions *actions;
@@ -4896,7 +4896,8 @@ flow_stats_ds(struct ofproto *ofproto, struct rule *rule, struct ds *results,
         ds_put_format(results, "n_offload_bytes=%"PRIu64", ",
                       stats.n_offload_bytes);
     }
-    cls_rule_format(&rule->cr, ofproto_get_tun_tab(ofproto), NULL, results);
+    cls_rule_format(&rule->cr, ofproto_get_tun_tab(rule->ofproto), NULL,
+                    results);
     ds_put_char(results, ',');
 
     ds_put_cstr(results, "actions=");
@@ -4918,7 +4919,7 @@ ofproto_get_all_flows(struct ofproto *p, struct ds *results,
         struct rule *rule;
 
         CLS_FOR_EACH (rule, cr, &table->cls) {
-            flow_stats_ds(p, rule, results, offload_stats);
+            ofproto_rule_stats_ds(results, rule, offload_stats);
         }
     }
 }

commit 89f4ac881c67969ea089b5ebc2429cc7584294e6

ofproto: Fix default pmd_id for ofproto/detrace.
The system and netdev datapath have different default pmd_id, which
resulted in empty output of ofproto/detrace with kernel datapath.

Make sure we use the correct default pmd_id when it's not specified
as an argument. At the same time move slightly adjusted test into
system tests so it is tested with both datapaths.

Fixes: 600125b2c380 ("ofproto: Add ofproto/detrace command to map UFIDs to OpenFlow.")
Signed-off-by: Ales Musil <amusil@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

ofproto/ofproto-dpif-upcall.c
@@ -3338,8 +3338,9 @@ static void
 upcall_unixctl_ofproto_detrace(struct unixctl_conn *conn, int argc,
                                const char *argv[], void *aux OVS_UNUSED)
 {
-    unsigned int pmd_id = NON_PMD_CORE_ID;
     const char *key_s = argv[1];
+    const char *pmd_str = NULL;
+    unsigned int pmd_id;
     ovs_u128 ufid;
 
     if (odp_ufid_from_string(key_s, &ufid) <= 0) {
@@ -3348,7 +3349,7 @@ upcall_unixctl_ofproto_detrace(struct unixctl_conn *conn, int argc,
     }
 
     if (argc == 3) {
-        const char *pmd_str = argv[2];
+        pmd_str = argv[2];
         if (!ovs_scan(pmd_str, "pmd=%d", &pmd_id)) {
             unixctl_command_reply_error(conn,
                                         "Invalid pmd argument format. "
@@ -3361,6 +3362,11 @@ upcall_unixctl_ofproto_detrace(struct unixctl_conn *conn, int argc,
     struct udpif *udpif;
 
     LIST_FOR_EACH (udpif, list_node, &all_udpifs) {
+        if (!pmd_str) {
+            const char *type = dpif_normalize_type(dpif_type(udpif->dpif));
+            pmd_id = !strcmp(type, "system") ? PMD_ID_NULL : NON_PMD_CORE_ID;
+        }
+
         struct udpif_key *ukey = ukey_lookup(udpif, &ufid, pmd_id);
         if (!ukey) {
             continue;

tests/ofproto-dpif.at
@@ -12797,62 +12797,6 @@ Datapath actions: psample(group=42,cookie=0x64000000c8),drop
 OVS_VSWITCHD_STOP("/Enabling an unsupported feature is very dangerous/d")
 AT_CLEANUP
 
-AT_SETUP([ofproto-dpif - Dump OF rules corresponding to UFID])
-OVS_VSWITCHD_START
-
-add_of_ports br0 1 2 3
-
-dnl Add some OpenFlow rules and groups.
-AT_DATA([groups.txt], [dnl
-group_id=1,type=select,selection_method=dp_hash,bucket=bucket_id:0,weight:100,actions=ct(commit,table=2,nat(dst=20.0.0.2))
-group_id=2,type=all,bucket=resubmit(,3),bucket=resubmit(,4)
-])
-AT_DATA([flows.txt], [dnl
-table=0,priority=100,cookie=0x12345678,in_port=p1,ip,nw_dst=10.0.0.2,actions=resubmit(,1)
-table=1,priority=200,ip,actions=group:1
-table=2,ip,actions=group:2
-table=3,ip,actions=p2
-table=4,ip,actions=p3
-])
-AT_CHECK([ovs-ofctl add-groups br0 groups.txt])
-AT_CHECK([ovs-ofctl add-flows br0 flows.txt])
-
-AT_CHECK([ovs-appctl netdev-dummy/receive p1 'ipv4(src=10.0.0.1,dst=10.0.0.2,proto=6),tcp(src=1,dst=2)'])
-AT_CHECK([ovs-appctl netdev-dummy/receive p1 'ipv4(src=10.0.0.1,dst=10.0.0.2,proto=6),tcp(src=1,dst=2)'])
-AT_CHECK([ovs-appctl revalidator/wait])
-AT_CHECK([ovs-appctl revalidator/pause])
-
-AT_CHECK([ovs-appctl dpctl/dump-flows | strip_used | strip_stats | strip_duration | strip_dp_hash | sort], [0], [dnl
-flow-dump from the main thread:
-recirc_id(0),in_port(1),packet_type(ns=0,id=0),eth_type(0x0800),ipv4(dst=10.0.0.2,frag=no), packets:0, bytes:0, used:0.0s, actions:hash(l4(0)),recirc(0x1)
-recirc_id(0x1),in_port(1),packet_type(ns=0,id=0),eth_type(0x0800),ipv4(frag=no), packets:0, bytes:0, used:0.0s, actions:ct(commit,nat(dst=20.0.0.2)),recirc(0x2)
-recirc_id(0x2),in_port(1),packet_type(ns=0,id=0),eth_type(0x0800),ipv4(frag=no), packets:0, bytes:0, used:0.0s, actions:2,3
-])
-
-ufid=$(ovs-appctl dpctl/dump-flows -m filter='recirc_id(0)' | parse_ufid)
-AT_CHECK([ovs-appctl ofproto/detrace $ufid | ofctl_strip], [0], [dnl
-cookie=0x12345678, n_packets=2, n_bytes=236, priority=100,ip,in_port=1,nw_dst=10.0.0.2,actions=resubmit(,1)
-table_id=1, n_packets=2, n_bytes=236, priority=200,ip,actions=group:1
-])
-
-ufid=$(ovs-appctl dpctl/dump-flows -m filter='recirc_id(0x1)' | parse_ufid)
-AT_CHECK([ovs-appctl ofproto/detrace $ufid | ofctl_strip], [0], [dnl
-group_id=1,type=select,selection_method=dp_hash,bucket=bucket_id:0,weight:100,actions=ct(commit,table=2,nat(dst=20.0.0.2))
-])
-
-ufid=$(ovs-appctl dpctl/dump-flows -m filter='recirc_id(0x2)' | parse_ufid)
-AT_CHECK([ovs-appctl ofproto/detrace $ufid | ofctl_strip], [0], [dnl
-table_id=2, n_packets=2, n_bytes=236, ip,actions=group:2
-table_id=3, n_packets=2, n_bytes=236, ip,actions=output:2
-table_id=4, n_packets=2, n_bytes=236, ip,actions=output:3
-group_id=2,type=all,bucket=bucket_id:0,actions=resubmit(,3),bucket=bucket_id:1,actions=resubmit(,4)
-])
-
-AT_CHECK([ovs-appctl revalidator/resume])
-
-OVS_VSWITCHD_STOP
-AT_CLEANUP
-
 AT_SETUP([ofproto-dpif - Cleanup missing datapath flows])
 
 OVS_VSWITCHD_START

tests/system-traffic.at
@@ -2530,6 +2530,92 @@ recirc_id(<recirc>),in_port(2),eth_type(0x86dd),ipv6(proto=58,frag=no),icmpv6(ty
 OVS_TRAFFIC_VSWITCHD_STOP
 AT_CLEANUP
 
+AT_SETUP([datapath - Dump OF rules corresponding to UFID])
+CHECK_NO_TC_OFFLOAD()
+OVS_TRAFFIC_VSWITCHD_START()
+
+ADD_NAMESPACES(at_ns0, at_ns1)
+
+ADD_VETH(p1, at_ns0, br0, "10.0.0.1/24")
+ADD_VETH(p2, at_ns1, br0, "10.0.0.2/24")
+
+AT_CHECK([ovs-vsctl -- set interface ovs-p1 ofport_request=1 \
+                    -- set interface ovs-p2 ofport_request=2])
+
+dnl Add some OpenFlow rules and groups.
+AT_DATA([groups.txt], [dnl
+group_id=1,type=select,selection_method=dp_hash,bucket=bucket_id:0,weight:100,actions=ct(commit,table=2)
+])
+AT_DATA([flows.txt], [dnl
+table=0,arp,actions=NORMAL
+table=0,priority=100,cookie=0x12345678,in_port=ovs-p1,ip,nw_dst=10.0.0.2,actions=resubmit(,1)
+table=0,priority=100,cookie=0xabcedf,in_port=ovs-p2,ip,nw_dst=10.0.0.1,actions=ct(table=3)
+table=1,priority=200,ip,actions=group:1
+table=2,ip,actions=ovs-p2
+table=3,ip,actions=ovs-p1
+])
+AT_CHECK([ovs-ofctl add-groups br0 groups.txt])
+AT_CHECK([ovs-ofctl add-flows br0 flows.txt])
+
+NS_CHECK_EXEC([at_ns0], [ping -q -c 10 -i 0.1 -W 2 10.0.0.2 | FORMAT_PING], [0], [dnl
+10 packets transmitted, 10 received, 0% packet loss, time 0ms
+])
+
+AT_CHECK([ovs-appctl revalidator/wait])
+AT_CHECK([ovs-appctl revalidator/pause])
+
+get_pmd_arg() {
+    local core=$(ovs-appctl dpctl/dump-flows | head -n1 | grep "flow-dump from pmd" | grep -o "[[-]]\?[[0-9]]\+$")
+    if test -z $core; then
+        echo ""
+    else
+        echo "pmd=$core"
+    fi
+}
+
+pmd_id=$(get_pmd_arg)
+
+AT_CHECK([ovs-appctl dpctl/dump-flows --names | grep "ipv4" | strip_used | strip_stats | \
+          strip_duration | strip_dp_hash | strip_recirc | \
+          sed 's/,packet_type(ns=[[0-9]]*,id=[[0-9]]*),/,/;s/,eth(),/,/;' | sort], [0], [dnl
+recirc_id(<recirc>),in_port(ovs-p1),eth_type(0x0800),ipv4(dst=10.0.0.2,frag=no), packets:0, bytes:0, used:0.0s, actions:hash(l4(0)),recirc(<recirc>)
+recirc_id(<recirc>),in_port(ovs-p1),eth_type(0x0800),ipv4(frag=no), packets:0, bytes:0, used:0.0s, actions:ct(commit),recirc(<recirc>)
+recirc_id(<recirc>),in_port(ovs-p1),eth_type(0x0800),ipv4(frag=no), packets:0, bytes:0, used:0.0s, actions:ovs-p2
+recirc_id(<recirc>),in_port(ovs-p2),eth_type(0x0800),ipv4(dst=10.0.0.1,frag=no), packets:0, bytes:0, used:0.0s, actions:ct,recirc(<recirc>)
+recirc_id(<recirc>),in_port(ovs-p2),eth_type(0x0800),ipv4(frag=no), packets:0, bytes:0, used:0.0s, actions:ovs-p1
+])
+
+ufid=$(ovs-appctl dpctl/dump-flows -m filter='recirc_id(0),in_port(ovs-p1),ipv4' | parse_ufid)
+AT_CHECK([ovs-appctl ofproto/detrace $ufid $pmd_id | ofctl_strip], [0], [dnl
+cookie=0x12345678, n_packets=10, n_bytes=980, priority=100,ip,in_port=1,nw_dst=10.0.0.2,actions=resubmit(,1)
+table_id=1, n_packets=10, n_bytes=980, priority=200,ip,actions=group:1
+])
+
+ufid=$(ovs-appctl dpctl/dump-flows -m filter='in_port(ovs-p1),ipv4' | grep "ct(commit)" | parse_ufid)
+AT_CHECK([ovs-appctl ofproto/detrace $ufid $pmd_id | ofctl_strip], [0], [dnl
+group_id=1,type=select,selection_method=dp_hash,bucket=bucket_id:0,weight:100,actions=ct(commit,table=2)
+])
+
+ufid=$(ovs-appctl dpctl/dump-flows -m filter='in_port(ovs-p1),ipv4' | grep "actions:ovs-p2" | parse_ufid)
+AT_CHECK([ovs-appctl ofproto/detrace $ufid $pmd_id | ofctl_strip], [0], [dnl
+table_id=2, n_packets=10, n_bytes=980, ip,actions=output:2
+])
+
+ufid=$(ovs-appctl dpctl/dump-flows -m filter='recirc_id(0),in_port(ovs-p2),ipv4' | grep "ct," | parse_ufid)
+AT_CHECK([ovs-appctl ofproto/detrace $ufid $pmd_id | ofctl_strip], [0], [dnl
+cookie=0xabcedf, n_packets=10, n_bytes=980, priority=100,ip,in_port=2,nw_dst=10.0.0.1,actions=ct(table=3)
+])
+
+ufid=$(ovs-appctl dpctl/dump-flows -m filter='in_port(ovs-p2),ipv4' | grep "actions:ovs-p1" | parse_ufid)
+AT_CHECK([ovs-appctl ofproto/detrace $ufid $pmd_id | ofctl_strip], [0], [dnl
+table_id=3, n_packets=10, n_bytes=980, ip,actions=output:1
+])
+
+AT_CHECK([ovs-appctl revalidator/resume])
+
+OVS_TRAFFIC_VSWITCHD_STOP
+AT_CLEANUP
+
 AT_BANNER([MPLS])
 
 AT_SETUP([mpls - encap header dp-support])