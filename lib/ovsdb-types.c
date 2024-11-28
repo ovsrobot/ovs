ovsrobot/ovs

commit e0e4266a90a28c2313cde6951d32d70680af327d

ovsdb-types: Add functions to compare types for equality.
Will be used in the next commit to optimize database conversion.

Acked-by: Han Zhou <hzhou@ovn.org>
Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

lib/ovsdb-types.c
@@ -276,6 +276,58 @@ ovsdb_base_type_is_valid(const struct ovsdb_base_type *base)
 }
 
 bool
+ovsdb_base_type_equals(const struct ovsdb_base_type *a,
+                       const struct ovsdb_base_type *b)
+{
+    if (a == b) {
+        return true;
+    }
+
+    if (a->type != b->type) {
+        return false;
+    }
+
+    if ((a->enum_ && !b->enum_) || (!a->enum_ && b->enum_)) {
+        return false;
+    } else if (a->enum_ &&
+               !ovsdb_datum_equals(a->enum_, b->enum_,
+                                   ovsdb_base_type_get_enum_type(a->type))) {
+        return false;
+    }
+
+    switch (a->type) {
+    case OVSDB_TYPE_VOID:
+        return true;
+
+    case OVSDB_TYPE_INTEGER:
+        return a->integer.min == b->integer.min
+               && a->integer.max == b->integer.max;
+
+    case OVSDB_TYPE_REAL:
+        return a->real.min == b->real.min && a->real.max == b->real.max;
+
+    case OVSDB_TYPE_BOOLEAN:
+        return true;
+
+    case OVSDB_TYPE_STRING:
+        return a->string.minLen == b->string.minLen
+               && a->string.maxLen == b->string.maxLen;
+
+    case OVSDB_TYPE_UUID:
+        /* Not comparing the table pointer here, only the table name, as this
+         * function can be used to compare types from different databases, so
+         * pointers will be different. */
+        return a->uuid.refType == b->uuid.refType
+               && nullable_string_is_equal(a->uuid.refTableName,
+                                           b->uuid.refTableName);
+
+    case OVSDB_N_TYPES:
+    default:
+        OVS_NOT_REACHED();
+    }
+}
+
+bool
 ovsdb_base_type_has_constraints(const struct ovsdb_base_type *base)
 {
     if (base->enum_) {
@@ -566,6 +618,15 @@ ovsdb_type_is_valid(const struct ovsdb_type *type)
             && ovsdb_base_type_is_valid(&type->value)
             && type->n_min <= 1
             && type->n_max >= 1);
+}
+
+bool
+ovsdb_type_equals(const struct ovsdb_type *a, const struct ovsdb_type *b)
+{
+    return ovsdb_base_type_equals(&a->key, &b->key)
+           && ovsdb_base_type_equals(&a->value, &b->value)
+           && a->n_min == b->n_min
+           && a->n_max == b->n_max;
 }
 
 static struct ovsdb_error *

lib/ovsdb-types.h
@@ -107,6 +107,8 @@ void ovsdb_base_type_clone(struct ovsdb_base_type *,
 void ovsdb_base_type_destroy(struct ovsdb_base_type *);
 
 bool ovsdb_base_type_is_valid(const struct ovsdb_base_type *);
+bool ovsdb_base_type_equals(const struct ovsdb_base_type *,
+                            const struct ovsdb_base_type *);
 bool ovsdb_base_type_has_constraints(const struct ovsdb_base_type *);
 void ovsdb_base_type_clear_constraints(struct ovsdb_base_type *);
 const struct ovsdb_type *ovsdb_base_type_get_enum_type(enum ovsdb_atomic_type);
@@ -157,6 +159,7 @@ void ovsdb_type_clone(struct ovsdb_type *, const struct ovsdb_type *);
 void ovsdb_type_destroy(struct ovsdb_type *);
 
 bool ovsdb_type_is_valid(const struct ovsdb_type *);
+bool ovsdb_type_equals(const struct ovsdb_type *, const struct ovsdb_type *);
 
 static inline bool ovsdb_type_is_scalar(const struct ovsdb_type *);
 static inline bool ovsdb_type_is_optional(const struct ovsdb_type *);

commit b8f8d0ca667a531962a8048f8083f832a723ff16

ovsdb-types: Fix use of uninitialized reference type.
Types for UUID columns that are not references (e.g. version column)
may end up with the refType not initialized.  At the same time, some
functions use the value during the type comparison, which may result
in a false negative type check:

 WARNING: MemorySanitizer: use-of-uninitialized-value
  0 0x75abf7 in ovsdb_base_type_equals lib/ovsdb-types.c:321:16
  1 0x75f899 in ovsdb_type_equals lib/ovsdb-types.c:626:12
  2 0x4fc607 in ovsdb_convert_table ovsdb/file.c:316:13
  3 0x4fb5ec in ovsdb_convert ovsdb/file.c:388:17
  4 0x598bcf in ovsdb_trigger_try ovsdb/trigger.c:313:25
  5 0x596902 in ovsdb_trigger_init ovsdb/trigger.c:66:12
  6 0x5189e8 in ovsdb_jsonrpc_trigger_create ovsdb/jsonrpc-server.c:1247:27
  7 0x51473d in ovsdb_jsonrpc_session_got_request ovsdb/jsonrpc-server.c:1112:13
  8 0x512ea2 in ovsdb_jsonrpc_session_run ovsdb/jsonrpc-server.c:677:17
  9 0x50d67d in ovsdb_jsonrpc_session_run_all ovsdb/jsonrpc-server.c:700:21
 10 0x50cc49 in ovsdb_jsonrpc_server_run ovsdb/jsonrpc-server.c:522:9
 11 0x4d756f in main_loop ovsdb/ovsdb-server.c:330:9
 12 0x4c4e52 in main ovsdb/ovsdb-server.c:890:5
 13 0x7f42bb in __libc_start_call_main
 14 0x7f42bb in __libc_start_main@GLIBC_2.2.5
 15 0x432b64 in _start (ovsdb/ovsdb-server+0x432b64)

Fix the initialization function to always set the refType to some
value.  Using the "strong" as we don't have an UNSPEC value in the
enumeration.  It will not be meaningful unless the refTable is also
set, in which case it will be properly updated, we just need the
value to be the same for all the columns that are not references.

Fixes: e0e4266a90a2 ("ovsdb-types: Add functions to compare types for equality.")
Signed-off-by: Ilya Maximets <i.maximets@ovn.org>
Signed-off-by: 0-day Robot <robot@bytheb.org>

lib/ovsdb-types.c
@@ -145,6 +145,7 @@ ovsdb_base_type_init(struct ovsdb_base_type *base, enum ovsdb_atomic_type type)
     case OVSDB_TYPE_UUID:
         base->uuid.refTableName = NULL;
         base->uuid.refTable = NULL;
+        base->uuid.refType = OVSDB_REF_STRONG;
         break;
 
     case OVSDB_N_TYPES: