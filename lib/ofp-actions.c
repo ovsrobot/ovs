ovsrobot/ovs

commit 933aaf9444a6e109d360425a98aac6cd0891ef60

ofp-actions: Ensure aligned accesses to masked fields.
For example is parsing the OVN "eth.dst[40] = 1;" action, which
triggered the following warning from UndefinedBehaviorSanitizer:

  lib/meta-flow.c:3210:9:
  runtime error: member access within misaligned address 0x000000de4e36
  for type 'const union mf_value', which requires 8 byte alignment
  0x000000de4e36: note: pointer points here
   00 00 00 00 01 00  00 00 00 00 00 00 00 00 70 4e de 00 00 00 00 00
               ^
   10 51 de 00 00 00 00 00  c0 4f

      0 0x5818bc in mf_format lib/meta-flow.c:3210
      1 0x5b6047 in format_SET_FIELD lib/ofp-actions.c:3342
      2 0x5d68ab in ofpact_format lib/ofp-actions.c:9213
      3 0x5d6ee0 in ofpacts_format lib/ofp-actions.c:9237
      4 0x410922 in test_parse_actions tests/test-ovn.c:1360

To avoid this we now change the internal representation of the set_field
actions, struct ofpact_set_field, such that the mask is always stored
at a correctly aligned address, multiple of OFPACT_ALIGNTO.

We also need to adapt the "ovs-ofctl show-flows - Oversized flow" test
because now the ofpact representation of the set_field action uses more
bytes in memory (for the extra alignment).  Change the test to use
dec_ttl instead.

Acked-by: Aaron Conole <aconole@redhat.com>
Signed-off-by: Dumitru Ceara <dceara@redhat.com>
Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

lib/ofp-actions.c
@@ -3381,21 +3381,28 @@ check_SET_FIELD(struct ofpact_set_field *a,
     return 0;
 }
 
-/* Appends an OFPACT_SET_FIELD ofpact with enough space for the value and mask
- * for the 'field' to 'ofpacts' and returns it.  Fills in the value from
- * 'value', if non-NULL, and mask from 'mask' if non-NULL.  If 'value' is
- * non-NULL and 'mask' is NULL, an all-ones mask will be filled in. */
+/* Appends an OFPACT_SET_FIELD ofpact with enough space for the value and a
+ * properly aligned mask for the 'field' to 'ofpacts' and returns it.  Fills
+ * in the value from 'value', if non-NULL, and mask from 'mask' if non-NULL.
+ * If 'value' is non-NULL and 'mask' is NULL, an all-ones mask will be
+ * filled in. */
 struct ofpact_set_field *
 ofpact_put_set_field(struct ofpbuf *ofpacts, const struct mf_field *field,
                      const void *value, const void *mask)
 {
+    /* Ensure there's enough space for:
+     * - value (8-byte aligned)
+     * - mask  (8-byte aligned)
+     * - padding (to make the whole ofpact_set_field 8-byte aligned)
+     */
+    size_t total_size = 2 * ROUND_UP(field->n_bytes, OFPACT_ALIGNTO);
     struct ofpact_set_field *sf = ofpact_put_SET_FIELD(ofpacts);
     sf->field = field;
 
     /* Fill in the value and mask if given, otherwise put zeroes so that the
      * caller may fill in the value and mask itself. */
     if (value) {
-        ofpbuf_put_uninit(ofpacts, 2 * field->n_bytes);
+        ofpbuf_put_uninit(ofpacts, total_size);
         sf = ofpacts->header;
         memcpy(sf->value, value, field->n_bytes);
         if (mask) {
@@ -3404,7 +3411,7 @@ ofpact_put_set_field(struct ofpbuf *ofpacts, const struct mf_field *field,
             memset(ofpact_set_field_mask(sf), 0xff, field->n_bytes);
         }
     } else {
-        ofpbuf_put_zeros(ofpacts, 2 * field->n_bytes);
+        ofpbuf_put_zeros(ofpacts, total_size);
         sf = ofpacts->header;
     }
     /* Update length. */

include/openvswitch/ofp-actions.h
@@ -549,7 +549,8 @@ struct ofpact_set_field {
         const struct mf_field *field;
     );
     union mf_value value[];  /* Significant value bytes followed by
-                              * significant mask bytes. */
+                              * significant mask bytes aligned at
+                              * OFPACT_ALIGNTO bytes. */
 };
 BUILD_ASSERT_DECL(offsetof(struct ofpact_set_field, value)
                   % OFPACT_ALIGNTO == 0);
@@ -557,9 +558,10 @@ BUILD_ASSERT_DECL(offsetof(struct ofpact_set_field, value)
                   == sizeof(struct ofpact_set_field));
 
 /* Use macro to not have to deal with constness. */
-#define ofpact_set_field_mask(SF)                               \
-    ALIGNED_CAST(union mf_value *,                              \
-                 (uint8_t *)(SF)->value + (SF)->field->n_bytes)
+#define ofpact_set_field_mask(SF)                                           \
+    ALIGNED_CAST(union mf_value *,                                          \
+                 (uint8_t *)(SF)->value +                                   \
+                            ROUND_UP((SF)->field->n_bytes, OFPACT_ALIGNTO))
 
 /* OFPACT_PUSH_VLAN/MPLS/PBB
  *

tests/ovs-ofctl.at
@@ -3258,7 +3258,7 @@ AT_SETUP([ovs-ofctl show-flows - Oversized flow])
 OVS_VSWITCHD_START
 
 printf " priority=90,icmp,reg15=0x8005,metadata=0x1,nw_dst=11.0.0.1,icmp_type=8,icmp_code=0 actions=" > flow.txt
-for i in `seq 1 1022`; do printf "set_field:0x399->reg13,set_field:0x$i->reg15,resubmit(,39),"; done >> flow.txt
+for i in `seq 1 2045`; do printf "dec_ttl,resubmit(,39),"; done >> flow.txt
 printf "resubmit(,39)\n" >> flow.txt
 
 AT_CHECK([ovs-ofctl -O OpenFlow15 add-flows br0 flow.txt])

commit d47b58e63188c73c0a4c6c7625ca9fa499b93dbd

ofp-actions: Fix use of uninitialized padding in set-field.
Commit 933aaf9444a6 re-aligned the fields, so the access to them is
aligned, but it also didn't initialize the added padding.  'ofpacts'
are frequently compared with memcmp() and being hashed as part of the
frozen state causing false negative comparisons and potentially frozen
state lookup failures.

Found by running make check-valgrind on 'continuation - data stack'
tests:

 Conditional jump or move depends on uninitialised value(s)
    at 0x4EBC82: mhash_add__ (hash.h:66)
    by 0x4EBC48: mhash_add (hash.h:78)
    by 0x4EB4F8: hash_add (hash.h:109)
    by 0x4EBDEC: hash_add64 (hash.h:114)
    by 0x4EBDAC: hash_add_words64 (hash.h:439)
    by 0x4EB6D2: hash_words64_inline (hash.h:136)
    by 0x4EB6A2: hash_words64__ (hash.c:73)
    by 0x4595F2: hash_words64 (hash.h:371)
    by 0x4593C6: hash_bytes64 (hash.h:399)
    by 0x458B76: frozen_state_hash (ofproto-dpif-rid.c:143)
    by 0x458CA4: recirc_alloc_id_ctx (ofproto-dpif-rid.c:280)
    by 0x483B85: finish_freezing__ (ofproto-dpif-xlate.c:5229)
    by 0x47171F: finish_freezing (ofproto-dpif-xlate.c:5271)
    by 0x46E8BB: xlate_actions (ofproto-dpif-xlate.c:8340)
    by 0x45DC7B: ofproto_trace__ (ofproto-dpif-trace.c:782)
    by 0x45D816: ofproto_trace (ofproto-dpif-trace.c:851)
    by 0x45E435: ofproto_unixctl_trace (ofproto-dpif-trace.c:490)
    by 0x609F5E: process_command (unixctl.c:310)
    by 0x6094B9: run_connection (unixctl.c:344)
    by 0x609397: unixctl_server_run (unixctl.c:395)
  Uninitialised value was created by a stack allocation
    at 0x432A44: handle_flow_mod (ofproto.c:6346)

Fix that by properly initializing the whole space allocated for the
set-field action.

Fixes: 933aaf9444a6 ("ofp-actions: Ensure aligned accesses to masked fields.")
Signed-off-by: Ilya Maximets <i.maximets@ovn.org>
Signed-off-by: 0-day Robot <robot@bytheb.org>

lib/ofp-actions.c
@@ -3402,21 +3402,21 @@ ofpact_put_set_field(struct ofpbuf *ofpacts, const struct mf_field *field,
     struct ofpact_set_field *sf = ofpact_put_SET_FIELD(ofpacts);
     sf->field = field;
 
-    /* Fill in the value and mask if given, otherwise put zeroes so that the
-     * caller may fill in the value and mask itself. */
+    /* Fill with all zeroes to make sure the padding is initialized. */
+    ofpbuf_put_zeros(ofpacts, total_size);
+    sf = ofpacts->header;
+
+    /* Fill in the value and mask if given, otherwise keep the zeroes
+     * so that the caller may fill in the value and mask itself. */
     if (value) {
-        ofpbuf_put_uninit(ofpacts, total_size);
-        sf = ofpacts->header;
         memcpy(sf->value, value, field->n_bytes);
         if (mask) {
             memcpy(ofpact_set_field_mask(sf), mask, field->n_bytes);
         } else {
             memset(ofpact_set_field_mask(sf), 0xff, field->n_bytes);
         }
-    } else {
-        ofpbuf_put_zeros(ofpacts, total_size);
-        sf = ofpacts->header;
     }
+
     /* Update length. */
     ofpact_finish_SET_FIELD(ofpacts, &sf);
 