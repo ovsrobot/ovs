ovsrobot/ovs

commit 2f196c80e7165150d956fc50de0db58b8964ee2e

netdev-dpdk: Use LSC interrupt mode.
Querying link status may get delayed for an undeterministic (long) time
with mlx5 ports. This is a consequence of the mlx5 driver calling ethtool
kernel API and getting stuck on the kernel RTNL lock while some other
operation is in progress under this lock.

One impact for long link status query is that it is called under the bond
lock taken in write mode periodically in bond_run().
In parallel, datapath threads may block requesting to read bonding related
info (like for example in bond_check_admissibility()).

The LSC interrupt mode is available with many DPDK drivers and is used by
default with testpmd.

It seems safe enough to switch on this feature by default in OVS.
We keep the per interface option to disable this feature in case of an
unforeseen bug.

Signed-off-by: David Marchand <david.marchand@redhat.com>
Reviewed-by: Robin Jarry <rjarry@redhat.com>
Acked-by: Mike Pattrick <mkp@redhat.com>
Acked-by: Maxime Coquelin <maxime.coquelin@redhat.com>
Acked-by: Kevin Traynor <ktraynor@redhat.com>
Acked-by: Aaron Conole <aconole@redhat.com>

lib/netdev-dpdk.c
@@ -2397,7 +2397,18 @@ netdev_dpdk_set_config(struct netdev *netdev, const struct smap *args,
         }
     }
 
-    lsc_interrupt_mode = smap_get_bool(args, "dpdk-lsc-interrupt", false);
+    lsc_interrupt_mode = smap_get_bool(args, "dpdk-lsc-interrupt", true);
+    if (lsc_interrupt_mode && !(*info.dev_flags & RTE_ETH_DEV_INTR_LSC)) {
+        if (smap_get(args, "dpdk-lsc-interrupt")) {
+            VLOG_WARN_BUF(errp, "'%s': link status interrupt is not "
+                          "supported.", netdev_get_name(netdev));
+            err = EINVAL;
+            goto out;
+        }
+        VLOG_DBG("'%s': not enabling link status interrupt.",
+                 netdev_get_name(netdev));
+        lsc_interrupt_mode = false;
+    }
     if (dev->requested_lsc_interrupt_mode != lsc_interrupt_mode) {
         dev->requested_lsc_interrupt_mode = lsc_interrupt_mode;
         netdev_request_reconfigure(netdev);

vswitchd/vswitch.xml
@@ -4647,12 +4647,12 @@ ovs-vsctl add-port br0 p0 -- set Interface p0 type=patch options:peer=p1 \
       <column name="options" key="dpdk-lsc-interrupt"
               type='{"type": "boolean"}'>
         <p>
-          Set this value to <code>true</code> to configure interrupt mode for
-          Link State Change (LSC) detection instead of poll mode for the DPDK
-          interface.
+          Set this value to <code>false</code> to configure poll mode for
+          Link State Change (LSC) detection instead of interrupt mode for the
+          DPDK interface.
         </p>
         <p>
-          If this value is not set, poll mode is configured.
+          If this value is not set, interrupt mode is configured.
         </p>
         <p>
           This parameter has an effect only on netdev dpdk interfaces.

commit b7257aeef16171c5af35688a631498525392be61

netdev-dpdk: Fix device info return value check.
rte_eth_dev_info_get() could fail due to device reset, etc.

The return value should be checked before the device info
pointer is dereferenced.

Fixes: 2f196c80e716 ("netdev-dpdk: Use LSC interrupt mode.")
Signed-off-by: Jay Ding <jay.ding@broadcom.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

lib/netdev-dpdk.c
@@ -2423,7 +2423,8 @@ netdev_dpdk_set_config(struct netdev *netdev, const struct smap *args,
     }
 
     lsc_interrupt_mode = smap_get_bool(args, "dpdk-lsc-interrupt", true);
-    if (lsc_interrupt_mode && !(*info.dev_flags & RTE_ETH_DEV_INTR_LSC)) {
+    if (lsc_interrupt_mode && !ret &&
+            !(*info.dev_flags & RTE_ETH_DEV_INTR_LSC)) {
         if (smap_get(args, "dpdk-lsc-interrupt")) {
             VLOG_WARN_BUF(errp, "'%s': link status interrupt is not "
                           "supported.", netdev_get_name(netdev));