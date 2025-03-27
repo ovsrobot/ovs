ovsrobot/ovs

commit d4a15647b91791f675ad35a989c744eba62efa95

ovsdb: raft: Enable cooperative multitasking.
The OVSDB server is mostly synchronous and single threaded.  The
OVSDB RAFT storage engine operate under strict deadlines with
operational impact should the deadline be overrun.

Register for cooperative multitasking so that long running
processing elsewhere in the program may yield to allow stable
maintenance of the cluster.

Signed-off-by: Frode Nordahl <frode.nordahl@canonical.com>
Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

ovsdb/raft.c
@@ -22,6 +22,7 @@
 #include <errno.h>
 #include <unistd.h>
 
+#include "cooperative-multitasking.h"
 #include "hash.h"
 #include "jsonrpc.h"
 #include "lockfile.h"
@@ -993,10 +994,13 @@ raft_reset_election_timer(struct raft *raft)
     raft->election_timeout = raft->election_base + duration;
 }
 
+#define RAFT_TIMER_THRESHOLD(t) (t / 3)
+
 static void
 raft_reset_ping_timer(struct raft *raft)
 {
-    raft->ping_timeout = time_msec() + raft->election_timer / 3;
+    raft->ping_timeout =
+        time_msec() + RAFT_TIMER_THRESHOLD(raft->election_timer);
 }
 
 static void
@@ -1371,6 +1375,8 @@ raft_take_leadership(struct raft *raft)
     }
 }
 
+static void raft_run_cb(void *arg);
+
 /* Closes everything owned by 'raft' that might be visible outside the process:
  * network connections, commands, etc.  This is part of closing 'raft'; it is
  * also used if 'raft' has failed in an unrecoverable way. */
@@ -1397,6 +1403,8 @@ raft_close__(struct raft *raft)
     LIST_FOR_EACH_SAFE (conn, list_node, &raft->conns) {
         raft_conn_close(conn);
     }
+
+    cooperative_multitasking_remove(&raft_run_cb, raft);
 }
 
 /* Closes and frees 'raft'.
@@ -2114,12 +2122,25 @@ raft_run(struct raft *raft)
         raft_reset_ping_timer(raft);
     }
 
+    cooperative_multitasking_set(
+        &raft_run_cb, (void *) raft, time_msec(),
+        RAFT_TIMER_THRESHOLD(raft->election_timer)
+        + RAFT_TIMER_THRESHOLD(raft->election_timer) / 10, "raft_run");
+
     /* Do this only at the end; if we did it as soon as we set raft->left or
      * raft->failed in handling the RemoveServerReply, then it could easily
      * cause references to freed memory in RPC sessions, etc. */
     if (raft->left || raft->failed) {
         raft_close__(raft);
     }
+}
+
+static void
+raft_run_cb(void *arg)
+{
+    struct raft *raft = (struct raft *) arg;
+
+    raft_run(raft);
 }
 
 static void

commit 47758e97dd8737c52f7cc2761c02308ce7973925

ovsdb: raft: Fix multitasking overrun warning when there is no leader.
When the cluster falls apart, there is no leader and so there are no
heartbeats that need to be sent or received on time.  In this case
the RAFT_TIMER_THRESHOLD is too small, as the process will only wake
up for a full election timer.  When this happens, cooperative
multitasking code will emit warnings and traces thinking there is a
timer overrun.

Fix that by setting the multitasking interval according to the full
election timer, if there is no known leader.

Fixes: d4a15647b917 ("ovsdb: raft: Enable cooperative multitasking.")
Signed-off-by: Ilya Maximets <i.maximets@ovn.org>
Signed-off-by: 0-day Robot <robot@bytheb.org>

ovsdb/raft.c
@@ -2170,9 +2170,14 @@ raft_run(struct raft *raft)
         raft_reset_ping_timer(raft);
     }
 
-    uint64_t interval = raft->joining
-                        ? RAFT_JOIN_TIMEOUT_MS
-                        : RAFT_TIMER_THRESHOLD(raft->election_timer);
+    uint64_t interval = RAFT_TIMER_THRESHOLD(raft->election_timer);
+
+    if (raft->joining) {
+        interval = RAFT_JOIN_TIMEOUT_MS;
+    } else if (uuid_is_zero(&raft->leader_sid)) {
+        /* There are no heartbeats to handle when there is no leader. */
+        interval = raft->election_timer;
+    }
     cooperative_multitasking_set(
         &raft_run_cb, (void *) raft, time_msec(),
         interval + interval / 10, "raft_run");