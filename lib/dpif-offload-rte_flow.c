f3cbd795df499314aa771a904f98a23e2461e03d
Eelco Chaudron <echaudro@redhat.com>
2025-09-08 15:38:54 +0200

    dpif-offload: Move the flow_get_n_flows() netdev-offload API to dpif.
    This patch moved the flow count APIs to the netdev-offload provider.
    
    Signed-off-by: Eelco Chaudron <echaudro@redhat.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/dpif-offload-rte_flow.c
+++ lib/dpif-offload-rte_flow.c
@@ -254,6 +254,33 @@ dpif_offload_rte_can_offload(struct dpif_offload *dpif_offload OVS_UNUSED,
     return netdev_dpdk_flow_api_supported(netdev, true);
 }
 
+static bool
+dpif_offload_rte_flow_get_n_offloaded_cb(
+    struct dpif_offload_port_mgr_port *port, void *aux)
+{
+    uint64_t *total = aux;
+
+    *total += netdev_offload_dpdk_flow_get_n_offloaded(port->netdev);
+    return false;
+}
+
+static uint64_t
+dpif_offload_rte_flow_get_n_offloaded(const struct dpif_offload *offload)
+{
+    struct dpif_offload_rte_flow *offload_rte = dpif_offload_rte_cast(offload);
+    uint64_t total = 0;
+
+    if (!dpif_offload_is_offload_enabled()) {
+        return 0;
+    }
+
+    dpif_offload_port_mgr_traverse_ports(
+        offload_rte->port_mgr, dpif_offload_rte_flow_get_n_offloaded_cb,
+        &total);
+
+    return total;
+}
+
 static int
 dpif_offload_rte_netdev_flow_flush(const struct dpif_offload *offload
                                    OVS_UNUSED, struct netdev *netdev)
@@ -273,6 +300,7 @@ struct dpif_offload_class dpif_offload_rte_flow_class = {
     .can_offload = dpif_offload_rte_can_offload,
     .port_add = dpif_offload_rte_port_add,
     .port_del = dpif_offload_rte_port_del,
+    .flow_get_n_offloaded = dpif_offload_rte_flow_get_n_offloaded,
     .netdev_flow_flush = dpif_offload_rte_netdev_flow_flush,
 };
 
