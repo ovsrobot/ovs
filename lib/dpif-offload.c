b63853813328ca49eb2f83ae13ad776705994b0a
Eelco Chaudron <echaudro@redhat.com>
2025-10-07 13:57:16 +0200

    dpif-offload: Allow configuration of offload provider priority.
    This patch adds support for configuring the priority of
    offload providers.  When multiple providers exist and
    support the same port, the 'hw-offload-priority' option
    allows specifying the order in which they are tried.
    
    Signed-off-by: Eelco Chaudron <echaudro@redhat.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/dpif-offload.c
+++ lib/dpif-offload.c
@@ -43,9 +43,13 @@ static const struct dpif_offload_class *base_dpif_offload_classes[] = {
 #ifdef DPDK_NETDEV
     &dpif_offload_dpdk_class,
 #endif
+    /* If you add an offload class to this structure, make sure you also
+     * update the dpif_offload_provider_priority_list below. */
     &dpif_offload_dummy_class,
     &dpif_offload_dummy_x_class,
 };
+
+static char *dpif_offload_provider_priority_list = "tc,dpdk,dummy,dummy_x";
 
 static int
 dpif_offload_register_provider__(const struct dpif_offload_class *class)
@@ -125,6 +129,12 @@ dpif_offload_show_classes(struct unixctl_conn *conn, int argc OVS_UNUSED,
 void
 dp_offload_initialize(void)
 {
+    static struct ovsthread_once once = OVSTHREAD_ONCE_INITIALIZER;
+
+    if (!ovsthread_once_start(&once)) {
+        return;
+    }
+
     unixctl_command_register("dpif/offload/classes", NULL, 0, 0,
                              dpif_offload_show_classes, NULL);
 
@@ -134,6 +144,7 @@ dp_offload_initialize(void)
 
         dpif_offload_register_provider(base_dpif_offload_classes[i]);
     }
+    ovsthread_once_done(&once);
 }
 
 static struct dp_offload*
@@ -186,9 +197,12 @@ static int
 dpif_offload_attach_providers_(struct dpif *dpif)
     OVS_REQUIRES(dpif_offload_mutex)
 {
+    struct ovs_list provider_list = OVS_LIST_INITIALIZER(&provider_list);
     const char *dpif_type_str = dpif_normalize_type(dpif_type(dpif));
     struct dp_offload *dp_offload;
+    struct dpif_offload *offload;
     struct shash_node *node;
+    char *tokens, *saveptr;
 
     /* Allocate and attach dp_offload to dpif. */
     dp_offload = xmalloc(sizeof *dp_offload);
@@ -198,34 +212,69 @@ dpif_offload_attach_providers_(struct dpif *dpif)
     ovs_list_init(&dp_offload->offload_providers);
     shash_add(&dpif_offload_providers, dp_offload->dpif_name, dp_offload);
 
-    /* Attach all the providers supporting this dpif type. */
+    /* Open all the providers supporting this dpif type. */
     SHASH_FOR_EACH (node, &dpif_offload_classes) {
         const struct dpif_offload_class *class = node->data;
+
         for (size_t i = 0; class->supported_dpif_types[i] != NULL; i++) {
             if (!strcmp(class->supported_dpif_types[i], dpif_type_str)) {
-                struct dpif_offload *offload;
-                int error;
-
-                error = class->open(class, dpif, &offload);
-                if (!error) {
-
-                    error = dpif_offload_attach_provider_to_dp_offload(
-                        dp_offload, offload);
-                    if (error) {
-                        VLOG_WARN("failed to add dpif offload provider "
-                                  "%s to %s: %s",
-                                  class->type, dpif_name(dpif),
-                                  ovs_strerror(error));
-                        class->close(offload);
-                    }
-                } else {
+                int error = class->open(class, dpif, &offload);
+                if (error) {
                     VLOG_WARN("failed to initialize dpif offload provider "
                               "%s for %s: %s",
                               class->type, dpif_name(dpif),
                               ovs_strerror(error));
+                } else {
+                    ovs_list_push_back(&provider_list,
+                                       &offload->dpif_list_node);
                 }
                 break;
             }
+        }
+    }
+
+    /* Attach all the providers based on the priority list. */
+    tokens = xstrdup(dpif_offload_provider_priority_list);
+
+    for (char *name = strtok_r(tokens, ",", &saveptr);
+         name;
+         name = strtok_r(NULL, ",", &saveptr)) {
+
+        LIST_FOR_EACH_SAFE (offload, dpif_list_node, &provider_list) {
+
+            if (!strcmp(name, offload->class->type)) {
+                int error;
+
+                ovs_list_remove(&offload->dpif_list_node);
+                error = dpif_offload_attach_provider_to_dp_offload(dp_offload,
+                                                                   offload);
+                if (error) {
+                    VLOG_WARN(
+                        "failed to add dpif offload provider %s to %s: %s",
+                        offload->class->type, dpif_name(dpif),
+                        ovs_strerror(error));
+
+                    offload->class->close(offload);
+                }
+                break;
+            }
+        }
+    }
+    free(tokens);
+
+    /* Add remaining entries in order. */
+    LIST_FOR_EACH_SAFE (offload, dpif_list_node, &provider_list) {
+        int error;
+
+        ovs_list_remove(&offload->dpif_list_node);
+        error = dpif_offload_attach_provider_to_dp_offload(dp_offload,
+                                                           offload);
+        if (error) {
+            VLOG_WARN("failed to add dpif offload provider %s to %s: %s",
+                      offload->class->type, dpif_name(dpif),
+                      ovs_strerror(error));
+
+            offload->class->close(offload);
         }
     }
 
@@ -390,4 +439,46 @@ int
 dpif_offload_dump_done(struct dpif_offload_dump *dump)
 {
     return dump->error == EOF ? 0 : dump->error;
+}
+
+void
+dpif_offload_set_global_cfg(const struct smap *other_cfg)
+{
+    static struct ovsthread_once init_once = OVSTHREAD_ONCE_INITIALIZER;
+    const char *priority = smap_get(other_cfg, "hw-offload-priority");
+
+    /* The 'hw-offload-priority' parameter can only be set at startup,
+     * any successive change needs a restart. */
+    if (ovsthread_once_start(&init_once)) {
+        /* Initialize the dpif-offload layer in case it's not yet initialized
+         * at the first invocation of setting the configuration. */
+        dp_offload_initialize();
+
+        /* If priority is not set keep the default value. */
+        if (priority) {
+            char *tokens = xstrdup(priority);
+            char *saveptr;
+
+            dpif_offload_provider_priority_list = xstrdup(priority);
+
+            /* Log a warning for unknown offload providers. */
+            for (char *name = strtok_r(tokens, ",", &saveptr);
+                 name;
+                 name = strtok_r(NULL, ",", &saveptr)) {
+
+                if (!shash_find(&dpif_offload_classes, name)) {
+                    VLOG_WARN("'hw-offload-priority' configuration has an "
+                              "unknown type; %s", name);
+                }
+            }
+            free(tokens);
+        }
+        ovsthread_once_done(&init_once);
+    } else {
+        if (priority && strcmp(priority,
+                               dpif_offload_provider_priority_list)) {
+            VLOG_INFO_ONCE("'hw-offload-priority' configuration changed; "
+                           "restart required");
+        }
+    }
 }
