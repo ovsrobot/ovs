cfd6d41d5295b70d69b41ce477283e6f7b9412af
Eelco Chaudron <echaudro@redhat.com>
2025-10-23 09:44:36 +0200

    netdev-offload: Fold netdev-offload APIs and files into dpif-offload.
    This patch simply renames the netdev-offload-tc|dpdk.c files to
    the corresponding dpif-offload-tc|dpdk-netdev.c files. This makes
    it clearer that offload is now controlled through the new
    dpif-offload provider framework.
    
    It does not rename all individual functions or data structures,
    as that would be too large a change with no real benefit.
    
    In addition, the netdev_set|get_hw_info() APIs were moved from
    netdev-offload.c to netdev.c, allowing the netdev-offload.c
    file to be removed. The same applies to the definitions previously
    located in netdev-offload.h.
    
    Signed-off-by: Eelco Chaudron <echaudro@redhat.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/dpif-offload-dpdk-private.h
+++ lib/dpif-offload-dpdk-private.h
@@ -22,13 +22,42 @@ struct dpif_offload_dpdk;
 struct netdev;
 
 /* DPIF offload dpdk implementation-specific functions.  These should only be
- * used by the associated netdev offload provider, i.e., netdev-offload-dpdk.
- */
+ * used by the associated netdev offload provider, i.e.,
+ * dpif-offload-dpdk-netdev. */
 unsigned int dpdk_offload_thread_id(void);
 struct netdev *dpif_offload_dpdk_get_netdev(
     const struct dpif_offload_dpdk *, odp_port_t port_no);
 void dpif_offload_dpdk_traverse_ports(
     const struct dpif_offload_dpdk *offload,
     bool (*cb)(struct netdev *, odp_port_t, void *), void *aux);
+
+
+/* dpif-offload-dpdk-netdev specific offload functions.  These should only be
+ * used by the associated dpif offload provider, i.e., dpif-offload-dpdk. */
+int netdev_offload_dpdk_init(struct netdev *,
+                             unsigned int offload_thread_count);
+void netdev_offload_dpdk_uninit(struct netdev *);
+int netdev_offload_dpdk_flow_flush(struct dpif_offload_dpdk *,
+                                   struct netdev *);
+uint64_t netdev_offload_dpdk_flow_get_n_offloaded(
+    struct netdev *, unsigned int offload_thread_count);
+uint64_t netdev_offload_dpdk_flow_get_n_offloaded_by_thread(
+    struct netdev *, unsigned int tid);
+int netdev_offload_dpdk_hw_miss_packet_recover(struct dpif_offload_dpdk *,
+                                               struct netdev *,
+                                               struct dp_packet *,
+                                               ovs_u128 **ufid);
+int netdev_offload_dpdk_flow_put(struct dpif_offload_dpdk *,
+                                 struct netdev *, struct match *,
+                                 struct nlattr *actions, size_t actions_len,
+                                 const ovs_u128 *ufid, uint32_t flow_mark,
+                                 odp_port_t orig_in_port,
+                                 struct dpif_flow_stats *);
+int netdev_offload_dpdk_flow_del(struct netdev *, const ovs_u128 *ufid,
+                                 struct dpif_flow_stats *);
+int netdev_offload_dpdk_flow_get(struct netdev *, struct match *,
+                                 struct nlattr **actions, const ovs_u128 *ufid,
+                                 struct dpif_flow_stats *,
+                                 struct dpif_flow_attrs *, struct ofpbuf *buf);
 
 #endif /* DPIF_OFFLOAD_DPDK_PRIVATE_H */
