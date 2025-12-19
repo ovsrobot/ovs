ovsrobot/ovs

commit f74e7df7450d6c31caaad66fe8b1dc923e86e9a7

ofproto-dpif: Always un-wildcard fields that are being set.
The ODP library has an optimization to not set a header if the field was
not changed, regardless of whether an action to set the field was
present.  That library is also responsible for un-wildcarding fields
that are bieng modified.  This leads to a problem where a packet matches
a flow that updates a field, but that particular packet's field already
has that value.  As such, an overly loose megaflow will be generated
that doesn't match on that field and the actions won't update it.  A
second packet that should have the field set will match that flow and
will not be modified.

This commit changes the behavior to always un-wildcard fields that are
being modified.  Since the ODP library updates the entire header if a
field in it is modified, and all those fields will be un-wildcarded, the
generated flows may be different.  However, they should be correct.

Bug #18946.

Reported-by: Jesse Gross <jesse@nicira.com>
Signed-off-by: Justin Pettit <jpettit@nicira.com>
Acked-by: Ben Pfaff <blp@nicira.com>

lib/nx-match.c
@@ -1304,6 +1304,7 @@ nxm_execute_reg_move(const struct ofpact_reg_move *move,
     union mf_value dst_value;
 
     memset(&mask_value, 0xff, sizeof mask_value);
+    mf_write_subfield_flow(&move->dst, &mask_value, &wc->masks);
     mf_write_subfield_flow(&move->src, &mask_value, &wc->masks);
 
     mf_get_value(move->dst.field, flow, &dst_value);
@@ -1322,10 +1323,14 @@ nxm_execute_reg_load(const struct ofpact_reg_load *load, struct flow *flow)
 
 void
 nxm_reg_load(const struct mf_subfield *dst, uint64_t src_data,
-             struct flow *flow)
+             struct flow *flow, struct flow_wildcards *wc)
 {
     union mf_subvalue src_subvalue;
+    union mf_subvalue mask_value;
     ovs_be64 src_data_be = htonll(src_data);
+
+    memset(&mask_value, 0xff, sizeof mask_value);
+    mf_write_subfield_flow(dst, &mask_value, &wc->masks);
 
     bitwise_copy(&src_data_be, sizeof src_data_be, 0,
                  &src_subvalue, sizeof src_subvalue, 0,
@@ -1479,7 +1484,8 @@ nxm_execute_stack_push(const struct ofpact_stack *push,
 
 void
 nxm_execute_stack_pop(const struct ofpact_stack *pop,
-                      struct flow *flow, struct ofpbuf *stack)
+                      struct flow *flow, struct flow_wildcards *wc,
+                      struct ofpbuf *stack)
 {
     union mf_subvalue *src_value;
 
@@ -1487,6 +1493,10 @@ nxm_execute_stack_pop(const struct ofpact_stack *pop,
 
     /* Only pop if stack is not empty. Otherwise, give warning. */
     if (src_value) {
+        union mf_subvalue mask_value;
+
+        memset(&mask_value, 0xff, sizeof mask_value);
+        mf_write_subfield_flow(&pop->subfield, &mask_value, &wc->masks);
         mf_write_subfield_flow(&pop->subfield, src_value, flow);
     } else {
         if (!VLOG_DROP_WARN(&rl)) {

ofproto/ofproto-dpif-xlate.c
@@ -1708,6 +1708,7 @@ compose_set_mpls_ttl_action(struct xlate_ctx *ctx, uint8_t ttl)
         return true;
     }
 
+    ctx->xout->wc.masks.mpls_lse |= htonl(MPLS_TTL_MASK);
     set_mpls_lse_ttl(&ctx->xin->flow.mpls_lse, ttl);
     return false;
 }
@@ -1887,7 +1888,8 @@ xlate_bundle_action(struct xlate_ctx *ctx,
                           slave_enabled_cb,
                           CONST_CAST(struct xbridge *, ctx->xbridge));
     if (bundle->dst.field) {
-        nxm_reg_load(&bundle->dst, ofp_to_u16(port), &ctx->xin->flow);
+        nxm_reg_load(&bundle->dst, ofp_to_u16(port), &ctx->xin->flow,
+                     &ctx->xout->wc);
     } else {
         xlate_output_action(ctx, port, 0, false);
     }
@@ -2038,12 +2040,14 @@ do_xlate_actions(const struct ofpact *ofpacts, size_t ofpacts_len,
             break;
 
         case OFPACT_SET_VLAN_VID:
+            wc->masks.vlan_tci |= htons(VLAN_VID_MASK | VLAN_CFI);
             flow->vlan_tci &= ~htons(VLAN_VID_MASK);
             flow->vlan_tci |= (htons(ofpact_get_SET_VLAN_VID(a)->vlan_vid)
                                | htons(VLAN_CFI));
             break;
 
         case OFPACT_SET_VLAN_PCP:
+            wc->masks.vlan_tci |= htons(VLAN_PCP_MASK | VLAN_CFI);
             flow->vlan_tci &= ~htons(VLAN_PCP_MASK);
             flow->vlan_tci |=
                 htons((ofpact_get_SET_VLAN_PCP(a)->vlan_pcp << VLAN_PCP_SHIFT)
@@ -2051,35 +2055,42 @@ do_xlate_actions(const struct ofpact *ofpacts, size_t ofpacts_len,
             break;
 
         case OFPACT_STRIP_VLAN:
+            memset(&wc->masks.vlan_tci, 0xff, sizeof wc->masks.vlan_tci);
             flow->vlan_tci = htons(0);
             break;
 
         case OFPACT_PUSH_VLAN:
             /* XXX 802.1AD(QinQ) */
+            memset(&wc->masks.vlan_tci, 0xff, sizeof wc->masks.vlan_tci);
             flow->vlan_tci = htons(VLAN_CFI);
             break;
 
         case OFPACT_SET_ETH_SRC:
+            memset(&wc->masks.dl_src, 0xff, sizeof wc->masks.dl_src);
             memcpy(flow->dl_src, ofpact_get_SET_ETH_SRC(a)->mac, ETH_ADDR_LEN);
             break;
 
         case OFPACT_SET_ETH_DST:
+            memset(&wc->masks.dl_dst, 0xff, sizeof wc->masks.dl_dst);
             memcpy(flow->dl_dst, ofpact_get_SET_ETH_DST(a)->mac, ETH_ADDR_LEN);
             break;
 
         case OFPACT_SET_IPV4_SRC:
+            memset(&wc->masks.nw_src, 0xff, sizeof wc->masks.nw_src);
             if (flow->dl_type == htons(ETH_TYPE_IP)) {
                 flow->nw_src = ofpact_get_SET_IPV4_SRC(a)->ipv4;
             }
             break;
 
         case OFPACT_SET_IPV4_DST:
+            memset(&wc->masks.nw_dst, 0xff, sizeof wc->masks.nw_dst);
             if (flow->dl_type == htons(ETH_TYPE_IP)) {
                 flow->nw_dst = ofpact_get_SET_IPV4_DST(a)->ipv4;
             }
             break;
 
         case OFPACT_SET_IPV4_DSCP:
+            wc->masks.nw_tos |= IP_DSCP_MASK;
             /* OpenFlow 1.0 only supports IPv4. */
             if (flow->dl_type == htons(ETH_TYPE_IP)) {
                 flow->nw_tos &= ~IP_DSCP_MASK;
@@ -2089,6 +2100,7 @@ do_xlate_actions(const struct ofpact *ofpacts, size_t ofpacts_len,
 
         case OFPACT_SET_L4_SRC_PORT:
             memset(&wc->masks.nw_proto, 0xff, sizeof wc->masks.nw_proto);
+            memset(&wc->masks.tp_src, 0xff, sizeof wc->masks.tp_src);
             if (is_ip_any(flow)) {
                 flow->tp_src = htons(ofpact_get_SET_L4_SRC_PORT(a)->port);
             }
@@ -2096,6 +2108,7 @@ do_xlate_actions(const struct ofpact *ofpacts, size_t ofpacts_len,
 
         case OFPACT_SET_L4_DST_PORT:
             memset(&wc->masks.nw_proto, 0xff, sizeof wc->masks.nw_proto);
+            memset(&wc->masks.tp_dst, 0xff, sizeof wc->masks.tp_dst);
             if (is_ip_any(flow)) {
                 flow->tp_dst = htons(ofpact_get_SET_L4_DST_PORT(a)->port);
             }
@@ -2131,7 +2144,8 @@ do_xlate_actions(const struct ofpact *ofpacts, size_t ofpacts_len,
             break;
 
         case OFPACT_STACK_POP:
-            nxm_execute_stack_pop(ofpact_get_STACK_POP(a), flow, &ctx->stack);
+            nxm_execute_stack_pop(ofpact_get_STACK_POP(a), flow, wc,
+                                  &ctx->stack);
             break;
 
         case OFPACT_PUSH_MPLS:
@@ -2156,6 +2170,7 @@ do_xlate_actions(const struct ofpact *ofpacts, size_t ofpacts_len,
             break;
 
         case OFPACT_DEC_TTL:
+            wc->masks.nw_ttl = 0xff;
             if (compose_dec_ttl(ctx, ofpact_get_DEC_TTL(a))) {
                 goto out;
             }

lib/multipath.c
@@ -114,7 +114,7 @@ multipath_execute(const struct ofpact_multipath *mp, struct flow *flow,
                                         mp->max_link + 1, mp->arg);
 
     flow_mask_hash_fields(flow, wc, mp->fields);
-    nxm_reg_load(&mp->dst, link, flow);
+    nxm_reg_load(&mp->dst, link, flow, wc);
 }
 
 static uint16_t

lib/nx-match.h
@@ -87,7 +87,7 @@ void nxm_execute_reg_move(const struct ofpact_reg_move *, struct flow *,
                           struct flow_wildcards *);
 void nxm_execute_reg_load(const struct ofpact_reg_load *, struct flow *);
 void nxm_reg_load(const struct mf_subfield *, uint64_t src_data,
-                  struct flow *);
+                  struct flow *, struct flow_wildcards *);
 
 char *nxm_parse_stack_action(struct ofpact_stack *, const char *)
     WARN_UNUSED_RESULT;
@@ -113,7 +113,8 @@ void nxm_execute_stack_push(const struct ofpact_stack *,
                             const struct flow *, struct flow_wildcards *,
                             struct ofpbuf *);
 void nxm_execute_stack_pop(const struct ofpact_stack *,
-                            struct flow *, struct ofpbuf *);
+                            struct flow *, struct flow_wildcards *,
+                            struct ofpbuf *);
 
 int nxm_field_bytes(uint32_t header);
 int nxm_field_bits(uint32_t header);

commit e58caa7b78b436650ed29711a4142b8426ad9b8f

tunnels: Don't unwildcard tunnel fields on write.
Unlike other fields, the tunnel ones don't really need to be
unwildcarded on writes (load, move, set_field, stack_pop).

The main reason for unwildcarding other fields is that we have
optimizations in odp-util that skip generation of set() actions for
packet fields that didn't change.  So, we have to match on them to
avoid installing an overly broad flow that will swallow unrelated
traffic that would have the set() action generated.

But this is not the case for tunnel fields, for which the set(tunnel)
action always contains the full set of required fields.  The action
can be skipped entirely if the tunnel metadata didn't change at all,
but this is only the case when we're sending a packet to the exact
same tunnel twice, which is not a problem, as only the second
set(tunnel) is skipped and all the correct metadata was already set
by the first one.  And sending a packet back to the same tunnel
without changing any metadata is not really possible, as at least the
source port will be zeroed out, and is not a reasonable thing to do
regardless.

Also, all the basic fields that are required to distinguish two tunnel
ports from each other are exact matched by default in tnl_wc_init().

Let's check if the destination field is a tunnel field and avoid
unnecessarily unwildcarding them.

This is needed because when the tunnel family changes from IPv4 to
IPv6, while setting tun_ipv6_src/dst we're unwildcarding those fields
in the match, making it match on both IPv4 and IPv6 addresses.  This
is fine from the OpenFlow point of view, since all these fields have
no prerequisites.  And the userspace daapath can handle this just fine.
But Linux kernel datapath is using a union to store the tunnel config
and so it rejects flows with both IPv4 and IPv6 fields matched.
This makes it very expensive to bridge IPv4 and IPv6 flow-based
tunnels together with the kernel datapath, as flow put always fails
and every packet has to go through userspace.

The change also allows to avoid matches on other fields that didn't
exist in the original tunnel, e.g., extra geneve metadata TLVs that
are added in transit.

We cannot just fixup the flow match at the xlate_wc_finish() stage,
because we may have extra fields unwildcarded by the classifier and
we do not have enough information to tell which are required and
which are not.  Tt may even be possible that it is required by the
OpenFlow rules to match on both IPv4 and IPv6 fields at the same time.
And this can even be a valid configuration to distinguish packets
that are not coming from a tunnel by checking if both metadata fields
are zero.  This is a separate and potentially more complex issue to
address and it may require introduction of new fields that would make
it possible to make decisions without looking at both sets of metadata,
e.g. tun_eth_type.  This commit is focusing only on matches coming
from the actions like set_field.

Fixes: f74e7df7450d ("ofproto-dpif: Always un-wildcard fields that are being set.")

include/openvswitch/meta-flow.h
@@ -2307,6 +2307,7 @@ void mf_set_flow_value_masked(const struct mf_field *,
                               const union mf_value *value,
                               const union mf_value *mask,
                               struct flow *);
+bool mf_is_tunnel_field(const struct mf_field *);
 bool mf_is_tun_metadata(const struct mf_field *);
 bool mf_is_any_metadata(const struct mf_field *);
 bool mf_is_frozen_metadata(const struct mf_field *);

lib/meta-flow.c
@@ -1784,6 +1784,115 @@ mf_set_flow_value_masked(const struct mf_field *field,
 }
 
 bool
+mf_is_tunnel_field(const struct mf_field *mf)
+{
+    switch (mf->id) {
+    case MFF_TUN_ID:
+    case MFF_TUN_SRC:
+    case MFF_TUN_DST:
+    case MFF_TUN_IPV6_SRC:
+    case MFF_TUN_IPV6_DST:
+    case MFF_TUN_FLAGS:
+    case MFF_TUN_TTL:
+    case MFF_TUN_TOS:
+    case MFF_TUN_GBP_ID:
+    case MFF_TUN_GBP_FLAGS:
+    case MFF_TUN_ERSPAN_VER:
+    case MFF_TUN_ERSPAN_IDX:
+    case MFF_TUN_ERSPAN_DIR:
+    case MFF_TUN_ERSPAN_HWID:
+    case MFF_TUN_GTPU_FLAGS:
+    case MFF_TUN_GTPU_MSGTYPE:
+    CASE_MFF_TUN_METADATA:
+        return true;
+
+    case MFF_DP_HASH:
+    case MFF_RECIRC_ID:
+    case MFF_PACKET_TYPE:
+    case MFF_CONJ_ID:
+    case MFF_METADATA:
+    case MFF_IN_PORT:
+    case MFF_IN_PORT_OXM:
+    case MFF_ACTSET_OUTPUT:
+    case MFF_SKB_PRIORITY:
+    case MFF_PKT_MARK:
+    case MFF_CT_STATE:
+    case MFF_CT_ZONE:
+    case MFF_CT_MARK:
+    case MFF_CT_LABEL:
+    case MFF_CT_NW_PROTO:
+    case MFF_CT_NW_SRC:
+    case MFF_CT_NW_DST:
+    case MFF_CT_IPV6_SRC:
+    case MFF_CT_IPV6_DST:
+    case MFF_CT_TP_SRC:
+    case MFF_CT_TP_DST:
+    CASE_MFF_REGS:
+    CASE_MFF_XREGS:
+    CASE_MFF_XXREGS:
+    case MFF_ETH_SRC:
+    case MFF_ETH_DST:
+    case MFF_ETH_TYPE:
+    case MFF_VLAN_TCI:
+    case MFF_DL_VLAN:
+    case MFF_VLAN_VID:
+    case MFF_DL_VLAN_PCP:
+    case MFF_VLAN_PCP:
+    case MFF_MPLS_LABEL:
+    case MFF_MPLS_TC:
+    case MFF_MPLS_BOS:
+    case MFF_MPLS_TTL:
+    case MFF_IPV4_SRC:
+    case MFF_IPV4_DST:
+    case MFF_IPV6_SRC:
+    case MFF_IPV6_DST:
+    case MFF_IPV6_LABEL:
+    case MFF_IP_PROTO:
+    case MFF_IP_DSCP:
+    case MFF_IP_DSCP_SHIFTED:
+    case MFF_IP_ECN:
+    case MFF_IP_TTL:
+    case MFF_IP_FRAG:
+    case MFF_ARP_OP:
+    case MFF_ARP_SPA:
+    case MFF_ARP_TPA:
+    case MFF_ARP_SHA:
+    case MFF_ARP_THA:
+    case MFF_TCP_SRC:
+    case MFF_TCP_DST:
+    case MFF_TCP_FLAGS:
+    case MFF_UDP_SRC:
+    case MFF_UDP_DST:
+    case MFF_SCTP_SRC:
+    case MFF_SCTP_DST:
+    case MFF_ICMPV4_TYPE:
+    case MFF_ICMPV4_CODE:
+    case MFF_ICMPV6_TYPE:
+    case MFF_ICMPV6_CODE:
+    case MFF_ND_TARGET:
+    case MFF_ND_SLL:
+    case MFF_ND_TLL:
+    case MFF_ND_RESERVED:
+    case MFF_ND_OPTIONS_TYPE:
+    case MFF_NSH_FLAGS:
+    case MFF_NSH_MDTYPE:
+    case MFF_NSH_NP:
+    case MFF_NSH_SPI:
+    case MFF_NSH_SI:
+    case MFF_NSH_C1:
+    case MFF_NSH_C2:
+    case MFF_NSH_C3:
+    case MFF_NSH_C4:
+    case MFF_NSH_TTL:
+        return false;
+
+    case MFF_N_IDS:
+    default:
+        OVS_NOT_REACHED();
+    }
+}
+
+bool
 mf_is_tun_metadata(const struct mf_field *mf)
 {
     return mf->id >= MFF_TUN_METADATA0 &&
@@ -2767,6 +2876,10 @@ unwildcard_subfield(const struct mf_subfield *sf, struct flow_wildcards *wc)
 /* Copies 'src' into 'dst' within 'flow', and sets all the bits in 'src' and
  * 'dst' to 1s in 'wc', if 'wc' is nonnull.
  *
+ * Note: doesn't set 'dst' to 1s in case 'dst' is a tunnel field, because it
+ * is not necessary, as ODP library doesn't rely on matching these fields
+ * when they are written but not read.
+ *
  * 'src' and 'dst' may overlap. */
 void
 mf_subfield_copy(const struct mf_subfield *src,
@@ -2777,7 +2890,9 @@ mf_subfield_copy(const struct mf_subfield *src,
     if (mf_are_prereqs_ok(dst->field, flow, wc)
         && mf_are_prereqs_ok(src->field, flow, wc)) {
         unwildcard_subfield(src, wc);
-        unwildcard_subfield(dst, wc);
+        if (!mf_is_tunnel_field(dst->field)) {
+            unwildcard_subfield(dst, wc);
+        }
 
         union mf_value src_value;
         union mf_value dst_value;

lib/nx-match.c
@@ -1850,8 +1850,12 @@ nxm_reg_load(const struct mf_subfield *dst, uint64_t src_data,
     union mf_subvalue mask_value;
     ovs_be64 src_data_be = htonll(src_data);
 
-    memset(&mask_value, 0xff, sizeof mask_value);
-    mf_write_subfield_flow(dst, &mask_value, &wc->masks);
+    /* ODP library doesn't rely on tunnel fields to be unwildcarded
+     * when written but not read.  No need to unwildcard them here. */
+    if (!mf_is_tunnel_field(dst->field)) {
+        memset(&mask_value, 0xff, sizeof mask_value);
+        mf_write_subfield_flow(dst, &mask_value, &wc->masks);
+    }
 
     bitwise_copy(&src_data_be, sizeof src_data_be, 0,
                  &src_subvalue, sizeof src_subvalue, 0,
@@ -1967,7 +1971,7 @@ nxm_execute_stack_push(const struct ofpact_stack *push,
     union mf_subvalue dst_value;
 
     mf_write_subfield_flow(&push->subfield,
-                           (union mf_subvalue *)&exact_match_mask,
+                           (union mf_subvalue *) &exact_match_mask,
                            &wc->masks);
 
     mf_read_subfield(&push->subfield, flow, &dst_value);
@@ -1991,9 +1995,13 @@ nxm_execute_stack_pop(const struct ofpact_stack *pop,
                    dst_bytes - src_bytes);
         }
         memcpy(&src_value.u8[sizeof src_value - src_bytes], src, src_bytes);
-        mf_write_subfield_flow(&pop->subfield,
-                               (union mf_subvalue *)&exact_match_mask,
-                               &wc->masks);
+        /* ODP library doesn't rely on tunnel fields to be unwildcarded
+         * when written but not read.  No need to unwildcard them here. */
+        if (!mf_is_tunnel_field(pop->subfield.field)) {
+            mf_write_subfield_flow(&pop->subfield,
+                                   (union mf_subvalue *) &exact_match_mask,
+                                   &wc->masks);
+        }
         mf_write_subfield_flow(&pop->subfield, &src_value, flow);
         return true;
     } else {

ofproto/ofproto-dpif-xlate.c
@@ -7665,7 +7665,13 @@ do_xlate_actions(const struct ofpact *ofpacts, size_t ofpacts_len,
             /* Set the field only if the packet actually has it. */
             if (mf_are_prereqs_ok(mf, flow, wc)) {
                 mf_set_mask_l3_prereqs(mf, flow, wc);
-                mf_mask_field_masked(mf, ofpact_set_field_mask(set_field), wc);
+                /* Tunnel fields do not need to be unwildcarded, as ODP library
+                 * doesn't rely on matching these fields when they are written
+                 * but not read. */
+                if (!mf_is_tunnel_field(mf)) {
+                    mf_mask_field_masked(mf, ofpact_set_field_mask(set_field),
+                                         wc);
+                }
                 mf_set_flow_value_masked(mf, set_field->value,
                                          ofpact_set_field_mask(set_field),
                                          flow);

tests/system-traffic.at
@@ -1264,6 +1264,107 @@ NS_CHECK_EXEC([at_ns0], [ping -s 3200 -q -c 3 -i 0.3 -W 2 10.1.1.2 | FORMAT_PING
 OVS_TRAFFIC_VSWITCHD_STOP
 AT_CLEANUP
 
+AT_SETUP([datapath - bridging IPv4 and IPv6 flow based geneve tunnels])
+OVS_CHECK_TUNNEL_TSO()
+OVS_CHECK_GENEVE()
+
+OVS_TRAFFIC_VSWITCHD_START()
+ADD_BR([br-underlay-0])
+ADD_BR([br-underlay-1])
+
+ADD_NAMESPACES(at_ns0)
+ADD_NAMESPACES(at_ns1)
+
+dnl Set up underlay link from host into the namespaces using veth pairs.
+ADD_VETH(p0, at_ns0, br-underlay-0, "172.31.1.1/24")
+AT_CHECK([ip addr add dev br-underlay-0 "172.31.1.100/24"])
+AT_CHECK([ip link set dev br-underlay-0 up])
+
+ADD_VETH(p1, at_ns1, br-underlay-1, "fc00::1/64", [], [], "nodad")
+AT_CHECK([ip addr add dev br-underlay-1 "fc00::100/64" nodad])
+AT_CHECK([ip link set dev br-underlay-1 up])
+
+dnl Set up a single flow-based OVS tunnel endpoint in a root namespace and
+dnl two native linux devices inside the test namespaces.
+dnl
+dnl  ns_gnv0                          |               ns_gnv1
+dnl  ip:        10.1.1.1/24           |               ip:        10.1.1.2/24
+dnl  remote_ip: 172.31.1.100          |               remote_ip: fc00::100
+dnl  vni:       4                     |               vni:       6
+dnl          |                        |                  |
+dnl          |                        |                  |
+dnl  p0                               |               p1
+dnl  ip: 172.31.1.1/24                |               ip: fc00::1/64
+dnl          |                  NS0   |   NS1            |
+dnl ---------|------------------------+------------------|--------------------
+dnl          |                                           |
+dnl       br-underlay-0:                           br-underlay-1:
+dnl       ip: 172.31.1.100/24                      ip: fc00::100/64
+dnl         ovs-p0                                   ovs-p1
+dnl          |                                           |
+dnl          |            br0                            |
+dnl       encap/decap --- ip: 10.1.1.100/24 --------- encap/decap
+dnl                         at_gnv0
+dnl                           remote_ip: flow
+dnl                           local_ip: flow
+dnl                           key: flow
+dnl
+
+dnl Using v6 macro for OVS tunnel to account for maximum overhead.
+ADD_OVS_TUNNEL6([geneve], [br0], [at_gnv0], [flow], [10.1.1.100/24],
+                [options:local_ip=flow options:key=flow])
+
+dnl Setup Linux native tunnels inside namespaces.  Again, using v6 macro for
+dnl the proper MTU configuration.
+ADD_NATIVE_TUNNEL6([geneve], [ns_gnv0], [at_ns0], [172.31.1.100], [10.1.1.1/24],
+                   [vni 4])
+ADD_NATIVE_TUNNEL6([geneve], [ns_gnv1], [at_ns1], [fc00::100], [10.1.1.2/24],
+                   [vni 6 udp6zerocsumtx udp6zerocsumrx])
+
+AT_DATA([flows.txt], [dnl
+in_port=at_gnv0,tun_id=4,actions=load:0->tun_src,load:0->tun_dst,set_field:fc00::100->tun_ipv6_src,set_field:fc00::1->tun_ipv6_dst,set_field:6->tun_id,output:in_port
+in_port=at_gnv0,tun_id=6,actions=load:0->tun_ipv6_src,load:0->tun_ipv6_dst,set_field:172.31.1.100->tun_src,set_field:172.31.1.1->tun_dst,set_field:4->tun_id,output:in_port
+])
+
+AT_CHECK([ovs-ofctl -OOpenFlow15 add-flows br0 flows.txt])
+AT_CHECK([ovs-ofctl add-flow br-underlay-0 "actions=normal"])
+AT_CHECK([ovs-ofctl add-flow br-underlay-1 "actions=normal"])
+
+dnl It many take longer for IPv6 address to become available.
+OVS_WAIT_UNTIL([ip netns exec at_ns1 ping6 -c 2 fc00::100])
+
+dnl First, check both underlays.
+NS_CHECK_EXEC([at_ns0], [ping -q -c 3 -i 0.3 -W 2 172.31.1.100 | FORMAT_PING], [0], [dnl
+3 packets transmitted, 3 received, 0% packet loss, time 0ms
+])
+NS_CHECK_EXEC([at_ns1], [ping6 -q -c 3 -i 0.3 -W 2 fc00::100 | FORMAT_PING], [0], [dnl
+3 packets transmitted, 3 received, 0% packet loss, time 0ms
+])
+
+dnl Now, check the overlay with different packet sizes.
+NS_CHECK_EXEC([at_ns0], [ping -q -c 3 -i 0.3 -W 2 10.1.1.2 | FORMAT_PING], [0], [dnl
+3 packets transmitted, 3 received, 0% packet loss, time 0ms
+])
+NS_CHECK_EXEC([at_ns0], [ping -s 1600 -q -c 3 -i 0.3 -W 2 10.1.1.2 | FORMAT_PING], [0], [dnl
+3 packets transmitted, 3 received, 0% packet loss, time 0ms
+])
+NS_CHECK_EXEC([at_ns0], [ping -s 3200 -q -c 3 -i 0.3 -W 2 10.1.1.2 | FORMAT_PING], [0], [dnl
+3 packets transmitted, 3 received, 0% packet loss, time 0ms
+])
+
+dnl There should be 6 tunnel flows in the datapath - 3 in v4 to v6 direction
+dnl for different fragment types (no, first, later) and 3 from v6 to v4.
+dnl Only count flows that are used in the datapath to make sure they are
+dnl actually matching the traffic.
+AT_CHECK([ovs-appctl dpctl/dump-flows | grep -v 'packets:0' | dnl
+          grep "eth_type(0x0800)" | grep -c tun_id], [0], [dnl
+6
+])
+
+OVS_TRAFFIC_VSWITCHD_STOP
+AT_CLEANUP
+
+
 AT_SETUP([datapath - handling of geneve corrupted metadata])
 OVS_CHECK_GENEVE()
 

tests/tunnel.at
@@ -993,13 +993,13 @@ AT_CHECK([ovs-ofctl add-flows br0 flows.txt])
 
 AT_CHECK([ovs-appctl ofproto/trace ovs-dummy 'recirc_id(0),tunnel(tun_id=0x0,src=1.1.1.1,dst=1.1.1.2,ttl=64,geneve({class=0xffff,type=0,len=4,0x11112222}),flags(df|key)),in_port(6081),skb_mark(0),eth(src=50:54:00:00:00:05,dst=50:54:00:00:00:07),eth_type(0x0800),ipv4(frag=no)'], [0], [stdout])
 AT_CHECK([tail -2 stdout], [0],
-  [Megaflow: recirc_id=0,eth,ip,tun_id=0,tun_src=1.1.1.1,tun_dst=1.1.1.2,tun_tos=0,tun_flags=+df-csum+key,tun_metadata0=0x11112222,tun_metadata1=NP,in_port=1,nw_ecn=0,nw_frag=no
+  [Megaflow: recirc_id=0,eth,ip,tun_id=0,tun_src=1.1.1.1,tun_dst=1.1.1.2,tun_tos=0,tun_flags=+df-csum+key,tun_metadata0=0x11112222,in_port=1,nw_ecn=0,nw_frag=no
 Datapath actions: set(tunnel(dst=1.1.1.1,ttl=64,tp_dst=6081,geneve({class=0xffff,type=0,len=4,0x11112222}{class=0xffff,type=0x1,len=4,0x55556666}),flags(df))),6081
 ])
 
 AT_CHECK([ovs-appctl ofproto/trace ovs-dummy 'recirc_id(0),tunnel(tun_id=0x0,src=1.1.1.1,dst=1.1.1.2,ttl=64,geneve({class=0xffff,type=0,len=4,0x33334444}),flags(df|key)),in_port(6081),skb_mark(0),eth(src=50:54:00:00:00:05,dst=50:54:00:00:00:07),eth_type(0x0800),ipv4(frag=no)'], [0], [stdout])
 AT_CHECK([tail -2 stdout], [0],
-  [Megaflow: recirc_id=0,eth,ip,tun_id=0,tun_src=1.1.1.1,tun_dst=1.1.1.2,tun_tos=0,tun_flags=+df-csum+key,tun_metadata0=0x33334444,tun_metadata1=NP,in_port=1,nw_ecn=0,nw_frag=no
+  [Megaflow: recirc_id=0,eth,ip,tun_id=0,tun_src=1.1.1.1,tun_dst=1.1.1.2,tun_tos=0,tun_flags=+df-csum+key,tun_metadata0=0x33334444,in_port=1,nw_ecn=0,nw_frag=no
 Datapath actions: set(tunnel(dst=1.1.1.1,ttl=64,tp_dst=6081,geneve({class=0xffff,type=0x1,len=4,0x77778888}),flags(df))),6081
 ])
 
@@ -1032,6 +1032,38 @@ AT_CHECK([tail -1 stdout], [0],
 AT_CHECK([ovs-appctl ofproto/trace ovs-dummy 'tunnel(tun_id=0x0,ipv6_src=2001:cafe::1,ipv6_dst=2001:cafe::2,ttl=64),in_port(4789)'], [0], [stdout])
 AT_CHECK([tail -1 stdout], [0],
   [Datapath actions: set(tunnel(dst=1.1.1.1,ttl=64,tp_dst=4789,flags(df))),4789
+])
+
+OVS_VSWITCHD_STOP
+AT_CLEANUP
+
+AT_SETUP([tunnel - concomitant IPv6 and IPv4 flow based tunnels])
+OVS_VSWITCHD_START([dnl
+    add-port br0 p1 -- set Interface p1 type=vxlan options:key=4 \
+        options:local_ip=flow options:remote_ip=flow ofport_request=1 \
+ -- add-port br0 p2 -- set Interface p2 type=vxlan options:key=6 \
+        options:local_ip=flow options:remote_ip=flow ofport_request=2])
+
+AT_DATA([flows.txt], [dnl
+in_port=1,actions=load:0->tun_src,load:0->tun_dst,set_field:2001:cafe::1->tun_ipv6_src,set_field:fc00::2->tun_ipv6_dst,2
+in_port=2,actions=load:0->tun_ipv6_src,load:0->tun_ipv6_dst,set_field:1.1.1.1->tun_src,set_field:4.4.4.4->tun_dst,1
+])
+OVS_VSWITCHD_DISABLE_TUNNEL_PUSH_POP
+
+AT_CHECK([ovs-ofctl -OOpenFlow15 add-flows br0 flows.txt])
+
+AT_CHECK([ovs-appctl ofproto/trace ovs-dummy \
+    'tunnel(tun_id=4,src=4.4.4.4,dst=1.1.1.1,ttl=64,flags(df|csum|key)),in_port(4789)'], [0], [stdout])
+AT_CHECK([tail -2 stdout], [0], [dnl
+Megaflow: recirc_id=0,eth,tun_id=0x4,tun_src=4.4.4.4,tun_dst=1.1.1.1,tun_tos=0,tun_flags=+df+csum+key,in_port=1,dl_type=0x05ff
+Datapath actions: set(tunnel(tun_id=0x6,ipv6_src=2001:cafe::1,ipv6_dst=fc00::2,ttl=64,tp_dst=4789,flags(df|csum|key))),4789
+])
+
+AT_CHECK([ovs-appctl ofproto/trace ovs-dummy \
+    'tunnel(tun_id=6,ipv6_src=fc00::2,ipv6_dst=2001:cafe::1,ttl=64,flags(df|csum|key)),in_port(4789)'], [0], [stdout])
+AT_CHECK([tail -2 stdout], [0], [dnl
+Megaflow: recirc_id=0,eth,tun_id=0x6,tun_ipv6_src=fc00::2,tun_ipv6_dst=2001:cafe::1,tun_tos=0,tun_flags=+df+csum+key,in_port=2,dl_type=0x05ff
+Datapath actions: set(tunnel(tun_id=0x4,src=1.1.1.1,dst=4.4.4.4,ttl=64,tp_dst=4789,flags(df|key))),4789
 ])
 
 OVS_VSWITCHD_STOP