ovsrobot/ovs

commit 5c30756a3739ace7d0e062f461aa1ebe79fa6f3b

ovs-router: Introduce ovs/route/rule/show command.
Add a new command `ovs-appctl ovs/route/rule/show [-6]` for printing OVS
internal routing rules database:

  ovs-appctl ovs/route/rule/show
  32763: from 7.7.7.17 lookup 20
  32764: from all lookup 15
  32765: from 7.7.7.7 lookup 10

The rules are sorted from the highest priority being the first to the
lowest being the last one. IPv6 rules are printed with the optional '-6'
flag.

Signed-off-by: Dima Chumak <dchumak@nvidia.com>
Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

lib/ovs-router.c
@@ -812,6 +812,112 @@ ovs_router_show(struct unixctl_conn *conn, int argc OVS_UNUSED,
 }
 
 static void
+ovs_router_rules_show_json(struct json *rule_entries, bool ipv6)
+{
+    struct router_rule *rule;
+    struct ds ds;
+
+    PVECTOR_FOR_EACH (rule, &rules) {
+        struct json *entry = json_object_create();
+
+        if (rule->ipv4 == ipv6) {
+            continue;
+        }
+
+        json_object_put(entry, "priority", json_integer_create(rule->prio));
+        json_object_put(entry, "invert", json_boolean_create(rule->invert));
+        json_object_put(entry, "ipv4", json_boolean_create(rule->ipv4));
+        json_object_put(entry, "src-prefix",
+                        json_integer_create(rule->src_prefix));
+        json_object_put(entry, "lookup",
+                        json_integer_create(rule->lookup_table));
+
+        if (rule->src_prefix) {
+            ds_init(&ds);
+            ipv6_format_mapped(&rule->from_addr, &ds);
+            json_object_put_string(entry, "from", ds_cstr_ro(&ds));
+            ds_destroy(&ds);
+        } else {
+            json_object_put_string(entry, "from", "all");
+        }
+
+        json_array_add(rule_entries, entry);
+    }
+}
+
+static char *
+standard_table_name(uint32_t table)
+{
+    switch (table) {
+    case CLS_DEFAULT:
+        return "default";
+    case CLS_MAIN:
+        return "main";
+    case CLS_LOCAL:
+        return "local";
+    }
+
+    return NULL;
+}
+
+static void
+ovs_router_rules_show_text(struct ds *ds, bool ipv6)
+{
+    struct router_rule *rule;
+
+    PVECTOR_FOR_EACH (rule, &rules) {
+        if (rule->ipv4 == ipv6) {
+            continue;
+        }
+        ds_put_format(ds, "%"PRIu32": ", rule->prio);
+        if (rule->invert) {
+            ds_put_format(ds, "not ");
+        }
+        ds_put_format(ds, "from ");
+        if (rule->src_prefix) {
+            ipv6_format_mapped(&rule->from_addr, ds);
+            if (!((IN6_IS_ADDR_V4MAPPED(&rule->from_addr) &&
+                   rule->src_prefix == 32) || rule->src_prefix == 128)) {
+                ds_put_format(ds, "/%"PRIu8" ", rule->src_prefix);
+            }
+        } else {
+            ds_put_cstr(ds, "all");
+        }
+        ds_put_format(ds, " ");
+        if (is_standard_table(rule->lookup_table)) {
+            ds_put_format(ds, "lookup %s\n",
+                          standard_table_name(rule->lookup_table));
+        } else {
+            ds_put_format(ds, "lookup %"PRIu32"\n", rule->lookup_table);
+        }
+    }
+}
+
+static void
+ovs_router_rules_show(struct unixctl_conn *conn, int argc OVS_UNUSED,
+                      const char *argv[] OVS_UNUSED, void *aux OVS_UNUSED)
+{
+    bool ipv6 = false;
+
+    if (argc > 1 && ovs_scan(argv[1], "-6")) {
+        ipv6 = true;
+    }
+
+    if (unixctl_command_get_output_format(conn) == UNIXCTL_OUTPUT_FMT_JSON) {
+        struct json *entries = json_array_create_empty();
+
+        ovs_router_rules_show_json(entries, ipv6);
+        unixctl_command_reply_json(conn, entries);
+    } else {
+        struct ds ds = DS_EMPTY_INITIALIZER;
+
+        ovs_router_rules_show_text(&ds, ipv6);
+        unixctl_command_reply(conn, ds_cstr(&ds));
+        ds_destroy(&ds);
+    }
+}
+
+static void
 ovs_router_lookup_cmd(struct unixctl_conn *conn, int argc,
                       const char *argv[], void *aux OVS_UNUSED)
 {
@@ -1009,6 +1115,8 @@ ovs_router_init(void)
         unixctl_command_register("ovs/route/lookup", "ip_addr "
                                  "[pkt_mark=mark]", 1, 2,
                                  ovs_router_lookup_cmd, NULL);
+        unixctl_command_register("ovs/route/rule/show", "[-6]", 0, 1,
+                                 ovs_router_rules_show, NULL);
         ovsthread_once_done(&once);
     }
 }

ofproto/ofproto-tnl-unixctl.man
@@ -17,6 +17,11 @@ contents of a specific table ID or of all routing tables is printed.
 .IP "\fBovs/route/del\fR \fIip\fR/\fIplen\fR [\fBpkt_mark\fR=\fImark\fR]"
 Delete ip/plen route from OVS routing table.
 .
+.IP "\fBovs/route/rule/show\fR [\fB-6\fR]"
+Print routing rules in OVS. This includes routing rules cached from the system
+routing policy database and user configured routing rules. By default only IPv4
+rules are displayed. For displaying IPv6 rules \fB-6\fR option is required.
+.
 .IP "\fBtnl/neigh/show\fR"
 .IP "\fBtnl/arp/show\fR"
 OVS builds ARP cache by snooping are messages. This command shows

tests/ovs-router.at
@@ -240,3 +240,23 @@ User: 2001:db8:babe::/64 dev br0 GW 2001:db8:cafe::2 SRC 2001:db8:cafe::1
 
 OVS_VSWITCHD_STOP
 AT_CLEANUP
+
+AT_SETUP([appctl - route/rule/show])
+AT_KEYWORDS([ovs_router])
+OVS_VSWITCHD_START([add-port br0 p1 -- set Interface p1 type=dummy])
+AT_CHECK([ovs-appctl netdev-dummy/ip4addr br0 192.0.2.1/24], [0], [OK
+])
+
+dnl Check standard rules exist.
+AT_CHECK([ovs-appctl ovs/route/rule/show], [0], [dnl
+0: from all lookup local
+32766: from all lookup main
+32767: from all lookup default
+])
+AT_CHECK([ovs-appctl ovs/route/rule/show -6], [0], [dnl
+0: from all lookup local
+32766: from all lookup main
+])
+
+OVS_VSWITCHD_STOP
+AT_CLEANUP

commit cff3c614952b9ade0f56f0b9c09728d0b8137ab0

ovs-router: Fix JSON object leak in ovs/router/rule/show command.
There is a memory leak when listing router rules in JSON format when a
rule doesn't match the requested address family.

Coverity report:
    CID 556928:         Resource leaks  (RESOURCE_LEAK)
    Variable "entry" going out of scope leaks the storage it points to.

Fixes: 5c30756a3739 ("ovs-router: Introduce ovs/route/rule/show command.")
Signed-off-by: Dima Chumak <dchumak@nvidia.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

lib/ovs-router.c
@@ -855,11 +855,12 @@ ovs_router_rules_show_json(struct json *rule_entries, bool ipv6)
     struct ds ds;
 
     PVECTOR_FOR_EACH (rule, &rules) {
-        struct json *entry = json_object_create();
+        struct json *entry;
 
         if (rule->ipv4 == ipv6) {
             continue;
         }
+        entry = json_object_create();
 
         json_object_put(entry, "priority", json_integer_create(rule->prio));
         json_object_put(entry, "user", json_integer_create(rule->user));