ovsrobot/ovs

commit 5c9b96ed5abd030d05b0bd24a6d46f6854337aa5

dpif-offload: Allow per-port offload provider priority config.
This patch adds support for configuring the priority of
offload providers at a port level.  When multiple providers
exist and support the same port, the 'hw-offload-priority'
option allows specifying the order in which they are tried.

Acked-by: Eli Britstein <elibr@nvidia.com>
Acked-by: Aaron Conole <aconole@redhat.com>
Signed-off-by: Eelco Chaudron <echaudro@redhat.com>
Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

lib/dpif-offload.c
@@ -23,6 +23,8 @@
 #include "netdev-provider.h"
 #include "unixctl.h"
 #include "util.h"
+#include "vswitch-idl.h"
+
 #include "openvswitch/dynamic-string.h"
 #include "openvswitch/shash.h"
 #include "openvswitch/vlog.h"
@@ -55,6 +57,7 @@ static const struct dpif_offload_class *base_dpif_offload_classes[] = {
 static char *dpif_offload_provider_priority_list = NULL;
 static atomic_bool offload_global_enabled = false;
 static atomic_bool offload_rebalance_policy = false;
+static struct smap port_order_cfg = SMAP_INITIALIZER(&port_order_cfg);
 
 static int
 dpif_offload_register_provider__(const struct dpif_offload_class *class)
@@ -423,11 +426,37 @@ dpif_offload_set_netdev_offload(struct netdev *netdev,
     ovsrcu_set(&netdev->dpif_offload, offload);
 }
 
+static bool
+dpif_offload_try_port_add(struct dpif_offload *offload, struct netdev *netdev,
+                          odp_port_t port_no)
+{
+    if (offload->class->can_offload(offload, netdev)) {
+        int err = offload->class->port_add(offload, netdev, port_no);
+
+        if (!err) {
+            VLOG_DBG("netdev %s added to dpif-offload provider %s",
+                     netdev_get_name(netdev), dpif_offload_name(offload));
+            return true;
+        } else {
+            VLOG_ERR("Failed adding netdev %s to dpif-offload provider "
+                     "%s, error %s",
+                     netdev_get_name(netdev), dpif_offload_name(offload),
+                     ovs_strerror(err));
+        }
+    } else {
+        VLOG_DBG("netdev %s failed can_offload for dpif-offload provider %s",
+                 netdev_get_name(netdev), dpif_offload_name(offload));
+    }
+    return false;
+}
+
 void
 dpif_offload_port_add(struct dpif *dpif, struct netdev *netdev,
                       odp_port_t port_no)
 {
     struct dpif_offload_provider_collection *collection;
+    const char *port_priority = smap_get(&port_order_cfg,
+                                         netdev_get_name(netdev));
     struct dpif_offload *offload;
 
     collection = dpif_get_offload_provider_collection(dpif);
@@ -435,23 +464,40 @@ dpif_offload_port_add(struct dpif *dpif, struct netdev *netdev,
         return;
     }
 
-    LIST_FOR_EACH (offload, dpif_list_node, &collection->list) {
-        if (offload->class->can_offload(offload, netdev)) {
-            int err = offload->class->port_add(offload, netdev, port_no);
-            if (!err) {
-                VLOG_DBG("netdev %s added to dpif-offload provider %s",
-                         netdev_get_name(netdev), dpif_offload_name(offload));
+    if (port_priority) {
+        char *tokens = xstrdup(port_priority);
+        char *saveptr;
+
+        VLOG_DBG("for netdev %s using port priority %s",
+                 netdev_get_name(netdev), port_priority);
+
+        for (char *name = strtok_r(tokens, ",", &saveptr);
+             name;
+             name = strtok_r(NULL, ",", &saveptr)) {
+            bool provider_added = false;
+
+            if (!strcmp("none", name)) {
                 break;
-            } else {
-                VLOG_ERR("Failed adding netdev %s to dpif-offload provider "
-                         "%s, error %s",
-                         netdev_get_name(netdev), dpif_offload_name(offload),
-                         ovs_strerror(err));
             }
-        } else {
-            VLOG_DBG(
-                "netdev %s failed can_offload for dpif-offload provider %s",
-                netdev_get_name(netdev), dpif_offload_name(offload));
+
+            LIST_FOR_EACH (offload, dpif_list_node, &collection->list) {
+                if (!strcmp(name, offload->class->type)) {
+                    provider_added = dpif_offload_try_port_add(offload, netdev,
+                                                               port_no);
+                    break;
+                }
+            }
+
+            if (provider_added) {
+                break;
+            }
+        }
+        free(tokens);
+    } else {
+        LIST_FOR_EACH (offload, dpif_list_node, &collection->list) {
+            if (dpif_offload_try_port_add(offload, netdev, port_no)) {
+                break;
+            }
         }
     }
 }
@@ -574,13 +620,16 @@ dpif_offload_dump_done(struct dpif_offload_dump *dump)
 }
 
 void
-dpif_offload_set_global_cfg(const struct smap *other_cfg)
+dpif_offload_set_global_cfg(const struct ovsrec_open_vswitch *cfg)
 {
     static struct ovsthread_once init_once = OVSTHREAD_ONCE_INITIALIZER;
-    const char *priority = smap_get(other_cfg, "hw-offload-priority");
+    const struct smap *other_cfg = &cfg->other_config;
+    const char *priority;
 
     /* The 'hw-offload-priority' parameter can only be set at startup,
      * any successive change needs a restart. */
+    priority = smap_get(other_cfg, "hw-offload-priority");
+
     if (ovsthread_once_start(&init_once)) {
         /* Initialize the dpif-offload layer in case it's not yet initialized
          * at the first invocation of setting the configuration. */
@@ -628,6 +677,25 @@ dpif_offload_set_global_cfg(const struct smap *other_cfg)
             }
 
             ovsthread_once_done(&once_enable);
+        }
+    }
+
+    /* Filter out the 'hw-offload-priority' per port setting we need it before
+     * ports are added, so we can assign the correct offload-provider.
+     * Note that we can safely rebuild the map here, as we only access this
+     * from the same (main) thread. */
+    smap_clear(&port_order_cfg);
+    for (int i = 0; i < cfg->n_bridges; i++) {
+        const struct ovsrec_bridge *br_cfg = cfg->bridges[i];
+
+        for (int j = 0; j < br_cfg->n_ports; j++) {
+            const struct ovsrec_port *port_cfg = br_cfg->ports[j];
+
+            priority = smap_get(&port_cfg->other_config,
+                                "hw-offload-priority");
+            if (priority) {
+                smap_add(&port_order_cfg, port_cfg->name, priority);
+            }
         }
     }
 }

vswitchd/vswitch.xml
@@ -2600,6 +2600,30 @@
         e.g. <code>fake-bridge-bridge-id</code>.
       </column>
 
+      <column name="other_config" key="hw-offload-priority"
+              type='{"type": "string"}'>
+        <p>
+          This configuration sets an explicit list of hardware offload
+          providers to try on this port.  The argument should be a
+          comma-separated list of hardware offload provider names, or the
+          word <code>none</code>.
+        </p>
+        <p>
+          If <code>none</code> is encountered in the list, further trying of
+          offload providers is stopped. For example, if the list only contains
+          <code>none</code>, hardware offload is disabled on this port even if
+          it is globally enabled. Note that unknown hardware offload provider
+          names are ignored.
+        </p>
+        <p>
+          If not set, uses the global
+          <ref table="Open_vSwitch" column="other_config"
+               key="hw-offload-priority"/>
+          configuration.  Changing this value after offload enablement requires
+          restarting the daemon.
+        </p>
+      </column>
+
       <column name="other_config" key="transient"
               type='{"type": "boolean"}'>
         <p>

lib/dpif-offload.h
@@ -32,7 +32,7 @@ struct dpif_offload_dump {
 
 
 /* Global functions. */
-void dpif_offload_set_global_cfg(const struct smap *other_cfg);
+void dpif_offload_set_global_cfg(const struct ovsrec_open_vswitch *);
 bool dpif_offload_enabled(void);
 bool dpif_offload_rebalance_policy_enabled(void);
 

vswitchd/bridge.c
@@ -3395,8 +3395,11 @@ bridge_run(void)
     }
     cfg = ovsrec_open_vswitch_first(idl);
 
+    if (cfg && ovsdb_idl_get_seqno(idl) != idl_seqno) {
+        dpif_offload_set_global_cfg(cfg);
+    }
+
     if (cfg) {
-        dpif_offload_set_global_cfg(&cfg->other_config);
         netdev_set_flow_api_enabled(&cfg->other_config);
         dpdk_init(&cfg->other_config);
         userspace_tso_init(&cfg->other_config);

commit 40166f063d6a1b7563bc12a11d1d682456234ff0

dpif-offload: Move provider priority to interface level.
The original series allowed the provider priority to be configured
on the port using the other-config:hw-offload-priority option.

However, this is not the correct level. The configuration should be
applied at the interface level, allowing, for example, specific bond
members to have different configurations.

This patch moves the configuration from the port to the interface.

Fixes: 5c9b96ed5abd ("dpif-offload: Allow per-port offload provider priority config.")
Signed-off-by: Eelco Chaudron <echaudro@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

lib/dpif-offload.c
@@ -59,7 +59,7 @@ static const struct dpif_offload_class *base_dpif_offload_classes[] = {
 static char *dpif_offload_provider_priority_list = NULL;
 static atomic_bool offload_global_enabled = false;
 static atomic_bool offload_rebalance_policy = false;
-static struct smap port_order_cfg = SMAP_INITIALIZER(&port_order_cfg);
+static struct smap iface_order_cfg = SMAP_INITIALIZER(&iface_order_cfg);
 
 static int
 dpif_offload_register_provider__(const struct dpif_offload_class *class)
@@ -527,7 +527,7 @@ dpif_offload_port_add(struct dpif *dpif, struct netdev *netdev,
                       odp_port_t port_no)
 {
     struct dpif_offload_provider_collection *collection;
-    const char *port_priority = smap_get(&port_order_cfg,
+    const char *port_priority = smap_get(&iface_order_cfg,
                                          netdev_get_name(netdev));
     struct dpif_offload *offload;
 
@@ -773,21 +773,26 @@ dpif_offload_set_global_cfg(const struct ovsrec_open_vswitch *cfg)
         }
     }
 
-    /* Filter out the 'hw-offload-priority' per port setting we need it before
-     * ports are added, so we can assign the correct offload-provider.
+    /* Filter out the 'hw-offload-priority' per interface setting we need it
+     * before ports are added, so we can assign the correct offload-provider.
      * Note that we can safely rebuild the map here, as we only access this
      * from the same (main) thread. */
-    smap_clear(&port_order_cfg);
+    smap_clear(&iface_order_cfg);
     for (int i = 0; i < cfg->n_bridges; i++) {
         const struct ovsrec_bridge *br_cfg = cfg->bridges[i];
 
         for (int j = 0; j < br_cfg->n_ports; j++) {
             const struct ovsrec_port *port_cfg = br_cfg->ports[j];
 
-            priority = smap_get(&port_cfg->other_config,
-                                "hw-offload-priority");
-            if (priority) {
-                smap_add(&port_order_cfg, port_cfg->name, priority);
+            for (int k = 0; k < port_cfg->n_interfaces; k++) {
+                const struct ovsrec_interface *iface_cfg
+                    = port_cfg->interfaces[k];
+
+                priority = smap_get(&iface_cfg->other_config,
+                                    "hw-offload-priority");
+                if (priority) {
+                    smap_add(&iface_order_cfg, iface_cfg->name, priority);
+                }
             }
         }
     }

vswitchd/vswitch.xml
@@ -2600,30 +2600,6 @@
         e.g. <code>fake-bridge-bridge-id</code>.
       </column>
 
-      <column name="other_config" key="hw-offload-priority"
-              type='{"type": "string"}'>
-        <p>
-          This configuration sets an explicit list of hardware offload
-          providers to try on this port.  The argument should be a
-          comma-separated list of hardware offload provider names, or the
-          word <code>none</code>.
-        </p>
-        <p>
-          If <code>none</code> is encountered in the list, further trying of
-          offload providers is stopped. For example, if the list only contains
-          <code>none</code>, hardware offload is disabled on this port even if
-          it is globally enabled. Note that unknown hardware offload provider
-          names are ignored.
-        </p>
-        <p>
-          If not set, uses the global
-          <ref table="Open_vSwitch" column="other_config"
-               key="hw-offload-priority"/>
-          configuration.  Changing this value after offload enablement requires
-          restarting the daemon.
-        </p>
-      </column>
-
       <column name="other_config" key="transient"
               type='{"type": "boolean"}'>
         <p>
@@ -4677,6 +4653,32 @@ ovs-vsctl add-port br0 p0 -- set Interface p0 type=patch options:peer=p1 \
         </p>
         <p>
           This parameter has an effect only on netdev dpdk interfaces.
+        </p>
+      </column>
+    </group>
+
+    <group title="Hardware Offload Configuration">
+      <column name="other_config" key="hw-offload-priority"
+              type='{"type": "string"}'>
+        <p>
+          This configuration sets an explicit list of hardware offload
+          providers to try on this Interface.  The argument should be a
+          comma-separated list of hardware offload provider names, or the
+          word <code>none</code>.
+        </p>
+        <p>
+          If <code>none</code> is encountered in the list, further trying of
+          offload providers is stopped.  For example, if the list only contains
+          <code>none</code>, hardware offload is disabled on this interface
+          even if it is globally enabled.  Note that unknown hardware offload
+          provider names are ignored.
+        </p>
+        <p>
+          If not set, uses the global
+          <ref table="Open_vSwitch" column="other_config"
+               key="hw-offload-priority"/>
+          configuration.  Changing this value after offload enablement requires
+          restarting the daemon.
         </p>
       </column>
     </group>

tests/ofproto-dpif.at
@@ -10242,16 +10242,16 @@ AT_SETUP([ofproto-dpif - offload - port priority order])
 AT_KEYWORDS([dpif-offload])
 OVS_VSWITCHD_START([add-port br0 p1 -- \
   set Interface p1 type=dummy ofport_request=1 -- \
-  set port p1 other_config:hw-offload-priority=dummy_x,dummy -- \
+  set Interface p1 other_config:hw-offload-priority=dummy_x,dummy -- \
   add-port br0 p2 -- \
   set Interface p2 type=dummy ofport_request=2 -- \
-  set port p2 other_config:hw-offload-priority=none -- \
+  set Interface p2 other_config:hw-offload-priority=none -- \
   add-port br0 p3 -- \
   set Interface p3 type=dummy ofport_request=3 -- \
-  set port p3 other_config:hw-offload-priority=dummy_x -- \
+  set Interface p3 other_config:hw-offload-priority=dummy_x -- \
   add-port br0 p4 -- \
   set Interface p4 type=dummy ofport_request=4 -- \
-  set port p4 other_config:hw-offload-priority=dummy])
+  set Interface p4 other_config:hw-offload-priority=dummy])
 
 AT_CHECK([ovs-appctl --format json --pretty dpif/offload/show], [0], [dnl
 {