ovsrobot/ovs

commit 9a2169a3f7bbe64780a6c9f17532694418a4bd01

bond: Do not flag a revalidation when adjusting time.
When adjusting bond parameters, any adjustment is considered sufficient
for triggering a rebalance.  This is a very simplistic config update
scheme that triggers a complete rebalance even if the time adjustment
would move the next expiration out beyond the last calculated expiration.

For the interval parameter only, we can simply recalculate the expiry
deadline and let the next bond_run() event do the rebalance if needed.
Even if the recalculation would cause the deadline to have occurred in
the past, it should execute on the next bond_run() anyway.  This is
still okay, as the rebalance interval timeout may not result in a
full rebalance anyway.

Reported-at: https://mail.openvswitch.org/pipermail/ovs-discuss/2025-April/053588.html
Acked-by: Ilya Maximets <i.maximets@ovn.org>
Signed-off-by: Aaron Conole <aconole@redhat.com>

ofproto/bond.c
@@ -459,8 +459,23 @@ bond_reconfigure(struct bond *bond, const struct bond_settings *s)
     }
 
     if (bond->rebalance_interval != s->rebalance_interval) {
+        if (s->rebalance_interval && bond->rebalance_interval) {
+            /* Recompute the next rebalance interval by moving the
+             * next_rebalance to be offset by the new interval.  In this
+             * case, if all that was updated is the rebalance interval,
+             * we can skip triggering the rest of the port reconfiguration. */
+            if (bond->next_rebalance) {
+                long long int old_start_time =
+                    bond->next_rebalance - s->rebalance_interval;
+                bond->next_rebalance =
+                    old_start_time + bond->rebalance_interval;
+            }
+        } else {
+            /* When the bond is doing a disable/enable of the rebalance
+             * interval, trigger the revalidation. */
+            revalidate = true;
+        }
         bond->rebalance_interval = s->rebalance_interval;
-        revalidate = true;
     }
 
     if (bond->balance != s->balance) {

commit 816770440a933cd591419152f2a03a864e99971c

bond: Fix next_rebalance offset calculation.
The current way of calculating "next_rebalance" when there is a change
in the bond rebalance period applies the current rebalance interval
(again) and substracts the new one.

This yields an unexpected new value for "next_rebalance": say the
current interval is 60s and we reduce it to 10s (and rebalancing has
just occurred), the current logic will set the next rebalancing in 110s
which is probably not what the user expects.

Swap current and new rebalance intervals in the offset calculation
logic.

Fixes: 9a2169a3f7bb ("bond: Do not flag a revalidation when adjusting time.")
Reported-by: Minxi Hou <mhou@redhat.com>
Signed-off-by: Adrian Moreno <amorenoz@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

ofproto/bond.c
@@ -468,9 +468,9 @@ bond_reconfigure(struct bond *bond, const struct bond_settings *s)
              * we can skip triggering the rest of the port reconfiguration. */
             if (bond->next_rebalance) {
                 long long int old_start_time =
-                    bond->next_rebalance - s->rebalance_interval;
+                    bond->next_rebalance - bond->rebalance_interval;
                 bond->next_rebalance =
-                    old_start_time + bond->rebalance_interval;
+                    old_start_time + s->rebalance_interval;
             }
         } else {
             /* When the bond is doing a disable/enable of the rebalance