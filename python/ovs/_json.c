ovsrobot/ovs

commit 1de4a08c220c67c3cd1a7f7c8e74f9c342b6d1ea

json: Use functions to access json arrays.
Internal implementation of JSON array will be changed in the future
commits.  Add access functions that users can rely on instead of
accessing the internals of 'struct json' directly and convert all the
users.  Structure fields are intentionally renamed to make sure that
no code is using the old fields directly.

json_array() function is removed, as not needed anymore.  Added new
functions:  json_array_size(), json_array_at(), json_array_set()
and json_array_pop().  These are enough to cover all the use cases
within OVS.

The change is fairly large, however, IMO, it's a much overdue cleanup
that we need even without changing the underlying implementation.

Acked-by: Mike Pattrick <mkp@redhat.com>
Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

python/ovs/_json.c
@@ -111,14 +111,14 @@ json_to_python(struct json *json)
             return dict;
         }
     case JSON_ARRAY:{
-            size_t i;
-            PyObject *arr = PyList_New(json->array.n);
+            size_t i, n = json_array_size(json);
+            PyObject *arr = PyList_New(n);
 
             if (arr == NULL) {
                 return PyErr_NoMemory();
             }
-            for (i = 0; i < json->array.n; i++) {
-                PyObject *item = json_to_python(json->array.elems[i]);
+            for (i = 0; i < n; i++) {
+                PyObject *item = json_to_python(json_array_at(json, i));
 
                 if (!item || PyList_SetItem(arr, i, item)) {
                     Py_XDECREF(arr);

include/openvswitch/json.h
@@ -59,8 +59,8 @@ const char *json_type_to_string(enum json_type);
 
 /* A JSON array. */
 struct json_array {
-    size_t n, n_allocated;
-    struct json **elems;
+    size_t size, allocated;
+    struct json **elements;
 };
 
 /* Maximum string length that can be stored inline ('\0' is not included). */
@@ -99,6 +99,8 @@ struct json *json_real_create(double);
 
 struct json *json_array_create_empty(void);
 void json_array_add(struct json *, struct json *element);
+void json_array_set(struct json *, size_t index, struct json *element);
+struct json *json_array_pop(struct json *);
 void json_array_trim(struct json *);
 struct json *json_array_create(struct json **, size_t n);
 struct json *json_array_create_1(struct json *);
@@ -119,7 +121,8 @@ void json_object_put_uuid(struct json *, const char *name,
 
 const char *json_string(const struct json *);
 const char *json_serialized_object(const struct json *);
-struct json_array *json_array(const struct json *);
+size_t json_array_size(const struct json *);
+const struct json *json_array_at(const struct json *, size_t index);
 struct shash *json_object(const struct json *);
 bool json_boolean(const struct json *);
 double json_real(const struct json *);

lib/json.c
@@ -226,30 +226,44 @@ struct json *
 json_array_create_empty(void)
 {
     struct json *json = json_create(JSON_ARRAY);
-    json->array.elems = NULL;
-    json->array.n = 0;
-    json->array.n_allocated = 0;
+    json->array.elements = NULL;
+    json->array.size = 0;
+    json->array.allocated = 0;
     return json;
 }
 
 void
 json_array_add(struct json *array_, struct json *element)
 {
-    struct json_array *array = json_array(array_);
-    if (array->n >= array->n_allocated) {
-        array->elems = x2nrealloc(array->elems, &array->n_allocated,
-                                  sizeof *array->elems);
+    struct json_array *array = &array_->array;
+    if (array->size >= array->allocated) {
+        array->elements = x2nrealloc(array->elements, &array->allocated,
+                                     sizeof *array->elements);
     }
-    array->elems[array->n++] = element;
+    array->elements[array->size++] = element;
+}
+
+void
+json_array_set(struct json *json, size_t index, struct json *element)
+{
+    ovs_assert(json_array_size(json) > index);
+    json->array.elements[index] = element;
+}
+
+struct json *
+json_array_pop(struct json *json)
+{
+    return json->array.size ? json->array.elements[--json->array.size] : NULL;
 }
 
 void
 json_array_trim(struct json *array_)
 {
-    struct json_array *array = json_array(array_);
-    if (array->n < array->n_allocated){
-        array->n_allocated = array->n;
-        array->elems = xrealloc(array->elems, array->n * sizeof *array->elems);
+    struct json_array *array = &array_->array;
+    if (array->size < array->allocated) {
+        array->allocated = array->size;
+        array->elements = xrealloc(array->elements,
+                                   array->size * sizeof *array->elements);
     }
 }
 
@@ -257,46 +271,46 @@ struct json *
 json_array_create(struct json **elements, size_t n)
 {
     struct json *json = json_create(JSON_ARRAY);
-    json->array.elems = elements;
-    json->array.n = n;
-    json->array.n_allocated = n;
+    json->array.elements = elements;
+    json->array.size = n;
+    json->array.allocated = n;
     return json;
 }
 
 struct json *
 json_array_create_1(struct json *elem0)
 {
-    struct json **elems = xmalloc(sizeof *elems);
-    elems[0] = elem0;
-    return json_array_create(elems, 1);
+    struct json **elements = xmalloc(sizeof *elements);
+    elements[0] = elem0;
+    return json_array_create(elements, 1);
 }
 
 struct json *
 json_array_create_2(struct json *elem0, struct json *elem1)
 {
-    struct json **elems = xmalloc(2 * sizeof *elems);
-    elems[0] = elem0;
-    elems[1] = elem1;
-    return json_array_create(elems, 2);
+    struct json **elements = xmalloc(2 * sizeof *elements);
+    elements[0] = elem0;
+    elements[1] = elem1;
+    return json_array_create(elements, 2);
 }
 
 struct json *
 json_array_create_3(struct json *elem0, struct json *elem1, struct json *elem2)
 {
-    struct json **elems = xmalloc(3 * sizeof *elems);
-    elems[0] = elem0;
-    elems[1] = elem1;
-    elems[2] = elem2;
-    return json_array_create(elems, 3);
+    struct json **elements = xmalloc(3 * sizeof *elements);
+    elements[0] = elem0;
+    elements[1] = elem1;
+    elements[2] = elem2;
+    return json_array_create(elements, 3);
 }
 
 bool
 json_array_contains_string(const struct json *json, const char *str)
 {
-    ovs_assert(json->type == JSON_ARRAY);
+    size_t n = json_array_size(json);
 
-    for (size_t i = 0; i < json->array.n; i++) {
-        const struct json *elem = json->array.elems[i];
+    for (size_t i = 0; i < n; i++) {
+        const struct json *elem = json_array_at(json, i);
 
         if (elem->type == JSON_STRING && !strcmp(json_string(elem), str)) {
             return true;
@@ -380,11 +394,18 @@ json_serialized_object(const struct json *json)
     return json->str_ptr;
 }
 
-struct json_array *
-json_array(const struct json *json)
+size_t
+json_array_size(const struct json *json)
 {
     ovs_assert(json->type == JSON_ARRAY);
-    return CONST_CAST(struct json_array *, &json->array);
+    return json->array.size;
+}
+
+const struct json *
+json_array_at(const struct json *json, size_t index)
+{
+    ovs_assert(json->type == JSON_ARRAY);
+    return (json->array.size > index) ? json->array.elements[index] : NULL;
 }
 
 struct shash *
@@ -416,7 +437,7 @@ json_integer(const struct json *json)
 }
 
 static void json_destroy_object(struct shash *object, bool yield);
-static void json_destroy_array(struct json_array *array, bool yield);
+static void json_destroy_array(struct json *json, bool yield);
 
 /* Frees 'json' and everything it points to, recursively. */
 void
@@ -428,7 +449,7 @@ json_destroy__(struct json *json, bool yield)
         break;
 
     case JSON_ARRAY:
-        json_destroy_array(&json->array, yield);
+        json_destroy_array(json, yield);
         break;
 
     case JSON_STRING:
@@ -478,26 +499,27 @@ json_destroy_object(struct shash *object, bool yield)
 }
 
 static void
-json_destroy_array(struct json_array *array, bool yield)
+json_destroy_array(struct json *json, bool yield)
 {
-    size_t i;
+    size_t i, n = json_array_size(json);
 
     if (yield) {
         cooperative_multitasking_yield();
     }
 
-    for (i = 0; i < array->n; i++) {
+    for (i = 0; i < n; i++) {
         if (yield) {
-            json_destroy_with_yield(array->elems[i]);
+            json_destroy_with_yield(
+                CONST_CAST(struct json *, json_array_at(json, i)));
         } else {
-            json_destroy(array->elems[i]);
+            json_destroy(CONST_CAST(struct json *, json_array_at(json, i)));
         }
     }
-    free(array->elems);
+    free(json->array.elements);
 }
 
 static struct json *json_deep_clone_object(const struct shash *object);
-static struct json *json_deep_clone_array(const struct json_array *array);
+static struct json *json_deep_clone_array(const struct json *);
 
 /* Returns a deep copy of 'json'. */
 struct json *
@@ -508,7 +530,7 @@ json_deep_clone(const struct json *json)
         return json_deep_clone_object(json->object);
 
     case JSON_ARRAY:
-        return json_deep_clone_array(&json->array);
+        return json_deep_clone_array(json);
 
     case JSON_STRING:
         return json_string_create(json_string(json));
@@ -554,16 +576,33 @@ json_deep_clone_object(const struct shash *object)
 }
 
 static struct json *
-json_deep_clone_array(const struct json_array *array)
+json_deep_clone_array(const struct json *json)
 {
-    struct json **elems;
-    size_t i;
+    struct json **elements;
+    size_t i, n;
 
-    elems = xmalloc(array->n * sizeof *elems);
-    for (i = 0; i < array->n; i++) {
-        elems[i] = json_deep_clone(array->elems[i]);
+    n = json_array_size(json);
+    switch (n) {
+    case 0:
+        return json_array_create_empty();
+    case 1:
+        return json_array_create_1(json_deep_clone(json_array_at(json, 0)));
+    case 2:
+        return json_array_create_2(json_deep_clone(json_array_at(json, 0)),
+                                   json_deep_clone(json_array_at(json, 1)));
+    case 3:
+        return json_array_create_3(json_deep_clone(json_array_at(json, 0)),
+                                   json_deep_clone(json_array_at(json, 1)),
+                                   json_deep_clone(json_array_at(json, 2)));
+    default:
+        break;
     }
-    return json_array_create(elems, array->n);
+
+    elements = xmalloc(n * sizeof *elements);
+    for (i = 0; i < n; i++) {
+        elements[i] = json_deep_clone(json_array_at(json, i));
+    }
+    return json_array_create(elements, n);
 }
 
 static size_t
@@ -584,13 +623,13 @@ json_hash_object(const struct shash *object, size_t basis)
 }
 
 static size_t
-json_hash_array(const struct json_array *array, size_t basis)
+json_hash_array(const struct json *json, size_t basis)
 {
-    size_t i;
+    size_t i, n = json_array_size(json);
 
-    basis = hash_int(array->n, basis);
-    for (i = 0; i < array->n; i++) {
-        basis = json_hash(array->elems[i], basis);
+    basis = hash_int(n, basis);
+    for (i = 0; i < n; i++) {
+        basis = json_hash(json_array_at(json, i), basis);
     }
     return basis;
 }
@@ -603,7 +642,7 @@ json_hash(const struct json *json, size_t basis)
         return json_hash_object(json->object, basis);
 
     case JSON_ARRAY:
-        return json_hash_array(&json->array, basis);
+        return json_hash_array(json, basis);
 
     case JSON_STRING:
         return hash_string(json_string(json), basis);
@@ -648,16 +687,16 @@ json_equal_object(const struct shash *a, const struct shash *b)
 }
 
 static bool
-json_equal_array(const struct json_array *a, const struct json_array *b)
+json_equal_array(const struct json *a, const struct json *b)
 {
-    size_t i;
+    size_t i, n = json_array_size(a);
 
-    if (a->n != b->n) {
+    if (n != json_array_size(b)) {
         return false;
     }
 
-    for (i = 0; i < a->n; i++) {
-        if (!json_equal(a->elems[i], b->elems[i])) {
+    for (i = 0; i < n; i++) {
+        if (!json_equal(json_array_at(a, i), json_array_at(b, i))) {
             return false;
         }
     }
@@ -681,7 +720,7 @@ json_equal(const struct json *a, const struct json *b)
         return json_equal_object(a->object, b->object);
 
     case JSON_ARRAY:
-        return json_equal_array(&a->array, &b->array);
+        return json_equal_array(a, b);
 
     case JSON_STRING:
         return !strcmp(json_string(a), json_string(b));
@@ -1603,7 +1642,7 @@ struct json_serializer {
 static void json_serialize(const struct json *, struct json_serializer *);
 static void json_serialize_object(const struct shash *object,
                                   struct json_serializer *);
-static void json_serialize_array(const struct json_array *,
+static void json_serialize_array(const struct json *,
                                  struct json_serializer *);
 static void json_serialize_string(const char *, struct ds *);
 
@@ -1666,7 +1705,7 @@ json_serialize(const struct json *json, struct json_serializer *s)
         break;
 
     case JSON_ARRAY:
-        json_serialize_array(&json->array, s);
+        json_serialize_array(json, s);
         break;
 
     case JSON_INTEGER:
@@ -1758,10 +1797,10 @@ json_serialize_object(const struct shash *object, struct json_serializer *s)
 }
 
 static void
-json_serialize_array(const struct json_array *array, struct json_serializer *s)
+json_serialize_array(const struct json *json, struct json_serializer *s)
 {
+    size_t i, n = json_array_size(json);
     struct ds *ds = s->ds;
-    size_t i;
 
     ds_put_char(ds, '[');
     s->depth++;
@@ -1770,15 +1809,15 @@ json_serialize_array(const struct json_array *array, struct json_serializer *s)
         cooperative_multitasking_yield();
     }
 
-    if (array->n > 0) {
+    if (n > 0) {
         indent_line(s);
 
-        for (i = 0; i < array->n; i++) {
+        for (i = 0; i < n; i++) {
             if (i) {
                 ds_put_char(ds, ',');
                 indent_line(s);
             }
-            json_serialize(array->elems[i], s);
+            json_serialize(json_array_at(json, i), s);
         }
     }
 

lib/ovsdb-cs.c
@@ -843,15 +843,17 @@ static enum condition_type
 condition_classify(const struct json *condition)
 {
     if (condition) {
-        const struct json_array *a = json_array(condition);
-        switch (a->n) {
+        switch (json_array_size(condition)) {
         case 0:
             return COND_FALSE;
 
-        case 1:
-            return (a->elems[0]->type == JSON_FALSE ? COND_FALSE
-                    : a->elems[0]->type == JSON_TRUE ? COND_TRUE
+        case 1: {
+            const struct json *cond = json_array_at(condition, 0);
+
+            return (cond->type == JSON_FALSE ? COND_FALSE
+                    : cond->type == JSON_TRUE ? COND_TRUE
                     : COND_OTHER);
+        }
 
         default:
             return COND_OTHER;
@@ -1387,9 +1389,9 @@ ovsdb_cs_db_parse_lock_notify(struct ovsdb_cs_db *db,
 {
     if (db->lock_name
         && params->type == JSON_ARRAY
-        && json_array(params)->n > 0
-        && json_array(params)->elems[0]->type == JSON_STRING) {
-        const char *lock_name = json_string(json_array(params)->elems[0]);
+        && json_array_size(params) > 0
+        && json_array_at(params, 0)->type == JSON_STRING) {
+        const char *lock_name = json_string(json_array_at(params, 0));
 
         if (!strcmp(db->lock_name, lock_name)) {
             ovsdb_cs_db_update_has_lock(db, new_has_lock);
@@ -1523,12 +1525,12 @@ ovsdb_cs_send_monitor_request(struct ovsdb_cs *cs, struct ovsdb_cs_db *db,
                 ovs_assert(mr->type == JSON_ARRAY);
 
                 struct json *mr0;
-                if (json_array(mr)->n == 0) {
+                if (json_array_size(mr) == 0) {
                     mr0 = json_object_create();
                     json_object_put(mr0, "columns", json_array_create_empty());
                     json_array_add(mr, mr0);
                 } else {
-                    mr0 = json_array(mr)->elems[0];
+                    mr0 = CONST_CAST(struct json *, json_array_at(mr, 0));
                 }
                 ovs_assert(mr0->type == JSON_OBJECT);
 
@@ -1585,21 +1587,21 @@ ovsdb_cs_db_parse_monitor_reply(struct ovsdb_cs_db *db,
     const struct json *table_updates;
     bool clear;
     if (version == 3) {
-        if (result->type != JSON_ARRAY || result->array.n != 3
-            || (result->array.elems[0]->type != JSON_TRUE &&
-                result->array.elems[0]->type != JSON_FALSE)
-            || result->array.elems[1]->type != JSON_STRING
+        if (result->type != JSON_ARRAY || json_array_size(result) != 3
+            || (json_array_at(result, 0)->type != JSON_TRUE &&
+                json_array_at(result, 0)->type != JSON_FALSE)
+            || json_array_at(result, 1)->type != JSON_STRING
             || !uuid_from_string(&db->last_id,
-                                 json_string(result->array.elems[1]))) {
+                                 json_string(json_array_at(result, 1)))) {
             struct ovsdb_error *error = ovsdb_syntax_error(
                 result, NULL, "bad monitor_cond_since reply format");
             log_parse_update_error(error);
             return;
         }
 
-        bool found = json_boolean(result->array.elems[0]);
+        bool found = json_boolean(json_array_at(result, 0));
         clear = !found;
-        table_updates = result->array.elems[2];
+        table_updates = json_array_at(result, 2);
     } else {
         clear = true;
         table_updates = result;
@@ -1626,7 +1628,7 @@ ovsdb_cs_db_parse_update_rpc(struct ovsdb_cs_db *db,
 
     struct json *params = msg->params;
     int n = version == 3 ? 3 : 2;
-    if (params->type != JSON_ARRAY || params->array.n != n) {
+    if (params->type != JSON_ARRAY || json_array_size(params) != n) {
         struct ovsdb_error *error = ovsdb_syntax_error(
             params, NULL, "%s must be an array with %u elements.",
             msg->method, n);
@@ -1634,12 +1636,12 @@ ovsdb_cs_db_parse_update_rpc(struct ovsdb_cs_db *db,
         return false;
     }
 
-    if (!json_equal(params->array.elems[0], db->monitor_id)) {
+    if (!json_equal(json_array_at(params, 0), db->monitor_id)) {
         return false;
     }
 
     if (version == 3) {
-        const char *last_id = json_string(params->array.elems[1]);
+        const char *last_id = json_string(json_array_at(params, 1));
         if (!uuid_from_string(&db->last_id, last_id)) {
             struct ovsdb_error *error = ovsdb_syntax_error(
                 params, NULL, "Last-id %s is not in UUID format.", last_id);
@@ -1648,7 +1650,8 @@ ovsdb_cs_db_parse_update_rpc(struct ovsdb_cs_db *db,
         }
     }
 
-    struct json *table_updates = params->array.elems[version == 3 ? 2 : 1];
+    const struct json *table_updates = json_array_at(params,
+                                                     version == 3 ? 2 : 1);
     ovsdb_cs_db_add_update(db, table_updates, version, false, false);
     return true;
 }
@@ -1661,8 +1664,8 @@ ovsdb_cs_handle_monitor_canceled(struct ovsdb_cs *cs,
     if (msg->type != JSONRPC_NOTIFY
         || strcmp(msg->method, "monitor_canceled")
         || msg->params->type != JSON_ARRAY
-        || msg->params->array.n != 1
-        || !json_equal(msg->params->array.elems[0], db->monitor_id)) {
+        || json_array_size(msg->params) != 1
+        || !json_equal(json_array_at(msg->params, 0), db->monitor_id)) {
         return false;
     }
 

lib/ovsdb-data.c
@@ -262,16 +262,16 @@ unwrap_json(const struct json *json, const char *name,
             enum json_type value_type, const struct json **value)
 {
     if (json->type != JSON_ARRAY
-        || json->array.n != 2
-        || json->array.elems[0]->type != JSON_STRING
-        || (name && strcmp(json_string(json->array.elems[0]), name))
-        || json->array.elems[1]->type != value_type)
+        || json_array_size(json) != 2
+        || json_array_at(json, 0)->type != JSON_STRING
+        || (name && strcmp(json_string(json_array_at(json, 0)), name))
+        || json_array_at(json, 1)->type != value_type)
     {
         *value = NULL;
         return ovsdb_syntax_error(json, NULL, "expected [\"%s\", <%s>]", name,
                                   json_type_to_string(value_type));
     }
-    *value = json->array.elems[1];
+    *value = json_array_at(json, 1);
     return NULL;
 }
 
@@ -279,11 +279,11 @@ static struct ovsdb_error *
 parse_json_pair(const struct json *json,
                 const struct json **elem0, const struct json **elem1)
 {
-    if (json->type != JSON_ARRAY || json->array.n != 2) {
+    if (json->type != JSON_ARRAY || json_array_size(json) != 2) {
         return ovsdb_syntax_error(json, NULL, "expected 2-element array");
     }
-    *elem0 = json->array.elems[0];
-    *elem1 = json->array.elems[1];
+    *elem0 = json_array_at(json, 0);
+    *elem1 = json_array_at(json, 1);
     return NULL;
 }
 
@@ -1276,9 +1276,9 @@ ovsdb_datum_from_json__(struct ovsdb_datum *datum,
 
     if (ovsdb_type_is_map(type)
         || (json->type == JSON_ARRAY
-            && json->array.n > 0
-            && json->array.elems[0]->type == JSON_STRING
-            && !strcmp(json_string(json->array.elems[0]), "set"))) {
+            && json_array_size(json) > 0
+            && json_array_at(json, 0)->type == JSON_STRING
+            && !strcmp(json_string(json_array_at(json, 0)), "set"))) {
         bool is_map = ovsdb_type_is_map(type);
         const char *class = is_map ? "map" : "set";
         const struct json *inner;
@@ -1290,7 +1290,7 @@ ovsdb_datum_from_json__(struct ovsdb_datum *datum,
             return error;
         }
 
-        n = inner->array.n;
+        n = json_array_size(inner);
         if (n < type->n_min || n > type->n_max) {
             if (type->n_min == 1 && type->n_max == 1) {
                 return ovsdb_syntax_error(json, NULL, "%s must have exactly "
@@ -1309,7 +1309,7 @@ ovsdb_datum_from_json__(struct ovsdb_datum *datum,
         datum->values = is_map ? xmalloc(n * sizeof *datum->values) : NULL;
         datum->refcnt = NULL;
         for (i = 0; i < n; i++) {
-            const struct json *element = inner->array.elems[i];
+            const struct json *element = json_array_at(inner, i);
             const struct json *key = NULL;
             const struct json *value = NULL;
 

commit 0da42ec653da8f373a2992cfd8c24e2284f66e71

python: Fix discarded const qualifier in json_to_python.
There is a warning while building JSON extension code:

  ovs/_json.c: In function 'json_to_python':
  ovs/_json.c:121:49:
    warning: passing argument 1 of 'json_to_python' discards 'const'
             qualifier from pointer target type [-Wdiscarded-qualifiers]
  121 |                 PyObject *item = json_to_python(json_array_at(json, i));
      |                                                 ^~~~~~~~~~~~~~~~~~~~~~
  ovs/_json.c:81:29: note: expected 'struct json *' but argument is
                           of type 'const struct json *'
   81 | json_to_python(struct json *json)
      |                ~~~~~~~~~~~~~^~~~

The warning can be seen in the CI job for the debian package build.

Fixes: 1de4a08c220c ("json: Use functions to access json arrays.")
Signed-off-by: Ilya Maximets <i.maximets@ovn.org>
Signed-off-by: 0-day Robot <robot@bytheb.org>

python/ovs/_json.c
@@ -78,7 +78,7 @@ Parser_is_done(json_ParserObject * self)
 }
 
 static PyObject *
-json_to_python(struct json *json)
+json_to_python(const struct json *json)
 {
     switch (json->type) {
     case JSON_NULL: