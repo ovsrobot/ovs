ovsrobot/ovs

commit aacad8685e3d873d15d7fc7974763655862477de

checkpatch: Fix regexp for if, while, etc inside macros.
This allows to use a one-character expression inside the 'if'
statement and multiple spaces before the line continuation character.

Fixes false positive in case like this:

  #define MACRO(ARG)     \
      if (a) {           \
          do_work(ARG);  \
      }

Fixes: 16770c6d9179 ("checkpatch: support macro continuation")
Signed-off-by: Ilya Maximets <i.maximets@samsung.com>
Acked-by: Aaron Conole <aconole@redhat.com>

utilities/checkpatch.py
@@ -172,7 +172,7 @@ __regex_has_xxx_mark = re.compile(r'.*xxx.*', re.IGNORECASE)
 __regex_added_doc_rst = re.compile(
                     r'\ndiff .*Documentation/.*rst\nnew file mode')
 __regex_empty_return = re.compile(r'\s*return;')
-__regex_if_macros = re.compile(r'^ +(%s) \([\S][\s\S]+[\S]\) { \\' %
+__regex_if_macros = re.compile(r'^ +(%s) \([\S]([\s\S]+[\S])*\) { +\\' %
                                __parenthesized_constructs)
 
 skip_leading_whitespace_check = False

tests/checkpatch.at
@@ -230,6 +230,11 @@ try_checkpatch \
    "COMMON_PATCH_HEADER
     +     $ctr (first_run) { \\
     "
+
+try_checkpatch \
+   "COMMON_PATCH_HEADER
+    +     $ctr (a) {    \\
+    "
 done
 AT_CLEANUP
 

commit cb6c02c1c8aec0e3ad4fef130e1045ae0aa1be5b

checkpatch.py: Fix a catastrophic backtracking issue.
The regex_if_macros accidentally implements a catastrophic backtracking
case during matching because it forces a greedy match and evaluation that
causes an explosion of combinations depending on how many blocks appear
in a parenthesized expression.

Consider an expression like:

  if (a b c d e f g h i j k) {

The regex engine will have to create an exponential number of matches that
can overload memory and cpu requirements.

Fix this by switching the inner-content match to use a non-greedy approach.

Fixes: aacad8685e3d ("checkpatch: Fix regexp for if, while, etc inside macros.")
Signed-off-by: Aaron Conole <aconole@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

utilities/checkpatch.py
@@ -158,7 +158,7 @@ __regex_has_xxx_mark = re.compile(r'.*xxx.*', re.IGNORECASE)
 __regex_added_doc_rst = re.compile(
                     r'\ndiff .*Documentation/.*rst\nnew file mode')
 __regex_empty_return = re.compile(r'\s*return;')
-__regex_if_macros = re.compile(r'^ +(%s) \([\S]([\s\S]+[\S])*\) { +\\' %
+__regex_if_macros = re.compile(r'^ +(%s) \([\S](?:[\s\S]*?[\S])?\) { +\\' %
                                __parenthesized_constructs)
 __regex_nonascii_characters = re.compile("[^\u0000-\u007f]")
 __regex_efgrep = re.compile(r'.*[ef]grep.*$')