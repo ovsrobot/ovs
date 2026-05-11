ovsrobot/ovs

commit e672ff9b4d224f4d7ef12b289811b0759a333e48

ofproto-dpif: Restore metadata and registers on recirculation.
xlate_actions() now considers an optional recirculation context (via
'xin') and restores OpenFlow pipeline metadata (registers, 'metadata',
etc.) based on it.  The recirculation context may contain an action
set and stack to be restored and further actions to be executed upon
recirculation.  It also contains a table_id number to be used for rule
lookup in cases where no post-recirculation actions are used.

The translation context internal metadata is restored using a new
internal action: UNROLL_XLATE action stores the translation context
data visible to OpenFlow controllers via PACKET_IN messages.  This
includes the current table number and the current rule cookie.
UNROLL_XLATE actions are inserted only when the remaining actions may
generate PACKET_IN messages.

These changes allow the post-MPLS recirculation to properly continue
with the pipeline metadata that existed at the time of recirculation.

The internal table is still consulted for bonds.

Signed-off-by: Jarno Rajahalme <jrajahalme@nicira.com>
Acked-by: Ethan Jackson <ethan@nicira.com>
Acked-by: Ben Pfaff <blp@nicira.com>

ofproto/ofproto-dpif-rid.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 Nicira, Inc.
+ * Copyright (c) 2014, 2015 Nicira, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,59 +16,351 @@
 
 #include <config.h>
 
-#include "id-pool.h"
-#include "ovs-thread.h"
+#include "ofpbuf.h"
+#include "ofproto-dpif.h"
 #include "ofproto-dpif-rid.h"
+#include "ofproto-provider.h"
+#include "openvswitch/vlog.h"
 
-struct recirc_id_pool {
-    struct ovs_mutex lock;
-    struct id_pool *rids;
-};
+VLOG_DEFINE_THIS_MODULE(ofproto_dpif_rid);
 
-#define RECIRC_ID_BASE  300
-#define RECIRC_ID_N_IDS  1024
+static struct ovs_mutex mutex;
 
-struct recirc_id_pool *
-recirc_id_pool_create(void)
-{
-    struct recirc_id_pool *pool;
+static struct cmap id_map;
+static struct cmap metadata_map;
 
-    pool = xmalloc(sizeof *pool);
-    pool->rids = id_pool_create(RECIRC_ID_BASE, RECIRC_ID_N_IDS);
-    ovs_mutex_init(&pool->lock);
+static struct ovs_list expiring OVS_GUARDED_BY(mutex);
+static struct ovs_list expired OVS_GUARDED_BY(mutex);
 
-    return pool;
-}
+static uint32_t next_id OVS_GUARDED_BY(mutex); /* Possible next free id. */
+
+#define RECIRC_POOL_STATIC_IDS 1024
 
 void
-recirc_id_pool_destroy(struct recirc_id_pool *pool)
+recirc_init(void)
 {
-    id_pool_destroy(pool->rids);
-    ovs_mutex_destroy(&pool->lock);
-    free(pool);
-}
+    static struct ovsthread_once once = OVSTHREAD_ONCE_INITIALIZER;
 
-uint32_t
-recirc_id_alloc(struct recirc_id_pool *pool)
-{
-    uint32_t id;
-    bool ret;
+    if (ovsthread_once_start(&once)) {
+        ovs_mutex_init(&mutex);
+        ovs_mutex_lock(&mutex);
+        next_id = 1; /* 0 is not a valid ID. */
+        cmap_init(&id_map);
+        cmap_init(&metadata_map);
+        list_init(&expiring);
+        list_init(&expired);
+        ovs_mutex_unlock(&mutex);
 
-    ovs_mutex_lock(&pool->lock);
-    ret = id_pool_alloc_id(pool->rids, &id);
-    ovs_mutex_unlock(&pool->lock);
-
-    if (!ret) {
-        return 0;
+        ovsthread_once_done(&once);
     }
 
-    return id;
+}
+
+/* This should be called by the revalidator once at each round (every 500ms or
+ * more). */
+void
+recirc_run(void)
+{
+    static long long int last = 0;
+    long long int now = time_msec();
+
+    /* Do maintenance at most 4 times / sec. */
+    ovs_mutex_lock(&mutex);
+    if (now - last > 250) {
+        struct recirc_id_node *node, *next;
+
+        last = now;
+
+        /* Nodes in 'expiring' and 'expired' lists have the refcount of zero,
+         * which means that while they can still be found (by id), no new
+         * references can be taken on them.  We have removed the entry from the
+         * 'metadata_map', at the time when refcount reached zero, causing any
+         * new translations to allocate a new ID.  This allows the expiring
+         * entry to be safely deleted while any sudden new use of the similar
+         * recirculation will safely start using a new recirculation ID.  When
+         * the refcount gets to zero, the node is also added to the 'expiring'
+         * list.  At any time after that the nodes in the 'expiring' list can
+         * be moved to the 'expired' list, from which they are deleted at least
+         * 250ms afterwards. */
+
+        /* Delete the expired.  These have been lingering for at least 250 ms,
+         * which should be enough for any ongoing recirculations to be
+         * finished. */
+        LIST_FOR_EACH_SAFE (node, next, exp_node, &expired) {
+            list_remove(&node->exp_node);
+            cmap_remove(&id_map, &node->id_node, node->id);
+            ovsrcu_postpone(free, node);
+        }
+
+        if (!list_is_empty(&expiring)) {
+            /* 'expired' is now empty, move nodes in 'expiring' to it. */
+            list_splice(&expired, list_front(&expiring), &expiring);
+        }
+    }
+    ovs_mutex_unlock(&mutex);
+}
+
+/* We use the id as the hash value, which works due to cmap internal rehashing.
+ * We also only insert nodes with unique IDs, so all possible hash collisions
+ * remain internal to the cmap. */
+static struct recirc_id_node *
+recirc_find__(uint32_t id)
+    OVS_REQUIRES(mutex)
+{
+    struct cmap_node *node = cmap_find_protected(&id_map, id);
+
+    return node ? CONTAINER_OF(node, struct recirc_id_node, id_node) : NULL;
+}
+
+/* Lockless RCU protected lookup.  If node is needed accross RCU quiescent
+ * state, caller should copy the contents. */
+const struct recirc_id_node *
+recirc_id_node_find(uint32_t id)
+{
+    const struct cmap_node *node = cmap_find(&id_map, id);
+
+    return node
+        ? CONTAINER_OF(node, const struct recirc_id_node, id_node)
+        : NULL;
+}
+
+static uint32_t
+recirc_metadata_hash(struct ofproto_dpif *ofproto, uint8_t table_id,
+                     struct recirc_metadata *md, struct ofpbuf *stack,
+                     uint32_t action_set_len, uint32_t ofpacts_len,
+                     const struct ofpact *ofpacts)
+{
+    uint32_t hash;
+
+    BUILD_ASSERT(OFPACT_ALIGNTO == sizeof(uint64_t));
+
+    hash = hash_pointer(ofproto, 0);
+    hash = hash_int(table_id, hash);
+    hash = hash_words64((const uint64_t *)md, sizeof *md / sizeof(uint64_t),
+                        hash);
+    if (stack && stack->size != 0) {
+        hash = hash_words64((const uint64_t *)stack->data,
+                            stack->size / sizeof(uint64_t), hash);
+    }
+    hash = hash_int(action_set_len, hash);
+    if (ofpacts_len) {
+        hash = hash_words64(ALIGNED_CAST(const uint64_t *, ofpacts),
+                            OFPACT_ALIGN(ofpacts_len) / sizeof(uint64_t),
+                            hash);
+    }
+    return hash;
+}
+
+static bool
+recirc_metadata_equal(const struct recirc_id_node *node,
+                      struct ofproto_dpif *ofproto, uint8_t table_id,
+                      struct recirc_metadata *md, struct ofpbuf *stack,
+                      uint32_t action_set_len, uint32_t ofpacts_len,
+                      const struct ofpact *ofpacts)
+{
+    return node->ofproto == ofproto
+        && node->table_id == table_id
+        && !memcmp(&node->metadata, md, sizeof *md)
+        && ((!node->stack && (!stack || stack->size == 0))
+            || (node->stack && stack && ofpbuf_equal(node->stack, stack)))
+        && node->action_set_len == action_set_len
+        && node->ofpacts_len == ofpacts_len
+        && (ofpacts_len == 0 || !memcmp(node->ofpacts, ofpacts, ofpacts_len));
+}
+
+/* Lockless RCU protected lookup.  If node is needed accross RCU quiescent
+ * state, caller should take a reference. */
+static struct recirc_id_node *
+recirc_find_equal(struct ofproto_dpif *ofproto, uint8_t table_id,
+                  struct recirc_metadata *md, struct ofpbuf *stack,
+                  uint32_t action_set_len, uint32_t ofpacts_len,
+                  const struct ofpact *ofpacts, uint32_t hash)
+{
+    struct recirc_id_node *node;
+
+    CMAP_FOR_EACH_WITH_HASH(node, metadata_node, hash, &metadata_map) {
+        if (recirc_metadata_equal(node, ofproto, table_id, md, stack,
+                                  action_set_len, ofpacts_len, ofpacts)) {
+            return node;
+        }
+    }
+    return NULL;
+}
+
+static struct recirc_id_node *
+recirc_ref_equal(struct ofproto_dpif *ofproto, uint8_t table_id,
+                 struct recirc_metadata *md, struct ofpbuf *stack,
+                 uint32_t action_set_len, uint32_t ofpacts_len,
+                 const struct ofpact *ofpacts, uint32_t hash)
+{
+    struct recirc_id_node *node;
+
+    do {
+        node = recirc_find_equal(ofproto, table_id, md, stack, action_set_len,
+                                 ofpacts_len, ofpacts, hash);
+
+        /* Try again if the node was released before we get the reference. */
+    } while (node && !ovs_refcount_try_ref_rcu(&node->refcount));
+
+    return node;
+}
+
+/* Allocate a unique recirculation id for the given set of flow metadata.
+ * The ID space is 2^^32, so there should never be a situation in which all
+ * the IDs are used up.  We loop until we find a free one.
+ * hash is recomputed if it is passed in as 0. */
+static struct recirc_id_node *
+recirc_alloc_id__(struct ofproto_dpif *ofproto, uint8_t table_id,
+                  struct recirc_metadata *md, struct ofpbuf *stack,
+                  uint32_t action_set_len, uint32_t ofpacts_len,
+                  const struct ofpact *ofpacts, uint32_t hash)
+{
+    struct recirc_id_node *node = xzalloc(sizeof *node +
+                                          OFPACT_ALIGN(ofpacts_len));
+    node->hash = hash;
+    ovs_refcount_init(&node->refcount);
+
+    node->ofproto = ofproto;
+    node->table_id = table_id;
+    memcpy(&node->metadata, md, sizeof node->metadata);
+    node->stack = (stack && stack->size) ? ofpbuf_clone(stack) : NULL;
+    node->action_set_len = action_set_len;
+    node->ofpacts_len = ofpacts_len;
+    if (ofpacts_len) {
+        memcpy(node->ofpacts, ofpacts, ofpacts_len);
+    }
+
+    ovs_mutex_lock(&mutex);
+    for (;;) {
+        /* Claim the next ID.  The ID space should be sparse enough for the
+           allocation to succeed at the first try.  We do skip the first
+           RECIRC_POOL_STATIC_IDS IDs on the later rounds, though, as some of
+           the initial allocations may be for long term uses (like bonds). */
+        node->id = next_id++;
+        if (OVS_UNLIKELY(!node->id)) {
+            next_id = RECIRC_POOL_STATIC_IDS + 1;
+            node->id = next_id++;
+        }
+        /* Find if the id is free. */
+        if (OVS_LIKELY(!recirc_find__(node->id))) {
+            break;
+        }
+    }
+    cmap_insert(&id_map, &node->id_node, node->id);
+    cmap_insert(&metadata_map, &node->metadata_node, node->hash);
+    ovs_mutex_unlock(&mutex);
+    return node;
+}
+
+/* Look up an existing ID for the given flow's metadata and optional actions.
+ */
+uint32_t
+recirc_find_id(struct ofproto_dpif *ofproto, uint8_t table_id,
+               struct recirc_metadata *md, struct ofpbuf *stack,
+               uint32_t action_set_len, uint32_t ofpacts_len,
+               const struct ofpact *ofpacts)
+{
+    /* Check if an ID with the given metadata already exists. */
+    struct recirc_id_node *node;
+    uint32_t hash;
+
+    hash = recirc_metadata_hash(ofproto, table_id, md, stack, action_set_len,
+                                ofpacts_len, ofpacts);
+    node = recirc_find_equal(ofproto, table_id, md, stack, action_set_len,
+                             ofpacts_len, ofpacts, hash);
+
+    return node ? node->id : 0;
+}
+
+/* Allocate a unique recirculation id for the given set of flow metadata and
+   optional actions. */
+uint32_t
+recirc_alloc_id_ctx(struct ofproto_dpif *ofproto, uint8_t table_id,
+                    struct recirc_metadata *md, struct ofpbuf *stack,
+                    uint32_t action_set_len, uint32_t ofpacts_len,
+                    const struct ofpact *ofpacts)
+{
+    struct recirc_id_node *node;
+    uint32_t hash;
+
+    /* Look up an existing ID. */
+    hash = recirc_metadata_hash(ofproto, table_id, md, stack, action_set_len,
+                                ofpacts_len, ofpacts);
+    node = recirc_ref_equal(ofproto, table_id, md, stack, action_set_len,
+                            ofpacts_len, ofpacts, hash);
+
+    /* Allocate a new recirc ID if needed. */
+    if (!node) {
+        ovs_assert(action_set_len <= ofpacts_len);
+
+        node = recirc_alloc_id__(ofproto, table_id, md, stack, action_set_len,
+                                 ofpacts_len, ofpacts, hash);
+    }
+
+    return node->id;
+}
+
+/* Allocate a unique recirculation id. */
+uint32_t
+recirc_alloc_id(struct ofproto_dpif *ofproto)
+{
+    struct recirc_metadata md;
+    struct recirc_id_node *node;
+    uint32_t hash;
+
+    memset(&md, 0, sizeof md);
+    md.in_port = OFPP_NONE;
+    hash = recirc_metadata_hash(ofproto, TBL_INTERNAL, &md, NULL, 0, 0, NULL);
+    node = recirc_alloc_id__(ofproto, TBL_INTERNAL, &md, NULL, 0, 0, NULL,
+                             hash);
+    return node->id;
 }
 
 void
-recirc_id_free(struct recirc_id_pool *pool, uint32_t id)
+recirc_id_node_unref(const struct recirc_id_node *node_)
+    OVS_EXCLUDED(mutex)
 {
-    ovs_mutex_lock(&pool->lock);
-    id_pool_free_id(pool->rids, id);
-    ovs_mutex_unlock(&pool->lock);
+    struct recirc_id_node *node = CONST_CAST(struct recirc_id_node *, node_);
+
+    if (node && ovs_refcount_unref(&node->refcount) == 1) {
+        ovs_mutex_lock(&mutex);
+        /* Prevent re-use of this node by removing the node from 'metadata_map'
+         */
+        cmap_remove(&metadata_map, &node->metadata_node, node->hash);
+        /* We keep the node in the 'id_map' so that it can be found as long
+         * as it lingers, and add it to the 'expiring' list. */
+        list_insert(&expiring, &node->exp_node);
+        ovs_mutex_unlock(&mutex);
+    }
+}
+
+void
+recirc_free_id(uint32_t id)
+{
+    const struct recirc_id_node *node;
+
+    node = recirc_id_node_find(id);
+    if (node) {
+        recirc_id_node_unref(node);
+    } else {
+        VLOG_ERR("Freeing nonexistent recirculation ID: %"PRIu32, id);
+    }
+}
+
+/* Called when 'ofproto' is destructed.  Checks for and clears any
+ * recirc_id leak.
+ * No other thread may have access to the 'ofproto' being destructed.
+ * All related datapath flows must be deleted before calling this. */
+void
+recirc_free_ofproto(struct ofproto_dpif *ofproto, const char *ofproto_name)
+{
+    struct recirc_id_node *n;
+
+    CMAP_FOR_EACH (n, metadata_node, &metadata_map) {
+        if (n->ofproto == ofproto) {
+            VLOG_ERR("recirc_id %"PRIu32
+                     " left allocated when ofproto (%s)"
+                     " is destructed", n->id, ofproto_name);
+        }
+    }
 }

ofproto/ofproto-dpif-rid.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 Nicira, Inc.
+ * Copyright (c) 2014, 2015 Nicira, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,29 +20,164 @@
 #include <stddef.h>
 #include <stdint.h>
 
-struct recirc_id_pool;
+#include "cmap.h"
+#include "list.h"
+#include "ofp-actions.h"
+#include "ovs-thread.h"
+
+struct ofproto_dpif;
+struct rule;
 
 /*
- * Recirculation ID pool.
- * ======================
- *
- * Recirculation ID needs to be unique for each datapath. Recirculation
- * ID pool keeps track recirculation ids.
- *
- * Typically, there is one recirculation ID pool for each backer.
- *
- * In theory, Recirculation ID can be any uint32_t value, except 0.
- * The implementation usually limits it to a smaller range to ease
- * debugging.
- *
- * Thread-safety
+ * Recirculation
  * =============
  *
- * All APIs are thread safe.
+ * Recirculation is a technique to allow a frame to re-enter the datapath
+ * packet processing path for one or multiple times to achieve more flexible
+ * packet processing, such modifying header fields after MPLS POP action and
+ * selecting bond a slave port for bond ports.
  *
+ * Data path and user space interface
+ * -----------------------------------
+ *
+ * Recirculation uses two uint32_t fields, recirc_id and dp_hash, and a RECIRC
+ * action.  The value recirc_id is used to select the next packet processing
+ * steps among multiple instances of recirculation.  When a packet initially
+ * enters the data path it is assigned with recirc_id 0, which indicates no
+ * recirculation.  Recirc_ids are managed by the user space, opaque to the
+ * data path.
+ *
+ * On the other hand, dp_hash can only be computed by the data path, opaque to
+ * the user space.  In fact, user space may not able to recompute the hash
+ * value.  The dp_hash value should be wildcarded for a newly received
+ * packet.  HASH action specifies whether the hash is computed, and if
+ * computed, how many fields are to be included in the hash computation.  The
+ * computed hash value is stored into the dp_hash field prior to recirculation.
+ *
+ * The RECIRC action sets the recirc_id field and then reprocesses the packet
+ * as if it was received on the same input port.  RECIRC action works like a
+ * function call; actions listed behind the RECIRC action will be executed
+ * after its execution.  RECIRC action can be nested, data path implementation
+ * limits the number of recirculation executed to prevent unreasonable nesting
+ * depth or infinite loop.
+ *
+ * User space recirculation context
+ * ---------------------------------
+ *
+ * Recirculation is hidden from the OpenFlow controllers.  Action translation
+ * code deduces when recirculation is necessary and issues a data path
+ * recirculation action.  All OpenFlow actions to be performed after
+ * recirculation are derived from the OpenFlow pipeline and are stored with the
+ * recirculation ID.  When the OpenFlow tables are changed in a way affecting
+ * the recirculation flows, new recirculation ID with new metadata and actions
+ * is allocated and the old one is timed out.
+ *
+ * Recirculation ID pool
+ * ----------------------
+ *
+ * Recirculation ID needs to be unique for all data paths.  Recirculation ID
+ * pool keeps track recirculation ids and stores OpenFlow pipeline translation
+ * context so that flow processing may continue after recirculation.
+ *
+ * A Recirculation ID can be any uint32_t value, except for that the value 0 is
+ * reserved for 'no recirculation' case.
+ *
+ * Thread-safety
+ * --------------
+ *
+ * All APIs are thread safe.
  */
-struct recirc_id_pool *recirc_id_pool_create(void);
-void  recirc_id_pool_destroy(struct recirc_id_pool *pool);
-uint32_t recirc_id_alloc(struct recirc_id_pool *pool);
-void recirc_id_free(struct recirc_id_pool *pool, uint32_t recirc_id);
+
+/* Metadata for restoring pipeline context after recirculation.  Helpers
+ * are inlined below to keep them together with the definition for easier
+ * updates. */
+BUILD_ASSERT_DECL(FLOW_WC_SEQ == 31);
+
+struct recirc_metadata {
+    /* Metadata in struct flow. */
+    struct flow_tnl tunnel;       /* Encapsulating tunnel parameters. */
+    ovs_be64 metadata;            /* OpenFlow Metadata. */
+    uint64_t regs[FLOW_N_XREGS];  /* Registers. */
+    ofp_port_t in_port;           /* Incoming port. */
+    ofp_port_t actset_output;     /* Output port in action set. */
+};
+
+static inline void
+recirc_metadata_from_flow(struct recirc_metadata *md,
+                          const struct flow *flow)
+{
+    memset(md, 0, sizeof *md);
+    md->tunnel = flow->tunnel;
+    md->metadata = flow->metadata;
+    memcpy(md->regs, flow->regs, sizeof md->regs);
+    md->in_port = flow->in_port.ofp_port;
+    md->actset_output = flow->actset_output;
+}
+
+static inline void
+recirc_metadata_to_flow(const struct recirc_metadata *md,
+                        struct flow *flow)
+{
+    flow->tunnel = md->tunnel;
+    flow->metadata = md->metadata;
+    memcpy(flow->regs, md->regs, sizeof flow->regs);
+    flow->in_port.ofp_port = md->in_port;
+    flow->actset_output = md->actset_output;
+}
+
+/* Pool node fields should NOT be modified after placing the node in the pool.
+ */
+struct recirc_id_node {
+    struct ovs_list exp_node OVS_GUARDED;
+    struct cmap_node id_node;
+    struct cmap_node metadata_node;
+    uint32_t id;
+    uint32_t hash;
+    struct ovs_refcount refcount;
+
+    /* Initial table for post-recirculation processing. */
+    uint8_t table_id;
+
+    /* Pipeline context for post-recirculation processing. */
+    struct ofproto_dpif *ofproto; /* Post-recirculation bridge. */
+    struct recirc_metadata metadata; /* Flow metadata. */
+    struct ofpbuf *stack;         /* Stack if any. */
+
+    /* Actions to be translated on recirculation. */
+    uint32_t action_set_len;      /* How much of 'ofpacts' consists of an
+                                   * action set? */
+    uint32_t ofpacts_len;         /* Size of 'ofpacts', in bytes. */
+    struct ofpact ofpacts[];      /* Sequence of "struct ofpacts". */
+};
+
+void recirc_init(void);
+
+/* This is only used for bonds and will go away when bonds implementation is
+ * updated to use this mechanism instead of internal rules. */
+uint32_t recirc_alloc_id(struct ofproto_dpif *);
+
+uint32_t recirc_alloc_id_ctx(struct ofproto_dpif *, uint8_t table_id,
+                             struct recirc_metadata *, struct ofpbuf *stack,
+                             uint32_t action_set_len, uint32_t ofpacts_len,
+                             const struct ofpact *);
+uint32_t recirc_find_id(struct ofproto_dpif *, uint8_t table_id,
+                        struct recirc_metadata *, struct ofpbuf *stack,
+                        uint32_t action_set_len, uint32_t ofpacts_len,
+                        const struct ofpact *);
+void recirc_free_id(uint32_t recirc_id);
+void recirc_free_ofproto(struct ofproto_dpif *, const char *ofproto_name);
+
+const struct recirc_id_node *recirc_id_node_find(uint32_t recirc_id);
+
+static inline bool recirc_id_node_try_ref_rcu(const struct recirc_id_node *n_)
+{
+    struct recirc_id_node *node = CONST_CAST(struct recirc_id_node *, n_);
+
+    return node ? ovs_refcount_try_ref_rcu(&node->refcount) : false;
+}
+
+void recirc_id_node_unref(const struct recirc_id_node *);
+
+void recirc_run(void);
+
 #endif

commit 57b9f89a0e09d854f9316a4cad3faff31e9727e7

ofproto-dpif: Rename recirc_free_ofproto to better match the code.
The old version of the function actually freed the remaining IDs, but
it's not the case since the cited commit.  Let's change the name and
the description to better reflect what this function is doing.

Fixes: e672ff9b4d22 ("ofproto-dpif: Restore metadata and registers on recirculation.")
Signed-off-by: Ilya Maximets <i.maximets@ovn.org>
Signed-off-by: 0-day Robot <robot@bytheb.org>

ofproto/ofproto-dpif-rid.c
@@ -347,12 +347,14 @@ recirc_free_id(uint32_t id)
     }
 }
 
-/* Called when 'ofproto' is destructed.  Checks for and clears any
- * recirc_id leak.
+/* Called when 'ofproto' is destructed.  Checks for any recirc_id leaks
+ * and logs an error if found.
+ *
  * No other thread may have access to the 'ofproto' being destructed.
  * All related datapath flows must be deleted before calling this. */
 void
-recirc_free_ofproto(struct ofproto_dpif *ofproto, const char *ofproto_name)
+recirc_leak_check_ofproto(struct ofproto_dpif *ofproto,
+                          const char *ofproto_name)
 {
     struct recirc_id_node *n;
 

ofproto/ofproto-dpif-rid.h
@@ -192,7 +192,8 @@ uint32_t recirc_alloc_id(struct ofproto_dpif *);
 uint32_t recirc_alloc_id_ctx(const struct frozen_state *);
 uint32_t recirc_find_id(const struct frozen_state *);
 void recirc_free_id(uint32_t recirc_id);
-void recirc_free_ofproto(struct ofproto_dpif *, const char *ofproto_name);
+void recirc_leak_check_ofproto(struct ofproto_dpif *,
+                               const char *ofproto_name);
 
 const struct recirc_id_node *recirc_id_node_find(uint32_t recirc_id);
 bool recirc_id_node_find_and_ref(uint32_t id);

ofproto/ofproto-dpif.c
@@ -1982,7 +1982,7 @@ destruct(struct ofproto *ofproto_, bool del)
     /* Wait for all the meter destroy work to finish. */
     ovsrcu_barrier();
     close_dpif_backer(ofproto->backer, del);
-    recirc_free_ofproto(ofproto, ofproto->up.name);
+    recirc_leak_check_ofproto(ofproto, ofproto->up.name);
 }
 
 static int