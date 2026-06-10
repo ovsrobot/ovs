ovsrobot/ovs

commit e60e935b1f37058e8a3e1a57d6a8fdd566df304c

Implement set-field for IPv6 ND fields (nd_target, nd_sll, and nd_tll).
This patch adds set-field operations for nd_target, nd_sll, and nd_tll
fields, with and without masks, using Nicira extensions and OpenFlow 1.2
protocol.

Signed-off-by: Randall A Sharo <randall.sharo at navy.mil>
Signed-off-by: Ben Pfaff <blp@nicira.com>

lib/odp-execute.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009, 2010, 2011, 2012, 2013, 2014 Nicira, Inc.
+ * Copyright (c) 2009, 2010, 2011, 2012, 2013, 2014, 2015 Nicira, Inc.
  * Copyright (c) 2013 Simon Horman
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -19,6 +19,7 @@
 #include "odp-execute.h"
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <netinet/icmp6.h>
 #include <netinet/ip6.h>
 #include <stdlib.h>
 #include <string.h>
@@ -182,6 +183,48 @@ set_arp(struct ofpbuf *packet, const struct ovs_key_arp *key,
 }
 
 static void
+odp_set_nd(struct ofpbuf *packet, const struct ovs_key_nd *key,
+           const struct ovs_key_nd *mask)
+{
+    const struct ovs_nd_msg *ns = ofpbuf_l4(packet);
+    const struct ovs_nd_opt *nd_opt = ofpbuf_get_nd_payload(packet);
+
+    if (OVS_LIKELY(ns && nd_opt)) {
+        int bytes_remain = ofpbuf_l4_size(packet) - sizeof(*ns);
+        ovs_be32 tgt_buf[4];
+        uint8_t sll_buf[ETH_ADDR_LEN] = {0};
+        uint8_t tll_buf[ETH_ADDR_LEN] = {0};
+
+        while (bytes_remain >= ND_OPT_LEN && nd_opt->nd_opt_len != 0) {
+            if (nd_opt->nd_opt_type == ND_OPT_SOURCE_LINKADDR
+                && nd_opt->nd_opt_len == 1) {
+                memcpy(sll_buf, nd_opt->nd_opt_data, ETH_ADDR_LEN);
+                ether_addr_copy_masked(sll_buf, key->nd_sll, mask->nd_sll);
+
+                /* A packet can only contain one SLL or TLL option */
+                break;
+            } else if (nd_opt->nd_opt_type == ND_OPT_TARGET_LINKADDR
+                       && nd_opt->nd_opt_len == 1) {
+                memcpy(tll_buf, nd_opt->nd_opt_data, ETH_ADDR_LEN);
+                ether_addr_copy_masked(tll_buf, key->nd_tll, mask->nd_tll);
+
+                /* A packet can only contain one SLL or TLL option */
+                break;
+            }
+
+            nd_opt += nd_opt->nd_opt_len;
+            bytes_remain -= nd_opt->nd_opt_len * ND_OPT_LEN;
+        }
+
+        packet_set_nd(packet,
+                      mask_ipv6_addr(ns->target.be32,
+                                     key->nd_target, mask->nd_target, tgt_buf),
+                      sll_buf,
+                      tll_buf);
+    }
+}
+
+static void
 odp_execute_set_action(struct dpif_packet *packet, const struct nlattr *a)
 {
     enum ovs_key_attr type = nl_attr_type(a);
@@ -259,6 +302,15 @@ odp_execute_set_action(struct dpif_packet *packet, const struct nlattr *a)
         set_arp(&packet->ofpbuf, nl_attr_get(a), NULL);
         break;
 
+    case OVS_KEY_ATTR_ND:
+        if (OVS_LIKELY(ofpbuf_get_nd_payload(&packet->ofpbuf))) {
+            const struct ovs_key_nd *nd_key
+                   = nl_attr_get_unspec(a, sizeof(struct ovs_key_nd));
+            packet_set_nd(&packet->ofpbuf, nd_key->nd_target,
+                          nd_key->nd_sll, nd_key->nd_tll);
+        }
+        break;
+
     case OVS_KEY_ATTR_DP_HASH:
         md->dp_hash = nl_attr_get_u32(a);
         dpif_packet_set_dp_hash(packet, md->dp_hash);
@@ -275,7 +327,6 @@ odp_execute_set_action(struct dpif_packet *packet, const struct nlattr *a)
     case OVS_KEY_ATTR_VLAN:
     case OVS_KEY_ATTR_ICMP:
     case OVS_KEY_ATTR_ICMPV6:
-    case OVS_KEY_ATTR_ND:
     case OVS_KEY_ATTR_TCP_FLAGS:
     case __OVS_KEY_ATTR_MAX:
     default:
@@ -348,6 +399,11 @@ odp_execute_masked_set_action(struct dpif_packet *packet,
                 get_mask(a, struct ovs_key_arp));
         break;
 
+    case OVS_KEY_ATTR_ND:
+        odp_set_nd(&packet->ofpbuf, nl_attr_get(a),
+                   get_mask(a, struct ovs_key_nd));
+        break;
+
     case OVS_KEY_ATTR_DP_HASH:
         md->dp_hash = nl_attr_get_u32(a)
             | (dpif_packet_get_dp_hash(packet) & ~*get_mask(a, uint32_t));
@@ -367,7 +423,6 @@ odp_execute_masked_set_action(struct dpif_packet *packet,
     case OVS_KEY_ATTR_VLAN:
     case OVS_KEY_ATTR_ICMP:
     case OVS_KEY_ATTR_ICMPV6:
-    case OVS_KEY_ATTR_ND:
     case OVS_KEY_ATTR_TCP_FLAGS:
     case __OVS_KEY_ATTR_MAX:
     default:

AUTHORS
@@ -124,6 +124,7 @@ pritesh                 pritesh.kothari@cisco.com
 Pravin B Shelar         pshelar@nicira.com
 Raju Subramanian        rsubramanian@nicira.com
 Rami Rosen              ramirose@gmail.com
+Randall Sharo           andall.sharo@navy.mil
 Ravi Kerur              Ravi.Kerur@telekom.com
 Reid Price              reid@nicira.com
 Remko Tronçon           git@el-tramo.be

lib/csum.c
@@ -113,6 +113,26 @@ recalc_csum32(ovs_be16 old_csum, ovs_be32 old_u32, ovs_be32 new_u32)
 }
 
 /* Returns the new checksum for a packet in which the checksum field previously
+ * contained 'old_csum' and in which a field that contained the 6 bytes at
+ * 'old_bytes' was changed to contain the 6 bytes at 'new_bytes'. */
+ovs_be16
+recalc_csum48(ovs_be16 old_csum, const void *old_bytes,
+              const void *new_bytes)
+{
+    ovs_be16 new_csum = old_csum;
+    const uint16_t *p16_old = old_bytes,
+                   *p16_new = new_bytes;
+    int i;
+
+    for (i = 0; i < 3; ++i) {
+        new_csum = recalc_csum16(new_csum, get_unaligned_be16(&p16_old[i]),
+                                 get_unaligned_be16(&p16_new[i]));
+    }
+
+    return new_csum;
+}
+
+/* Returns the new checksum for a packet in which the checksum field previously
  * contained 'old_csum' and in which a field that contained 'old_u32[4]' was
  * changed to contain 'new_u32[4]'. */
 ovs_be16

lib/csum.h
@@ -28,6 +28,8 @@ uint32_t csum_continue(uint32_t partial, const void *, size_t);
 ovs_be16 csum_finish(uint32_t partial);
 ovs_be16 recalc_csum16(ovs_be16 old_csum, ovs_be16 old_u16, ovs_be16 new_u16);
 ovs_be16 recalc_csum32(ovs_be16 old_csum, ovs_be32 old_u32, ovs_be32 new_u32);
+ovs_be16 recalc_csum48(ovs_be16 old_csum, const void *old_bytes,
+                       const void *new_bytes);
 ovs_be16 recalc_csum128(ovs_be16 old_csum, ovs_16aligned_be32 old_u32[4],
                         const ovs_be32 new_u32[4]);
 

lib/meta-flow.h
@@ -1343,7 +1343,7 @@ enum OVS_PACKED_ENUM mf_field_id {
      * Maskable: bitwise.
      * Formatting: IPv6.
      * Prerequisites: ND.
-     * Access: read-only.
+     * Access: read/write.
      * NXM: NXM_NX_ND_TARGET(23) since v1.1.
      * OXM: OXM_OF_IPV6_ND_TARGET(31) since OF1.2 and v1.7.
      */
@@ -1357,7 +1357,7 @@ enum OVS_PACKED_ENUM mf_field_id {
      * Maskable: bitwise.
      * Formatting: Ethernet.
      * Prerequisites: ND solicit.
-     * Access: read-only.
+     * Access: read/write.
      * NXM: NXM_NX_ND_SLL(24) since v1.1.
      * OXM: OXM_OF_IPV6_ND_SLL(32) since OF1.2 and v1.7.
      */
@@ -1371,7 +1371,7 @@ enum OVS_PACKED_ENUM mf_field_id {
      * Maskable: bitwise.
      * Formatting: Ethernet.
      * Prerequisites: ND advert.
-     * Access: read-only.
+     * Access: read/write.
      * NXM: NXM_NX_ND_TLL(25) since v1.1.
      * OXM: OXM_OF_IPV6_ND_TLL(33) since OF1.2 and v1.7.
      */

lib/odp-util.c
@@ -2844,6 +2844,8 @@ static void put_ipv6_key(const struct ovs_key_ipv6 *, struct flow *,
                          bool is_mask);
 static void get_arp_key(const struct flow *, struct ovs_key_arp *);
 static void put_arp_key(const struct ovs_key_arp *, struct flow *);
+static void get_nd_key(const struct flow *, struct ovs_key_nd *);
+static void put_nd_key(const struct ovs_key_nd *, struct flow *);
 
 /* These share the same layout. */
 union ovs_key_tp {
@@ -4187,6 +4189,45 @@ commit_set_arp_action(const struct flow *flow, struct flow *base_flow,
     return 0;
 }
 
+static void
+get_nd_key(const struct flow *flow, struct ovs_key_nd *nd)
+{
+    memcpy(nd->nd_target, &flow->nd_target, sizeof flow->nd_target);
+    /* nd_sll and nd_tll are stored in arp_sha and arp_tha, respectively */
+    memcpy(nd->nd_sll, flow->arp_sha, ETH_ADDR_LEN);
+    memcpy(nd->nd_tll, flow->arp_tha, ETH_ADDR_LEN);
+}
+
+static void
+put_nd_key(const struct ovs_key_nd *nd, struct flow *flow)
+{
+    memcpy(&flow->nd_target, &flow->nd_target, sizeof flow->nd_target);
+    /* nd_sll and nd_tll are stored in arp_sha and arp_tha, respectively */
+    memcpy(flow->arp_sha, nd->nd_sll, ETH_ADDR_LEN);
+    memcpy(flow->arp_tha, nd->nd_tll, ETH_ADDR_LEN);
+}
+
+static enum slow_path_reason
+commit_set_nd_action(const struct flow *flow, struct flow *base_flow,
+                     struct ofpbuf *odp_actions,
+                     struct flow_wildcards *wc, bool use_masked)
+{
+    struct ovs_key_nd key, mask, base;
+
+    get_nd_key(flow, &key);
+    get_nd_key(base_flow, &base);
+    get_nd_key(&wc->masks, &mask);
+
+    if (commit(OVS_KEY_ATTR_ND, use_masked, &key, &base, &mask, sizeof key,
+               odp_actions)) {
+        put_nd_key(&base, base_flow);
+        put_nd_key(&mask, &wc->masks);
+        return SLOW_ACTION;
+    }
+
+    return 0;
+}
+
 static enum slow_path_reason
 commit_set_nw_action(const struct flow *flow, struct flow *base,
                      struct ofpbuf *odp_actions, struct flow_wildcards *wc,
@@ -4204,7 +4245,7 @@ commit_set_nw_action(const struct flow *flow, struct flow *base,
 
     case ETH_TYPE_IPV6:
         commit_set_ipv6_action(flow, base, odp_actions, wc, use_masked);
-        break;
+        return commit_set_nd_action(flow, base, odp_actions, wc, use_masked);
 
     case ETH_TYPE_ARP:
         return commit_set_arp_action(flow, base, odp_actions, wc);

lib/ofp-print.c
@@ -82,6 +82,14 @@ ofp_packet_to_string(const void *data, size_t len)
         struct sctp_header *sh = ofpbuf_l4(&buf);
         ds_put_format(&ds, " sctp_csum:%"PRIx32,
                       ntohl(get_16aligned_be32(&sh->sctp_csum)));
+    } else if (flow.nw_proto == IPPROTO_ICMP && l4_size >= ICMP_HEADER_LEN) {
+        struct icmp_header *icmph = ofpbuf_l4(&buf);
+        ds_put_format(&ds, " icmp_csum:%"PRIx16,
+                      ntohs(icmph->icmp_csum));
+    } else if (flow.nw_proto == IPPROTO_ICMPV6 && l4_size >= ICMP6_HEADER_LEN) {
+        struct icmp6_header *icmp6h = ofpbuf_l4(&buf);
+        ds_put_format(&ds, " icmp6_csum:%"PRIx16,
+                      ntohs(icmp6h->icmp6_cksum));
     }
 
     ds_put_char(&ds, '\n');

lib/ofpbuf.h
@@ -104,6 +104,7 @@ static inline const void *ofpbuf_get_tcp_payload(const struct ofpbuf *);
 static inline const void *ofpbuf_get_udp_payload(const struct ofpbuf *);
 static inline const void *ofpbuf_get_sctp_payload(const struct ofpbuf *);
 static inline const void *ofpbuf_get_icmp_payload(const struct ofpbuf *);
+static inline const void *ofpbuf_get_nd_payload(const struct ofpbuf *);
 
 void ofpbuf_use(struct ofpbuf *, void *, size_t);
 void ofpbuf_use_stack(struct ofpbuf *, void *, size_t);
@@ -372,6 +373,12 @@ static inline const void *ofpbuf_get_icmp_payload(const struct ofpbuf *b)
 {
     return OVS_LIKELY(ofpbuf_l4_size(b) >= ICMP_HEADER_LEN)
         ? (const char *)ofpbuf_l4(b) + ICMP_HEADER_LEN : NULL;
+}
+
+static inline const void *ofpbuf_get_nd_payload(const struct ofpbuf *b)
+{
+    return OVS_LIKELY(ofpbuf_l4_size(b) >= ND_MSG_LEN)
+        ? (const char *)ofpbuf_l4(b) + ND_MSG_LEN : NULL;
 }
 
 #ifdef DPDK_NETDEV

commit 946c8c2c0f66d63aa62a72d80987fc5880fa976b

odp-execute: Validate ND option length in odp_set_nd().
Commit e150d409 ("packets: Validate ND option length in
packet_set_nd().", Coverity CID 278405) added a bounds check to the
Neighbor Discovery option walk in packet_set_nd(), but the
structurally identical option walk in odp_set_nd() was left unguarded.

odp_set_nd() walks the ND link-layer-address options to locate the SLL
or TLL option before delegating the actual packet edit to
packet_set_nd().  Its loop only checks that ND_LLA_OPT_LEN bytes remain
and that lla_opt->len is non-zero; it does not verify that the full
option (lla_opt->len * ND_LLA_OPT_LEN bytes) fits within bytes_remain
before advancing past it.  A crafted ND packet carrying an oversized
option length therefore advances lla_opt beyond the L4 payload and
underflows bytes_remain, and the next iteration reads lla_opt->type /
lla_opt->len out of bounds.  This is the same defect, in the same
option-walk pattern, that CID 278405 reported for packet_set_nd().

Apply the same loop condition used by the parent fix so an option is
only processed when it fully fits within the remaining bytes.

Fixes: e60e935b1f37 ("Implement set-field for IPv6 ND fields (nd_target, nd_sll, and nd_tll).")
Signed-off-by: Yuliang Xiao <xyl1509410143@gmail.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

lib/odp-execute.c
@@ -356,7 +356,8 @@ odp_set_nd(struct dp_packet *packet, const struct ovs_key_nd *key,
         struct eth_addr sll_buf = eth_addr_zero;
         struct eth_addr tll_buf = eth_addr_zero;
 
-        while (bytes_remain >= ND_LLA_OPT_LEN && lla_opt->len != 0) {
+        while (bytes_remain >= ND_LLA_OPT_LEN && lla_opt->len != 0
+               && bytes_remain >= (lla_opt->len * ND_LLA_OPT_LEN)) {
             if (lla_opt->type == ND_OPT_SOURCE_LINKADDR
                 && lla_opt->len == 1) {
                 sll_buf = lla_opt->mac;