ovsrobot/ovs

commit 422e90437854c441a0b274d3bf06ce7d6bbf9de0

make: Remove the Linux datapath.
Update the necessary make and configure files to remove the Linux
datapath and then remove the datapath.

Move datapath/linux/compat/include/linux/openvswitch.h to
include/linux/openvswitch.h because it is needed to generate header
files used by the userspace switch.

Also remove references to the Linux datapath from auxiliary files
and utilities since it is no longer supported.

Signed-off-by: Greg Rose <gvrose8192@gmail.com>
Reviewed-by: David Marchand <david.marchand@redhat.com>
Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

utilities/ovs-dev.py
@@ -106,7 +106,7 @@ def conf():
         pass  # Directory exists.
 
     os.chdir(BUILD_GCC)
-    _sh(*(configure + ["--with-linux=/lib/modules/%s/build" % uname()]))
+    _sh(*(configure))
 
     try:
         _sh("clang --version", check=True)
@@ -184,12 +184,9 @@ def tag():
     ctags = ['ctags', '-R', '-f', '.tags']
 
     try:
-        _sh(*(ctags + ['--exclude="datapath/"']))
+        _sh(*ctags)
     except:
-        try:
-            _sh(*ctags)  # Some versions of ctags don't have --exclude
-        except:
-            pass
+        pass
 
     try:
         _sh('cscope', '-R', '-b')
@@ -351,7 +348,7 @@ Basic Configuration:
 
     # First install the basic requirements needed to build Open vSwitch.
     sudo apt-get install git build-essential libtool autoconf pkg-config \\
-            libssl-dev gdb libcap-ng-dev linux-headers-`uname -r`
+            libssl-dev gdb libcap-ng-dev
 
     # Next clone the Open vSwitch source.
     git clone https://github.com/openvswitch/ovs.git %(ovs)s
@@ -361,14 +358,6 @@ Basic Configuration:
 
     # Build the switch.
     %(v)s conf make
-
-    # Install the kernel module
-    sudo insmod %(ovs)s/datapath/linux/openvswitch.ko
-
-    # If needed, manually load all required vport modules:
-    sudo insmod %(ovs)s/datapath/linux/vport-vxlan.ko
-    sudo insmod %(ovs)s/datapath/linux/vport-geneve.ko
-    [...]
 
     # Run the switch.
     %(v)s run

commit eec674f4903f7adbb4b3ead5b6f13da8565e0d56

utilities: ovs-dev: Remove modinst command.
The 'modules_install' command doesn't exist since the kernel module
was removed from the tree.

Not sure if anyone is using that script, but we should keep it
consistent.

Fixes: 422e90437854 ("make: Remove the Linux datapath.")
Signed-off-by: Ilya Maximets <i.maximets@ovn.org>
Signed-off-by: 0-day Robot <robot@bytheb.org>

utilities/ovs-dev.py
@@ -298,36 +298,6 @@ def run():
 commands.append(run)
 
 
-def modinst():
-    if not os.path.exists("/lib/modules"):
-        print("Missing modules directory.  Is this a Linux system?")
-        sys.exit(1)
-
-    sudo()
-    try:
-        _sh("rmmod", "openvswitch")
-    except subprocess.CalledProcessError:
-        pass  # Module isn't loaded
-
-    try:
-        _sh("rm -f /lib/modules/%s/extra/openvswitch.ko" % uname())
-        _sh("rm -f /lib/modules/%s/extra/vport-*.ko" % uname())
-    except subprocess.CalledProcessError:
-        pass  # Module isn't installed
-
-    conf()
-    make()
-    make("modules_install")
-
-    _sh("modprobe", "openvswitch")
-    _sh("dmesg | grep openvswitch | tail -1")
-    _sh("find /lib/modules/%s/ -iname vport-*.ko -exec insmod '{}' \\;"
-        % uname())
-
-
-commands.append(modinst)
-
-
 def env():
     print("export PATH=" + ENV["PATH"])
 
@@ -370,13 +340,12 @@ Commands:
     kill    - Kill all running instances of ovs.
     reset   - Reset any runtime configuration in %(run)s.
     run     - Run ovs.
-    modinst - Build ovs and install the kernel module.
     env     - Print the required path environment variable.
     doc     - Print this message.
 
 Note:
-    If running as non-root user, "kill", "reset", "run" and "modinst"
-    will always run as the root user, by rerun the commands with "sudo".
+    If running as non-root user, "kill", "reset" and "run" will always run
+    as the root user, by rerun the commands with "sudo".
 """ % {"ovs": OVS_SRC, "v": sys.argv[0], "run": RUNDIR})
     sys.exit(0)
 